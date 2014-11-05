aeb670bf5f6d8a18178cc1c15cdcbb0b5340402f
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-05 13:38:43 -0800

    [Example] Add new example bundle
    Add an example bundle containing custom extensions,
    which can be recognized by extensions from another
    bundle. WTD-518.

--- example/builtins/src/ExampleDirective.js
+++ example/builtins/src/ExampleDirective.js
@@ -8,6 +8,10 @@ define(
     function () {
         "use strict";
 
+        var HAS_EXTENSIONS = "A directive loaded these example extensions.",
+            NO_EXTENSIONS = "A directive tried to load example extensions," +
+                    " but found none.";
+
         /**
          *
          * @constructor
@@ -15,9 +19,9 @@ define(
         function ExampleDirective(examples) {
             // Build up a template from example extensions
             var template = examples.length > 0 ?
-                    "A directive loaded these example extensions:<ul>" :
-                    "This came from a directive.<ul>";
+                    HAS_EXTENSIONS : NO_EXTENSIONS;
 
+            template += "<ul>"
             examples.forEach(function (e) {
                 template += "<li>" + e.text + "</li>";
             });
