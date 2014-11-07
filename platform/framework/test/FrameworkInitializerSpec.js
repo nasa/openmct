bfcfa0127db7c392dc2a307e1591846d47e03439
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-06 19:44:34 -0800

    [Framework] Spec for FrameworkInitializer
    Complete specs for initial implementation of framework
    layer, WTD-518.

--- platform/framework/test/FrameworkInitializerSpec.js
+++ platform/framework/test/FrameworkInitializerSpec.js
@@ -1,14 +1,50 @@
-/*global define,Promise,describe,it,expect,beforeEach*/
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,runs*/
 
 /**
  * FrameworkInitializerSpec. Created by vwoeltje on 11/6/14.
  */
 define(
-    [],
-    function () {
+    ["../src/FrameworkInitializer"],
+    function (FrameworkInitializer) {
         "use strict";
 
-        describe("", function () {
+        describe("The framework initializer", function () {
+            var initializer;
+
+            function appender(name) {
+                return function (value) {
+                    return Promise.resolve(value.concat([name]));
+                };
+            }
+
+            beforeEach(function () {
+                initializer = new FrameworkInitializer(
+                    { loadBundles: appender("loader") },
+                    { resolveBundles: appender("resolver") },
+                    { registerExtensions: appender("registrar") },
+                    { bootstrap: appender("bootstrapper")}
+                );
+            });
+
+            // Really just delegates work, can only verify the
+            // order of calls.
+            it("calls injected stages in order", function () {
+                var result;
+
+                initializer.runApplication([]).then(function (v) { result = v; });
+
+                waitsFor(
+                    function () { return result !== undefined; },
+                    "promise resolution",
+                    250
+                );
+
+                runs(function () {
+                    expect(result).toEqual(
+                        ["loader", "resolver", "registrar", "bootstrapper"]
+                    );
+                });
+            });
 
         });
     }
