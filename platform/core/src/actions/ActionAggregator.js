5dd660035a9e709f5da662cf3ec0f1178f695e39
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-20 13:24:55 -0800

    [Core] Add comments to ActionAggregator
    Add JSDoc comments to Action Aggregator to satisfy
    code standards in the platform/core bundle, for
    WTD-573.

--- platform/core/src/actions/ActionAggregator.js
+++ platform/core/src/actions/ActionAggregator.js
@@ -5,8 +5,14 @@ define(
         "use strict";
 
         /**
-         * An action service factory which aggregates
-         * multiple other action services.
+         * The ActionAggregator makes several actionService
+         * instances act as those they were one. When requesting
+         * actions for a given context, results from all
+         * services will be assembled and concatenated.
+         *
+         * @constructor
+         * @param {ActionProvider[]} actionProviders an array
+         *        of action services
          */
         function ActionAggregator(actionProviders) {
 
@@ -21,6 +27,24 @@ define(
             }
 
             return {
+                /**
+                 * Get a list of actions which are valid in a given
+                 * context.
+                 *
+                 * @param {ActionContext} the context in which
+                 *        the action will occur; this is a
+                 *        JavaScript object containing key-value
+                 *        pairs. Typically, this will contain a
+                 *        field "domainObject" which refers to
+                 *        the domain object that will be acted
+                 *        upon, but may contain arbitrary information
+                 *        recognized by specific providers.
+                 * @return {Action[]} an array of actions which
+                 *        may be performed in the provided context.
+                 *
+                 * @method
+                 * @memberof ActionAggregator
+                 */
                 getActions: getActions
             };
         }
