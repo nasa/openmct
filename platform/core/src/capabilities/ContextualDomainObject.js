b9651b45d04b9505478dd835ad70e3caee9aa3f3
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-20 13:56:23 -0800

    [Core] Add JSDoc for ContextualDomainObject
    Add JSDoc for the domain object wrapper used to
    expose a 'context' capability; part of
    ongoing documentation to meet code standards in
    platform/core in preparation for integration.
    WTD-573.

--- platform/core/src/capabilities/ContextualDomainObject.js
+++ platform/core/src/capabilities/ContextualDomainObject.js
@@ -9,14 +9,28 @@ define(
         "use strict";
 
         /**
+         * Wraps a domain object, such that it exposes a `context` capability.
+         * A domain object may be contained by multiple other domain objects;
+         * the `context` capability allows two instances of the same domain
+         * object to be distinguished from one another based on which
+         * specific instance of a containing object exposed them (by way of a
+         * `composition` capability.)
+         *
+         * @param {DomainObject} domainObject the domain object for which
+         *        context should be exposed
+         * @param {DomainObject} parentObject the domain object from which
+         *        the wrapped object was retrieved
          *
          * @constructor
          */
         function ContextualDomainObject(domainObject, parentObject) {
+            // Prototypally inherit from the domain object, and
+            // instantiate its context capability ahead of time.
             var contextualObject = Object.create(domainObject),
                 contextCapability =
                     new ContextCapability(parentObject, domainObject);
 
+            // Intercept requests for a context capability.
             contextualObject.getCapability = function (name) {
                 return name === "context" ?
                         contextCapability :
