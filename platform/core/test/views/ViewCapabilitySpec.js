2b27c1cabc5df4538ac5bea9f0e980e1b68b41e3
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-21 18:30:25 -0800

    [Core] Spec for view provider, capability
    Add spec for view provider and capability to complete
    test coverage of the transitioned platform/core bundle.
    WTD-573.

--- platform/core/test/views/ViewCapabilitySpec.js
+++ platform/core/test/views/ViewCapabilitySpec.js
@@ -9,6 +9,30 @@ define(
         "use strict";
 
         describe("A view capability", function () {
+            var mockViewService,
+                mockDomainObject,
+                views = [ {key: "someView"} ],
+                view;
+
+            beforeEach(function () {
+                mockViewService = jasmine.createSpyObj(
+                    "viewService",
+                    [ "getViews" ]
+                );
+                mockDomainObject = jasmine.createSpyObj(
+                    "domainObject",
+                    [ "getId", "getModel", "getCapability" ]
+                );
+                mockViewService.getViews.andReturn(views);
+                view = new ViewCapability(mockViewService, mockDomainObject);
+            });
+
+            it("issues invocations to the view service", function () {
+                expect(view.invoke()).toEqual(views);
+                expect(mockViewService.getViews).toHaveBeenCalledWith(
+                    mockDomainObject
+                );
+            });
 
         });
     }
--- platform/core/test/views/ViewProviderSpec.js
+++ platform/core/test/views/ViewProviderSpec.js
@@ -9,6 +9,67 @@ define(
         "use strict";
 
         describe("The view provider", function () {
+            var viewA = {
+                    key: "a"
+                },
+                viewB = {
+                    key: "b",
+                    needs: [ "someCapability" ]
+                },
+                viewC = {
+                    key: "c",
+                    needs: [ "someCapability" ],
+                    delegation: true
+                },
+                capabilities = {},
+                delegates = {},
+                delegation,
+                mockDomainObject = {},
+                provider;
+
+            beforeEach(function () {
+                // Simulate the expected API
+                mockDomainObject.hasCapability = function (c) {
+                    return capabilities[c] !== undefined;
+                };
+                mockDomainObject.getCapability = function (c) {
+                    return capabilities[c];
+                };
+                mockDomainObject.useCapability = function (c, v) {
+                    return capabilities[c] && capabilities[c].invoke(v);
+                };
+
+                capabilities = {};
+                delegates = {};
+
+                delegation = {
+                    doesDelegateCapability: function (c) {
+                        return delegates[c] !== undefined;
+                    }
+                };
+
+                provider = new ViewProvider([viewA, viewB, viewC]);
+            });
+
+            it("reports views provided as extensions", function () {
+                capabilities.someCapability = true;
+                expect(provider.getViews(mockDomainObject))
+                    .toEqual([viewA, viewB, viewC]);
+            });
+
+            it("filters views by needed capabilities", function () {
+                //capabilities.someCapability = true;
+                expect(provider.getViews(mockDomainObject))
+                    .toEqual([viewA]);
+            });
+
+            it("allows delegation of needed capabilities when specified", function () {
+                //capabilities.someCapability = true;
+                capabilities.delegation = delegation;
+                delegates.someCapability = true;
+                expect(provider.getViews(mockDomainObject))
+                    .toEqual([viewA, viewC]);
+            });
 
         });
     }
