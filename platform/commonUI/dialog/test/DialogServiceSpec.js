f04e02359a95a4d066c4924b0b2ed079f9a76e42
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-25 14:44:16 -0800

    [Common UI] Skeleton spec for dialogs
    Skeleton specs for bundle platform/commonUI/dialog,
    responsible for displaying dialog overlays. Part of
    ongoing transition of common user interface elements
    for WTD-574.

---
+++ platform/commonUI/dialog/test/DialogServiceSpec.js
@@ -0,0 +1,31 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+/**
+ * MCTIncudeSpec. Created by vwoeltje on 11/6/14.
+ */
+define(
+    ["../src/DialogService"],
+    function (DialogService) {
+        "use strict";
+
+        describe("The dialog service", function () {
+
+            it("adds an overlay when user input is requested", function () {
+
+            });
+
+            it("allows user input to be canceled", function () {
+
+            });
+
+            it("passes back the result of user input when confirmed", function () {
+
+            });
+
+            it("logs a warning when a dialog is already showing", function () {
+
+            });
+
+        });
+    }
+);
---
+++ platform/commonUI/dialog/test/OverlayServiceSpec.js
@@ -0,0 +1,23 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+/**
+ * MCTIncudeSpec. Created by vwoeltje on 11/6/14.
+ */
+define(
+    ["../src/OverlayService"],
+    function (OverlayService) {
+        "use strict";
+
+        describe("The dialog service", function () {
+
+            it("prepends an mct-include to create overlays", function () {
+
+            });
+
+            it("removes the prepended element on request", function () {
+
+            });
+
+        });
+    }
+);
