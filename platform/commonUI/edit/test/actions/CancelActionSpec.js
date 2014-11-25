2f7746df536a5d24dbc512f8d2837580e7e29891
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-11-25 09:35:55 -0800

    [Common UI] Add specs for Save/Cancel
    Add specs for the save and cancel conclude-editing
    actions, part of bundle platform/commonUI/edit
    (Edit Mode) which is transitioned among the various
    common UI bundles. WTD-574

--- platform/commonUI/edit/test/actions/CancelActionSpec.js
+++ platform/commonUI/edit/test/actions/CancelActionSpec.js
@@ -6,7 +6,72 @@ define(
         "use strict";
 
         describe("The Cancel action", function () {
+            var mockLocation,
+                mockDomainObject,
+                mockEditorCapability,
+                actionContext,
+                action;
 
+            function mockPromise(value) {
+                return {
+                    then: function (callback) {
+                        return mockPromise(callback(value));
+                    }
+                };
+            }
+
+            beforeEach(function () {
+                mockLocation = jasmine.createSpyObj(
+                    "$location",
+                    [ "path" ]
+                );
+                mockDomainObject = jasmine.createSpyObj(
+                    "domainObject",
+                    [ "getCapability", "hasCapability" ]
+                );
+                mockEditorCapability = jasmine.createSpyObj(
+                    "editor",
+                    [ "save", "cancel" ]
+                );
+
+
+                actionContext = {
+                    domainObject: mockDomainObject
+                };
+
+                mockDomainObject.hasCapability.andReturn(true);
+                mockDomainObject.getCapability.andReturn(mockEditorCapability);
+                mockEditorCapability.cancel.andReturn(mockPromise(true));
+
+                action = new CancelAction(mockLocation, actionContext);
+
+            });
+
+            it("only applies to domain object with an editor capability", function () {
+                expect(CancelAction.appliesTo(actionContext)).toBeTruthy();
+                expect(mockDomainObject.hasCapability).toHaveBeenCalledWith("editor");
+
+                mockDomainObject.hasCapability.andReturn(false);
+                mockDomainObject.getCapability.andReturn(undefined);
+                expect(CancelAction.appliesTo(actionContext)).toBeFalsy();
+            });
+
+            it("invokes the editor capability's save functionality when performed", function () {
+                // Verify precondition
+                expect(mockEditorCapability.cancel).not.toHaveBeenCalled();
+                action.perform();
+
+                // Should have called cancel
+                expect(mockEditorCapability.cancel).toHaveBeenCalled();
+
+                // Definitely shouldn't call save!
+                expect(mockEditorCapability.save).not.toHaveBeenCalled();
+            });
+
+            it("returns to browse when performed", function () {
+                action.perform();
+                expect(mockLocation.path).toHaveBeenCalledWith("/browse");
+            });
         });
     }
 );
