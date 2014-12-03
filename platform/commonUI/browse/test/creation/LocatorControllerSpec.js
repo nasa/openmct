1753aa67843806419712bc59ab507dc6938976c8
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-12-03 14:04:44 -0800

    [Forms] Add placeholder spec for Locator
    Add placeholder spec for the LocatorController, introduced
    to support forms used from the Create menu. Part of ongoing
    integration of forms bundle, WTD-593.

---
+++ platform/commonUI/browse/test/creation/LocatorControllerSpec.js
@@ -0,0 +1,15 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+/**
+ * MCTRepresentationSpec. Created by vwoeltje on 11/6/14.
+ */
+define(
+    ["../../src/creation/LocatorController"],
+    function (LocatorController) {
+        "use strict";
+
+        describe("The locator controller", function () {
+
+        });
+    }
+);
