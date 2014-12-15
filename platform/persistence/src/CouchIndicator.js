c1ea620341d6bc8ccab59c446adb446e035c0751
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-12-15 12:34:16 -0800

    [Indicators] Implement CouchDB indicator
    Initial implementation of an indicator which reflects
    current connection to CouchDB. WTD-608.

--- platform/persistence/src/CouchIndicator.js
+++ platform/persistence/src/CouchIndicator.js
@@ -5,18 +5,49 @@ define(
     function () {
         "use strict";
 
-        function CouchIndicator($http, $interval, PATH, INTERVAL) {
-            function updateIndicator() {
+        var CONNECTED = {
+                text: "Connected",
+                glyphClass: "ok"
+            },
+            DISCONNECTED = {
+                text: "Disconnected",
+                glyphClass: "err"
+            },
+            PENDING = {
+                text: "Checking connection..."
+            };
 
+        function CouchIndicator($http, $interval, PATH, INTERVAL) {
+            var state = PENDING;
+
+            function handleError(err) {
+                state = DISCONNECTED;
             }
 
+            function handleResponse(response) {
+                var data = response.data;
+
+                state = data.error ? DISCONNECTED : CONNECTED;
+            }
+
+            function updateIndicator() {
+                $http.get(PATH).then(handleResponse, handleError);
+            }
+
+            updateIndicator();
             $interval(updateIndicator, INTERVAL);
 
             return {
                 getGlyph: function () {
                     return "D";
+                },
+                getGlyphClass: function () {
+                    return state.glyphClass;
+                },
+                getText: function () {
+                    return state.text;
                 }
-            }
+            };
 
         }
 
