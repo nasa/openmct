8ce693af0ad78fb30afd97c6c0bb4af4e9802f49
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-12-16 13:47:25 -0800

    [Indicators] Add skeleton specs
    Add skeleton specs for the bottom bar controller (introduced
    to handle display of indicators introduced as extensions)
    and the Couch indicator (introduced to exercise the above,
    and to report connection status relative to the Couch
    database.) WTD-608.

---
+++ platform/commonUI/general/test/BottomBarControllerSpec.js
@@ -0,0 +1,13 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+define(
+    ["../src/BottomBarController"],
+    function (BottomBarController) {
+        "use strict";
+
+        describe("The bottom bar controller", function () {
+            var testIndicators,
+                controller;
+        });
+    }
+);
---
+++ platform/persistence/test/CouchIndicatorSpec.js
@@ -0,0 +1,12 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+define(
+    ["../src/CouchIndicator"],
+    function (CouchIndicator) {
+        "use strict";
+
+        describe("The CouchDB status indicator", function () {
+
+        });
+    }
+);
