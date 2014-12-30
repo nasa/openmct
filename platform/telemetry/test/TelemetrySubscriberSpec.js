f7e3c9479bae02e5f7ffc0e701f2c5caa357b55f
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-12-30 14:10:51 -0800

    [Telemetry] Add skeleton specs
    Add empty test scripts for new scripts introduced to
    the platform/telemetry bundle in support of
    the autoflow tabular view, WTD-614.

---
+++ platform/telemetry/test/TelemetrySubscriberSpec.js
@@ -0,0 +1,12 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+define(
+    ["../src/TelemetrySubscriber"],
+    function (TelemetrySubscriber) {
+        "use strict";
+
+        describe("The telemetry subscriber", function () {
+
+        });
+    }
+);
---
+++ platform/telemetry/test/TelemetrySubscriptionSpec.js
@@ -0,0 +1,12 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+define(
+    ["../src/TelemetrySubscription"],
+    function (TelemetrySubscription) {
+        "use strict";
+
+        describe("A telemetry subscription", function () {
+
+        });
+    }
+);
