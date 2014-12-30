dd1b8f7c2fbda3cdb416eccd227399c41f7521f6
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-12-30 14:08:37 -0800

    [Common UI] Add skeleton specs
    Add empty test scripts for new scripts introduced to
    the platform/commonUI/general bundle in support of
    the autoflow tabular view, WTD-614.

---
+++ platform/commonUI/general/test/controllers/GetterSetterControllerSpec.js
@@ -0,0 +1,12 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+define(
+    ["../../src/controllers/GetterSetterController"],
+    function (GetterSetterController) {
+        "use strict";
+
+        describe("The getter-setter controller", function () {
+
+        });
+    }
+);
---
+++ platform/commonUI/general/test/directives/MCTResizeSpec.js
@@ -0,0 +1,12 @@
+/*global define,Promise,describe,it,expect,beforeEach,waitsFor,jasmine*/
+
+define(
+    ["../../src/directives/MCTResize"],
+    function (MCTResize) {
+        "use strict";
+
+        describe("The mct-resize directive", function () {
+
+        });
+    }
+);
