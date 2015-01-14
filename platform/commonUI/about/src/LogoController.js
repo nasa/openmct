f7a33659b40ab3151369b4b618ac563f4b5ac2d4
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-01-14 13:15:24 -0800

    [About] Add JSDoc
    Add in-line documentation to controllers introduced to
    support the About dialog. WTD-667.

--- platform/commonUI/about/src/LogoController.js
+++ platform/commonUI/about/src/LogoController.js
@@ -5,8 +5,18 @@ define(
     function () {
         "use strict";
 
+        /**
+         * The LogoController provides functionality to the application
+         * logo in the bottom-right of the user interface.
+         * @constructor
+         * @param {OverlayService} overlayService the overlay service
+         */
         function LogoController(overlayService) {
             return {
+                /**
+                 * Display the About dialog.
+                 * @memberof LogoController#
+                 */
                 showAboutDialog: function () {
                     overlayService.createOverlay("overlay-about");
                 }
