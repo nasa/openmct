668a1e4534fe7de32be082488fa8e883240b7c58
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-02-20 15:29:37 -0800

    [Fixed Position] Add empty specs
    Add empty specs for classes introduced in support of
    Add menu options for fixed position view, WTD-880.

---
+++ platform/features/layout/test/elements/ElementFactorySpec.js
@@ -0,0 +1,11 @@
+/*global define,describe,it,expect,beforeEach,jasmine*/
+
+define(
+    ['../../src/elements/ElementFactory'],
+    function (ElementFactory) {
+        "use strict";
+
+        describe("The fixed position element factory", function () {
+        });
+    }
+);
---
+++ platform/features/layout/test/elements/LineProxySpec.js
@@ -0,0 +1,11 @@
+/*global define,describe,it,expect,beforeEach,jasmine*/
+
+define(
+    ['../../src/elements/LineProxy'],
+    function (LineProxy) {
+        "use strict";
+
+        describe("A fixed position line proxy", function () {
+        });
+    }
+);
