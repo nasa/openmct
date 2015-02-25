f11eced6b90a87cc074e8b2c34360f0efd1fce1b
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-02-25 15:46:36 -0800

    [Common UI] Add spec for MCT scroll directive parent
    Add spec for MCTScroll, which will act as a parent class
    for mct-scroll-x and mct-scroll-y directives, WTD-920.

---
+++ platform/commonUI/general/test/directives/MCTScrollSpec.js
@@ -0,0 +1,90 @@
+/*global define,describe,it,expect,jasmine,beforeEach*/
+define(
+    ['../../src/directives/MCTScroll'],
+    function (MCTScroll) {
+        "use strict";
+
+        var EVENT_PROPERTY = "testProperty",
+            ATTRIBUTE = "testAttribute",
+            EXPRESSION = "some.expression";
+
+
+        // MCTScroll is the commonality between mct-scroll-x and
+        // mct-scroll-y; it gets the event property to watch and
+        // the attribute which contains the associated assignable
+        // expression.
+        describe("An mct-scroll-* directive", function () {
+            var mockParse,
+                mockParsed,
+                mockScope,
+                mockElement,
+                testAttrs,
+                mctScroll;
+
+            beforeEach(function () {
+                mockParse = jasmine.createSpy('$parse');
+                mockParsed = jasmine.createSpy('parsed');
+                mockParsed.assign = jasmine.createSpy('assign');
+
+                mockScope = jasmine.createSpyObj('$scope', ['$watch']);
+                mockElement = [{ scrollLeft: 42 }];
+                mockElement.on = jasmine.createSpy('on');
+
+                mockParse.andReturn(mockParsed);
+
+                testAttrs = {};
+                testAttrs[ATTRIBUTE] = EXPRESSION;
+
+                mctScroll = new MCTScroll(
+                    mockParse,
+                    EVENT_PROPERTY,
+                    ATTRIBUTE
+                );
+                mctScroll.link(mockScope, mockElement, testAttrs);
+            });
+
+            it("is available for attributes", function () {
+                expect(mctScroll.restrict).toEqual('A');
+            });
+
+            it("does not create an isolate scope", function () {
+                expect(mctScroll.scope).toBeUndefined();
+            });
+
+            it("watches for changes in observed expression", function () {
+                expect(mockScope.$watch).toHaveBeenCalledWith(
+                    EXPRESSION,
+                    jasmine.any(Function)
+                );
+                // Should have been only watch (other tests need this to be true)
+                expect(mockScope.$watch.calls.length).toEqual(0);
+            });
+
+            it("listens for scroll events", function () {
+                expect(mockElement.on).toHaveBeenCalledWith(
+                    'scroll',
+                    jasmine.any(Function)
+                );
+                // Should have been only listener (other tests need this to be true)
+                expect(mockElement.on.calls.length).toEqual(0);
+            });
+
+            it("publishes initial scroll state", function () {
+                expect(mockParse).toHaveBeenCalledWith(EXPRESSION);
+                expect(mockParsed.assign).toHaveBeenCalledWith(42);
+            });
+
+            it("updates scroll state when scope changes", function () {
+                mockScope.$watch.mostRecentCall.args[1](64);
+                expect(mockElement[0].scrollLeft).toEqual(64);
+            });
+
+            it("updates scope when scroll state changes", function () {
+                mockElement[0].scrollLeft = 12321;
+                mockElement.on.mostRecentCall.args({ target: mockElement[0] });
+                expect(mockParsed.assign).toHaveBeenCalledWith(12321);
+            });
+
+        });
+    }
+);
