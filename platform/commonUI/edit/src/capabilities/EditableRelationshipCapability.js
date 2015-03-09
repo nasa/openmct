bbe26cd06c5b6d18ba96f87ceeaf0c1c30294e96
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-03-09 15:29:25 -0700

    [Core] Edit-wrap relationship capability
    Wrap objects retrieved via the relationship capability
    with Edit mode caching etc, for WTD-1007.

--- platform/commonUI/edit/src/capabilities/EditableRelationshipCapability.js
+++ platform/commonUI/edit/src/capabilities/EditableRelationshipCapability.js
@@ -7,7 +7,7 @@ define(
         'use strict';
 
         /**
-         * Wrapper for the "context" capability;
+         * Wrapper for the "relationship" capability;
          * ensures that any domain objects reachable in Edit mode
          * are also wrapped as EditableDomainObjects.
          *
@@ -15,20 +15,21 @@ define(
          * associated cache; the constructor signature is particular
          * to a pattern used there and may contain unused arguments.
          */
-        return function EditableContextCapability(
-            contextCapability,
+        return function EditableRelationshipCapability(
+            relationshipCapability,
             editableObject,
             domainObject,
             cache
         ) {
             // This is a "lookup" style capability (it looks up other
-            // domain objects), and it should be idempotent
+            // domain objects), but we do not want to return the same
+            // specific value every time (composition may change)
             return new EditableLookupCapability(
-                contextCapability,
+                relationshipCapability,
                 editableObject,
                 domainObject,
                 cache,
-                true // Idempotent
+                false // Not idempotent
             );
         };
     }
