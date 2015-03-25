d88a0237c6242779686d37497d261fbc0e59b2a4
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-03-24 17:43:06 -0700

    [Persistence] Continue adding tests
    Continue adding test cases for revision checking bundle, WTD-1033.

--- platform/persistence/queue/test/QueuingPersistenceCapabilitySpec.js
+++ platform/persistence/queue/test/QueuingPersistenceCapabilitySpec.js
@@ -7,7 +7,40 @@ define(
         "use strict";
 
         describe("A queuing persistence capability", function () {
+            var mockQueue,
+                mockPersistence,
+                mockDomainObject,
+                persistence;
 
+            beforeEach(function () {
+                mockQueue = jasmine.createSpyObj('queue', ['put']);
+                mockPersistence = jasmine.createSpyObj(
+                    'persistence',
+                    ['persist', 'refresh']
+                );
+                mockDomainObject = {};
+                persistence = new QueuingPersistenceCapability(
+                    mockQueue,
+                    mockPersistence,
+                    mockDomainObject
+                );
+            });
+
+            it("puts a request for persistence into the queue on persist", function () {
+                // Verify precondition
+                expect(mockQueue.put).not.toHaveBeenCalled();
+                // Invoke persistence
+                persistence.persist();
+                // Should have queued
+                expect(mockQueue.put).toHaveBeenCalledWith(
+                    mockDomainObject,
+                    mockPersistence
+                );
+            });
+
+            it("exposes other methods from the wrapped persistence capability", function () {
+                expect(persistence.refresh).toBe(mockPersistence.refresh);
+            });
         });
     }
 );
