29584f2a7eda96aaea25566dede2c43b4c793f09
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-03-25 11:36:43 -0700

    [Persistence] Complete tests for queue
    Complete tests for platform/persistence/queue, WTD-1033.

--- platform/persistence/queue/test/PersistenceQueueSpec.js
+++ platform/persistence/queue/test/PersistenceQueueSpec.js
@@ -7,6 +7,28 @@ define(
         "use strict";
 
         describe("The persistence queue", function () {
+            var mockQ,
+                mockTimeout,
+                mockDialogService,
+                queue;
+
+            beforeEach(function () {
+                mockQ = jasmine.createSpyObj("$q", ['defer']);
+                mockTimeout = jasmine.createSpy("$timeout");
+                mockDialogService = jasmine.createSpyObj(
+                    'dialogService',
+                    ['getUserChoice']
+                );
+                queue = new PersistenceQueue(mockQ, mockTimeout, mockDialogService);
+            });
+
+            // PersistenceQueue is just responsible for handling injected
+            // dependencies and wiring the PersistenceQueueImpl and its
+            // handlers. Functionality is tested there, so our test here is
+            // minimal (get back expected interface, no exceptions)
+            it("provides a queue with a put method", function () {
+                expect(queue.put).toEqual(jasmine.any(Function));
+            });
 
         });
     }
