30013f2bf5449365b3b66a2a0451516bf1454f6a
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-04-01 16:48:58 -0700

    [Licenses] Add script for license headers
    Add script to add license headers, WTD-1051.

---
+++ scripts/licenses.sh
@@ -0,0 +1,12 @@
+LICENSE=`cat $3`
+MATCHES=`find $1 -name "*.$2" | grep -v /lib/`
+
+for i in $MATCHES
+do
+  if ! grep -q "$LICENSE" $i
+  then
+    cat "$3" "$i" > "$i".new && mv "$i".new "$i"
+  fi
+done
+
+echo "$LICENSE"
