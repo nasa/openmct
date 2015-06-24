fd81c5c859ece11c982a887e912043552cc007a3
Sarah Hale <sarahlauhale@gmail.com>
2015-06-24 15:19:47 -0700

    [Events] No Period
    No longer asks for a period when creating a
    new Event Message Generator. #18.

--- example/eventGenerator/src/EventTelemetry.js
+++ example/eventGenerator/src/EventTelemetry.js
@@ -19,7 +19,7 @@
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
-/*global define,Promise*/
+/*global define */
 
 /**
  * Module defining EventTelemetry. 
