fdfa35d1da796818352ecf7a012489d605e3db20
Charles Hacskaylo <charlesh88@gmail.com>
2015-06-24 11:42:29 -0700

    [Frontend] New symbols font and CSS adjustments
    WTD-980
    WTD-396
    Changed glyph for stacked plots;
    Normalized button line-heights and size of required symbol;
    Updated symbol art in symbols font;
    Restored mistakenly removed "menu" gesture to grid-item;

--- platform/features/plot/src/modes/PlotModeOptions.js
+++ platform/features/plot/src/modes/PlotModeOptions.js
@@ -29,7 +29,7 @@ define(
         var STACKED = {
                 key: "stacked",
                 name: "Stacked",
-                glyph: "8",
+                glyph: "m",
                 factory: PlotStackMode
             },
             OVERLAID = {
