d2fe71d82bc078adc624fe4e88de5a16de28f9bf
Sarah Hale <sarahlauhale@gmail.com>
2015-06-24 17:08:03 -0700

    [Events] Fixed tabs
    Removed tabs from EventListController and replaced
    with spaces. #18.

--- platform/features/events/src/EventListController.js
+++ platform/features/events/src/EventListController.js
@@ -27,107 +27,107 @@
  * Modified by shale on 06/23/2015.
  */
 define(
-	["./DomainColumn", "./RangeColumn", "./EventListPopulator"],
-	function (DomainColumn, RangeColumn, EventListPopulator) {
-		"use strict";
+    ["./DomainColumn", "./RangeColumn", "./EventListPopulator"],
+    function (DomainColumn, RangeColumn, EventListPopulator) {
+        "use strict";
         
-		var ROW_COUNT = 100;
+        var ROW_COUNT = 100;
 
-		/**
-		 * The EventListController is responsible for populating
-		 * the contents of the event list view.
-		 * @constructor
-		 */
-		function EventListController($scope, formatter) {
-			var populator;
+        /**
+         * The EventListController is responsible for populating
+         * the contents of the event list view.
+         * @constructor
+         */
+        function EventListController($scope, formatter) {
+            var populator;
             
-			// Get a set of populated, ready-to-display rows for the
-			// latest data values.
-			function getRows(telemetry) {
-				var datas = telemetry.getResponse(),
-					objects = telemetry.getTelemetryObjects();
+            // Get a set of populated, ready-to-display rows for the
+            // latest data values.
+            function getRows(telemetry) {
+                var datas = telemetry.getResponse(),
+                    objects = telemetry.getTelemetryObjects();
                 
-				return populator.getRows(datas, objects, ROW_COUNT);
-			}
+                return populator.getRows(datas, objects, ROW_COUNT);
+            }
 
-			// Update the contents
-			function updateRows() {
-				var telemetry = $scope.telemetry;
-				$scope.rows = telemetry ? getRows(telemetry) : [];
-			}
+            // Update the contents
+            function updateRows() {
+                var telemetry = $scope.telemetry;
+                $scope.rows = telemetry ? getRows(telemetry) : [];
+            }
 
-			// Set up columns based on telemetry metadata. This will
-			// include one column for each domain and range type, as
-			// well as a column for the domain object name.
-			function setupColumns(telemetry) {
-				var domainKeys = {},
-					rangeKeys = {},
-					columns = [],
-					metadata;
+            // Set up columns based on telemetry metadata. This will
+            // include one column for each domain and range type, as
+            // well as a column for the domain object name.
+            function setupColumns(telemetry) {
+                var domainKeys = {},
+                    rangeKeys = {},
+                    columns = [],
+                    metadata;
 
-				// Add a domain to the set of columns, if a domain
-				// with the same key has not yet been inclued.
-				function addDomain(domain) {
-					var key = domain.key;
-					if (key && !domainKeys[key]) {
-						domainKeys[key] = true;
-						columns.push(new DomainColumn(domain, formatter));
-					}
-				}
+                // Add a domain to the set of columns, if a domain
+                // with the same key has not yet been inclued.
+                function addDomain(domain) {
+                    var key = domain.key;
+                    if (key && !domainKeys[key]) {
+                        domainKeys[key] = true;
+                        columns.push(new DomainColumn(domain, formatter));
+                    }
+                }
 
                 // Add a range col to the set of columns, if a range
-				// with the same key has not yet been included.
-				function addRange(range) {
-					var key = range.key;
-					if (key && !rangeKeys[key]) {
-						rangeKeys[key] = true;
-						columns.push(new RangeColumn(range, formatter));
-					}
-				}
+                // with the same key has not yet been included.
+                function addRange(range) {
+                    var key = range.key;
+                    if (key && !rangeKeys[key]) {
+                        rangeKeys[key] = true;
+                        columns.push(new RangeColumn(range, formatter));
+                    }
+                }
                 
-				// We cannot proceed if the telemetry controller
-				// is not available; clear all rows/columns.
-				if (!telemetry) {
-					columns = [];
-					$scope.rows = [];
-					$scope.headers = [];
-					return;
-				}
+                // We cannot proceed if the telemetry controller
+                // is not available; clear all rows/columns.
+                if (!telemetry) {
+                    columns = [];
+                    $scope.rows = [];
+                    $scope.headers = [];
+                    return;
+                }
 
-				// Add domain, range, event msg columns
-				metadata = telemetry.getMetadata();
-				(metadata || []).forEach(function (metadata) {
-					(metadata.domains || []).forEach(addDomain);
-				});
-				(metadata || []).forEach(function (metadata) {
-					(metadata.ranges || []).forEach(addRange);
-				});
+                // Add domain, range, event msg columns
+                metadata = telemetry.getMetadata();
+                (metadata || []).forEach(function (metadata) {
+                    (metadata.domains || []).forEach(addDomain);
+                });
+                (metadata || []).forEach(function (metadata) {
+                    (metadata.ranges || []).forEach(addRange);
+                });
                 
                 // Add default domain and range columns if none
-				// were described in metadata.
-				if (Object.keys(domainKeys).length < 1) {
-					columns.push(new DomainColumn({name: "Time"}, formatter));
-				}
-				if (Object.keys(rangeKeys).length < 1) {
-					columns.push(new RangeColumn({name: "Message"}, formatter));
-				}
+                // were described in metadata.
+                if (Object.keys(domainKeys).length < 1) {
+                    columns.push(new DomainColumn({name: "Time"}, formatter));
+                }
+                if (Object.keys(rangeKeys).length < 1) {
+                    columns.push(new RangeColumn({name: "Message"}, formatter));
+                }
                 
-				// We have all columns now; use them to initializer
-				// the populator, which will use them to generate
-				// actual rows and headers.
-				populator = new EventListPopulator(columns);
+                // We have all columns now; use them to initializer
+                // the populator, which will use them to generate
+                // actual rows and headers.
+                populator = new EventListPopulator(columns);
                 
-				// Initialize headers
-				$scope.headers = populator.getHeaders();
+                // Initialize headers
+                $scope.headers = populator.getHeaders();
 
-				// Fill in the contents of the rows.
-				updateRows();
-			}
+                // Fill in the contents of the rows.
+                updateRows();
+            }
 
-			$scope.$on("telemetryUpdate", updateRows);
-			$scope.$watch("telemetry", setupColumns);
-		}
+            $scope.$on("telemetryUpdate", updateRows);
+            $scope.$watch("telemetry", setupColumns);
+        }
 
-		return EventListController;
-	}
+        return EventListController;
+    }
 );
