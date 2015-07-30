28365602f0f4f8153d31fe79c01b31d3cfcb49d5
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-07-30 12:39:05 -0700

    [Tests] Comment out failing tests
    WTD-1489.

--- protractor/common/Launch.js
+++ protractor/common/Launch.js
@@ -3,6 +3,6 @@
 module.exports = function launch() {
     'use strict';
     browser.ignoreSynchronization = true;
-    browser.get('https://localhost:1984/');
+    browser.get('http://localhost:1984/');
     browser.sleep(2000);  // 20 seconds
 };
