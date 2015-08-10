85f9d5a2da1c1e622ae9c6ca04b76d7cd7a880a6
Jesse <jesse.t.sanderfer@nasa.gov>
2015-08-10 15:26:25 -0700

    Added Start Run Stop Scripts

---
+++ protractor/bin/stop.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+var shell = require("shelljs/global");
+var ps = require('psnode');
+var S = require('string');
+ 
+// A simple pid lookup 
+ps.list(function(err, results) {
+ 
+  results.forEach(function( process ){
+          //Killing Node
+          if((process.command.indexOf("node app.js")) != -1) {
+              console.log();
+              console.log( 'Killing Node: %s', process.command);
+              ps.kill(process.pid, function(err, stdout) {
+              if (err) {
+                 throw new Error(err);
+              }
+              console.log(stdout);
+              });
+          }
+          if((process.command.indexOf("webdriver")) != -1) {
+              console.log();
+              console.log( 'Killing WebDriver: %s', process.command);
+              ps.kill(process.pid, function(err, stdout) {
+              if (err){
+                  throw new Error(err);
+              }
+              console.log(stdout);
+              });
+          }
+      });
+});
+
+
