4481c44c4bda753a3fba029f3405fee8361c24c2
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-08-17 11:20:23 -0700

    [Code Style] Refactor search to use prototypes
    WTD-1482.

--- platform/persistence/elastic/src/ElasticsearchSearchProvider.js
+++ platform/persistence/elastic/src/ElasticsearchSearchProvider.js
@@ -44,17 +44,52 @@ define(
          * @param $http Angular's $http service, for working with urls.
          * @param {ObjectService} objectService the service from which
          *        domain objects can be gotten.
-         * @param ROOT the constant ELASTIC_ROOT which allows us to 
+         * @param root the constant `ELASTIC_ROOT` which allows us to
          *        interact with ElasticSearch.
          */
-        function ElasticsearchSearchProvider($http, objectService, ROOT) {
-            
-            // Add the fuzziness operator to the search term 
+        function ElasticsearchSearchProvider($http, objectService, root) {
+            this.$http = $http;
+            this.objectService = objectService;
+            this.root = root;
+        }
+
+        /**
+         * Searches through the filetree for domain objects using a search
+         *   term. This is done through querying elasticsearch. Returns a
+         *   promise for a result object that has the format
+         *   {hits: searchResult[], total: number, timedOut: boolean}
+         *   where a searchResult has the format
+         *   {id: string, object: domainObject, score: number}
+         *
+         * Notes:
+         *   * The order of the results is from highest to lowest score,
+         *     as elsaticsearch determines them to be.
+         *   * Uses the fuzziness operator to get more results.
+         *   * More about this search's behavior at
+         *     https://www.elastic.co/guide/en/elasticsearch/reference/current/search-uri-request.html
+         *
+         * @param searchTerm The text input that is the query.
+         * @param timestamp The time at which this function was called.
+         *   This timestamp is used as a unique identifier for this
+         *   query and the corresponding results.
+         * @param maxResults (optional) The maximum number of results
+         *   that this function should return.
+         * @param timeout (optional) The time after which the search should
+         *   stop calculations and return partial results. Elasticsearch
+         *   does not guarentee that this timeout will be strictly followed.
+         */
+        ElasticsearchSearchProvider.prototype.query = function query(searchTerm, timestamp, maxResults, timeout) {
+            var $http = this.$http,
+                objectService = this.objectService,
+                root = this.root,
+                esQuery;
+
+            // Add the fuzziness operator to the search term
             function addFuzziness(searchTerm, editDistance) {
                 if (!editDistance) {
                     editDistance = '';
                 }
-                
+
                 return searchTerm.split(' ').map(function (s) {
                     // Don't add fuzziness for quoted strings
                     if (s.indexOf('"') !== -1) {
@@ -64,11 +99,11 @@ define(
                     }
                 }).join(' ');
             }
-            
+
             // Currently specific to elasticsearch
             function processSearchTerm(searchTerm) {
                 var spaceIndex;
-                
+
                 // Cut out any extra spaces
                 while (searchTerm.substr(0, 1) === ' ') {
                     searchTerm = searchTerm.substring(1, searchTerm.length);
@@ -79,18 +114,18 @@ define(
                 spaceIndex = searchTerm.indexOf('  ');
                 while (spaceIndex !== -1) {
                     searchTerm = searchTerm.substring(0, spaceIndex) +
-                                    searchTerm.substring(spaceIndex + 1, searchTerm.length);
+                    searchTerm.substring(spaceIndex + 1, searchTerm.length);
                     spaceIndex = searchTerm.indexOf('  ');
                 }
-                
+
                 // Add fuzziness for completeness
                 searchTerm = addFuzziness(searchTerm);
-                
+
                 return searchTerm;
             }
-            
-            // Processes results from the format that elasticsearch returns to 
-            // a list of searchResult objects, then returns a result object 
+
+            // Processes results from the format that elasticsearch returns to
+            // a list of searchResult objects, then returns a result object
             // (See documentation for query for object descriptions)
             function processResults(rawResults, timestamp) {
                 var results = rawResults.data.hits.hits,
@@ -99,25 +134,25 @@ define(
                     scores = {},
                     searchResults = [],
                     i;
-                
+
                 // Get the result objects' IDs
                 for (i = 0; i < resultsLength; i += 1) {
                     ids.push(results[i][ID]);
                 }
-                
+
                 // Get the result objects' scores
                 for (i = 0; i < resultsLength; i += 1) {
                     scores[ids[i]] = results[i][SCORE];
                 }
-                
+
                 // Get the domain objects from their IDs
                 return objectService.getObjects(ids).then(function (objects) {
                     var j,
                         id;
-                    
+
                     for (j = 0; j < resultsLength; j += 1) {
                         id = ids[j];
-                        
+
                         // Include items we can get models for
                         if (objects[id].getModel) {
                             // Format the results as searchResult objects
@@ -128,7 +163,7 @@ define(
                             });
                         }
                     }
-                    
+
                     return {
                         hits: searchResults,
                         total: rawResults.data.hits.total,
@@ -136,76 +171,43 @@ define(
                     };
                 });
             }
-            
-            // For documentation, see query below.
-            function query(searchTerm, timestamp, maxResults, timeout) {
-                var esQuery;
-                
-                // Check to see if the user provided a maximum 
-                // number of results to display
-                if (!maxResults) {
-                    // Else, we provide a default value. 
-                    maxResults = DEFAULT_MAX_RESULTS;
-                }
-                
-                // If the user input is empty, we want to have no search results.
-                if (searchTerm !== '' && searchTerm !== undefined) {
-                    // Process the search term
-                    searchTerm = processSearchTerm(searchTerm);
 
-                    // Create the query to elasticsearch
-                    esQuery = ROOT + "/_search/?q=" + searchTerm +
-                                     "&size=" + maxResults;
-                    if (timeout) {
-                        esQuery += "&timeout=" + timeout;
-                    }
 
-                    // Get the data...
-                    return $http({
-                        method: "GET",
-                        url: esQuery
-                    }).then(function (rawResults) {
-                        // ...then process the data 
-                        return processResults(rawResults, timestamp);
-                    }, function (err) {
-                        // In case of error, return nothing. (To prevent
-                        //   infinite loading time.)
-                        return {hits: [], total: 0};
-                    });
-                } else {
-                    return {hits: [], total: 0};
-                }
+            // Check to see if the user provided a maximum
+            // number of results to display
+            if (!maxResults) {
+                // Else, we provide a default value.
+                maxResults = DEFAULT_MAX_RESULTS;
             }
-            
-            return {
-                /**
-                 * Searches through the filetree for domain objects using a search 
-                 *   term. This is done through querying elasticsearch. Returns a
-                 *   promise for a result object that has the format
-                 *   {hits: searchResult[], total: number, timedOut: boolean}
-                 *   where a searchResult has the format
-                 *   {id: string, object: domainObject, score: number}
-                 *
-                 * Notes:
-                 *   * The order of the results is from highest to lowest score, 
-                 *     as elsaticsearch determines them to be. 
-                 *   * Uses the fuzziness operator to get more results. 
-                 *   * More about this search's behavior at 
-                 *     https://www.elastic.co/guide/en/elasticsearch/reference/current/search-uri-request.html
-                 * 
-                 * @param searchTerm The text input that is the query. 
-                 * @param timestamp The time at which this function was called.
-                 *   This timestamp is used as a unique identifier for this 
-                 *   query and the corresponding results. 
-                 * @param maxResults (optional) The maximum number of results 
-                 *   that this function should return.
-                 * @param timeout (optional) The time after which the search should 
-                 *   stop calculations and return partial results. Elasticsearch 
-                 *   does not guarentee that this timeout will be strictly followed. 
-                 */
-                query: query
-            };
-        }
+
+            // If the user input is empty, we want to have no search results.
+            if (searchTerm !== '' && searchTerm !== undefined) {
+                // Process the search term
+                searchTerm = processSearchTerm(searchTerm);
+
+                // Create the query to elasticsearch
+                esQuery = root + "/_search/?q=" + searchTerm +
+                "&size=" + maxResults;
+                if (timeout) {
+                    esQuery += "&timeout=" + timeout;
+                }
+
+                // Get the data...
+                return this.$http({
+                    method: "GET",
+                    url: esQuery
+                }).then(function (rawResults) {
+                    // ...then process the data
+                    return processResults(rawResults, timestamp);
+                }, function (err) {
+                    // In case of error, return nothing. (To prevent
+                    //   infinite loading time.)
+                    return {hits: [], total: 0};
+                });
+            } else {
+                return {hits: [], total: 0};
+            }
+        };
 
 
         return ElasticsearchSearchProvider;
