2542255342a9d3919a35e9934fb4619f19247819
Shivam Dave <sdave@ucsc.edu>
2015-08-26 10:16:22 -0700

    [Mobile] AgentService call
    window referred to in TreeNodeController added as
    keyword to skip in top comment.

--- platform/commonUI/general/src/controllers/TreeNodeController.js
+++ platform/commonUI/general/src/controllers/TreeNodeController.js
@@ -19,7 +19,7 @@
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
-/*global define,Promise*/
+/*global define,Promise,window*/
 
 /**
  * Module defining TreeNodeController. Created by vwoeltje on 11/10/14.
