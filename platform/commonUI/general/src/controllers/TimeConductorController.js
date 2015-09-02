91fe3d798f61caa903905f95ade35bb69fab55ea
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-09-02 16:31:58 -0700

    [Time Conductor] Begin adding controller
    WTD-1515

---
+++ platform/commonUI/general/src/controllers/TimeConductorController.js
@@ -0,0 +1,62 @@
+/*****************************************************************************
+ * Open MCT Web, Copyright (c) 2014-2015, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT Web is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT Web includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+/*global define,Promise*/
+
+define(
+    [],
+    function () {
+        "use strict";
+
+        /**
+         * A ToggleController is used to activate/deactivate things.
+         * A common usage is for "twistie"
+         *
+         * @memberof platform/commonUI/general
+         * @constructor
+         */
+        function TimeConductorController($scope) {
+            var tickCount = 2;
+
+            $scope.state = false;
+
+            $scope.ticks = [0, 1];
+
+            function updateTicks() {
+                var i;
+                $scope.ticks = [];
+                for (i = 0; i < tickCount; i += 1) {
+                    $scope.ticks.push(i / (tickCount - 1));
+                }
+            }
+
+            function updateSpanWidth(w) {
+                // Space about 100px apart
+                tickCount = Math.max(Math.floor(w / 100), 2);
+                updateTicks();
+            }
+
+            $scope.$watch("spanWidth", updateSpanWidth);
+        }
+
+        return TimeConductorController;
+    }
+);
