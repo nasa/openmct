2aeebff652b2098193d348ae8162260f9a67e955
Charles Hacskaylo <charles.f.hacskaylo@nasa.gov>
2015-10-06 14:41:58 -0700

    Merging in Andrew's work so far on progress and blocking dialogs
    open #163
    open #170
    
    Squashed commit of the following:
    
    commit ec7edb58ca75b409d330b5434999c67a0ec14e33
    Author: Henry <akhenry@aitutaki.ndc.nasa.gov>
    Date:   Mon Oct 5 10:39:06 2015 -0700
    
        Rename dialogSeverity to messageServity for reuse with notifications
    
    commit d20abe01dda50f7508444ed65b207cb6210e5fff
    Author: Henry <akhenry@aitutaki.ndc.nasa.gov>
    Date:   Fri Oct 2 16:40:29 2015 -0700
    
        Fixed docs
    
    commit 227da1849826af54c66932dba55f692e085194b5
    Author: Henry <akhenry@aitutaki.ndc.nasa.gov>
    Date:   Fri Oct 2 16:27:41 2015 -0700
    
        Added semicolon
    
    commit 22d424f96e74e44230f100cde2108aff4ef10944
    Author: Henry <akhenry@aitutaki.ndc.nasa.gov>
    Date:   Fri Oct 2 16:26:29 2015 -0700
    
        Fixed code errors
    
    commit 2c77c3647c326f91fdbe2d63a8e3f1c3040d7397
    Author: Henry <akhenry@aitutaki.ndc.nasa.gov>
    Date:   Fri Oct 2 16:24:01 2015 -0700
    
        Initial commit of blocking dialog service with test code to demonstrate usage

---
+++ testing/dialogTest/src/DialogLaunchController.js
@@ -0,0 +1,94 @@
+/*****************************************************************************
+ * Open MCT Web, Copyright (c) 2014-2015, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT Web is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT Web includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+/*global define*/
+
+define(
+    [],
+    function () {
+        "use strict";
+
+        function DialogLaunchController($scope, dialogService, $timeout, $log, messageSeverity) {
+            $scope.launchProgress = function () {
+                var model = {
+                    title: "Progress dialog example",
+                    progress: 0,
+                    hint: "Calculating...",
+                    unknownDuration: false,
+                    severity: messageSeverity.INFO,
+                    actions: [
+                        {
+                            label: "Cancel Operation",
+                            action: function () {
+                                $log.debug("Operation cancelled");
+                                dialogService.dismiss();
+                            }
+                        },
+                        {
+                            label: "Do something else...",
+                            action: function () {
+                                $log.debug("Something else pressed");
+                            }
+                        }
+                    ]
+                };
+
+                function incrementProgress() {
+                    model.progress = Math.min(100, Math.floor(model.progress + Math.random() * 30));
+                    model.progressText = ["Estimated time remaining: about ", 60 - Math.floor((model.progress / 100) * 60), " seconds"].join(" ");
+                    if (model.progress < 100) {
+                        $timeout(incrementProgress, 1000);
+                    }
+                }
+
+                if (dialogService.showBlockingMessage(model)) {
+                    //Do processing here
+                    model.hint = "Processing 100 objects...";
+                    $timeout(incrementProgress, 1000);
+                } else {
+                    $log.error("Could not display modal dialog");
+                }
+            };
+
+            $scope.launchError = function () {
+                var model = {
+                    title: "Error Message Title",
+                    hint: "Something happened. It was not so good.",
+                    severity: messageSeverity.ERROR,
+                    actions: [
+                        {
+                            label: "OK",
+                            action: function () {
+                                $log.debug("OK Pressed");
+                                dialogService.dismiss();
+                            }
+                        }
+                    ]
+                };
+
+                if (!dialogService.showBlockingMessage(model)) {
+                    $log.error("Could not display modal dialog");
+                }
+            };
+        }
+        return DialogLaunchController;
+    }
+);
