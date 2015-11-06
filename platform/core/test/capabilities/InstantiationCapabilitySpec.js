64607b8e56d0e1444a3c5ec0164b62e0946357be
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-11-06 15:47:33 -0800

    [Creation] Remove errant comments

--- platform/core/test/capabilities/InstantiationCapabilitySpec.js
+++ platform/core/test/capabilities/InstantiationCapabilitySpec.js
@@ -21,9 +21,6 @@
  *****************************************************************************/
 /*global define,Promise,describe,it,xdescribe,expect,beforeEach,waitsFor,jasmine*/
 
-/**
- * ContextCapability. Created by vwoeltje on 11/6/14.
- */
 define(
     ["../../src/capabilities/InstantiationCapability"],
     function (InstantiationCapability) {
