4ad007f882b79f98edea4b72ac0450704c2b430c
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-11-12 14:52:09 -0800

    [Common UI] Add z-index to popups
    Temporary workaround for nasa/openmctweb#298

--- platform/commonUI/general/src/directives/MCTPopup.js
+++ platform/commonUI/general/src/directives/MCTPopup.js
@@ -51,6 +51,8 @@ define(
                     position = [ rect.left, rect.top ],
                     popup = popupService.display(div, position);
 
+                div.css('z-index', 75);
+
                 transclude(function (clone) {
                     div.append(clone);
                 });
