d4d45c2efe106f7798de16800c3120a867d7d3e0
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-11-13 13:03:17 -0800

    [Observables] Add observable class

---
+++ platform/observables/src/Observable.js
@@ -0,0 +1,36 @@
+/*global define*/
+
+define(
+    [],
+    function () {
+        'use strict';
+
+        function Observable(initialValue) {
+            this.value = initialValue;
+            this.listeners = [];
+        }
+
+        Observable.prototype.get = function () {
+            return this.value;
+        };
+
+        Observable.prototype.set = function (newValue) {
+            this.value = newValue;
+            this.listeners.forEach(function (listener) {
+                listener(newValue);
+            });
+        };
+
+        Observable.prototype.observe = function (listener) {
+            var self = this;
+            this.listeners.push(listener);
+            return function () {
+                self.listeners = self.listeners.filter(function (fn) {
+                    return fn !== listener;
+                });
+            };
+        };
+
+        return Observable;
+    }
+);
