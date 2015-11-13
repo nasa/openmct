4f7b557a13be99d2b4f38c3fe9b7c0c4bee3a48e
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-11-13 09:15:28 -0800

    [Representation] Add mct-refresh
    ...to replace contents on mutation.

---
+++ platform/representation/src/MCTRefresh.js
@@ -0,0 +1,33 @@
+/*global define*/
+define(
+    [],
+    function () {
+        'use strict';
+
+        function MCTRefresh() {
+
+            function link(scope, elem, attrs, ctrl, transclude) {
+                var domainObject = scope.$eval(attrs.mctRefresh),
+                    mutation = domainObject.getCapability('mutation'),
+                    unlisten;
+
+                function recreateContents() {
+                    transclude(function (clone) {
+                        elem.empty();
+                        elem.append(clone);
+                    });
+                }
+
+                unlisten = mutation.listen(recreateContents);
+                scope.$on("$destroy", unlisten);
+            }
+
+            return {
+                transclude: true,
+                link: link
+            };
+        }
+
+        return MCTRefresh;
+    }
+);
