fc53dbd8a4cbbaa76a457564287760d2c17e8669
Charles Hacskaylo <charlesh88@gmail.com>
2015-11-20 15:23:08 -0800

    [Frontend] Themes added to Timelines
    open #208
    Espresso and Snow now supported; bulk of
    work done except for minor cleanups, like zoom
    buttons; Dragging not working currently in Timelines;

--- platform/features/timeline/res/compile-themes-sass.sh
+++
@@ -1,7 +0,0 @@
-echo "*** Compiling timeline-espresso"
-cd espresso/res
-compass compile --force
-
-# echo "*** Compiling timeline-snow"
-# cd espresso/snow
-# compass compile --force
---
+++ platform/features/timeline/res/sass-compile.sh
@@ -0,0 +1,4 @@
+date
+
+echo "*** Compiling sass"
+compass compile --force
