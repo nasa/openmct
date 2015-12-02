b450d36472c8b221b01c8b0e09fc0d851a54bbc5
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-12-02 13:23:46 -0800

    [Representation] Add JSDoc to OneWayBinder

--- platform/representation/src/OneWayBinder.js
+++ platform/representation/src/OneWayBinder.js
@@ -26,6 +26,18 @@ define(
     function () {
         'use strict';
 
+        /**
+         * Supports the "one-way" binding behavior of `mct-representation`
+         * and `mct-include`; watches expression associated with attributes
+         * in a parent scope, then passes these into the child scope when
+         * they change (but does not assign anything back to the parent
+         * scope if the child changes.)
+         * @constructor
+         * @memberof platform/representation
+         * @param scope the Angular scope to watch
+         * @param attrs the relevant attributes, as passed into the `link`
+         *              function of the relevant directive
+         */
         function OneWayBinder(scope, attrs) {
             var self = this;
 
@@ -42,10 +54,26 @@ define(
             });
         }
 
+        /**
+         * One-way bind an attribute. The value of the named attribute will
+         * be watched as an Angular expression in the parent scope; its
+         * value will be exposed in the child scope as a property of
+         * the same name.
+         * @param {string} attr the name of the attribute to watch
+         * @param {Function} [callback] a callback to invoke with new values
+         */
         OneWayBinder.prototype.bind = function (attr, callback) {
             this.alias(attr, attr, callback);
         };
 
+        /**
+         * One-way bind an attribute. As `bind`, but allows the property
+         * name in the child scope used to expose these values to be
+         * specified as something different from the attribute name.
+         * @param {string} attr the name of the attribute to watch
+         * @param {string} property the name of the property to use in scope
+         * @param {Function} [callback] a callback to invoke with new values
+         */
         OneWayBinder.prototype.alias = function (attr, property, callback) {
             var scope = this.scope;
 
@@ -61,6 +89,14 @@ define(
             scope[property] = this.parent.$eval(this.attrs[attr]);
         };
 
+        /**
+         * Watch for changes in this attribute. The named attribute's value
+         * will be watched as an Angular expression in the parent scope,
+         * and the provided callback will be invoked with the value of that
+         * expression when changes occur.
+         * @param {string} attr the name of the attribute to watch
+         * @param {Function} callback the callback to invoke with new values
+         */
         OneWayBinder.prototype.watch = function (attr, callback) {
             var expr = this.attrs[attr];
             this.unwatches.push(this.parent.$watch(
