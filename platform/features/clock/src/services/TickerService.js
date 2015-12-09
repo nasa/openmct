714ae3b9dcd3c253d3053bec8c7bf763f6a4a4b5
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-12-09 12:39:01 -0800

    [Clocks/Timers] Update remaining classes
    Complete code style changes for Clocks/Timers.

--- platform/features/clock/src/services/TickerService.js
+++ platform/features/clock/src/services/TickerService.js
@@ -30,23 +30,23 @@ define(
          * Calls functions every second, as close to the actual second
          * tick as is feasible.
          * @constructor
+         * @memberof platform/features/clock
          * @param $timeout Angular's $timeout
          * @param {Function} now function to provide the current time in ms
          */
         function TickerService($timeout, now) {
-            var callbacks = [],
-                last = now() - 1000;
+            var self = this;
 
             function tick() {
                 var timestamp = now(),
                     millis = timestamp % 1000;
 
                 // Only update callbacks if a second has actually passed.
-                if (timestamp >= last + 1000) {
-                    callbacks.forEach(function (callback) {
+                if (timestamp >= self.last + 1000) {
+                    self.callbacks.forEach(function (callback) {
                         callback(timestamp);
                     });
-                    last = timestamp - millis;
+                    self.last = timestamp - millis;
                 }
 
                 // Try to update at exactly the next second
@@ -55,34 +55,34 @@ define(
 
             tick();
 
-            return {
-                /**
-                 * Listen for clock ticks. The provided callback will
-                 * be invoked with the current timestamp (in milliseconds
-                 * since Jan 1 1970) at regular intervals, as near to the
-                 * second boundary as possible.
-                 *
-                 * @method listen
-                 * @name TickerService#listen
-                 * @param {Function} callback callback to invoke
-                 * @returns {Function} a function to unregister this listener
-                 */
-                listen: function (callback) {
-                    callbacks.push(callback);
-
-                    // Provide immediate feedback
-                    callback(last);
-
-                    // Provide a deregistration function
-                    return function () {
-                        callbacks = callbacks.filter(function (cb) {
-                            return cb !== callback;
-                        });
-                    };
-                }
-            };
-
+            this.callbacks = [];
+            this.last = now() - 1000;
         }
+
+        /**
+         * Listen for clock ticks. The provided callback will
+         * be invoked with the current timestamp (in milliseconds
+         * since Jan 1 1970) at regular intervals, as near to the
+         * second boundary as possible.
+         *
+         * @param {Function} callback callback to invoke
+         * @returns {Function} a function to unregister this listener
+         */
+        TickerService.prototype.listen = function (callback) {
+            var self = this;
+
+            self.callbacks.push(callback);
+
+            // Provide immediate feedback
+            callback(this.last);
+
+            // Provide a deregistration function
+            return function () {
+                self.callbacks = self.callbacks.filter(function (cb) {
+                    return cb !== callback;
+                });
+            };
+        };
 
         return TickerService;
     }
