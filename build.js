5b0af8773bb5f802d61e0d810b86fd730cc5aa4a
Victor Woeltjen <victor.woeltjen@nasa.gov>
2015-12-10 10:37:22 -0800

    [Optimization] First working requirejs build

---
+++ build.js
@@ -0,0 +1,35 @@
+/*global require*/
+var requirejs = require("requirejs"),
+    globby = require("globby"),
+    fs = require("fs"),
+    bundles = fs.readFileSync("bundles.json"),
+    files = [];
+
+function trimJsExtension(filename) {
+    return filename.replace(".js", "");
+}
+
+JSON.parse(bundles).forEach(function (bundle) {
+    files = files.concat(globby.sync(bundle + "/src/**/*.js"));
+});
+
+
+requirejs.optimize({
+    baseUrl: ".",
+    name: "platform/framework/src/Main",
+    out: "main.built.js",
+    paths: {
+        'es6-promise': 'platform/framework/lib/es6-promise-2.0.0.min',
+        'moment': 'platform/telemetry/lib/moment.min',
+        'moment-duration-format': 'platform/features/clock/lib/moment-duration-format',
+        'uuid': 'platform/core/lib/uuid'
+    },
+    shim: {
+        'moment-duration-format': {
+            deps: [ 'moment' ]
+        }
+    },
+    include: files.map(trimJsExtension),
+    exclude: globby.sync("platform/framework/lib/**/*.js").map(trimJsExtension),
+    optimize: "none"
+});
