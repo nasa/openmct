918c667496df3bcbf69d78d93d419a5a2ffb3fac
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-01-27 15:00:59 -0800

    [Build] Update code coverage report path

--- karma.conf.js
+++ karma.conf.js
@@ -76,7 +76,7 @@ module.exports = function(config) {
 
         // Code coverage reporting.
         coverageReporter: {
-            dir: "target/coverage"
+            dir: "dist/coverage"
         },
 
         // HTML test reporting.
