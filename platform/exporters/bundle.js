92e49d6b76f7b333710814ba9ad5ad07bd564bbb
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-02-05 16:10:30 -0800

    [CSV Export] Add licensing info for run-time
    Add licensing info for CSV.js and FileSaver.js as licenses
    which can be displayed at run-time.

--- platform/exporters/bundle.js
+++ platform/exporters/bundle.js
@@ -37,6 +37,28 @@ define([
                         return new ExportService(saveAs);
                     }
                 }
+            ],
+            licenses: [
+                {
+                    "name": "CSV.js",
+                    "version": "3.6.4",
+                    "author": "Kash Nouroozi",
+                    "description": "Encoder for CSV (comma separated values) export",
+                    "website": "https://github.com/knrz/CSV.js",
+                    "copyright": "Copyright (c) 2014 Kash Nouroozi",
+                    "license": "license-mit",
+                    "link": "https://github.com/knrz/CSV.js/blob/3.6.4/LICENSE"
+                },
+                {
+                    "name": "FileSaver.js",
+                    "version": "0.0.2",
+                    "author": "Eli Grey",
+                    "description": "File download initiator (for file exports)",
+                    "website": "https://github.com/eligrey/FileSaver.js/",
+                    "copyright": "Copyright © 2015 Eli Grey.",
+                    "license": "license-mit",
+                    "link": "https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md"
+                }
             ]
         }
     });
