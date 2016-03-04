4f85616632bd03937547cce9cfcedde5399319d1
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-03-04 11:46:15 -0800

    [Build] Fix FullscreenAction spec
    ...to reflect acquisition of screenfull dependency via
    AMD

--- platform/commonUI/browse/test/windowing/FullscreenActionSpec.js
+++ platform/commonUI/browse/test/windowing/FullscreenActionSpec.js
@@ -24,26 +24,25 @@
  * MCTRepresentationSpec. Created by vwoeltje on 11/6/14.
  */
 define(
-    ["../../src/windowing/FullscreenAction"],
-    function (FullscreenAction) {
+    ["../../src/windowing/FullscreenAction", "screenfull"],
+    function (FullscreenAction, screenfull) {
 
         describe("The fullscreen action", function () {
             var action,
-                oldScreenfull;
+                oldToggle;
 
             beforeEach(function () {
                 // Screenfull is not shimmed or injected, so
                 // we need to spy on it in the global scope.
-                oldScreenfull = window.screenfull;
+                oldToggle = screenfull.toggle;
 
-                window.screenfull = {};
-                window.screenfull.toggle = jasmine.createSpy("toggle");
+                screenfull.toggle = jasmine.createSpy("toggle");
 
                 action = new FullscreenAction({});
             });
 
             afterEach(function () {
-                window.screenfull = oldScreenfull;
+                screenfull.toggle = oldToggle;
             });
 
             it("toggles fullscreen mode when performed", function () {
