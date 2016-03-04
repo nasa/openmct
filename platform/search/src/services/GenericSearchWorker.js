6aeb156a5864d81cbafb81a83e9d5aa5b69704de
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-03-04 11:09:41 -0800

    [Build] Declare global self in worker

--- platform/search/src/services/GenericSearchWorker.js
+++ platform/search/src/services/GenericSearchWorker.js
@@ -20,6 +20,8 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
+/*global self*/
+
 /**
  * Module defining GenericSearchWorker. Created by shale on 07/21/2015.
  */
