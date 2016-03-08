40895ec1b97bad45600527fb6f05c024f7b8b501
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-03-08 10:07:21 -0800

    [Timelines] Begin spec for exporter

---
+++ platform/features/timeline/test/actions/TimelineCSVExporterSpec.js
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Open MCT Web, Copyright (c) 2009-2015, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT Web is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT Web includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+/*global define,describe,it,expect,beforeEach,waitsFor,jasmine,window,afterEach*/
+
+define(
+    ['../../src/actions/TimelineCSVExporter'],
+    function (TimelineCSVExporter) {
+        describe("TimelineCSVExporter", function () {
+            var mockDomainObjects,
+                exporter;
+
+            beforeEach(function () {
+                mockDomainObjects = [];
+                exporter = new TimelineCSVExporter(mockDomainObjects);
+            });
+
+            it("includes one row per domain object", function () {
+                var mockCallback = jasmine.createSpy('callback');
+                exporter.rows().then(mockCallback);
+                waitsFor(function () {
+                    return mockCallback.calls.length > 0;
+                });
+                runs(function () {
+                    expect(mockCallback.mostRecentCall.args[0].length)
+                        .toEqual(mockDomainObjects.length);
+                });
+            });
+
+
+        });
+    }
+);
