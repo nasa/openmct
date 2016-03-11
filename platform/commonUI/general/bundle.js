4f293f22a63b52fc34217a481028fc2dbb9eaa90
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-03-11 12:38:40 -0800

    [Tree] Add Zepto dependency
    ...to support implementation of a jQuery-less tree.

--- platform/commonUI/general/bundle.js
+++ platform/commonUI/general/bundle.js
@@ -535,6 +535,16 @@ define([
                     "copyright": "Copyright (c) Nicolas Gallagher and Jonathan Neal",
                     "license": "license-mit",
                     "link": "https://github.com/necolas/normalize.css/blob/v1.1.2/LICENSE.md"
+                },
+                {
+                    "name": "Zepto",
+                    "version": "1.1.6",
+                    "description": "DOM manipulation",
+                    "author": "Thomas Fuchs",
+                    "website": "http://zeptojs.com/",
+                    "copyright": "Copyright (c) 2010-2016 Thomas Fuchs",
+                    "license": "license-mit",
+                    "link": "https://github.com/madrobby/zepto/blob/master/MIT-LICENSE"
                 }
             ]
         }
