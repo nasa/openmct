934eb13813254294d75fc8052ece74be96beea8d
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-04-08 16:54:01 -0700

    [Build] Remove obsolete jslint tags
    ES5 mode is enabled by default in JSHint, tags no longer necessary per
    https://github.com/nasa/openmct/pull/724#discussion_r55095722

--- platform/core/src/capabilities/PersistenceCapability.js
+++ platform/core/src/capabilities/PersistenceCapability.js
@@ -19,8 +19,6 @@
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
-/*jslint es5: true */
-
 
 define(
     function () {
--- platform/core/test/capabilities/PersistenceCapabilitySpec.js
+++ platform/core/test/capabilities/PersistenceCapabilitySpec.js
@@ -19,7 +19,6 @@
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
-/*jslint es5: true */
 
 /**
  * PersistenceCapabilitySpec. Created by vwoeltje on 11/6/14.
