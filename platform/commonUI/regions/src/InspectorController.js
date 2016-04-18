4b786d3536e7f4223fd791ab9b9652b791b4c9cb
Henry <akhenry@gmail.com>
2016-04-18 16:42:22 -0700

    Removed sysouts

--- platform/commonUI/regions/src/InspectorController.js
+++ platform/commonUI/regions/src/InspectorController.js
@@ -32,8 +32,7 @@ define(
          */
         function InspectorController($scope, policyService) {
             var domainObject = $scope.domainObject,
-                typeCapability = domainObject.getCapability('type'),
-                listener;
+                typeCapability = domainObject.getCapability('type');
 
             /**
              * Filters region parts to only those allowed by region policies
@@ -51,16 +50,7 @@ define(
                 $scope.regions = filterRegions(typeCapability.getDefinition().inspector || new InspectorRegion());
             }
 
-            //Listen for changes to object status that might necessitate
-            // recalculation of screen regions.
-            //listener =
-            // domainObject.getCapability("status").listen(setRegions);
-
             setRegions();
-
-            $scope.$on("$destroy", function() {
-                listener();
-            })
         }
 
         return InspectorController;
