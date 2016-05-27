b73b824e55560a98ccc1e814284286a3bddeccc9
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-05-27 11:43:35 -0700

    [API] Add EventEmitter dep

--- test-main.js
+++ test-main.js
@@ -48,6 +48,7 @@ requirejs.config({
         "angular-route": "bower_components/angular-route/angular-route.min",
         "csv": "bower_components/comma-separated-values/csv.min",
         "es6-promise": "bower_components/es6-promise/promise.min",
+        "EventEmitter": "bower_components/eventemitter3/index",
         "moment": "bower_components/moment/moment",
         "moment-duration-format": "bower_components/moment-duration-format/lib/moment-duration-format",
         "saveAs": "bower_components/FileSaver.js/FileSaver.min",
@@ -63,6 +64,9 @@ requirejs.config({
         },
         "angular-route": {
             "deps": [ "angular" ]
+        },
+        "EventEmitter": {
+            "exports": "EventEmitter"
         },
         "moment-duration-format": {
             "deps": [ "moment" ]
