02f34d1c04ff20028cd8f6143bac4c82f4451dc1
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-08-08 12:28:13 -0700

    [Persistence] Begin testing TransactingMutationListener

---
+++ platform/core/test/runs/TransactingMutationListenerSpec.js
@@ -0,0 +1,63 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2016, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+define(
+    ["../../src/runs/TransactingMutationListener"],
+    function (TransactingMutationListener) {
+
+        describe("TransactingMutationListener", function () {
+            var mockTopic,
+                mockMutationTopic,
+                mockTransactionService;
+
+            beforeEach(function () {
+                mockTopic = jasmine.createSpy('topic');
+                mockMutationTopic =
+                    jasmine.createSpyObj('mutation', [ 'listen' ]);
+                mockTransactionService =
+                    jasmine.createSpyObj('transactionService', [
+                        'isActive',
+                        'startTransaction',
+                        'addToTransaction',
+                        'commit'
+                    ]);
+
+                mockTopic.andCallFake(function (t) {
+                    return (t === 'mutation') && mockMutationTopic;
+                });
+
+
+                return new TransactingMutationListener(
+                    mockTopic,
+                    mockTransactionService
+                );
+            });
+
+            it("listens for mutation", function () {
+                expect(mockMutationTopic.listen)
+                    .toHaveBeenCalledWith(jasmine.any(Function));
+            });
+
+
+        });
+    }
+);
