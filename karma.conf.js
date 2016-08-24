dc414321ffd1bb63fa6c320bd2b1705a728ac19f
Anil Pradyumna Chakicherla <pradyumna.chakicherla@isteer.com>
2016-08-24 18:33:29 +0530

    adds Chrome to  karma browsers

--- karma.conf.js
+++ karma.conf.js
@@ -74,6 +74,7 @@ module.exports = function(config) {
         // Specify browsers to run tests in.
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: [
+            'PhantomJS',
             'Chrome'
         ],
 
