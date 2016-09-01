bcc32c76d06316d6ce718f5015ba365c85e8efd7
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-09-01 14:42:55 -0700

    [API] Module-level JSDoc

--- src/openmct.js
+++ src/openmct.js
@@ -1,5 +1,10 @@
 define(['./MCT'], function (MCT) {
     /**
+     * Open MCT is an extensible web application for building mission
+     * control user interfaces. This module is itself an instance of
+     * [MCT]{@link module:openmct.MCT}, which provides an interface for
+     * configuring and executing the application.
+     *
      * @exports openmct
      */
     var openmct = new MCT();
