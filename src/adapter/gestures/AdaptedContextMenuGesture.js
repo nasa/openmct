43e35179a3cbfc2a1d9add40f8cffd42ba38b7fd
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-09-30 12:49:08 -0700

    [API] Continue decrufting

--- src/adapter/gestures/AdaptedContextMenuGesture.js
+++
@@ -1,9 +0,0 @@
-define(
-    function () {
-        function AdaptedContextMenuGesture(openmct, $els, domainObject) {
-            this.destroy = openmct.gestures.contextual($els[0], domainObject);
-        }
-
-        return AdaptedContextMenuGesture;
-    }
-);
