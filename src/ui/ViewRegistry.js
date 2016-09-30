3f20c1bb945a4162b1367ab1d550c40110806566
Victor Woeltjen <victor.woeltjen@nasa.gov>
2016-09-29 17:34:41 -0700

    [API] Update ViewRegistry to match docs

--- src/ui/ViewRegistry.js
+++ src/ui/ViewRegistry.js
@@ -1,4 +1,10 @@
 define([], function () {
+    /**
+     * A ViewRegistry maintains the definitions for different kinds of views
+     * that may occur in different places in the user interface.
+     * @interface ViewRegistry
+     * @memberof module:openmct
+     */
     function ViewRegistry() {
         this.providers = [];
     }
@@ -9,6 +15,13 @@ define([], function () {
         });
     };
 
+    /**
+     * Register a new type of view.
+     *
+     * @param {module:openmct.ViewProvider} provider the provider for this view
+     * @method addProvider
+     * @memberof module:openmct.ViewRegistry#
+     */
     ViewRegistry.prototype.addProvider = function (provider) {
         this.providers.push(provider);
     };
