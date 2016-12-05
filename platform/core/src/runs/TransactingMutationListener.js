86b337ec88eeedb067ac823f7839bca5d0494da0
Henry <akhenry@gmail.com>
2016-12-05 15:49:50 -0800

    Revert "[Edit Mode] Fix issue with navigation not triggering unsaved changes message for new objects. Removed logic that prevented mutation of new objects from triggering a transactional persist call. Fixes #1278"
    This reverts commit 7dda85cc5f2783c2665ee2c65ce8bdc6a8adccce.

--- platform/core/src/runs/TransactingMutationListener.js
+++ platform/core/src/runs/TransactingMutationListener.js
@@ -40,17 +40,19 @@ define([], function () {
             var wasActive = transactionService.isActive();
             cacheService.put(domainObject.getId(), domainObject.getModel());
 
-            if (!wasActive) {
-                transactionService.startTransaction();
-            }
+            if (persistence.persisted()) {
+                if (!wasActive) {
+                    transactionService.startTransaction();
+                }
 
-            transactionService.addToTransaction(
-                persistence.persist.bind(persistence),
-                persistence.refresh.bind(persistence)
-            );
+                transactionService.addToTransaction(
+                    persistence.persist.bind(persistence),
+                    persistence.refresh.bind(persistence)
+                );
 
-            if (!wasActive) {
-                transactionService.commit();
+                if (!wasActive) {
+                    transactionService.commit();
+                }
             }
         });
     }
