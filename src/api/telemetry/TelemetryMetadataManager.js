fc103e9b3088546cb531939facac1869dd5c9efd
Pete Richards <peter.l.richards@nasa.gov>
2017-03-31 16:03:35 -0700

    Fix Enum Formatting

--- src/api/telemetry/TelemetryMetadataManager.js
+++ src/api/telemetry/TelemetryMetadataManager.js
@@ -100,6 +100,18 @@ define([
             delete valueMetadata.hints.y;
         }
 
+        if (valueMetadata.format == 'enum') {
+            if (!valueMetadata.values) {
+                valueMetadata.values = _.pluck(valueMetadata.enumerations, 'value');
+            }
+            if (!valueMetadata.hasOwnProperty('max')) {
+                valueMetadata.max = _.max(valueMetadata.values) + 1;
+            }
+            if (!valueMetadata.hasOwnProperty('min')) {
+                valueMetadata.min = _.min(valueMetadata.values) - 1;
+            }
+        }
+
         if (!valueMetadata.hints.hasOwnProperty('priority')) {
             valueMetadata.hints.priority = index;
         }
