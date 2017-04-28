69b06364eb81034bb420c22a950030d5586c1d4a
Henry <akhenry@gmail.com>
2017-04-27 19:02:35 -0700

    [Example] Add units to MSL example. Recreates functionality from the old live demo

--- example/msl/src/MSLDataDictionary.js
+++ example/msl/src/MSLDataDictionary.js
@@ -1,9 +1,9 @@
 /*****************************************************************************
- * Open MCT, Copyright (c) 2014-2017, United States Government
+ * Open MCT Web, Copyright (c) 2014-2015, United States Government
  * as represented by the Administrator of the National Aeronautics and Space
  * Administration. All rights reserved.
  *
- * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * Open MCT Web is licensed under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0.
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  *
- * Open MCT includes source code licensed under additional open source
+ * Open MCT Web includes source code licensed under additional open source
  * licenses. See the Open Source Licenses file (LICENSES.md) included with
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
@@ -44,31 +44,31 @@ define(
                         {
                             "name": "Min. Air Temperature",
                             "identifier": "min_temp",
-                            "units": "degrees",
+                            "units": "Degrees (C)",
                             "type": "float"
                         },
                         {
                             "name": "Max. Air Temperature",
                             "identifier": "max_temp",
-                            "units": "degrees",
+                            "units": "Degrees (C)",
                             "type": "float"
                         },
                         {
                             "name": "Atmospheric Pressure",
                             "identifier": "pressure",
-                            "units": "pascals",
+                            "units": "Millibars",
                             "type": "float"
                         },
                         {
                             "name": "Min. Ground Temperature",
                             "identifier": "min_gts_temp",
-                            "units": "degrees",
+                            "units": "Degrees (C)",
                             "type": "float"
                         },
                         {
                             "name": "Max. Ground Temperature",
                             "identifier": "max_gts_temp",
-                            "units": "degrees",
+                            "units": "Degrees (C)",
                             "type": "float"
                         }
                     ]
