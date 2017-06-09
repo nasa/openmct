bc7b1923ac5ae8ab62ae5a0c63109eceb0aa5e15
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-09 14:44:10 -0300

    Update LegacyTelemetryProvider.js

--- src/api/telemetry/LegacyTelemetryProvider.js
+++ src/api/telemetry/LegacyTelemetryProvider.js
@@ -22,45 +22,113 @@
 
 define([
     '../objects/object-utils'
-], function (
-    utils
-) {
+], utils => {
     /**
      * @implements module:openmct.TelemetryAPI~TelemetryProvider
      * @constructor
      */
-    function LegacyTelemetryProvider(openmct, instantiate) {
-        this.telemetryApi = openmct.telemetry;
-        this.instantiate = instantiate;
-    }
+    class LegacyTelemetryProvider {
+        constructor(openmct, instantiate) {
+            this.telemetryApi = openmct.telemetry;
+            this.instantiate = instantiate;
+        }
 
-    /**
-     * Can provide telemetry for all objects that have the "telemetry"
-     * capability
-     *
-     * @see module:openmct.TelemetryAPI~TelemetryProvider#canProvideTelemetry
-     */
-    LegacyTelemetryProvider.prototype.canProvideTelemetry = function (domainObject) {
-        return this.instantiate(utils.toOldFormat(domainObject),
-            utils.makeKeyString(domainObject.identifier)).hasCapability("telemetry");
-    };
+        /**
+         * Can provide telemetry for all objects that have the "telemetry"
+         * capability
+         *
+         * @see module:openmct.TelemetryAPI~TelemetryProvider#canProvideTelemetry
+         */
+        canProvideTelemetry(domainObject) {
+            return this.instantiate(utils.toOldFormat(domainObject),
+                utils.makeKeyString(domainObject.identifier)).hasCapability("telemetry");
+        }
+
+        /**
+         * @typedef {object} ConvertedTelemetryObject
+         * Telemetry data objects are converted from TelemetrySeries. Metadata is used
+         * to populate the returned object with attributes corresponding to the keys
+         * of domains and ranges. The attribute values are those returned by calls to
+         * [TelemetrySeries.getDomainValue()]{@link TelemetrySeries#getDomainValue}
+         * and [TelemetrySeries.getRangeValue()]{@link TelemetrySeries#getRangeValue}.
+         */
+
+        /**
+         * @see module:openmct.TelemetryAPI~TelemetryProvider#request
+         * @param {module:openmct.DomainObject}
+         * @param {module:openmct.TelemetryAPI~TelemetryRequest} options
+         *        options for this request. Passed straight through to legacy provider
+         * @returns {Promise.<ConvertedTelemetryObject[]>} a promise for an array of
+         *          telemetry data.
+         */
+        request(domainObject, request) {
+            const metadata = this.telemetryApi.getMetadata(domainObject);
+            const oldObject = this.instantiate(utils.toOldFormat(domainObject), utils.makeKeyString(domainObject.identifier));
+            const capability = oldObject.getCapability("telemetry");
+
+            return capability.requestData(request).then(telemetrySeries => Promise.resolve(adaptSeries(domainObject, metadata, telemetrySeries))).catch(error => Promise.reject(error));
+        }
+
+        /**
+         * @callback LegacyTelemetryProvider~SubscribeCallback
+         * @param {ConvertedTelemetryObject}
+         */
+
+        /**
+         * @see module:openmct.TelemetryAPI~TelemetryProvider#request
+         * @param {module:openmct.DomainObject}
+         * @param {LegacyTelemetryProvider~SubscribeCallback} callback will be called with a single datum when
+         * new data is available.
+         * @param {module:openmct.TelemetryAPI~TelemetryRequest} options
+         *        options for this request. Passed straight through to legacy provider
+         * @returns {platform|telemetry.TelemetrySubscription|*}
+         */
+        subscribe(domainObject, callback, request) {
+            const metadata = this.telemetryApi.getMetadata(domainObject);
+            const oldObject = this.instantiate(utils.toOldFormat(domainObject), utils.makeKeyString(domainObject.identifier));
+            const capability = oldObject.getCapability("telemetry");
+
+            function callbackWrapper(series) {
+                callback(createDatum(domainObject, metadata, series, series.getPointCount() - 1));
+            }
+
+            return capability.subscribe(callbackWrapper, request);
+        }
+
+        limitEvaluator(domainObject) {
+            const oldObject = this.instantiate(
+                utils.toOldFormat(domainObject),
+                utils.makeKeyString(domainObject.identifier));
+            const limitEvaluator = oldObject.getCapability("limit");
+
+            if (!limitEvaluator) {
+                return;
+            }
+
+            return {
+                evaluate(datum, property) {
+                    return limitEvaluator.evaluate(datum, property.key);
+                }
+            };
+        }
+    }
 
     LegacyTelemetryProvider.prototype.supportsRequest =
         LegacyTelemetryProvider.prototype.supportsSubscribe =
         LegacyTelemetryProvider.prototype.canProvideTelemetry;
 
     function createDatum(domainObject, metadata, legacySeries, i) {
-        var datum;
+        let datum;
 
         if (legacySeries.getDatum) {
             datum = legacySeries.getDatum(i);
         } else {
             datum = {};
-            metadata.valuesForHints(['domain']).forEach(function (metadatum) {
+            metadata.valuesForHints(['domain']).forEach(metadatum => {
                 datum[metadatum.key] = legacySeries.getDomainValue(i, metadatum.key);
             });
 
-            metadata.valuesForHints(['range']).forEach(function (metadatum) {
+            metadata.valuesForHints(['range']).forEach(metadatum => {
                 datum[metadatum.key] = legacySeries.getRangeValue(i, metadatum.key);
             });
         }
@@ -77,93 +145,20 @@ define([
     }
 
     function adaptSeries(domainObject, metadata, legacySeries) {
-        var series = [];
+        const series = [];
 
-        for (var i = 0; i < legacySeries.getPointCount(); i++) {
+        for (let i = 0; i < legacySeries.getPointCount(); i++) {
             series.push(createDatum(domainObject, metadata, legacySeries, i));
         }
 
         return series;
     }
 
-    /**
-     * @typedef {object} ConvertedTelemetryObject
-     * Telemetry data objects are converted from TelemetrySeries. Metadata is used
-     * to populate the returned object with attributes corresponding to the keys
-     * of domains and ranges. The attribute values are those returned by calls to
-     * [TelemetrySeries.getDomainValue()]{@link TelemetrySeries#getDomainValue}
-     * and [TelemetrySeries.getRangeValue()]{@link TelemetrySeries#getRangeValue}.
-     */
-
-    /**
-     * @see module:openmct.TelemetryAPI~TelemetryProvider#request
-     * @param {module:openmct.DomainObject}
-     * @param {module:openmct.TelemetryAPI~TelemetryRequest} options
-     *        options for this request. Passed straight through to legacy provider
-     * @returns {Promise.<ConvertedTelemetryObject[]>} a promise for an array of
-     *          telemetry data.
-     */
-    LegacyTelemetryProvider.prototype.request = function (domainObject, request) {
-        var metadata = this.telemetryApi.getMetadata(domainObject);
-        var oldObject = this.instantiate(utils.toOldFormat(domainObject), utils.makeKeyString(domainObject.identifier));
-        var capability = oldObject.getCapability("telemetry");
-
-        return capability.requestData(request).then(function (telemetrySeries) {
-            return Promise.resolve(adaptSeries(domainObject, metadata, telemetrySeries));
-        }).catch(function (error) {
-            return Promise.reject(error);
-        });
-    };
-
-    /**
-     * @callback LegacyTelemetryProvider~SubscribeCallback
-     * @param {ConvertedTelemetryObject}
-     */
-
-    /**
-     * @see module:openmct.TelemetryAPI~TelemetryProvider#request
-     * @param {module:openmct.DomainObject}
-     * @param {LegacyTelemetryProvider~SubscribeCallback} callback will be called with a single datum when
-     * new data is available.
-     * @param {module:openmct.TelemetryAPI~TelemetryRequest} options
-     *        options for this request. Passed straight through to legacy provider
-     * @returns {platform|telemetry.TelemetrySubscription|*}
-     */
-    LegacyTelemetryProvider.prototype.subscribe = function (domainObject, callback, request) {
-        var metadata = this.telemetryApi.getMetadata(domainObject);
-        var oldObject = this.instantiate(utils.toOldFormat(domainObject), utils.makeKeyString(domainObject.identifier));
-        var capability = oldObject.getCapability("telemetry");
-
-        function callbackWrapper(series) {
-            callback(createDatum(domainObject, metadata, series, series.getPointCount() - 1));
-        }
-
-        return capability.subscribe(callbackWrapper, request);
-    };
-
-    LegacyTelemetryProvider.prototype.limitEvaluator = function (domainObject) {
-        var oldObject = this.instantiate(
-            utils.toOldFormat(domainObject),
-            utils.makeKeyString(domainObject.identifier));
-        var limitEvaluator = oldObject.getCapability("limit");
-
-        if (!limitEvaluator) {
-            return;
-        }
-
-        return {
-            evaluate: function (datum, property) {
-                return limitEvaluator.evaluate(datum, property.key);
-            }
-        };
-    };
-
-    return function (openmct, instantiate) {
+    return (openmct, instantiate) => {
         // Legacy provider should always be the fallback.
-        var provider = new LegacyTelemetryProvider(openmct, instantiate);
+        const provider = new LegacyTelemetryProvider(openmct, instantiate);
         openmct.telemetry.legacyProvider = provider;
         openmct.telemetry.requestProviders.push(provider);
         openmct.telemetry.subscriptionProviders.push(provider);
     };
-
 });
