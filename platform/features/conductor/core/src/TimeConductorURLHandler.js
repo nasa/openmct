555ec72c58467c4533118adfa0d5190715c6dc4e
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-06-13 15:50:48 -0700

    [Time Conductor] Skeleton class for URL handling
    #1550

---
+++ platform/features/conductor/core/src/TimeConductorURLHandler.js
@@ -0,0 +1,15 @@
+define([], function () {
+    function TimeConductorURLHandler(tcViewService) {
+        this.tcViewService = tcViewService;
+    }
+
+    TimeConductorURLHandler.prototype.updateParams = function () {
+
+    };
+
+    TimeConductorURLHandler.prototype.updateView = function (searchParams) {
+
+    };
+
+    return TimeConductorURLHandler;
+});
