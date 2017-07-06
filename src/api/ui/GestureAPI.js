b5f170368a61cc19d7e0d49a91adce0347aa567d
Luis Schubert <schubert.luis@gmail.com>
2017-07-06 15:47:26 -0700

    [API] Gesture API initial implementation

--- src/api/ui/GestureAPI.js
+++ src/api/ui/GestureAPI.js
@@ -20,49 +20,104 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-define([], function () {
-    /**
-     * Allows support for common user actions to be attached to views.
-     * @interface GestureAPI
-     * @memberof module:openmct
-     */
-    function GestureAPI(selectGesture, contextMenuGesture) {
-        this.selectGesture = selectGesture;
-        this.contextMenuGesture = contextMenuGesture;
-    }
+ define([
+     'zepto',
+     '../../../platform/core/src/capabilities/ContextualDomainObject.js'
+ ], function (
+     $,
+     ContextualDomainObject
+ ) {
+     /**
+      * Allows support for common user actions to be attached to views.
+      * @interface GestureAPI
+      * @memberof module:openmct
+      */
+     function GestureAPI(openmct, objectUtils) {
+         this.openmct = openmct;
+         this.objectUtils = objectUtils;
+     }
 
-    /**
-     * Designate an HTML element as selectable, and associated with a
-     * particular object.
-     *
-     * @param {HTMLElement} htmlElement the element to make selectable
-     * @param {*} item the object which should become selected when this
-     *        element is clicked.
-     * @returns {Function} a function to remove selectability from this
-     *          HTML element.
-     * @method selectable
-     * @memberof module:openmct.GestureAPI#
-     */
-    GestureAPI.prototype.selectable = function (htmlElement, item) {
-        return this.selectGesture.apply(htmlElement, item);
-    };
+     /**
+      * Designate an HTML element as selectable, and associated with a
+      * particular object.
+      *
+      * @param {HTMLElement} htmlElement the element to make selectable
+      * @param {*} item the object which should become selected when this
+      *        element is clicked.
+      * @returns {Function} a function to remove selectability from this
+      *          HTML element.
+      * @method selectable
+      * @memberof module:openmct.GestureAPI#
+      */
+     GestureAPI.prototype.selectable = function (htmlElement, item) {
+         // TODO: implement selectable
+     };
 
 
-    /**
-     * Designate an HTML element as having a context menu associated with
-     * the provided item.
-     *
-     * @private
-     * @param {HTMLElement} htmlElement the element to make selectable
-     * @param {*} item the object for which a context menu should appear
-     * @returns {Function} a function to remove this geture from this
-     *          HTML element.
-     * @method selectable
-     * @memberof module:openmct.GestureAPI#
-     */
-    GestureAPI.prototype.contextMenu = function (htmlElement, item) {
-        return this.contextMenuGesture.apply(htmlElement, item);
-    };
+     /**
+      * Designate an HTML element as having a context menu associated with
+      * the provided item.
+      *
+      //TODO: should this really be private?
+      * @private
+      * @param {HTMLElement} htmlElement the element containing the context menu
+      * @param {*} childObject the object for which a context menu should appear
+      * @param {*} parentObject the object which has the context required for
+      *        showing the context menu
+      * @returns {Function} a function to remove this gesture from this
+      *          HTML element.
+      * @method contextMenu
+      * @memberof module:openmct.GestureAPI#
+      */
+     GestureAPI.prototype.contextMenu = function (htmlElement, childObject, parentObject) {
+         var gestureService = this.openmct.$injector.get('gestureService');
+         var instantiate = this.openmct.$injector.get('instantiate');
 
-    return GestureAPI;
-});
+         var childKeystring = this.objectUtils.makeKeyString(childObject.identifier);
+         var childOldformat = this.objectUtils.toOldFormat(childObject);
+         var childOldObject = instantiate(childOldformat, childKeystring);
+
+         var parentKeystring = this.objectUtils.makeKeyString(parentObject.identifier);
+         var parentOldformat = this.objectUtils.toOldFormat(parentObject);
+         var parentOldObject = instantiate(parentOldformat, parentKeystring);
+
+         var contextObject = new ContextualDomainObject(childOldObject, parentOldObject);
+
+
+         return gestureService.attachGestures($(htmlElement), contextObject, ['menu']);
+     };
+
+     /**
+      * Designate an HTML element as having a info popover associated with
+      * the provided item.
+      *
+      //
+      * @private
+      * @param {HTMLElement} htmlElement the element to make selectable
+      * @param {*} childObject the object for which a info popover should appear
+      * @param {*} parentObject the object which has the context required for
+      *        showing the info popover
+      * @returns {Function} a function to remove this gesture from this
+      *          HTML element.
+      * @method info
+      * @memberof module:openmct.GestureAPI#
+      */
+     GestureAPI.prototype.info = function (htmlElement, childObject, parentObject) {
+         var gestureService = this.openmct.$injector.get('gestureService');
+         var instantiate = this.openmct.$injector.get('instantiate');
+
+         var childKeystring = this.objectUtils.makeKeyString(childObject.identifier);
+         var childOldformat = this.objectUtils.toOldFormat(childObject);
+         var childOldObject = instantiate(childOldformat, childKeystring);
+
+         var parentKeystring = this.objectUtils.makeKeyString(parentObject.identifier);
+         var parentOldformat = this.objectUtils.toOldFormat(parentObject);
+         var parentOldObject = instantiate(parentOldformat, parentKeystring);
+
+         var contextObject = new ContextualDomainObject(childOldObject, parentOldObject);
+
+         return gestureService.attachGestures($(htmlElement), contextObject, ['info']);
+     }
+
+     return GestureAPI;
+ });
