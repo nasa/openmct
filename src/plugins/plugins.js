13bafd4131ab7cd60704fcc305d32e440d9fd411
Pete Richards <peter.l.richards@nasa.gov>
2017-07-12 17:28:11 -0700

    [LADTable] es6 etch LAD table
    This is experimental usage of es6, etch, and some new packaging tools
    in order to implement a simple LAD table.

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -26,14 +26,16 @@ define([
     '../../example/generator/plugin',
     '../../platform/features/autoflow/plugin',
     './timeConductor/plugin',
-    '../../example/imagery/plugin'
+    '../../example/imagery/plugin',
+    'es6!./ladTable/plugin'
 ], function (
     _,
     UTCTimeSystem,
     GeneratorPlugin,
     AutoflowPlugin,
     TimeConductorPlugin,
-    ExampleImagery
+    ExampleImagery,
+    LADTable,
 ) {
     var bundleMap = {
         CouchDB: 'platform/persistence/couch',
@@ -116,6 +118,8 @@ define([
     };
 
     plugins.ExampleImagery = ExampleImagery;
+
+    plugins.LADTable = LADTable;
 
     return plugins;
 });
