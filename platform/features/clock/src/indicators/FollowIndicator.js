2ad47139c40a2eb1cbb2269b6ae7eb2403dc597d
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-09-20 12:11:41 -0700

    [Timers] Frontend updates for time-of-interest
    Squashed commit of the following:
    
    commit 370b910d361303d41d8bfbe6a40dc15ce54d9d1b
    Author: Charles Hacskaylo <charlesh88@gmail.com>
    Date:   Wed Sep 20 10:59:00 2017 -0700
    
        [Frontend] Fix in FollowIndicator.js
    
        Fixes #1688
    
    commit 883d1feb32dbc86f3fa4f7724f4ced9670e7b8ad
    Author: Charles Hacskaylo <charlesh88@gmail.com>
    Date:   Wed Sep 20 10:36:56 2017 -0700
    
        [Frontend] Styling and content on Follow indicator
    
        Fixes #1688
    
    commit cff85fbbdedbae51ae8eb5ca21c06065128c3e4a
    Author: Charles Hacskaylo <charlesh88@gmail.com>
    Date:   Wed Sep 20 10:09:19 2017 -0700
    
        [Frontend] Styling complete on Follow Line
    
        Fixes #1688
    
    commit 563a86b69f8e42896a3e1356120aefaf6e322652
    Author: Charles Hacskaylo <charles.f.hacskaylo@nasa.gov>
    Date:   Mon Sep 18 16:05:53 2017 -0700
    
        [Front-end] WIP Markup and CSS for Follow Line
    
        Fixes #1688
        Added line icon, style refinement;
    
    commit fc49e5d02303118b6e498a99d2d75ef1d9398da2
    Author: Charles Hacskaylo <charles.f.hacskaylo@nasa.gov>
    Date:   Mon Sep 18 15:07:35 2017 -0700
    
        [Front-end] WIP Markup and CSS for Follow Line
    
        Fixes #1688
        Moved TimelineTOIController up 2 levels of markup hierarchy
        to allow Follow Lines, one in each split pane;
        Follow LInes markup and CSS in progress;
    
    commit 8ec3c422910915e9da66d2ee3b6e49140a4adb3f
    Author: Charles Hacskaylo <charlesh88@gmail.com>
    Date:   Wed Sep 13 16:46:14 2017 -0700
    
        [Frontend] WIP Timeline Follow Line
    
        Fixes #1688
        VERY WIP! Initial move of styles into classes;

--- platform/features/clock/src/indicators/FollowIndicator.js
+++ platform/features/clock/src/indicators/FollowIndicator.js
@@ -23,7 +23,7 @@
 define(
     ['moment'],
     function (moment) {
-        var NO_TIMER = "No timer being followed.";
+        var NO_TIMER = "No timer being followed";
 
         /**
          * Indicator that displays the active timer, as well as its
@@ -40,12 +40,12 @@ define(
         };
 
         FollowIndicator.prototype.getCssClass = function () {
-            return "icon-clock";
+            return (this.timerService.getTimer()) ? "icon-timer s-status-ok" : "icon-timer";
         };
 
         FollowIndicator.prototype.getText = function () {
             var timer = this.timerService.getTimer();
-            return timer ? timer.getModel().name : NO_TIMER;
+            return (timer) ? 'Following timer ' + timer.getModel().name : NO_TIMER;
         };
 
         FollowIndicator.prototype.getDescription = function () {
