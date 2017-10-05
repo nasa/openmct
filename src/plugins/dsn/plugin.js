95cb60ac3e65a8843931b6463b42a1796a52f0bf
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2017-10-05 16:08:47 +1100

    [Plugins] Added object provider for root object
    Addresses #1656

--- src/plugins/dsn/plugin.js
+++ src/plugins/dsn/plugin.js
@@ -6,14 +6,43 @@ define([
 ], function (
 
 ) {
-    var DSN_KEY = 'dsn',
+    var dictionary,
+        DSN_DICTIONARY_URI = 'src/plugins/dsn/res/dsn-dictionary.json',
+        DSN_KEY = 'dsn',
         DSN_NAMESPACE = 'deep.space.network';
+
+    function getDsnDictionary() {
+        // TODO: Replace http with library from npm
+        return http.get(DSN_DICTIONARY_URI)
+            .then(function (result) {
+                return result.data;
+            });
+    }
+
+    var objectProvider = {
+        get: function (identifier) {
+            if (identifier.key === DSN_KEY) {
+                return Promise.resolve({
+                    identifier: dictionary.identifier,
+                    name: dictionary.name,
+                    type: dictionary.type,
+                    location: dictionary.location
+                });
+            }
+        }
+    };
 
     function DsnPlugin() {
         return function install(openmct) {
             openmct.objects.addRoot({
                 namespace: DSN_NAMESPACE,
                 key: DSN_KEY
+            });
+
+            // Add providers after the dictionary has been fetched
+            getDsnDictionary().then(function (dsnDictionary) {
+                dictionary = dsnDictionary;
+                openmct.objects.addProvider(DSN_NAMESPACE, objectProvider);
             });
         };
     }
