fda6eeb47c0b50c114340a27735ce712fe7b3748
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2017-11-20 18:27:25 +1100

    [Plugins] Added JSDoc for utility functions
    Addresses #1656

--- src/plugins/dsn/DsnUtils.js
+++ src/plugins/dsn/DsnUtils.js
@@ -6,6 +6,13 @@ define([
 ) {
     'use strict';
 
+    /**
+     * Converts a domain object's identifier to an object.
+     *
+     * @param {string} identifier - The identifier to convert.  The namespace and key
+     * need to be separated by a colon (eg. 'my.namespace:my.key').
+     * @returns {Object} identifier
+     */
     function deserializeIdentifier(identifier) {
         var tokens = identifier.split(':');
         return {
@@ -14,6 +21,13 @@ define([
         };
     }
 
+    /**
+     * Converts a domain object's identifier to a string, separating the namespace and
+     * key with a colon.
+     *
+     * @param {Object} identifier - The identifier to convert.
+     * @returns {string} The identifier as a string (eg. 'my.namespace:my.key').
+     */
     function serializeIdentifier(identifier) {
         return identifier.namespace + ':' + identifier.key;
     }
