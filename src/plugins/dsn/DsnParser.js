8ff4fb136b6a7cc9c887f3e3e810ced261247f3e
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2017-11-23 11:56:28 +1100

    [Plugins] Declared private functions on prototype with jsDoc
    Addresses #1656

--- src/plugins/dsn/DsnParser.js
+++ src/plugins/dsn/DsnParser.js
@@ -9,10 +9,17 @@ define([], function () {
      * @constructor
      */
     function DsnParser() {
-
+        this.dsn = { data: {} };
     }
 
-    function parseStationTag(stationElement) {
+    /**
+     * Parses a station tag contained in the Deep Space Network's XML.
+     *
+     * @private
+     * @param {Element} stationElement - The station to parse.
+     * @returns {object} An object containing the station's data.
+     */
+    DsnParser.prototype.parseStationTag = function (stationElement) {
         var key = stationElement.getAttribute('name'),
             station = {};
 
@@ -22,9 +29,17 @@ define([], function () {
         station[key + '.time.zone.offset'] = parseInt(stationElement.getAttribute('timeZoneOffset'), 10);
 
         return station;
-    }
+    };
 
-    function parseDishTag(dishElement) {
+    /**
+     * Parses a dish tag contained in the Deep Space Network's XML.
+     *
+     * @private
+     * @param {Element} dishElement - The dish to parse.
+     * @returns {object} An object containing data about the dish's antenna, down signals,
+     * up signals and targets.
+     */
+    DsnParser.prototype.parseDishTag = function (dishElement) {
         var children = dishElement.children,
             key,
             dish = {};
@@ -83,18 +98,32 @@ define([], function () {
         }
 
         return dish;
-    }
-
-    function parseTimestampTag(element) {
-        return element.textContent;
-    }
+    };
 
     /**
-     * Parses the station, dish and timestamp tags contained in the
-     * Deep Space Network's XML.
+     * Parses the timestamp tag contained in the Deep Space Network's XML.
+     *
+     * @private
+     * @param {Element} timestampElement - The timestamp to parse.
+     * @returns {string} The time in milliseconds since the UNIX epoch.
+     */
+    DsnParser.prototype.parseTimestampTag = function (timestampElement) {
+        return timestampElement.textContent;
+    };
+
+    /**
+     * @typedef DsnData
+     * @type {object}
+     * @property {object} data - An object containing properties that match the values of domain
+     * object identifier keys and their corresponding telemetry values.
+     * @property {string} timestamp - The time in milliseconds since the UNIX epoch.
+     */
+
+    /**
+     * Parses the station, dish and timestamp tags contained in the Deep Space Network's XML.
      *
      * @param {Document} xmlDocument - The XML document representing the Deep Space Network's data.
-     * @returns {Object} The parsed XML as an object with properties that match the
+     * @returns {DsnData} The parsed XML as an object with properties that match the
      * values of domain object identifier keys.
      */
     DsnParser.prototype.parseXml = function (xmlDocument) {
@@ -106,17 +135,17 @@ define([], function () {
 
             switch (element.tagName) {
                 case 'station':
-                    Object.assign(dsn, parseStationTag(element));
+                    Object.assign(this.dsn.data, this.parseStationTag(element));
                     break;
                 case 'dish':
-                    Object.assign(dsn, parseDishTag(element));
+                    Object.assign(this.dsn.data, this.parseDishTag(element));
                     break;
                 case 'timestamp':
-                    dsn.timestamp = parseTimestampTag(element);
+                    this.dsn.timestamp = this.parseTimestampTag(element);
             }
         }
 
-        return dsn;
+        return this.dsn;
     };
 
     return DsnParser;
