9e6a2de585ddb38b9db3647330843d01757fcbc8
Henry <akhenry@gmail.com>
2018-01-17 11:41:11 -0800

    Adding Indicator API spec

--- platform/features/clock/src/indicators/FollowIndicator.js
+++ platform/features/clock/src/indicators/FollowIndicator.js
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Open MCT, Copyright (c) 2009-2016, United States Government
+ * Open MCT, Copyright (c) 2009-2018, United States Government
  * as represented by the Administrator of the National Aeronautics and Space
  * Administration. All rights reserved.
  *
