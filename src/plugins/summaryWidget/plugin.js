d79001e7afb3c07b429ef34f77c275a029457974
Henry <akhenry@gmail.com>
2018-03-29 11:03:55 -0700

    Merged summary-widget-telemetry-provider

--- src/plugins/summaryWidget/plugin.js
+++ src/plugins/summaryWidget/plugin.js
@@ -1,4 +1,14 @@
-define(['./src/SummaryWidget', './SummaryWidgetsCompositionPolicy'], function (SummaryWidget, SummaryWidgetsCompositionPolicy) {
+define([
+    './SummaryWidgetsCompositionPolicy',
+    './src/telemetry/SummaryWidgetMetadataProvider',
+    './src/telemetry/SummaryWidgetTelemetryProvider',
+    './src/views/SummaryWidgetViewProvider'
+], function (
+    SummaryWidgetsCompositionPolicy,
+    SummaryWidgetMetadataProvider,
+    SummaryWidgetTelemetryProvider,
+    SummaryWidgetViewProvider
+) {
 
     function plugin() {
 
@@ -9,8 +19,40 @@ define(['./src/SummaryWidget', './SummaryWidgetsCompositionPolicy'], function (S
             cssClass: 'icon-summary-widget',
             initialize: function (domainObject) {
                 domainObject.composition = [];
-                domainObject.configuration = {};
+                domainObject.configuration = {
+                    ruleOrder: ['default'],
+                    ruleConfigById: {
+                        default: {
+                            name: 'Default',
+                            label: 'Unnamed Rule',
+                            message: '',
+                            id: 'default',
+                            icon: ' ',
+                            style: {
+                                'color': '#ffffff',
+                                'background-color': '#38761d',
+                                'border-color': 'rgba(0,0,0,0)'
+                            },
+                            description: 'Default appearance for the widget',
+                            conditions: [{
+                                object: '',
+                                key: '',
+                                operation: '',
+                                values: []
+                            }],
+                            jsCondition: '',
+                            trigger: 'any',
+                            expanded: 'true'
+                        }
+                    },
+                    testDataConfig: [{
+                        object: '',
+                        key: '',
+                        value: ''
+                    }]
+                };
                 domainObject.openNewTab = 'thisTab';
+                domainObject.telemetry = {};
             },
             form: [
                 {
@@ -40,26 +82,14 @@ define(['./src/SummaryWidget', './SummaryWidgetsCompositionPolicy'], function (S
             ]
         };
 
-        function initViewProvider(openmct) {
-            return {
-                name: 'Widget View',
-                view: function (domainObject) {
-                    return new SummaryWidget(domainObject, openmct);
-                },
-                canView: function (domainObject) {
-                    return (domainObject.type === 'summary-widget');
-                },
-                editable: true,
-                key: 'summaryWidgets'
-            };
-        }
-
         return function install(openmct) {
             openmct.types.addType('summary-widget', widgetType);
-            openmct.objectViews.addProvider(initViewProvider(openmct));
             openmct.legacyExtension('policies', {category: 'composition',
                 implementation: SummaryWidgetsCompositionPolicy, depends: ['openmct']
             });
+            openmct.telemetry.addProvider(new SummaryWidgetMetadataProvider(openmct));
+            openmct.telemetry.addProvider(new SummaryWidgetTelemetryProvider(openmct));
+            openmct.objectViews.addProvider(new SummaryWidgetViewProvider(openmct));
         };
     }
 
---
+++ src/plugins/summaryWidget/src/telemetry/SummaryWidgetRuleSpec.js
@@ -0,0 +1,163 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2018, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+define([
+    './SummaryWidgetRule'
+], function (
+    SummaryWidgetRule
+) {
+    describe('SummaryWidgetRule', function () {
+
+        var rule;
+        var telemetryState;
+
+        beforeEach(function () {
+            var formatMap = {
+                raw: {
+                    parse: function (datum) {
+                        return datum.value;
+                    }
+                }
+            };
+
+            telemetryState = {
+                objectId: {
+                    formats: formatMap,
+                    lastDatum: {
+                    }
+                },
+                otherObjectId: {
+                    formats: formatMap,
+                    lastDatum: {
+                    }
+                }
+            };
+        });
+
+        it('allows single condition rules with any', function () {
+            rule = new SummaryWidgetRule({
+                trigger: 'any',
+                conditions: [{
+                    object: 'objectId',
+                    key: 'raw',
+                    operation: 'greaterThan',
+                    values: [
+                        10
+                    ]
+                }]
+            });
+
+            telemetryState.objectId.lastDatum.value = 5;
+            expect(rule.evaluate(telemetryState)).toBe(false);
+            telemetryState.objectId.lastDatum.value = 15;
+            expect(rule.evaluate(telemetryState)).toBe(true);
+        });
+
+        it('allows single condition rules with all', function () {
+            rule = new SummaryWidgetRule({
+                trigger: 'all',
+                conditions: [{
+                    object: 'objectId',
+                    key: 'raw',
+                    operation: 'greaterThan',
+                    values: [
+                        10
+                    ]
+                }]
+            });
+
+            telemetryState.objectId.lastDatum.value = 5;
+            expect(rule.evaluate(telemetryState)).toBe(false);
+            telemetryState.objectId.lastDatum.value = 15;
+            expect(rule.evaluate(telemetryState)).toBe(true);
+        });
+
+        it('can combine multiple conditions with all', function () {
+            rule = new SummaryWidgetRule({
+                trigger: 'all',
+                conditions: [{
+                    object: 'objectId',
+                    key: 'raw',
+                    operation: 'greaterThan',
+                    values: [
+                        10
+                    ]
+                }, {
+                    object: 'otherObjectId',
+                    key: 'raw',
+                    operation: 'greaterThan',
+                    values: [
+                        20
+                    ]
+                }]
+            });
+
+            telemetryState.objectId.lastDatum.value = 5;
+            telemetryState.otherObjectId.lastDatum.value = 5;
+            expect(rule.evaluate(telemetryState)).toBe(false);
+            telemetryState.objectId.lastDatum.value = 5;
+            telemetryState.otherObjectId.lastDatum.value = 25;
+            expect(rule.evaluate(telemetryState)).toBe(false);
+            telemetryState.objectId.lastDatum.value = 15;
+            telemetryState.otherObjectId.lastDatum.value = 5;
+            expect(rule.evaluate(telemetryState)).toBe(false);
+            telemetryState.objectId.lastDatum.value = 15;
+            telemetryState.otherObjectId.lastDatum.value = 25;
+            expect(rule.evaluate(telemetryState)).toBe(true);
+
+        });
+
+        it('can combine multiple conditions with any', function () {
+            rule = new SummaryWidgetRule({
+                trigger: 'any',
+                conditions: [{
+                    object: 'objectId',
+                    key: 'raw',
+                    operation: 'greaterThan',
+                    values: [
+                        10
+                    ]
+                }, {
+                    object: 'otherObjectId',
+                    key: 'raw',
+                    operation: 'greaterThan',
+                    values: [
+                        20
+                    ]
+                }]
+            });
+
+            telemetryState.objectId.lastDatum.value = 5;
+            telemetryState.otherObjectId.lastDatum.value = 5;
+            expect(rule.evaluate(telemetryState)).toBe(false);
+            telemetryState.objectId.lastDatum.value = 5;
+            telemetryState.otherObjectId.lastDatum.value = 25;
+            expect(rule.evaluate(telemetryState)).toBe(true);
+            telemetryState.objectId.lastDatum.value = 15;
+            telemetryState.otherObjectId.lastDatum.value = 5;
+            expect(rule.evaluate(telemetryState)).toBe(true);
+            telemetryState.objectId.lastDatum.value = 15;
+            telemetryState.otherObjectId.lastDatum.value = 25;
+            expect(rule.evaluate(telemetryState)).toBe(true);
+        });
+    });
+});
