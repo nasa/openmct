a77c81711ddf8bce5ef78e17a5b6f9bc896678b2
Deep Tailor <deep.j.tailor@nasa.gov>
2018-05-29 13:15:17 -0700

    fixes issue 2041 allows user to select caret position in notebook entries while in layout

--- platform/features/notebook/src/controllers/NotebookController.js
+++ platform/features/notebook/src/controllers/NotebookController.js
@@ -147,30 +147,9 @@ define(
                 });
             };
 
-            function setCaretToEndOfContenteditable(contentEditableElement) {
-                var range,selection;
-                if (document.createRange) {
-                    range = document.createRange();//Create a range (a range is a like the selection but invisible)
-                    range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
-                    range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-                    selection = window.getSelection();//get the selection object (allows you to change selection)
-                    selection.removeAllRanges();//remove any selections already made
-                    selection.addRange(range);//make the range you have just created the visible selection
-                }
-            }
-
-            $scope.selectContentEditable = function ($event) {
-                var child = $($event.srcElement).children()[0];
-
-                if (child) {
-                    $($event.srcElement).children()[0].focus();
-                }
-            };
-
             $scope.textFocus = function ($event, entryId) {
                 if ($event.srcElement) {
                     $scope.currentEntryValue = $event.srcElement.innerText;
-                    setCaretToEndOfContenteditable($event.srcElement);
                 } else {
                     $event.target.innerText = '';
                 }
