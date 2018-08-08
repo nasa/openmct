50be48342136ed568ecc82a6bd7a98c2e5b19853
charlesh88 <charlesh88@gmail.com>
2018-08-08 16:04:41 -0700

    Markup / scss refactor WIP
    Fixes #2140
    - Mixins file added;
    - Markup and -shell CSS from codepen brought over;

--- src/ui/components/layout/Layout.vue
+++ src/ui/components/layout/Layout.vue
@@ -1,20 +1,108 @@
 <template>
-  <div class="Layout">
-      <mct-tree ref="tree"></mct-tree>
-      <mct-main ref="main"></mct-main>
-      <mct-inspector ref="inspector"></mct-inspector>
-      <mct-status ref="status"></mct-status>
-  </div>
+<div class="l-shell">
+    <div class="l-shell__main">
+        <div class="l-shell__tree l-pane" style="width: 400px">
+            <div class="l-pane__elem c-search">
+                <input placeholder="Search" />
+            </div>
+            <ul class="l-pane__elem l-tree">
+                <li class="l-tree__item-h">
+                    <div class="l-tree__item" onclick="alert('tree item click');">
+                        <span class="c-view-control"></span><span class="c-object-name icon-folder">
+        Tree item x with a long name that forces ellipsis to occur
+        </span>
+                    </div>
+                    <ul class="l-tree">
+                        <li class="l-tree__item-h">
+                            <div class="l-tree__item" onclick="alert('tree item click');">
+                                <span class="c-view-control"></span><span class="c-object-name icon-folder">Tree item y</span>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="c-splitter c-splitter--horz js-t-split-o"></div>
+        <div class="l-shell__object l-object-view">
+            <div class="l-object-view__object-h">l-object-h</div>
+            <div class="l-object-view__time-conductor-h c-time-conductor">c-time-conductor</div>
+        </div>
+        <div class="c-splitter c-splitter--horz js-o-split-i"></div>
+        <div class="l-shell__inspector l-inspector" style="width: 200px">
+            <div class="l-inspector__properties" style="height: 300px">
+                l-inspector__properties
+            </div>
+            <div class="c-splitter c-splitter--vert js-p-split-e"></div>
+            <div class="l-inspector__elements">
+                l-inspector__elements
+            </div>
+        </div>
+    </div>
+    <div class="l-shell__status">
+        l-shell_status
+    </div>
+</div>
+
+
+
+
+<!--<div class="l-shell">
+  <mct-tree ref="tree"></mct-tree>
+  <mct-main ref="main"></mct-main>
+  <mct-inspector ref="inspector"></mct-inspector>
+  <mct-status ref="status"></mct-status>
+</div>-->
 </template>
 
 
 <style lang="scss">
-.Layout {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    position: absolute
-}
+    @import "~styles/constants";
+    /******************************* SHELL */
+    .l-shell {
+        position: absolute;
+        top: 0; right: 0; bottom: 0; left: 0;
+        display: flex;
+        flex-flow: column nowrap;
+
+        /********** HEAD */
+        &__status {
+            background: #666;
+            color: #ccc;
+            flex: 0 1 auto;
+            height: 40px;
+            order: 1;
+            padding: $interiorMarginLg;
+        }
+
+        /********** MAIN */
+        &__main {
+            flex: 1 1 auto;
+            display: flex;
+            flex-flow: row nowrap;
+            order: 2;
+        }
+
+        &__tree,
+        &__inspector {
+            max-width: 30%;
+            min-width: 5%;
+        }
+
+        &__tree {
+            background: #f90;
+            display: flex;
+            flex-flow: column nowrap;
+            padding: $interiorMarginLg;
+        }
+
+        &__object {
+            flex: 1 1 auto;
+        }
+
+        &__inspector {
+            background: #f9c;
+        }
+    }
 </style>
 
 <script>
--- src/ui/components/layout/MctStatus.vue
+++ src/ui/components/layout/MctStatus.vue
@@ -1,16 +1,9 @@
 <template>
-    <div class="MCT_Status">I'm the status bar</div>
+    <div class="l-shell-head">I'm the status bar</div>
 </template>
 
 <style lang="scss">
-.MCT_Status {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    height: 20px;
-    background: black;
-}
+
 </style>
 
 <script>
