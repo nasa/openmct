ca055ab765bccb2d18edd6c3e12034d8e8ba7364
charlesh88 <charlesh88@gmail.com>
2018-09-11 10:05:46 -0700

    Object browse bar VERY WIP

--- src/ui/components/controls/MainViewBrowseBar.vue
+++ src/ui/components/controls/MainViewBrowseBar.vue
@@ -3,17 +3,28 @@
         <a class="l-bar__nav-to-parent-button c-icon-button icon-pointer-left"></a>
         <div class="l-bar__object-name--w icon-folder">
             <span class="l-bar__object-name u-input-inline">Object Name</span>
-
         </div>
         <div class="l-bar__context-available"></div>
+
+
+        <div class="l-bar__view-switcher c-menu-button--w">
+            <div class="c-menu-button icon-thumbs-strip">
+                Grid
+            </div>
+            <MenuPlaceholder v-if="showMenu"></MenuPlaceholder>
+        </div>
+
 
     </div>
 </template>
 
 <script>
+    import MenuPlaceholder from '../controls/CreateButton.vue';
+
     export default {
-        name: "MainViewBrowseBar.vue"
-// Comment
+        components: {
+            MenuPlaceholder
+        }
     }
 </script>
 
