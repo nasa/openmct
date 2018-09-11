6737c71c4563f70e2fba9ab37439b8f3852f9a4d
charlesh88 <charlesh88@gmail.com>
2018-09-11 11:41:20 -0700

    Object browse bar WIP
    - view-control renamed to disclosure-triangle;
    - Good progress on object browse bar elements;

--- src/ui/components/controls/MainViewBrowseBar.vue
+++ src/ui/components/controls/MainViewBrowseBar.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="l-main-view-browse-bar">
-        <a class="l-bar__nav-to-parent-button c-icon-button icon-pointer-left"></a>
-        <div class="l-bar__object-name--w icon-folder">
-            <span class="l-bar__object-name u-input-inline">Object Name</span>
-        </div>
-        <div class="l-bar__context-available"></div>
-
-
-        <div class="l-bar__view-switcher c-menu-button--w">
-            <div class="c-menu-button icon-thumbs-strip">
-                Grid
+    <div class="l-browse-bar">
+        <div class="l-browse-bar__start">
+            <a class="l-browse-bar__nav-to-parent-button c-icon-button icon-pointer-left"></a>
+            <div class="l-browse-bar__object-name--w icon-folder">
+                <span class="l-browse-bar__object-name c-input-inline">
+                    Object Name Lorem ipsum dolor sit amet
+                </span>
             </div>
-            <MenuPlaceholder v-if="showMenu"></MenuPlaceholder>
+            <div class="l-browse-bar__context-actions c-disclosure-button"></div>
+        </div>
+
+        <div class="l-browse-bar__end">
+            <div class="l-browse-bar__view-switcher c-menu-button--w c-menu-button--menus-left">
+                <div class="c-menu-button icon-thumbs-strip"
+                     @click="toggleMenu">
+                    Grid
+                </div>
+                <MenuPlaceholder v-if="showMenu"></MenuPlaceholder>
+            </div>
         </div>
 
 
@@ -19,15 +25,59 @@
 </template>
 
 <script>
-    import MenuPlaceholder from '../controls/CreateButton.vue';
+    import MenuPlaceholder from '../controls/ContextMenu.vue';
 
     export default {
+        props: {
+            showMenu: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            toggleMenu: function () {
+                this.showMenu = !this.showMenu;
+            },
+        },
         components: {
             MenuPlaceholder
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    @import "~styles/sass-base";
+
+    /******************************* START */
+
+    .l-browse-bar {
+        &.l-browse-bar,
+        [class*="__"] {
+            display: flex;
+            align-items: center;
+        }
+
+        &__start {
+            flex: 1 1 100%;
+            font-size: 1.4em;
+        }
+
+        &__end {
+            flex: 0 0 auto;
+        }
+
+        &__nav-to-parent-button {
+            // This is an icon-button
+            $p: $interiorMarginLg;
+            margin-right: $interiorMargin;
+            padding-left: $p;
+            padding-right: $p;
+        }
+
+        &__object-name--w:before {
+            opacity: 0.5;
+            margin-right: $interiorMargin;
+        }
+    }
 
 </style>
--- src/ui/components/controls/viewControl.vue
+++ src/ui/components/controls/viewControl.vue
@@ -1,42 +1,11 @@
 <template>
-    <span class="c-view-control"
+    <span class="c-disclosure-triangle"
     :class="{
-        'c-view-control--expanded' : expanded,
+        'c-disclosure-triangle--expanded' : expanded,
         'is-enabled' : enabled
     }"
     @click="toggle"></span>
 </template>
-
-<style lang="scss">
-    @import "~styles/sass-base";;
-
-    .c-view-control {
-        $d: 12px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex: 0 0 auto;
-        width: $d;
-        position: relative;
-
-        &.is-enabled:before {
-            $s: .65;
-            content: $glyph-icon-arrow-right-equilateral;
-            display: block;
-            font-family: symbolsfont;
-            font-size: 1rem * $s;
-            position: absolute;
-            transform-origin: floor(($d / 2) * $s); // This is slightly better than 'center'
-            transition: transform 100ms ease-in-out;
-        }
-
-        &--expanded {
-            &:before {
-                transform: rotate(90deg);
-            }
-        }
-    }
-</style>
 
 <script>
     export default {
