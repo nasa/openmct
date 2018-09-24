3b3c2fae6b5ddda3803a6bbcbf014146b783fd2a
Deep Tailor <deep.j.tailor@nasa.gov>
2018-09-24 16:23:12 -0700

    working overlayService

---
+++ src/ui/overlayService/overlay.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="abs overlay l-large-view">
+    <div class="abs blocker" v-on:click="destroy"></div> 
+
+    <div class="abs outer-holder">
+
+        <a 
+            class="close icon-x-in-circle"
+            v-on:click="destoy">
+        </a>
+
+        <div class="abs inner-holder l-flex-col"> 
+            <div class="t-contents flex-elem holder grows">
+            <div ref="element"></div>
+            </div>
+
+            <div class="bottom-bar flex-elem holder" v-on:click="destroy">
+                <a class="t-done s-button major">Done</a> 
+            </div> 
+        </div> 
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  inject: ['destroy', 'element'],
+  mounted() {
+      this.$refs.element.appendChild(this.element);
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
---
+++ src/ui/overlayService/overlayService.js
@@ -0,0 +1,79 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2018, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+define([
+    './overlay.vue',
+    'vue'
+], function (
+    OverlayComponent,
+    Vue
+) {
+
+    function OverlayService() {
+        this.activeOverlays = [];
+        this.overlayId = 0;
+    }
+
+    OverlayService.prototype.show  = function (element) {
+        if(this.activeOverlays.length) {
+            this.activeOverlays[this.activeOverlays.length - 1].overlay.classList.add('invisible');
+        }
+
+        this.overlay = document.createElement('div');
+        this.overlay.classList.add('abs');
+
+        document.body.appendChild(this.overlay);
+
+        this.component = new Vue({
+            provide: {
+                destroy: this.destroy.bind(this),
+                element: element
+            },
+            components: {
+                OverlayComponent: OverlayComponent.default
+            },
+            template: '<overlay-component></overlay-component>'
+        });
+
+        this.overlay.appendChild(this.component.$mount().$el);
+
+        this.activeOverlays.push({
+            overlay: this.overlay,
+            component: this.component
+        });
+    };
+
+    OverlayService.prototype.destroy = function () {
+        var lastActiveOverlayObject = this.activeOverlays.pop(),
+            lastActiveOverlay = lastActiveOverlayObject.overlay,
+            lastActiveComponent = lastActiveOverlayObject.component;
+
+        lastActiveComponent.$destroy(true);
+        document.body.removeChild(lastActiveOverlay);
+
+        if (this.activeOverlays.length) {
+            this.activeOverlays[this.activeOverlays.length - 1].overlay.classList.remove('invisible');
+        }
+    };
+
+    return OverlayService;
+});
