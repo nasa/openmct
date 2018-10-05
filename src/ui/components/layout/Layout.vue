814b40461430a2e6fbe4d630bf66a9d9c2ffacf6
charlesh88 <charlesh88@gmail.com>
2018-10-05 15:13:38 -0700

    Fixes for Inspector Plot properties, and more
    - Fixed overflow problem when Inspector collapsed;
    - Temp legacy styling for Plot inspection, series options, etc.
    - Factored out non-useful gridTwoColumn mixin;
    - Brought back legacy tree.scss file in legacy-styles.scss;

--- src/ui/components/layout/Layout.vue
+++ src/ui/components/layout/Layout.vue
@@ -63,6 +63,7 @@
         top: 0; right: 0; bottom: 0; left: 0;
         display: flex;
         flex-flow: column nowrap;
+        overflow: hidden;
 
         &__status {
             background: $colorBodyFg;
