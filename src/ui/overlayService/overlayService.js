6cb05bc74aa91d5878366823b92448e7dcf63e59
Deep Tailor <deep.j.tailor@nasa.gov>
2018-10-11 10:59:55 -0700

    added jsdocs for overlayService

--- src/ui/overlayService/overlayService.js
+++ src/ui/overlayService/overlayService.js
@@ -30,6 +30,15 @@ define([
     Vue
 ) {
 
+    /**
+     * The OverlayService is responsible for pre-pending templates to
+     * the body of the document, which is useful for displaying templates
+     * which need to block the full screen.
+     *
+     * @memberof platform/ui/overlayService
+     * @constructor
+    */
+
     function OverlayService() {
         this.activeOverlays = [];
         this.overlayId = 0;
@@ -37,6 +46,25 @@ define([
         this.showBlockingMessage = this.showBlockingMessage.bind(this);
     }
 
+    /**
+     * A description of option properties that can be passed into the overlay
+    * @typedef options
+        * @property {string} cssClass class to be applied to overlay.
+        * supported classes are - l-large-view, l-dialog, l-message
+        * @property {array} buttons optional button objects with label and callback properties
+        * @property {function} onDestroy callback to be called when overlay is destroyed
+        * (useful for clearing out listeners and destroying components)
+    */
+
+    /**
+     * Add a new overlay to the document. This will be
+     * prepended to the document body
+     *
+     * @param {object} element a DOM element, that needs to be inserted into the Overlay
+     * @param {object} options
+     * @returns {object} with a dismiss function that can be called to dimiss/destroy the overlay from
+     * the calling code.
+     */
     OverlayService.prototype.show  = function (element, options) {
         if(this.activeOverlays.length) {
             this.activeOverlays[this.activeOverlays.length - 1].overlay.classList.add('invisible');
@@ -98,9 +126,50 @@ define([
     OverlayService.prototype.destroy = function () {
         var lastActiveOverlayObject = this.activeOverlays[this.activeOverlays.length - 1];
 
-        lastActiveOverlayObject.dialog.dismiss(lastActiveOverlayObject.id);
+        lastActiveOverlayObject.dialog.dismiss();
     };
 
+    /**
+     * A description of the model options that may be passed to the
+     * showBlockingMessage method. Note that the DialogModel described
+     * here is shared with the Notifications framework.
+     * @see NotificationService
+     *
+    * @typedef model
+        * @property {string} title the title to use for the dialog
+        * @property {string} severity the severity level of this message.
+        * These are defined in a bundle constant with key 'dialogSeverity'
+        * @property {string} hint the 'hint' message to show below the title
+        * @property {string} actionText text that indicates a current action,
+        * shown above a progress bar to indicate what's happening.
+        * @property {number} progress a percentage value (1-100)
+        * indicating the completion of the blocking task
+        * @property {string} progressText the message to show below a
+        * progress bar to indicate progress. For example, this might be
+        * used to indicate time remaining, or items still to process.
+        * @property {boolean} unknownProgress some tasks may be
+        * impossible to provide an estimate for. Providing a true value for
+        * this attribute will indicate to the user that the progress and
+        * duration cannot be estimated.
+        * @property {Object} primaryOption an action that will
+        * be added to the dialog as a button. The primary action can be
+        * used as the suggested course of action for the user. Making it
+        * distinct from other actions allows it to be styled differently,
+        * and treated preferentially in banner mode.
+        * @property {buttons[]} buttons a list of buttons with title and callback properties that will
+        * be added to the dialog.
+    */
+
+    /**
+     * Displays a blocking (modal) dialog. This dialog can be used for
+     * displaying messages that require the user's
+     * immediate attention. The message may include an indication of
+     * progress, as well as a series of actions that
+     * the user can take if necessary
+     * @param {model} dialogModel defines options for the dialog
+     * @returns {object} with a dismiss function that can be called from the calling code
+     * to dismiss/destroy the dialog
+    */
     OverlayService.prototype.showBlockingMessage = function (model) {
         let component = new Vue({
             provide: {
