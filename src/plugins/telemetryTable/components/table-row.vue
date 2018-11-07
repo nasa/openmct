c073a21ba6ad87ea8420f0f92868193354d245c9
Andrew Henry <andrew.k.henry@nasa.gov>
2018-11-07 11:04:56 -0800

    [Table] Custom column widths and order (#2204)
    * Renders with resize hotzones
    
    * Implemented basic reordering of columns
    
    * Refactored column headers into component
    
    * Custom column widths persist during resize
    
    * Initial working version of fixed column sizes
    
    * Only calculate default sizes when first data received.
    
    * Further fixes for fixed column widths. Add option to switch to auto-widths
    
    * Fixed bug with table auto-sizing
    
    * Only allow reorder and resize in edit mode
    
    * Bug fixes
    
    * Allow for scroll bar width
    
    * Bug fix with tables reverting to 100% width
    
    * Fixed bug with drop position indicator when scrolled
    
    * Moved events on to component
    
    * Do not throttle mouse events. Let Vue throttle them
    
    * Do not hard code vertical offset for drop target
    
    * Addressed review issues
    
    * Clarified mouse event handling on column resize

--- src/plugins/telemetryTable/components/table-row.vue
+++ src/plugins/telemetryTable/components/table-row.vue
@@ -1,7 +1,29 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2018, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
 <template>
 <tr :style="{ top: rowTop }" :class="rowLimitClass">
-    <td v-for="(title, key, headerIndex) in headers"
-        :style="{ width: columnWidths[headerIndex], 'max-width': columnWidths[headerIndex]}"
+    <td v-for="(title, key) in headers" 
+        :key="key"
+        :style="columnWidths[key] === undefined ? {} : { width: columnWidths[key] + 'px', 'max-width': columnWidths[key] + 'px'}"
         :title="formattedRow[key]"
         :class="cellLimitClasses[key]">{{formattedRow[key]}}</td>
 </tr>
@@ -30,11 +52,8 @@ export default {
             required: true
         },
         columnWidths: {
-            type: Array,
-            required: false,
-            default() {
-                return [];
-            },
+            type: Object,
+            required: true
         },
         rowIndex: {
             type: Number,
