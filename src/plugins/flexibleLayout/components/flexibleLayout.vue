282385a543bf9b45b6117b9977053f66012f5482
Deep Tailor <deep.j.tailor@nasa.gov>
2018-12-04 12:11:55 -0800

    update domainObject on change, and cahnge toolBarProvider to function that returns an object

--- src/plugins/flexibleLayout/components/flexibleLayout.vue
+++ src/plugins/flexibleLayout/components/flexibleLayout.vue
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2018, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+ 
 <template>
     <div class="c-fl">
         <div class="c-fl__empty"
@@ -566,6 +588,7 @@ export default {
             return roundedValue;
         },
         updateDomainObject(newDomainObject) {
+            this.domainObject = newDomainObject;
             this.rowsLayout = newDomainObject.configuration.rowsLayout;
             this.containers = newDomainObject.configuration.containers;
         },
