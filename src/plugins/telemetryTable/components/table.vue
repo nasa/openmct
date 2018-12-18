ec4c443299b529482cbc16772af9b23393a51739
Charles Hacskaylo <charlesh88@gmail.com>
2018-12-18 11:07:09 -0800

    Misc ui 2 (#2248)
    * Create button disabled when editing
    * New disabled mixin;
    * Tree styling
    * Fixed icons for fullscreen toggle button
    * Local controls fixed for Imagery and Plots
    * Range control styling updated;
    * Plot styling, significant mods
    * Disclosure controls improved;
    * New _legacy-plots.scss file added, no longer loads legacy plot SCSS
    files;
    * Removed 12px crosshair cursor in legend hover;
    * Inspector tree styling in plot options
    * Fix z-indexing related to Overlays

--- src/plugins/telemetryTable/components/table.vue
+++ src/plugins/telemetryTable/components/table.vue
@@ -106,7 +106,6 @@
 
 <style lang="scss">
     @import "~styles/sass-base";
-    @import "~styles/table";
 
     .c-telemetry-table__drop-target {
         position: absolute;
@@ -126,6 +125,10 @@
             flex: 1 0 auto;
             width: 100px;
             vertical-align: middle; // This is crucial to hiding f**king 4px height injected by browser by default
+        }
+
+        td {
+            color: $colorTelemFresh;
         }
 
         /******************************* WRAPPERS */
