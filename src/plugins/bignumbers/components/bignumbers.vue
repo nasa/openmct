ba09a17b29e8007124be3e392b23cae4ac27531d
charlesh88 <charlesh88@gmail.com>
2019-02-02 19:20:04 -0800

    Get bignumbers object wired up
    - Added subscription methods from gauge (thanks Deep!);
    - JS now splits number into decimal and integer;
    - Mod to integer SVG display to use lengthAdjust=spacingAndGlyphs;

--- src/plugins/bignumbers/components/bignumbers.vue
+++ src/plugins/bignumbers/components/bignumbers.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="c-bignumbers">
         <svg class="c-bignumbers__int" viewBox="0 0 52 32">
-            <text textLength=100% lengthAdjust=spacing x="0" y="32">{{ numToStr(this.curValInt, 3) }}</text>
+            <text textLength=100% lengthAdjust=spacingAndGlyphs x="0" y="32">{{ this.curValInt }}</text>
         </svg>
         <svg class="c-bignumbers__dec" viewBox="0 0 40 20">
-            <text textLength=100% lengthAdjust=spacing x="0" y="20">.{{ numToStr(this.curValDec, 3) }}</text>
+            <text textLength=100% lengthAdjust=spacing x="0" y="20">.{{ this.curValDec }}</text>
         </svg>
         <svg class="c-bignumbers__units" viewBox="0 0 45 11">
             <text textLength=100% lengthAdjust=spacingAndGlyphs x="0" y="11">{{ this.units }}</text>
@@ -62,29 +62,70 @@
 <script>
     export default {
         name: "bignumbers",
-        inject: ['domainObject'],
+        inject: ['openmct', 'domainObject', 'composition'],
         data: function () {
             let config = this.domainObject.configuration,
                 units = config.units;
             console.log(config);
             return {
-                curValInt: 10,
-                curValDec: 70,
+                curValInt: 0,
+                curValDec: 0,
                 units: units
 
             }
         },
         methods: {
-            numToStr: function(val, length) {
+            getInt: function(val, digits) {
+                // Extract integer portion of val and zero-pad it if its length < digits
+                return this.zeroPad(Math.floor(val), digits);
+            },
+            getDec: function(val, digits) {
+                // Extract decimal portion of val to the specified number of digits
+                return Number.parseFloat(val).toFixed(digits).split('.')[1];
+            },
+            zeroPad: function(val, length) {
                 // Zero pads an integer and returns it as a string
-                let s = val.toString();
-                for (var i = 0; i <= (length - s.length); i++) {
-                    s = '0' + s;
+                let s = Math.abs(val).toString();
+                if (s.length < length) {
+                    for (let i = 0; i <= (length - s.length); i++) {
+                        s = '0' + s;
+                    }
+                }
+                if (val < 0) {
+                    s = '-' + s;
                 }
                 return s;
+            },
+            updateValue(datum) {
+                let cv = this.formats[this.valueKey].format(datum);
+                this.curValInt = this.getInt(cv, 3);
+                this.curValDec = this.getDec(cv, 3);
+            },
+            subscribe(domainObject) {
+                this.metadata = this.openmct.telemetry.getMetadata(domainObject);
+                this.formats = this.openmct.telemetry.getFormatMap(this.metadata);
+
+                this.valueKey = this
+                    .metadata
+                    .valuesForHints(['range'])[0].key;
+                this.unsubscribe = this.openmct
+                    .telemetry
+                    .subscribe(domainObject, this.updateValue.bind(this), {});
+                this.openmct
+                    .telemetry
+                    .request(domainObject, {strategy: 'latest'})
+                    .then((values) => values.forEach(this.updateValue));
             }
         },
         computed: {
+        },
+        mounted() {
+            this.composition.on('add', this.subscribe);
+            this.composition.load();
+        },
+        destroyed() {
+            this.composition.off('add', this.subscribe);
+            this.unsubscribe();
         }
     }
 </script>
