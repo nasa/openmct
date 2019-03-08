8d3cf0ac00952b375146ccb742657f1d947ef6c4
Gabriel Kalani <gabrielsilva1956@gmail.com>
2019-03-07 23:03:30 -0300

    refactor: ViewCapability.js

--- platform/core/src/views/ViewCapability.js
+++ platform/core/src/views/ViewCapability.js
@@ -26,33 +26,34 @@
 define(
     [],
     function () {
+     /**
+      * A `view` capability can be used to retrieve an array of
+      * all views (or, more specifically, the declarative metadata
+      * thereabout) which are applicable to a specific domain
+      * object.
+      *
+      * @memberof platform/core
+      * @implements {Capability}
+      * @constructor
+      */
+     class ViewCapability {
+      constructor(viewService, domainObject) {
+          this.viewService = viewService;
+          this.domainObject = domainObject;
+      }
 
-        /**
-         * A `view` capability can be used to retrieve an array of
-         * all views (or, more specifically, the declarative metadata
-         * thereabout) which are applicable to a specific domain
-         * object.
-         *
-         * @memberof platform/core
-         * @implements {Capability}
-         * @constructor
-         */
-        function ViewCapability(viewService, domainObject) {
-            this.viewService = viewService;
-            this.domainObject = domainObject;
-        }
+      /**
+       * Get all view definitions which are applicable to
+       * this object.
+       * @returns {View[]} an array of view definitions
+       *          which are applicable to this object.
+       * @memberof platform/core.ViewCapability#
+       */
+      invoke() {
+          return this.viewService.getViews(this.domainObject);
+      }
+     }
 
-        /**
-         * Get all view definitions which are applicable to
-         * this object.
-         * @returns {View[]} an array of view definitions
-         *          which are applicable to this object.
-         * @memberof platform/core.ViewCapability#
-         */
-        ViewCapability.prototype.invoke = function () {
-            return this.viewService.getViews(this.domainObject);
-        };
-
-        return ViewCapability;
+     return ViewCapability;
     }
 );
