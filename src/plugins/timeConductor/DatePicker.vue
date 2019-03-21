e8e70679930da226b07aecacb918af381571df53
Charles Hacskaylo <charlesh88@gmail.com>
2019-03-21 15:07:16 -0700

    Fix legacy messages (#2323)
    * Fix legacy message styling
    
    - Code cleanup;
    - Enable constants-mobile;
    - Add _legacy-messages.scss;
    - Add status color fg colors to theme constants;
    
    * CSS refactoring, significant migration for messages classes
    
    - Many messages classes migrated;
    - c-click-icon > c-icon-button;
    - c-click-icon rewritten;
    - __close-btn refined in dialogs;
    
    * About and splash screen styling
    
    - Fixed splash in About screen;
    
    * Update _mixins.scss
    
    * Convert c-overlay__close-button to button
    
    - <a> -> <button>;
    - Set color of button;
    - Normalized naming from close-btn to close-button;
    
    * Fixed brightness filter issue on elements in overlays in VISTA;
    
    * Fix dismiss button

--- src/plugins/timeConductor/DatePicker.vue
+++ src/plugins/timeConductor/DatePicker.vue
@@ -21,7 +21,7 @@
  *****************************************************************************/
 <template>
     <div class="c-ctrl-wrapper c-ctrl-wrapper--menus-up c-datetime-picker__wrapper" ref="calendarHolder">
-        <a class="c-click-icon icon-calendar"
+        <a class="c-icon-button icon-calendar"
            @click="toggle"></a>
         <div class="c-menu c-menu--mobile-modal c-datetime-picker"
              v-if="open">
@@ -30,10 +30,10 @@
                         @click="toggle"></button>
             </div>
             <div class="c-datetime-picker__pager c-pager l-month-year-pager">
-                <div class="c-pager__prev c-click-icon icon-arrow-left"
+                <div class="c-pager__prev c-icon-button icon-arrow-left"
                    @click.stop="changeMonth(-1)"></div>
                 <div class="c-pager__month-year">{{model.month}} {{model.year}}</div>
-                <div class="c-pager__next c-click-icon icon-arrow-right"
+                <div class="c-pager__next c-icon-button icon-arrow-right"
                    @click.stop="changeMonth(1)"></div>
             </div>
             <div class="c-datetime-picker__calendar c-calendar">
@@ -91,7 +91,7 @@
         grid-template-columns: auto 1fr auto;
         align-items: center;
 
-        .c-click-icon {
+        .c-icon-button {
             font-size: 0.8em;
         }
 
