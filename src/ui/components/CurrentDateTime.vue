38c1a981e889be8af34e4d97ac90da44338005e9
charlesh88 <charlesh88@gmail.com>
2019-06-25 10:10:58 -0700

    Clock element as a standalone component
    - Was intended to replace clock indicator;
    - WIP!

---
+++ src/ui/components/CurrentDateTime.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="c-indicator no-collapse icon-clock c-current-datetime">
+        <span class="label">{{ currentDateTime }} UTC</span>
+    </div>
+</template>
+
+<script>
+    import moment from 'moment';
+
+    export default {
+        data() {
+            return {
+                currentDateTime: moment.utc().format('LTS')
+            }
+        },
+        mounted: function() {
+            setInterval(()=>{
+                this.currentDateTime = moment.utc().format('YYYY/MM/DD HH:mm:ss')
+            },100)
+        }
+    }
+</script>
