b340fda476c402bd6fce3db1dff7ec1b55949e21
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2019-10-31 15:42:59 -0700

    fixed console error with save + using this.$nextTick instead of setTimeout.

--- src/api/overlays/components/OverlayComponent.vue
+++ src/api/overlays/components/OverlayComponent.vue
@@ -182,10 +182,12 @@
         },
         inject: ['dismiss', 'element', 'buttons', 'dismissable'],
         mounted() {
-            this.$refs.element.appendChild(this.element);
-            setTimeout(() => {
-                this.getElementForFocus().focus();
-            }, 0);
+            const element = this.$refs.element;
+            element.appendChild(this.element);
+            const elementForFocus = this.getElementForFocus() || element;
+            this.$nextTick(() => {
+                elementForFocus.focus();
+            });
         },
         methods: {
             destroy: function () {
