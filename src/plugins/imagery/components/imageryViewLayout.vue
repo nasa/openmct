87eb965e235c8f82b836059a84464b579ab2112f
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2019-11-14 16:19:31 -0800

    use multipane vue component for resize + cleanup + style adjustments.

--- src/plugins/imagery/components/imageryViewLayout.vue
+++ src/plugins/imagery/components/imageryViewLayout.vue
@@ -1,80 +1,74 @@
 <template>
-    <div class="imagery-layout"
-                :class="{'unnsynced': paused()}"
-                :style="{'height': `${elementHeight}px`}">
-        <div class="main-image-wrapper c-imagery has-local-controls"
-                :style="{'height': `${imageSize}%`}">
-            <div class="h-local-controls h-local-controls--overlay-content c-local-controls--show-on-hover l-flex-row c-imagery__lc">
-                <span class="holder flex-elem grows c-imagery__lc__sliders">
-                    <input class="icon-brightness" type="range"
-                            min="0"
-                            max="500"
-                            v-model="filters.brightness" />
-                    <input class="icon-contrast" type="range"
-                            min="0"
-                            max="500"
-                            v-model="filters.contrast" />
-                </span>
-                <span class="holder flex-elem t-reset-btn-holder c-imagery__lc__reset-btn">
-                    <a class="s-icon-button icon-reset t-btn-reset" 
-                            @click="filters={brightness: 100, contrast: 100}"></a>
-                </span>
-            </div>
+    <multipane class="c-imagery-layout" type="vertical">
+        <pane>
+            <div class="main-image-wrapper c-imagery has-local-controls">
+                <div class="h-local-controls h-local-controls--overlay-content c-local-controls--show-on-hover l-flex-row c-imagery__lc">
+                    <span class="holder flex-elem grows c-imagery__lc__sliders">
+                        <input class="icon-brightness" type="range"
+                                min="0"
+                                max="500"
+                                v-model="filters.brightness" />
+                        <input class="icon-contrast" type="range"
+                                min="0"
+                                max="500"
+                                v-model="filters.contrast" />
+                    </span>
+                    <span class="holder flex-elem t-reset-btn-holder c-imagery__lc__reset-btn">
+                        <a class="s-icon-button icon-reset t-btn-reset" 
+                                @click="filters={brightness: 100, contrast: 100}"></a>
+                    </span>
+                </div>
 
-            <div class="main-image s-image-main"
-                    :class="{'paused': paused(),'stale':false }"
-                    :style="{'background-image': `url(${getImageUrl()})`,
-                        'filter': `brightness(${filters.brightness}%) contrast(${filters.contrast}%)`}">
-            </div>
-        </div>
-        <resize-handle
-                    :index="0"
-                    :isEditing=true
-                    :orientation="'vertical'"
-                    @init-move="startContainerResizing"
-                    @move="containerResizing"
-                    @end-move="endContainerResizing">
-        </resize-handle>
-        <div class="l-image-main-controlbar flex-elem l-flex-row">
-                <div class="l-datetime-w flex-elem grows">
-                    <a class="c-button show-thumbs sm hidden icon-thumbs-strip" />
-                    <span class="l-time">{{getTime()}}</span>
+                <div class="main-image s-image-main"
+                        :class="{'paused unnsynced': paused(),'stale':false }"
+                        :style="{'background-image': `url(${getImageUrl()})`,
+                            'filter': `brightness(${filters.brightness}%) contrast(${filters.contrast}%)`}">
                 </div>
-                <div class="h-local-controls flex-elem">
-                    <a class="c-button icon-pause pause-play"
-                            @click="paused(!paused())"
-                            :class="{'is-paused': paused()}" />
+
+                <div class="l-image-controller flex-elem l-flex-row">
+                    <div class="l-datetime-w flex-elem grows">
+                        <a class="c-button show-thumbs sm hidden icon-thumbs-strip" />
+                        <span class="l-time">{{getTime()}}</span>
+                    </div>
+                    <div class="h-local-controls flex-elem">
+                        <a class="c-button icon-pause pause-play"
+                                @click="paused(!paused())"
+                                :class="{'is-paused': paused()}" />
+                    </div>
                 </div>
-        </div>
-        <div class="thumbs-layout" ref="thumbsWrapper"
-                @scroll="handleScroll"
-                :style="{'height': `${100 - imageSize}%`}">
-            <div class="l-image-thumb-item"
-                    :class="{selected: image.selected}"
-                    v-for="(image, index) in imageHistory"
-                    :key="index"
-                    @click="setSelectedImage(image)">
-                <img class="l-thumb" v-bind:src="getImageUrl(image)">
-                <div class="l-time">{{getTime(image)}}</div>
             </div>
-        </div>
-    </div>
+        </pane>
+        <pane class="c-inspector__elements" handle="before">
+            <div class="c-elements-pool">
+                <div class="thumbs-layout" ref="thumbsWrapper"
+                        @scroll="handleScroll">
+                    <div class="l-image-thumb-item"
+                            :class="{selected: image.selected}"
+                            v-for="(image, index) in imageHistory"
+                            :key="index"
+                            @click="setSelectedImage(image)">
+                        <img class="l-thumb" v-bind:src="getImageUrl(image)">
+                        <div class="l-time">{{getTime(image)}}</div>
+                    </div>
+                </div>
+            </div>
+        </pane>
+    </multipane>
 </template>
 
 <style lang="scss">
     @import '~styles/sass-base';
 
-    .imagery-layout {
+    .c-imagery-layout {
         display: flex;
         flex-direction: column;
         overflow: auto;
 
-        &.unnsynced{
-            @include sUnsynced();
-        }
-
         .main-image-wrapper {
-            display: block;
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            padding-bottom: 5px;
         }
 
         .main-image {
@@ -82,32 +76,33 @@
             background-repeat: no-repeat;
             background-size: contain;
             height: 100%;
-            width: 100%;
+
+            &.unnsynced{
+                @include sUnsynced();
+            }
         }
 
-        .image-thumbs-wrapper {
-            display: table;
+        .l-image-controller {
+            padding: 5px 0 0 0;
         }
 
         .thumbs-layout {
-            display: flex;
-            flex-wrap: wrap;
+            margin-top: 5px;
             overflow: auto;
         }
     }
 </style>
 
 <script>
-import ResizeHandle from  '../../flexibleLayout/components/resizeHandle.vue';
+import multipane from '@/ui/layout/multipane.vue';
+import pane from '@/ui/layout/pane.vue';
 import _ from 'lodash';
-
-const DEFAULT_IMAGE_SIZE = 80;
-const MIN_IMAGE_SIZE = 30;
 
 export default {
     inject: ['openmct', 'domainObject'],
     components: {
-        ResizeHandle
+        multipane,
+        pane,
     },
     data() {
         return {
@@ -122,12 +117,10 @@ export default {
             image: {
                 selected: ''
             },
-            imageSize: DEFAULT_IMAGE_SIZE,
             imageFormat: '',
             imageHistory: [],
             imageUrl: '',
             isPaused: false,
-            lockMove: true,
             requestCount: 0,
             timeFormat: '',
         }
@@ -199,15 +192,16 @@ export default {
                 }.bind(this));
         },
         scrollToBottom () {
-            if (this.isPaused || !this.$refs.thumbsWrapper) {
+            if (this.isPaused || !this.$refs.thumbsWrapper || !this.autoScroll) {
                 return;
             }
+
             const scrollHeight = this.$refs.thumbsWrapper.scrollHeight || 0;
-            if (scrollHeight && this.autoScroll) {
-                setTimeout(() => {
-                    this.$refs.thumbsWrapper.scrollTop = scrollHeight;
-                }, 0);
+            if (!scrollHeight) {
+                return;
             }
+
+            setTimeout(() => this.$refs.thumbsWrapper.scrollTop = scrollHeight, 0);
         },
         setSelectedImage (image) {
             this.imageUrl = this.getImageUrl(image);
@@ -216,29 +210,13 @@ export default {
             this.unselectAllImages();
             image.selected = true;
         },
-        startContainerResizing(index) {
-            this.lockMove = false;
-        },
-        containerResizing(index, delta, event) {
-            if (this.lockMove) {
-                return;
-            }
-            _.throttle(() => {
-                let percentageMoved = Math.round(delta / this.elementHeight * 100);
-                this.imageSize = Math.min(DEFAULT_IMAGE_SIZE, Math.max(this.imageSize + percentageMoved, MIN_IMAGE_SIZE));
-            }, 1000, { 'trailing': true })(index, delta, event);
-        },
         getElSize () {
             if (!this.$el) {
                 return;
             }
 
             this.elementHeight = this.$el.parentElement.offsetHeight;
-
             return this.elementHeight;
-        },
-        endContainerResizing(event) {
-            this.lockMove = true;
         },
         stopListening () {
             if (this.unsubscribe) {
