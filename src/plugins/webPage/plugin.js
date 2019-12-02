6d343174e8f9c725a94d2b1caebec44ba5d44c66
Deep Tailor <deep.j.tailor@nasa.gov>
2019-12-02 15:05:25 -0800

    rename view provider

--- src/plugins/webPage/plugin.js
+++ src/plugins/webPage/plugin.js
@@ -20,7 +20,7 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-import WebPageViewProvider from './WebPageViewProvider.js';
+import WebPageViewProvider from './WebPageView.js';
 
 export default function plugin() {
     return function install(openmct) {
