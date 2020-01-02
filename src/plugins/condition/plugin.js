5f80b3773b2d692ef759616d5c283c8647e6fd75
Joel McKinnon <joel.g.mckinnon@nasa.gov>
2020-01-02 15:19:57 -0800

    restructured folders

--- src/plugins/condition/plugin.js
+++ src/plugins/condition/plugin.js
@@ -19,8 +19,20 @@
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
+import ConditionSetViewProvider from './ConditionSetViewProvider.js';
 
 export default function ConditionPlugin() {
+    const conditionSetType = {
+        name: 'Condition Set',
+        key: 'conditionSet',
+        description: 'A set of one or more conditions based on user-specified criteria.',
+        creatable: true,
+        cssClass: 'icon-folder',
+        initialize: function (domainObject) {
+            domainObject.composition = [];
+        }
+    };
+
     return function install(openmct) {
         openmct.types.addType('condition', {
             name: 'Condition',
@@ -31,5 +43,10 @@ export default function ConditionPlugin() {
                 domainObject.composition = [];
             }
         });
+
+        openmct.types.addType('conditionSet', conditionSetType);
+
+        openmct.objectViews.addProvider(new ConditionSetViewProvider(openmct));
+
     }
 }
