2bb2bb6a1ba7df4dfac322202f4cc76535200218
Shefali Joshi <simplyrender@gmail.com>
2020-02-25 10:41:58 -0800

    Adds conditional style inspector provider (#2655)
    * resolved conflicts
    
    * Revert "Condition reorder"
    
    * Adds conditional style inspector provider
    Adds condition style tabbed view (this needs to be extended to allow more than one pane per tab in a separate issue)
    
    * Fixes linting issues
    
    * Merge from topic-conditionals
    
    * In order to accomodate two tab layout for the Conditionals feature, rename the following:
    - openmct.inspectorViews registry to openmct.propertiesInspector
    - InspectorViewRegistry.prototype.addProvider to InspectorViewRegistry.prototype.addViewProvider.
    Replace occurances of the same to the new names.
    
    In a subsequent commit expect a new view registry for the styles inspector view registry.
    
    * Use 'styles' property on domain objects to indicate that they should have a styles inspector tab - Note that this will not show up on existing objects but only ones that are created after this feature is added.
    Use 'styles' property on domain objects to determine if a styles view can be viewed
    Removed the TabbedInspectorView and repurposed the InspectorViews to show both a properties or a styles registry.
    Simplified markup in Inspector.vue
    
    * Addresses review comments:
    1. Go back to using inspectorViews
    2. Remove stylesInspector registry
    3. Hardcode Styles Inspector component view
    4. Styles tab can be viewed for all creatable objects except for the folders, webPages and conditionSets
    5. ConditionalStylesInspectorViewProvider is no longer needed because we are hardcoding the styles view component.
    
    Co-authored-by: Joel McKinnon <JoelMcKinnon@users.noreply.github.com>

---
+++ src/ui/inspector/StylesInspectorView.vue
@@ -0,0 +1,52 @@
+<template>
+<div></div>
+</template>
+
+<style>
+</style>
+
+<script>
+import ConditionalStylesView from '../../plugins/condition/components/inspector/ConditionalStylesView.vue';
+import Vue from 'vue';
+
+export default {
+    inject: ['openmct'],
+    data() {
+        return {
+            selection: []
+        }
+    },
+    mounted() {
+        this.openmct.selection.on('change', this.updateSelection);
+        this.updateSelection(this.openmct.selection.get());
+    },
+    destroyed() {
+        this.openmct.selection.off('change', this.updateSelection);
+    },
+    methods: {
+        updateSelection(selection) {
+            this.selection = selection;
+
+            if (this.component) {
+                this.component.$destroy();
+                this.component = undefined;
+                this.$el.innerHTML = '';
+            }
+
+            let viewContainer = document.createElement('div');
+            this.$el.append(viewContainer);
+            this.component = new Vue({
+                provide: {
+                    openmct: this.openmct
+                },
+                el: viewContainer,
+                components: {
+                    ConditionalStylesView
+                },
+                template: '<conditional-styles-view></conditional-styles-view>'
+            });
+
+        }
+    }
+}
+</script>
