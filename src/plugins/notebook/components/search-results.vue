5c1882dd83db0601c0b91bbb88b633084fd0d3ad
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-02-26 17:18:38 -0800

    Added search results for notebook.

---
+++ src/plugins/notebook/components/search-results.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <header>Search Reults</header>
+    <div v-for="(result, index) in results"
+         :key="index"
+    >
+        <Result :result="result" />
+    </div>
+</div>
+</template>
+
+<script>
+import Result from './result.vue';
+
+export default {
+    inject: ['openmct', 'domainObject'],
+    components: {
+        Result
+    },
+    props:{
+        results: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    },
+    data() {
+        return {}
+    },
+    watch: {
+        results(newResults) {}
+    },
+    mounted() {
+    }
+}
+</script>
