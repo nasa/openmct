9e0d184bdf89f4a26fd0aa3f973c7570390b1c25
Joel McKinnon <joel.g.mckinnon@nasa.gov>
2020-03-02 16:57:33 -0800

    wip: default style

--- src/plugins/condition/components/inspector/ConditionalStylesView.vue
+++ src/plugins/condition/components/inspector/ConditionalStylesView.vue
@@ -1,20 +1,40 @@
 <template>
 <div class="c-properties__row--span-all">
     <div class="controls">
-        <label>ABC</label>
-        <button class="c-icon-button--swatched icon-paint-bucket"></button>
-        <button class="c-icon-button--swatched icon-line-horz"></button>
-        <button class="c-icon-button--swatched icon-font"></button>
+        <div class="preview">ABC</div>
+        <button class="c-icon-button--swatched icon-paint-bucket"
+                @click="applyStyle('background-color')"
+        ></button>
+        <button class="c-icon-button--swatched icon-line-horz"
+                @click="applyStyle('border')"
+        ></button>
+        <button class="c-icon-button--swatched icon-font"
+                @click="applyStyle('color')"
+        ></button>
     </div>
 </div>
 </template>
 
 <script>
+const previewElem = document.querySelector('.preview');
 
 export default {
     name: 'ConditionalStylesView',
     inject: [
         'openmct'
-    ]
+    ],
+    data() {
+        return {
+            backgroundColor: 'red',
+            border: 'green',
+            color: 'blue'
+        }
+    },
+    methods: {
+        applyStyle(type) {
+            console.log('applyStyle', previewElem);
+            previewElem.style[`${type}`] = this.backgroundColor;
+        }
+    }
 }
 </script>
