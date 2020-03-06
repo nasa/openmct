4675fc8ae69e21bb6d62fa9ea9fa10a72bf544f9
Shefali Joshi <simplyrender@gmail.com>
2020-03-06 14:09:52 -0800

    Conditional styles (#2718)
    * Hardcoded prototype - conditional styles for display layout or generator domain objects only.
    Needs Architectural review
    
    * Updates to ConditionalStylesView
    
    * Adds background colors list
    
    * Show conditional styles for subObjectViews and telemetryViews
    
    * Uses telemetry provider to determine which style is active
    Removes hardcoded conditionSet work used for prototype
    
    * Fixes failing test
    
    * Add default styles to conditionalStyles when a condition set is added to the domain object.
    
    * Use EventEmitter alias instead of eventEmitter3 in imports
    Change variable name for better readability and clarity
    Remove unused code
    
    * Change StyleRuleManager to accept conditionStyle objects instead of domainObjects
    
    * Uses a map for conditional styles instead of a list in order to improve performance
    
    * Use in-built api to check for identifier equality
    Pass missing arguments
    
    * Removes unnecessary object get call.

---
+++ src/plugins/condition/components/inspector/ConditionalStyle.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <div v-if="condition"
+         class="holder c-c-button-wrapper align-left"
+    >
+        <div>{{ condition.configuration.name }}</div>
+    </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    components: {
+    },
+    inject: [
+        'openmct'
+    ],
+    props: {
+        conditionIdentifier: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            condition: null
+        }
+    },
+    destroyed() {
+    },
+    mounted() {
+        this.openmct.objects.get(this.conditionIdentifier).then((conditionDomainObject) => {
+            this.condition = conditionDomainObject;
+        });
+    }
+}
+</script>
--- src/plugins/condition/components/inspector/ConditionalStylesView.vue
+++ src/plugins/condition/components/inspector/ConditionalStylesView.vue
@@ -1,14 +1,110 @@
 <template>
-<div>Conditional Styles inspector view</div>
+<div>
+    <div v-if="!conditionalStyles.length"
+         class="holder c-c-button-wrapper align-left">
+        <button
+            class="c-c-button c-c-button--minor add-criteria-button"
+            @click="addConditionSet">
+            <span class="c-c-button__label">Use conditional styling</span>
+        </button>
+    </div>
+    <div v-else>
+        <div class="holder c-c-button-wrapper align-left">
+            <button
+                class="c-c-button c-c-button--minor add-criteria-button"
+                @click="removeConditionSet">
+                <span class="c-c-button__label">Remove conditional styling</span>
+            </button>
+        </div>
+        <ul>
+            <li v-for="conditionStyle in conditionalStyles"
+                :key="conditionStyle.conditionIdentifier.key">
+                <conditional-style :condition-identifier="conditionStyle.conditionIdentifier"
+                                   :condition-style="conditionStyle.style"/>
+            </li>
+        </ul>
+    </div>
+</div>
 </template>
 
 <script>
 
+import ConditionalStyle from "./ConditionalStyle.vue";
 export default {
     components: {
+        ConditionalStyle
     },
     inject: [
-        'openmct'
-    ]
+        'openmct',
+        'domainObject',
+        'layoutItem'
+    ],
+    data() {
+        return {
+            conditionalStyles: []
+        }
+    },
+    mounted() {
+        if (this.layoutItem) {
+            //TODO: Handle layout items
+        }
+        if (this.domainObject.configuration) {
+            this.defautStyle = this.domainObject.configuration.defaultStyle;
+            if (this.domainObject.configuration.conditionalStyle) {
+                this.conditionalStyles = this.domainObject.configuration.conditionalStyle.styles || [];
+            }
+        }
+    },
+    methods: {
+        addConditionSet() {
+            //TODO: this.conditionSetIdentifier will be set by the UI before calling this
+            this.conditionSetIdentifier = {
+                namespace: '',
+                key: 'bb0f61ad-268d-4d3e-bb30-90ca4a2053c4'
+            };
+            this.initializeConditionalStyles();
+        },
+        removeConditionSet() {
+            this.conditionSetIdentifier = '';
+            this.conditionalStyles = [];
+            this.persist(undefined);
+        },
+        initializeConditionalStyles() {
+            const backgroundColors = [{backgroundColor: 'red'},{backgroundColor: 'orange'}, {backgroundColor: 'blue'}];
+            this.openmct.objects.get(this.conditionSetIdentifier).then((conditionSetDomainObject) => {
+                conditionSetDomainObject.configuration.conditionCollection.forEach((identifier, index) => {
+                    this.conditionalStyles.push({
+                        conditionIdentifier: identifier,
+                        style: backgroundColors[index]
+                    });
+                });
+                this.persist({
+                    defaultStyle: this.defaultStyle || {backgroundColor: 'inherit'},
+                    conditionSetIdentifier: this.conditionSetIdentifier,
+                    styles: this.conditionalStyles
+                });
+            });
+        },
+        findStyleByConditionId(id) {
+            for(let i=0, ii=this.conditionalStyles.length; i < ii; i++) {
+                if (this.openmct.objects.makeKeyString(this.conditionalStyles[i].conditionIdentifier) === this.openmct.objects.makeKeyString(id)) {
+                    return {
+                        index: i,
+                        item: this.conditionalStyles[i]
+                    };
+                }
+            }
+        },
+        updateConditionalStyle(conditionIdentifier, style) {
+            let found = this.findStyleByConditionId(conditionIdentifier);
+            if (found) {
+                this.conditionalStyles[found.index].style = style;
+            }
+            this.persist(undefined);
+        },
+        persist(conditionalStyle) {
+            this.openmct.objects.mutate(this.domainObject, 'configuration.conditionalStyle', conditionalStyle);
+        }
+    }
 }
 </script>
