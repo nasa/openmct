9ba4e7df535418e68b17ce976913f9b9546d0e81
David Tsay <3614296+davetsay@users.noreply.github.com>
2020-03-31 12:31:07 -0700

    remove no default condition output

--- src/plugins/condition/components/ConditionSet.vue
+++ src/plugins/condition/components/ConditionSet.vue
@@ -66,7 +66,7 @@ export default {
     data() {
         return {
             currentConditionOutput: '',
-            defaultConditionOutput: 'No output available',
+            defaultConditionOutput: '',
             telemetryObjs: [],
             testData: {}
         }
