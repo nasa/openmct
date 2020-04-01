2a1f9fd06324e0b193b5b3a22d564cbe261d41f1
Joel McKinnon <joel.g.mckinnon@nasa.gov>
2020-04-01 11:10:10 -0700

    removed recursive call

--- src/plugins/condition/components/Criterion.vue
+++ src/plugins/condition/components/Criterion.vue
@@ -172,7 +172,7 @@ export default {
                     }
                 }
             }
-            this.updateMetadataOptions();
+            // this.updateMetadataOptions();
             return inputCount;
         }
     },
