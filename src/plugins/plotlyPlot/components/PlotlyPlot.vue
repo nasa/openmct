8158afc29ad370e4c1bfdd2d84bd19811602ab59
Joel McKinnon <joel.g.mckinnon@nasa.gov>
2020-05-05 16:00:48 -0700

    added hardcoded test plot

--- src/plugins/plotlyPlot/components/PlotlyPlot.vue
+++ src/plugins/plotlyPlot/components/PlotlyPlot.vue
@@ -1,16 +1,25 @@
 <template>
 <div class="l-iframe abs">
-    render plot here
+    <div id="tester" style="width:600px;height:250px;"></div>
 </div>
 </template>
 
 <script>
+import Plotly from 'plotly.js-dist'
+
 export default {
     inject: ['openmct', 'domainObject'],
     data: function () {
         return {
             currentDomainObject: this.domainObject
         }
+    },
+    mounted() {
+        let TESTER = document.getElementById('tester');
+        Plotly.newPlot(TESTER, [{
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16] }], {
+            margin: {t: 0} });
     }
 }
 </script>
