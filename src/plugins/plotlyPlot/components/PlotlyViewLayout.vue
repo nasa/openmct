8db27809ef782cb9d59f577458018e2561bbcdaa
Joel McKinnon <joel.g.mckinnon@nasa.gov>
2020-05-07 13:00:54 -0700

    rendering a plotly plot from SWG

--- src/plugins/plotlyPlot/components/PlotlyViewLayout.vue
+++ src/plugins/plotlyPlot/components/PlotlyViewLayout.vue
@@ -4,6 +4,7 @@
 
 <script>
 import Plotly from 'plotly.js-dist';
+import moment from 'moment';
 
 export default {
     inject: ['openmct', 'domainObject', 'objectPath'],
@@ -11,25 +12,16 @@ export default {
 
         return {
             telemetryObjects: []
-            // currentDomainObject: this.domainObject
         }
     },
     mounted() {
-        // this.composition = this.openmct.composition.get(this.domainObject);
-        // this.composition.on('add', this.addTelemetry);
-        // this.composition.load();
-        // this.addTelemetryObject(this.domainObject);
-        this.metadata = this.openmct.telemetry.getMetadata(this.domainObject);
+        this.plotElement = document.querySelector('.l-view-section');
 
-        console.log('this.metadata', this.metadata);
+        this.composition = this.openmct.composition.get(this.domainObject);
+        this.composition.on('add', this.addTelemetry);
+        this.composition.load();
 
-        // this.keystring = this.openmct.objects.makeKeyString(this.domainObject.identifier);
-        // this.subscribe(this.domainObject);
-        let plot = document.querySelector('.l-view-section');
-        Plotly.newPlot(plot, [{
-            x: [1, 2, 3, 4, 5],
-            y: [1, 2, 4, 8, 16]
-        }], this.getLayout(), {displayModeBar: false});
+
     },
     methods: {
         getLayout() {
@@ -45,18 +37,18 @@ export default {
                 },
                 xaxis: {
                     // title: this.plotAxisTitle.xAxisTitle,
-                    zeroline: false,
-                    hoverformat: ".2r"
+                    title: 'UTC',
+                    zeroline: false
                 },
                 yaxis: {
                     // title: this.plotAxisTitle.yAxisTitle,
-                    zeroline: false,
-                    hoverformat: ".2r"
+                    title: 'Sine',
+                    zeroline: false
                 },
                 margin: {
-                    l: 20,
+                    l: 40,
                     r: 10,
-                    b: 20,
+                    b: 40,
                     t: 10
                 },
                 paper_bgcolor: 'transparent',
@@ -66,27 +58,45 @@ export default {
         addTelemetryObject(telemetryObject) {
             return this.openmct.telemetry.request(telemetryObject)
                 .then(telemetryData => {
-                    console.log('telemetryData', telemetryData);
+                    this.createPlot(telemetryData, telemetryObject);
                 });
+        },
+        formatDatumX(datum) {
+            let timestamp = moment.utc(datum.utc).format('YYYY-MM-DDTHH:mm:ss[Z]');
+            return timestamp;
+        },
+        formatDatumY(datum) {
+            return datum.sin;
+        },
+        createPlot(telemetryData, telemetryObject) {
+            let x = [],
+                y = [];
+
+            telemetryData.forEach((datum, index) => {
+                x.push(this.formatDatumX(datum));
+                y.push(this.formatDatumY(datum));
+            })
+
+            let data = [{
+                x,
+                y,
+                mode: 'line'
+            }];
+
+            Plotly.newPlot(
+                this.plotElement,
+                data,
+                this.getLayout(),
+                { displayModeBar: false }
+            )
+
+            this.subscribe(telemetryObject);
         },
         subscribe(domainObject) {
-            this.date = ''
-            this.openmct.objects.get(this.keystring)
-                .then((object) => {
-                    const metadata = this.openmct.telemetry.getMetadata(this.domainObject);
-                    console.log('metadata', metadata);
-                    // this.timeKey = this.openmct.time.timeSystem().key;
-                    // this.timeFormat = this.openmct.telemetry.getValueFormatter(metadata.value(this.timeKey));
-                    // // this.imageFormat = this.openmct.telemetry.getValueFormatter(metadata.valuesForHints(['image'])[0]);
-                    // this.unsubscribe = this.openmct.telemetry
-                    //     .subscribe(this.domainObject, (datum) => {
-                    //         this.updateHistory(datum);
-                    //         this.updateValues(datum);
-                    //     });
-
-                    // this.requestHistory(this.openmct.time.bounds());
-                });
-        },
+            this.openmct.telemetry.subscribe(domainObject, (datum) => {
+                this.updateData(datum)
+            })
+        }
     }
 }
 </script>
