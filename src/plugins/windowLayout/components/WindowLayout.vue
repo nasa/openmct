9f0af90663a8197ab325783cfb2a6a99d7e2f76a
Joshi <simplyrender@gmail.com>
2020-05-14 11:53:53 -0700

    WIP - Open 2 windows, close them and open the saved 2 windows

--- src/plugins/windowLayout/components/WindowLayout.vue
+++ src/plugins/windowLayout/components/WindowLayout.vue
@@ -1,17 +1,25 @@
 <template>
-<div class="c-indicator c-indicator--clickable icon-save s-status-caution">
+<div v-if="!isOpener"
+     class="c-indicator c-indicator--clickable icon-save s-status-caution"
+>
     <span class="label c-indicator__label">
-        <button @click="persistWindowInformation">Save Windows</button>
-        <button @click="openWindow">Open New Window</button>
+        <button @click="open2Windows">Open 2 Windows</button>
+        <button @click="closeAllWindows">Close Windows</button>
+        <button @click="openSavedWindows">Open Saved Windows</button>
     </span>
 </div>
 </template>
 <script>
 export default {
     inject: ['openmct'],
+    computed: {
+        isOpener() {
+            return window.opener;
+        }
+    },
     mounted() {
         this.openWindows = {};
-        this.windowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+        this.windowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes,innerHeight=480,innerWidth=640,screenY=100";
         window.addEventListener("message", this.receiveMessage, false);
         if (window.opener) {
             window.opener.postMessage({
@@ -25,12 +33,11 @@ export default {
                 },'*');
             });
             window.addEventListener('blur', () => {
-                console.log('blurred');
                 window.opener.postMessage({
                     query: 'QUERY__SIZE',
                     info: {
-                        outerHeight: window.outerHeight,
-                        outerWidth: window.outerWidth,
+                        innerHeight: window.innerHeight,
+                        innerWidth: window.innerWidth,
                         screenLeft: window.screenLeft,
                         screenTop: window.screenTop,
                         screenWidth: screen.width,
@@ -49,9 +56,47 @@ export default {
             window.localStorage.setItem('openmct-windowlayout-items',
                 JSON.stringify(this.getOpenWindowSpecifications()));
         },
-        openWindow() {
+        getScreenX() {
+            return !Object.keys(this.openWindows).length ? 100: (screen.width - (Object.keys(this.openWindows).length*640));
+        },
+        open2Windows() {
+            this.openWindow(`${this.windowFeatures},screenX=${this.getScreenX()}`);
+            this.openWindow(`${this.windowFeatures},screenX=${this.getScreenX()}`);
+        },
+        openWindow(windowFeatures) {
             let newWindowName = `Open MCT Window ${Object.keys(this.openWindows).length+1}`;
-            this.openWindows[newWindowName] = window.open(window.location.href, newWindowName, this.windowFeatures);
+            this.openWindows[newWindowName] = { windowReference: window.open(window.location.href, newWindowName, windowFeatures)};
+        },
+        moveWindow() {
+            const key = Object.keys(this.openWindows)[0];
+            this.openWindows[key].postMessage({
+                command: 'moveTo',
+                params: [window.screenLeft + 40, window.screenTop + 40]
+            }, 'http://localhost:8080');
+        },
+        openSavedWindows() {
+            const persistedWindowObjs = window.localStorage.getItem('openmct-windowlayout-items');
+            if (persistedWindowObjs) {
+                const windowObjs = JSON.parse(persistedWindowObjs);
+                windowObjs.forEach(windowObj => {
+                    let newWindowName = windowObj.name;
+                    const features = `menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes,innerHeight=${windowObj.info.innerHeight || 480},innerWidth=${windowObj.info.innerWidth || 640},screenX=${windowObj.info.screenLeft || this.getScreenX()},screenY=${windowObj.info.screenTop || 100}`;
+                    const windowReference = window.open(window.location.href, newWindowName, features);
+                    this.openWindows[newWindowName] = {
+                        windowReference,
+                        name: newWindowName,
+                        info: windowObj.info
+                    }
+                });
+            }
+        },
+        closeAllWindows() {
+            this.persistWindowInformation();
+            Object.keys(this.openWindows).forEach((windowName => {
+                const windowObj = this.openWindows[windowName];
+                windowObj.closedByOpener = true;
+                windowObj.windowReference.close();
+            }));
         },
         receiveMessage(event) {
             const { data, origin, source} = event;
@@ -59,32 +104,63 @@ export default {
             case 'http://localhost:8080':
                 if (data) {
                     if (data.status === 'READY') {
-                        let newWindowReference = this.openWindows[data.name];
-                        newWindowReference.postMessage('QUERY__SIZE', 'http://localhost:8080');
+                        let newWindowReference = this.openWindows[data.name].windowReference;
+                        newWindowReference.postMessage({
+                            command: 'QUERY__SIZE'
+                        }, 'http://localhost:8080');
                     } else if (data.status === 'CLOSED') {
+                        const closedByOpener = this.openWindows[data.name].closedByOpener;
                         this.openWindows[data.name] = undefined;
                         delete this.openWindows[data.name];
-                        this.persistWindowInformation();
-                    } else if (data === 'QUERY__SIZE') {
-                        source.postMessage({
-                            query: 'QUERY__SIZE',
-                            info: {
-                                outerHeight: window.outerHeight,
-                                outerWidth: window.outerWidth,
-                                screenLeft: window.screenLeft,
-                                screenTop: window.screenTop,
-                                screenWidth: screen.width,
-                                screenHeight: screen.height,
-                                screenAvailableWidth: screen.availWidth,
-                                screenAvailHeight: screen.availHeight,
-                                devicePixelRatio: window.devicePixelRatio
-                            },
-                            name: window.name
-                        }, origin);
-                    } else if (data.query === 'QUERY__SIZE') {
-                        this.openWindows[data.name].info = data.info;
-                        console.log(data.info);
-                        this.persistWindowInformation();
+                        if (!closedByOpener) {
+                            this.persistWindowInformation();
+                        }
+                    } else if (data.command) {
+                        switch(data.command) {
+                        case 'QUERY__SIZE':
+                            source.postMessage({
+                                query: data.command,
+                                info: {
+                                    innerHeight: window.innerHeight,
+                                    innerWidth: window.innerWidth,
+                                    screenLeft: window.screenLeft,
+                                    screenTop: window.screenTop,
+                                    screenWidth: screen.width,
+                                    screenHeight: screen.height,
+                                    screenAvailableWidth: screen.availWidth,
+                                    screenAvailHeight: screen.availHeight,
+                                    devicePixelRatio: window.devicePixelRatio
+                                },
+                                name: window.name
+                            }, origin);
+                            break;
+                        default:
+                            window[data.command](...data.params);
+                            this.$nextTick(() => {
+                                source.postMessage({
+                                    query: data.command,
+                                    name: window.name
+                                }, origin);
+                            });
+                            break;
+                        }
+                    } else if (data.query) {
+                        if (data.info) {
+                            if (!this.openWindows[data.name]) {
+                                return;
+                            }
+                            this.openWindows[data.name].info = data.info;
+                            console.log(data.info);
+                            this.persistWindowInformation();
+                        } else {
+                            if (!this.openWindows[data.name]) {
+                                return;
+                            }
+                            let newWindowReference = this.openWindows[data.name].windowReference;
+                            newWindowReference.postMessage({
+                                command: 'QUERY__SIZE'
+                            }, 'http://localhost:8080');
+                        }
                     }
                 }
 
