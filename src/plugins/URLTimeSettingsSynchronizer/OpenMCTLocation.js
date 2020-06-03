c0e7a13f78453f799989a50de3ea30fbb69f943a
Andrew Henry <akhenry@gmail.com>
2020-06-03 14:16:51 -0700

    Refactored to external class

--- src/plugins/URLTimeSettingsSynchronizer/OpenMCTLocation.js
+++ src/plugins/URLTimeSettingsSynchronizer/OpenMCTLocation.js
@@ -1,20 +1,18 @@
 import objectUtils from '../../api/objects/object-utils.js';
+import _ from 'lodash';
 
 export class OpenMCTLocation {
-    constructor(location) {
-        this.location = location;
+    constructor() {
+        this.synchronizeWithLocation = _.debounce(this.synchronizeWithLocation);
     }
     setSearchParam(paramName, paramValue) {
         this.scheduleSearchParamUpdate(paramName, paramValue);
-        this.scheduleSynchronizeWithLocation();
-    }
-    scheduleSearchParamUpdate(paramName, paramValue) {
-        this.pendingLocationUpdates.push((location) => {
-            location.searchParams.set(paramName, paramValue);
-        })
     }
     getSearchParam(paramName) {
         return this.getHashRelativeURL().searchParams.get(paramName);
+    }
+    getObjectPath() {
+        return this.getHashRelativeURL().pathname;
     }
     setObjectPath(objectPath) {
         let objectPathString;
@@ -28,12 +26,43 @@ export class OpenMCTLocation {
         }
 
         this.schedulePathUpdate(objectPathString);
-        this.scheduleSynchronizeWithLocation();
     }
-    getObjectPath() {
-        return this.getHashRelativeURL().pathname;
+
+    /**
+     * @private
+     * @param {String} paramName
+     * @param {String} paramValue
+     */
+    scheduleSearchParamUpdate(paramName, paramValue) {
+        this.pendingLocationUpdates.push((url) => {
+            url.searchParams.set(paramName, paramValue);
+        });
+        this.synchronizeWithLocation();
     }
+
+    /**
+     * @private
+     * @param {String} path
+     */
+    schedulePathUpdate(path) {
+        this.pendingLocationUpdates.push((url) => {
+            url.pathName = path;
+        });
+        this.synchronizeWithLocation();
+    }
+
+    synchronizeWithLocation() {
+        let url = this.getHashRelativeURL();
+        this.pendingLocationUpdates.forEach(
+            updateFunction => updateFunction(url)
+        );
+        this.location.hash = `${url.pathname}${url.search}`;
+    }
+
+    /**
+     * @private
+     */
     getHashRelativeURL() {
-        return new URL(this.location.hash.substring(1), this.location.origin);
+        return new URL(window.location.hash.substring(1), window.location.origin);
     }
 }
