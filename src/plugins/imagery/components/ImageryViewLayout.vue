24d19cedbfe012330b82ae6ed4c123e44e8e043e
Joshi <simplyrender@gmail.com>
2020-06-05 13:28:30 -0700

    Use domainObject configuration to identify if we should save layers

--- src/plugins/imagery/components/ImageryViewLayout.vue
+++ src/plugins/imagery/components/ImageryViewLayout.vue
@@ -101,7 +101,7 @@ export default {
         let layersMetadata = metaDataValues.layers;
         if (layersMetadata) {
             this.layers = layersMetadata;
-            if (this.domainObject.type === 'example.imagery' && layersMetadata.length) {
+            if (this.domainObject.configuration && layersMetadata.length) {
                 let persistedLayers = this.domainObject.configuration.layers;
                 layersMetadata.forEach((layer) => {
                     const persistedLayer = persistedLayers.find((object) =>{ return object.name === layer.name; });
@@ -128,7 +128,7 @@ export default {
         this.scrollToRight();
     },
     beforeDestroy() {
-        if (this.domainObject.type === 'example.imagery') {
+        if (this.domainObject.configuration) {
             this.openmct.objects.mutate(this.domainObject, 'configuration.layers', this.layers);
         }
         this.visibleLayers = [];
