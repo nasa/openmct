771fb9c04464480e3bacc6ed482b622b3f6fafeb
Deep Tailor <deep.j.tailor@nasa.gov>
2020-06-19 11:44:17 -0700

    [Display Layout] Allow multiple selection, duplication, and changing types (#3083)
    * enable multiple selection
    
    * enable object duplication
    
    * enable copy styles
    
    * enable converting plots and tables to alpha numerics
    
    * enable merging multiple alpha numerics
    
    * change icon for viewSwitcher
    
    * allow users to merge overlay plots into a stacked plot
    
    * New glyph for alphanumeric
    
    Co-authored-by: charlesh88 <charlesh88@gmail.com>

--- src/plugins/displayLayout/components/ImageView.vue
+++ src/plugins/displayLayout/components/ImageView.vue
@@ -95,6 +95,13 @@ export default {
             }
 
             this.context.index = newIndex;
+        },
+        item(newItem) {
+            if (!this.context) {
+                return;
+            }
+
+            this.context.layoutItem = newItem;
         }
     },
     mounted() {
