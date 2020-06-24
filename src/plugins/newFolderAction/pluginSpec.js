6f674930d9d92f86c88fe8b87913abacd3033cb1
Deep Tailor <deep.j.tailor@nasa.gov>
2020-06-23 18:34:19 -0700

    remove fdescribe

--- src/plugins/newFolderAction/pluginSpec.js
+++ src/plugins/newFolderAction/pluginSpec.js
@@ -24,7 +24,7 @@ import {
     resetApplicationState
 } from 'utils/testing';
 
-fdescribe("the plugin", () => {
+describe("the plugin", () => {
     let openmct,
         compositionAPI,
         newFolderAction,
