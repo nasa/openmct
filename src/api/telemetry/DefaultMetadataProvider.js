b94d0ca71a8666d80606ada787b52eb1a3fa1ed7
David Tsay <david.e.tsay@nasa.gov>
2020-07-08 16:39:18 -0700

    change copyright from 2014-2018 to 2014-2020

--- src/api/telemetry/DefaultMetadataProvider.js
+++ src/api/telemetry/DefaultMetadataProvider.js
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Open openmct, Copyright (c) 2014-2018, United States Government
+ * Open openmct, Copyright (c) 2014-2020, United States Government
  * as represented by the Administrator of the National Aeronautics and Space
  * Administration. All rights reserved.
  *
