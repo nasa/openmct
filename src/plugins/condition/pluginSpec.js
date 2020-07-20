64785633f9740bcd908b5104917700891eb4a8b5
Joshi <simplyrender@gmail.com>
2020-07-20 14:10:01 -0700

    Revert focused tests

--- src/plugins/condition/pluginSpec.js
+++ src/plugins/condition/pluginSpec.js
@@ -456,7 +456,7 @@ describe('the plugin', function () {
             };
         });
 
-        fit('should evaluate as stale when telemetry is not received in the allotted time', (done) => {
+        it('should evaluate as stale when telemetry is not received in the allotted time', (done) => {
 
             let conditionMgr = new ConditionManager(conditionSetDomainObject, openmct);
             conditionMgr.on('conditionSetResultUpdated', mockListener);
@@ -475,7 +475,7 @@ describe('the plugin', function () {
             }, 1500);
         });
 
-        fit('should not evaluate as stale when telemetry is received in the allotted time', (done) => {
+        it('should not evaluate as stale when telemetry is received in the allotted time', (done) => {
             const date = Date.now();
             conditionSetDomainObject.configuration.conditionCollection[0].configuration.criteria[0].input = ["2"];
             let conditionMgr = new ConditionManager(conditionSetDomainObject, openmct);
