44bfcf33ef488a984b75b4188473b6f2256fea49
Andrew Henry <akhenry@gmail.com>
2020-07-23 17:45:49 -0700

    Removed placeholder provider

--- src/plugins/persistence/couch/plugin.js
+++ src/plugins/persistence/couch/plugin.js
@@ -3,24 +3,6 @@ const NAMESPACE = '';
 
 export default function CouchPlugin(config) {
     return function install(openmct) {
-        //openmct.objects.addProvider('', getProvider());
         openmct.objects.addProvider(NAMESPACE, new CouchObjectProvider(openmct, config, NAMESPACE));
-    }
-
-    function getProvider() {
-        return {
-            get: function (identifier) {
-                console.log('GET!');
-                return Promise.resolve({identifier, name: "Test Folder", type: "folder"});
-            },
-            create: function (model) {
-                console.log('CREATE!');
-                return Promise.resolve(model);
-            },
-            update: function (model) {
-                console.log('UPDATE!');
-                return Promise.resolve(model);
-            }
-        };
     }
 }
