5bbd6e30063c418be9a85bf4170cc717d5b084e3
David Tsay <david.e.tsay@nasa.gov>
2020-09-04 16:01:03 -0700

    reset x-axis selection on bounds change

--- src/plugins/plot/src/telemetry/PlotController.js
+++ src/plugins/plot/src/telemetry/PlotController.js
@@ -90,7 +90,7 @@ define([
 
     PlotController.prototype.followTimeConductor = function () {
         this.listenTo(this.openmct.time, 'bounds', this.updateDisplayBounds, this);
-        this.listenTo(this.openmct.time, 'timeSystem', this.onTimeSystemChange, this);
+        this.listenTo(this.openmct.time, 'timeSystem', this.syncXAxisToTimeSystem, this);
         this.synchronized(true);
     };
 
@@ -177,7 +177,7 @@ define([
         return config;
     };
 
-    PlotController.prototype.onTimeSystemChange = function (timeSystem) {
+    PlotController.prototype.syncXAxisToTimeSystem = function (timeSystem) {
         this.config.xAxis.set('key', timeSystem.key);
         this.config.xAxis.emit('resetSeries');
     };
@@ -215,10 +215,20 @@ define([
      * Track latest display bounds.  Forces update when not receiving ticks.
      */
     PlotController.prototype.updateDisplayBounds = function (bounds, isTick) {
+<<<<<<< HEAD
+=======
+        const xAxisKey = this.config.xAxis.get('key');
+        const timeSystem = this.openmct.time.timeSystem();
+>>>>>>> 0afdf3c9c... reset x-axis selection on bounds change
         const newRange = {
             min: bounds.start,
             max: bounds.end
         };
+
+        if (xAxisKey !== timeSystem.key) {
+            this.syncXAxisToTimeSystem(timeSystem);
+        }
+
         this.config.xAxis.set('range', newRange);
         if (!isTick) {
             this.skipReloadOnInteraction = true;
