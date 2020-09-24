9dd3dd2b6b33a387910dabcc44bc454ee9446c23
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2020-09-23 18:00:42 -0700

    removing logging adding check for undefined child

--- platform/search/src/services/GenericSearchProvider.js
+++ platform/search/src/services/GenericSearchProvider.js
@@ -191,16 +191,17 @@ define([
 
         var domainObject = objectUtils.toNewFormat(model, id);
         var composition = this.openmct.composition.get(domainObject);
-        console.log('composition', composition);
+
         if (!composition) {
             return;
         }
 
         composition.load(domainObject)
             .then(function (children) {
-                console.log('children', children);
                 children.forEach(function (child) {
-                    provider.scheduleForIndexing(objectUtils.makeKeyString(child.identifier));
+                    if (child !== undefined) {
+                        provider.scheduleForIndexing(objectUtils.makeKeyString(child.identifier));
+                    }
                 });
             });
     };
