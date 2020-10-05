a180bf7c02022a8ce7e89aa69616c7ce7070e50e
Deep Tailor <deep.j.tailor@nasa.gov>
2020-10-05 11:16:34 -0700

    fix lint error

--- src/plugins/notebook/pluginSpec.js
+++ src/plugins/notebook/pluginSpec.js
@@ -20,7 +20,7 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-import { createOpenMct, createMouseEvent, resetApplicationState } from 'utils/testing';
+import { createOpenMct, resetApplicationState } from 'utils/testing';
 import NotebookPlugin from './plugin';
 import Vue from 'vue';
 
