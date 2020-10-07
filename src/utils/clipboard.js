b3d95e1b1322f73384a87fd22e892c3ca292aa90
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-10-07 07:30:04 -0700

    removed navigator.permissions.query check

--- src/utils/clipboard.js
+++ src/utils/clipboard.js
@@ -1,41 +1,10 @@
 class Clipboard {
-    constructor() {
-        this.clipboardRead = false;
-        this.clipboardWrite = false;
-
-        this.init();
-    }
-
-    init() {
-        navigator.permissions.query({ name: 'clipboard-write' })
-            .then(result => {
-                if (result.state === 'granted' || result.state === 'prompt') {
-                    this.clipboardWrite = true;
-                }
-            });
-
-        navigator.permissions.query({ name: 'clipboard-read' })
-            .then(result => {
-                if (result.state === 'granted' || result.state === 'prompt') {
-                    this.clipboardRead = true;
-                }
-            });
-    }
-
     updateClipboard(newClip) {
-        if (!this.clipboardWrite) {
-            return Promise.reject('Error: No permission to write clipboard');
-        }
-
         // return promise
         return navigator.clipboard.writeText(newClip);
     }
 
     readClipboard() {
-        if (!this.clipboardRead) {
-            return Promise.reject('Error: No permission to read clipboard');
-        }
-
         // return promise
         return navigator.clipboard.readText();
     }
