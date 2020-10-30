7cdafa9d02c5c0de39c8080a9f89a6db42ce1282
Andrew Henry <akhenry@gmail.com>
2020-10-30 16:04:54 -0700

    Added some documentation

--- src/api/objects/MutableDomainObject.js
+++ src/api/objects/MutableDomainObject.js
@@ -24,6 +24,15 @@ import utils from './object-utils.js';
 
 const ANY_OBJECT_EVENT = 'mutation';
 
+/**
+ * Wraps a domain object to keep its model synchronized with other instances of the same object.
+ * 
+ * Creating a MutableDomainObject will automatically register listeners to keep its model in sync. As such, developers
+ * should be careful to call $destroy on the MutableDomainObject in order to avoid memory leaks.
+ * All Open MCT API functions that provide objects will provide MutableDomainObjects where possible, and will manage 
+ * their lifecycle for you. Most of the time it is not necessary to create a MutableDomainObject directly, and if you do
+ * you will be responsible for its lifecycle.
+ */
 class MutableDomainObject {
     constructor(eventEmitter) {
         Object.defineProperties(this, {
--- src/api/objects/ObjectAPI.js
+++ src/api/objects/ObjectAPI.js
@@ -185,7 +185,13 @@ define([
 
     /**
      * Will fetch object, returning it as a MutableDomainObject IF the object is mutable.
-     * Requires object to be destroyed after use.
+     * Before using this function, you should ask yourself whether you really need it. The platform will always provide
+     * MutableDomainObjects from API functions if the underlying object can be mutated. The platform will manage the
+     * lifecycle of any `MutableDomainObjects` that it provides. If you use `getAsMutable` you are responsible for
+     * managing lifecycle yourself. `.$destroy` should be called when the object is no longer needed.
+     *
+     * @returns {Promise.<MutableDomainObject|DomainObject>} a promise that will resolve with a MutableDomainObject if 
+     * the object can be mutated, or a DomainObject if not.
      */
     ObjectAPI.prototype.getAsMutable = function (identifier) {
         return this.get(identifier).then((object) => {
