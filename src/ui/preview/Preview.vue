feb212cb406549473022e00fcbce1fa30fdefbc2
Deep Tailor <deep.j.tailor@nasa.gov>
2020-11-18 13:17:50 -0800

    fix lint issues

--- src/ui/preview/Preview.vue
+++ src/ui/preview/Preview.vue
@@ -79,7 +79,7 @@ export default {
 
         if (this.actionCollection) {
             this.actionCollection.destroy();
-        } 
+        }
     },
     destroyed() {
         this.view.destroy();
