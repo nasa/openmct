be80d737af395b3435a0b6397c8ca825098ed635
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2020-11-24 10:57:29 -0800

    removing comments

--- platform/entanglement/bundle.js
+++ platform/entanglement/bundle.js
@@ -21,7 +21,6 @@
  *****************************************************************************/
 
 define([
-    // "./src/actions/MoveAction",
     "./src/actions/LinkAction",
     "./src/actions/SetPrimaryLocationAction",
     "./src/services/LocatingCreationDecorator",
@@ -33,7 +32,6 @@ define([
     "./src/services/CopyService",
     "./src/services/LocationService"
 ], function (
-    // MoveAction,
     LinkAction,
     SetPrimaryLocationAction,
     LocatingCreationDecorator,
@@ -54,21 +52,6 @@ define([
             "configuration": {},
             "extensions": {
                 "actions": [
-                    // {
-                    //     "key": "move",
-                    //     "name": "Move",
-                    //     "description": "Move object to another location.",
-                    //     "cssClass": "icon-move",
-                    //     "category": "contextual",
-                    //     "group": "action",
-                    //     "priority": 9,
-                    //     "implementation": MoveAction,
-                    //     "depends": [
-                    //         "policyService",
-                    //         "locationService",
-                    //         "moveService"
-                    //     ]
-                    // },
                     {
                         "key": "link",
                         "name": "Create Link",
