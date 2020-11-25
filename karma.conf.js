6ede1a308dbe19977f5f82dc72e99abc08470ff6
Andrew Henry <akhenry@gmail.com>
2020-11-24 17:52:13 -0800

    Revert coverage threshold

--- karma.conf.js
+++ karma.conf.js
@@ -86,7 +86,7 @@ module.exports = (config) => {
             reports: ['html', 'lcovonly', 'text-summary'],
             thresholds: {
                 global: {
-                    lines: 65.8
+                    lines: 65
                 }
             }
         },
