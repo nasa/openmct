6e635d36de1aa3823d0e1099c2dd8be4d2726107
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-12-07 13:18:27 -0800

    added default value for imageMeta

--- src/utils/imageLargeView/imageLargeView.js
+++ src/utils/imageLargeView/imageLargeView.js
@@ -2,16 +2,14 @@ import Vue from 'vue';
 
 import ImageLargeView from './imageLargeView.html';
 
-export function openInImageLargeView(openmct, imageSrc, imageMeta) {
+export function openInImageLargeView(openmct, imageSrc, imageMeta = {}) {
     const DEFAULT_FILTERS = {
         brightness: 100,
         contrast: 100
     };
 
     const alt = imageMeta.alt || 'missing image';
-    const filters = imageMeta && imageMeta.filters
-        ? imageMeta.filters
-        : DEFAULT_FILTERS;
+    const filters = imageMeta.filters || DEFAULT_FILTERS;
     const style = `filter: brightness(${filters.brightness}%) contrast(${filters.contrast}%)`;
     const element = new Vue({
         data: () => {
