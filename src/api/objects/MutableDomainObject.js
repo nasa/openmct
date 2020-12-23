0a7782c750ec7b77380908b9f0fbc815ebc3ed4e
Andrew Henry <akhenry@gmail.com>
2020-12-23 10:09:20 -0800

    Fixed bug in deleted property detection

--- src/api/objects/MutableDomainObject.js
+++ src/api/objects/MutableDomainObject.js
@@ -114,7 +114,7 @@ class MutableDomainObject {
 
         mutable.$observe('$_synchronize_model', (updatedObject) => {
             let clone = JSON.parse(JSON.stringify(updatedObject));
-            let deleted = _.difference(Object.keys(updatedObject), Object.keys(updatedObject));
+            let deleted = _.difference(Object.keys(mutable), Object.keys(updatedObject));
             deleted.forEach((propertyName) => delete mutable[propertyName]);
             Object.assign(mutable, clone);
         });
