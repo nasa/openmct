18ac1eaedf977720f682fb23e8c124108b9326c8
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-12-24 14:42:58 -0800

    Inspector Font style menus

--- src/ui/inspector/styles/FontStyleEditor.vue
+++ src/ui/inspector/styles/FontStyleEditor.vue
@@ -1,31 +1,31 @@
 <template>
 <div class="c-toolbar">
-    <toolbar-select-menu
-        class="menus-to-left menus-no-icon"
-        :options="fontSizeMenuOptions"
-        @change="setFontSize"
-    />
-    <div class="c-toolbar__separator"></div>
-    <toolbar-select-menu
-        class="menus-to-left menus-no-icon"
-        :options="fontMenuOptions"
-        @change="setFont"
-    />
+    <div class="c-menu-button c-ctrl-wrapper c-ctrl-wrapper--menus-left">
+        <button
+            class="c-icon-button c-button--menu icon-font-size"
+            @click.prevent.stop="showFontSizeMenu"
+        >
+            <span class="c-button__label">{{ fontStyle.fontSize }}</span>
+        </button>
+    </div>
+    <div class="c-menu-button c-ctrl-wrapper c-ctrl-wrapper--menus-left">
+        <button
+            class="c-icon-button c-button--menu icon-font"
+            @click.prevent.stop="showFontMenu"
+        >
+            <span class="c-button__label">{{ fontStyle.font }}</span>
+        </button>
+    </div>
 </div>
 </template>
 
 <script>
-import ToolbarSelectMenu from '@/ui/toolbar/components/toolbar-select-menu.vue';
-
 import {
     FONT_SIZES,
     FONTS
 } from '@/ui/inspector/styles/constants';
 
 export default {
-    components: {
-        ToolbarSelectMenu
-    },
     inject: ['openmct'],
     props: {
         fontStyle: {
@@ -34,31 +34,39 @@ export default {
         }
     },
     computed: {
-        fontMenuOptions() {
-            return {
-                control: 'select-menu',
-                icon: "icon-font",
-                title: "Set font style",
-                value: this.fontStyle.font,
-                options: FONTS
-            };
+        fontMenu() {
+            return FONTS.map(font => {
+                return {
+                    cssClass: font.cssClass || '',
+                    name: font.name,
+                    description: font.name,
+                    callBack: () => this.setFont(font.value)
+                };
+            });
         },
-        fontSizeMenuOptions() {
-            return {
-                control: 'select-menu',
-                icon: "icon-font-size",
-                title: "Set font size",
-                value: this.fontStyle.fontSize,
-                options: FONT_SIZES
-            };
+        fontSizeMenu() {
+            return FONT_SIZES.map(fontSize => {
+                return {
+                    cssClass: fontSize.cssClass || '',
+                    name: fontSize.name,
+                    description: fontSize.name,
+                    callBack: () => this.setFontSize(fontSize.value)
+                };
+            });
         }
     },
     methods: {
         setFont(font) {
-            this.$emit('set-font-property', { font: font });
+            this.$emit('set-font-property', { font });
         },
         setFontSize(fontSize) {
-            this.$emit('set-font-property', { fontSize: fontSize });
+            this.$emit('set-font-property', { fontSize });
+        },
+        showFontMenu() {
+            this.openmct.menus.showMenu(event.x, event.y, this.fontMenu);
+        },
+        showFontSizeMenu() {
+            this.openmct.menus.showMenu(event.x, event.y, this.fontSizeMenu);
         }
     }
 };
