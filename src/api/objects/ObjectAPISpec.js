9e4655d308661679894e3d953002c8d070d03a03
David Tsay <david.e.tsay@nasa.gov>
2021-01-09 21:33:15 -0800

    add results timing tests

--- src/api/objects/ObjectAPISpec.js
+++ src/api/objects/ObjectAPISpec.js
@@ -130,20 +130,28 @@ fdescribe("The Object API", () => {
     });
 
     describe("the search function", () => {
+        const MOCK_PROVIDER_KEY = 'mockProvider';
+        const ANOTHER_MOCK_PROVIDER_KEY = 'anotherMockProvider';
+        const MOCK_PROVIDER_SEARCH_DELAY = 15000;
+        const ANOTHER_MOCK_PROVIDER_SEARCH_DELAY = 20000;
+        const TOTAL_TIME_ELAPSED = 21000;
+        const BASE_TIME = new Date(2021, 0, 1);
+
         let mockObjectProvider;
         let anotherMockObjectProvider;
         let mockFallbackProvider;
         let resultsGenerator;
         let fallbackProviderSearchResults;
-        let promises;
+        let resultsPromises;
 
         beforeEach(() => {
             jasmine.clock().install();
+            jasmine.clock().mockDate(BASE_TIME);
 
+            resultsPromises = [];
             fallbackProviderSearchResults = {
                 hits: []
             };
-            promises = [];
 
             mockObjectProvider = jasmine.createSpyObj("mock object provider", [
                 "search"
@@ -158,21 +166,50 @@ fdescribe("The Object API", () => {
             objectAPI.addProvider('other-objects', anotherMockObjectProvider);
             objectAPI.supersecretSetFallbackProvider(mockFallbackProvider);
 
-            mockObjectProvider.search.and.callFake(() => delayedResolve(1000));
-            anotherMockObjectProvider.search.and.callFake(() => delayedResolve(20000));
-            mockFallbackProvider.superSecretFallbackSearch.and.callFake(() => delayedResolve(50, fallbackProviderSearchResults));
+            mockObjectProvider.search.and.callFake(() => {
+                return new Promise(resolve => {
+                    const mockProviderSearch = {
+                        name: MOCK_PROVIDER_KEY,
+                        start: new Date()
+                    };
+
+                    setTimeout(() => {
+                        mockProviderSearch.end = new Date();
+
+                        return resolve(mockProviderSearch);
+                    }, MOCK_PROVIDER_SEARCH_DELAY);
+                });
+            });
+            anotherMockObjectProvider.search.and.callFake(() => {
+                return new Promise(resolve => {
+                    const anotherMockProviderSearch = {
+                        name: ANOTHER_MOCK_PROVIDER_KEY,
+                        start: new Date()
+                    };
+
+                    setTimeout(() => {
+                        anotherMockProviderSearch.end = new Date();
+
+                        return resolve(anotherMockProviderSearch);
+                    }, ANOTHER_MOCK_PROVIDER_SEARCH_DELAY);
+                });
+            });
+            mockFallbackProvider.superSecretFallbackSearch.and.callFake(
+                () => new Promise(
+                    resolve => setTimeout(
+                        () => resolve(fallbackProviderSearchResults),
+                        50
+                    )
+                )
+            );
 
             resultsGenerator = objectAPI.search('foo');
 
-            jasmine.clock().tick(30000);
-
-            for (const results of resultsGenerator) {
-                promises.push(results);
+            for (const resultsPromise of resultsGenerator) {
+                resultsPromises.push(resultsPromise);
             }
 
-            function delayedResolve(time, value) {
-                return new Promise(resolve => setTimeout(() => resolve(value), time));
-            }
+            jasmine.clock().tick(TOTAL_TIME_ELAPSED);
         });
 
         afterEach(() => {
@@ -188,8 +225,32 @@ fdescribe("The Object API", () => {
             expect(mockFallbackProvider.superSecretFallbackSearch).toHaveBeenCalled();
         });
 
-        it("provides a results generator so that consumers can process results from each provider immediately when available", () => {
+        it("provides results as a generator so that consumers can process each providers results when returned", () => {
+            expect(resultsGenerator.next).toBeDefined();
+            expect(resultsGenerator.next().done).toBeTrue();
+        });
 
+        it("provides each providers results as promises that resolve in parallel", async () => {
+            const results = await Promise.all(resultsPromises);
+            const mockProviderResults = results.find(
+                result => result.name === MOCK_PROVIDER_KEY
+            );
+            const anotherMockProviderResults = results.find(
+                result => result.name === ANOTHER_MOCK_PROVIDER_KEY
+            );
+            const mockProviderStart = mockProviderResults.start.getTime();
+            const mockProviderEnd = mockProviderResults.end.getTime();
+            const anotherMockProviderStart = anotherMockProviderResults.start.getTime();
+            const anotherMockProviderEnd = anotherMockProviderResults.end.getTime();
+            const searchElapsedTime = Math.max(mockProviderEnd, anotherMockProviderEnd)
+                - Math.min(mockProviderEnd, anotherMockProviderEnd);
+
+            expect(mockProviderStart).toBeLessThan(anotherMockProviderEnd);
+            expect(anotherMockProviderStart).toBeLessThan(mockProviderEnd);
+            expect(searchElapsedTime).toBeLessThan(
+                MOCK_PROVIDER_SEARCH_DELAY
+                + ANOTHER_MOCK_PROVIDER_SEARCH_DELAY
+            );
         });
     });
 });
