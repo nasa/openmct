1499286bee36c8ca308ae112e3c2737e686a518c
Shefali Joshi <simplyrender@gmail.com>
2021-02-19 06:48:17 -0800

    Plots refactor to use Vue and es6 instead of angular (#3586)
    * Initial commit of plot refactor for vuejs
    
    * Use es6 classes instead of using extend
    
    * Use classList api to add and remove classes
    
    * Remove angular specific event mechanisms
    
    * Refactor plot legend into smaller components
    
    * Refactor moving config into MctPlot component. Fix Legend issues.
    
    * Refactor XAxis and YAxis into their own components
    
    * Remove commented out code
    
    * Remove empty initialize method
    
    * Fix grid lines and initialize function revert.
    
    * Check that plots views are available only to domainObjects that have range and domain
    
    * Make css class a computed property
    
    * Remove unnecessary legacyObject conversion
    
    * Remove comments and commented out code
    
    * Remove use of private for vue methods
    
    * Remove console logs
    
    * Fixes Y-axis ticks display
    
    * Adds stacked plots and overlay plots
    
    * Fix css for stacked plots
    
    * Disable Vue plots
    
    * Rename Stacked plot item component
    
    * Address Review comment: Remove unnecessary event emitted
    
    * Address review comments: Add a note about why nextTick is needed
    
    * Fix bug with legend when multiple plots are being displayed
    
    * Change LinearScale to a class
    
    * Remove duplicated comment
    
    * Adds missing copyright info
    
    * Revert change to stackedplotItem
    
    * Adds basic tests
    
    * Fixes broken test.
    
    * Adds new test
    
    * Fix linting errors. Adds tests
    
    * Adds tests
    
    * Adds tests for stacked plots
    
    * Adds more tests
    
    * Removes fdescribe
    
    * Adds tests for y-axis ticks
    
    * Tests for addition of series to plots
    
    * Adds more tests
    
    * Adds cursor guides test
    
    * Adds tests for interceptors
    
    * Adds more plots tests for x and y scale
    
    * Use config store
    
    * Adding goToOriginalAction tests
    
    * Modified tests to increase coverage, and added teardown for application router
    
    * Fixed linting issues
    
    Co-authored-by: Andrew Henry <akhenry@gmail.com>

---
+++ src/plugins/plot/vue/single/lib/ColorPalette.js
@@ -0,0 +1,101 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2020, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+/**
+     * A color palette stores a set of colors and allows for different
+     * methods of color allocation.
+     *
+     * @constructor
+     */
+import { isDefaultColor, COLOR_PALETTE } from './ColorHelper';
+import Color from './Color';
+
+/**
+ * A color palette stores a set of colors and allows for different
+ * methods of color allocation.
+ *
+ * @constructor
+ */
+function ColorPalette() {
+    const allColors = this.allColors = COLOR_PALETTE.map(function (color) {
+        return new Color(color);
+    });
+    this.colorGroups = [[], [], []];
+    for (let i = 0; i < allColors.length; i++) {
+        this.colorGroups[i % 3].push(allColors[i]);
+    }
+
+    this.reset();
+}
+
+/**
+ *
+ */
+ColorPalette.prototype.groups = function () {
+    return this.colorGroups;
+};
+
+ColorPalette.prototype.reset = function () {
+    this.availableColors = this.allColors.slice();
+};
+
+ColorPalette.prototype.remove = function (color) {
+    this.availableColors = this.availableColors.filter(function (c) {
+        return !c.equalTo(color);
+    });
+};
+
+ColorPalette.prototype.return = function (color) {
+    if (isDefaultColor(color)) {
+        this.availableColors.unshift(color);
+    }
+};
+
+ColorPalette.prototype.getByHexString = function (hexString) {
+    const color = Color.fromHexString(hexString);
+
+    return color;
+};
+
+/**
+ * @returns {Color} the next unused color in the palette.  If all colors
+ * have been allocated, it will wrap around.
+ */
+ColorPalette.prototype.getNextColor = function () {
+    if (!this.availableColors.length) {
+        console.warn('Color Palette empty, reusing colors!');
+        this.reset();
+    }
+
+    return this.availableColors.shift();
+};
+
+/**
+ * @param {number} index the index of the color to return.  An index
+ * value larger than the size of the index will wrap around.
+ * @returns {Color}
+ */
+ColorPalette.prototype.getColor = function (index) {
+    return this.colors[index % this.colors.length];
+};
+
+export default ColorPalette;
