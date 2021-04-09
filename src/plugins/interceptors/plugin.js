c86b2ce9407951015cc03594fc610b6cb5336b3d
Mandlik, Nikhil K. (ARC-TI)[KBR Wyle Services, LLC] <nikhil.k.mandlik@nasa.gov>
2021-04-09 13:14:54 -0700

    added Interceptor to migrate notebook images.

--- src/plugins/interceptors/plugin.js
+++ src/plugins/interceptors/plugin.js
@@ -1,9 +1,11 @@
 import missingObjectInterceptor from "./missingObjectInterceptor";
 import myItemsInterceptor from "./myItemsInterceptor";
+import notebookInterceptor from './notebookInterceptor';
 
 export default function plugin() {
     return function install(openmct) {
         myItemsInterceptor(openmct);
         missingObjectInterceptor(openmct);
+        notebookInterceptor(openmct);
     };
 }
--- src/plugins/notebook/utils/notebook-migration.js
+++ src/plugins/notebook/utils/notebook-migration.js
@@ -1,35 +1,43 @@
 import { createNotebookImageDomainObject, getThumbnailURLFromimageUrl } from './notebook-image';
 import { mutateObject } from './notebook-entries';
 
-export function notebookImageMigration(openmct, identifier) {
-    openmct.objects.get(identifier)
-        .then(domainObject => {
-            const configuration = domainObject.configuration;
-            const notebookEntries = configuration.entries;
+export async function notebookImageMigration(openmct, identifier, object) {
+    const domainObject = object || await openmct.objects.get(identifier);
+    const configuration = domainObject.configuration;
+    const notebookEntries = configuration.entries;
 
-            configuration.sections.forEach(section => {
-                const sectionId = section.id;
-                section.pages.forEach(page => {
-                    const pageId = page.id;
-                    const pageEntries = notebookEntries[sectionId][pageId] || [];
-                    pageEntries.forEach(entry => {
-                        entry.embeds.forEach(async (embed) => {
-                            const snapshot = embed.snapshot;
-                            const fullSizeImageURL = snapshot.src;
-                            if (fullSizeImageURL) {
-                                const thumbnailImageURL = await getThumbnailURLFromimageUrl(fullSizeImageURL);
-                                const object = await createNotebookImageDomainObject(openmct, fullSizeImageURL, thumbnailImageURL);
+    const imageMigrationVer = configuration.imageMigrationVer;
+    if (imageMigrationVer && imageMigrationVer === 'v1') {
+        return;
+    }
 
-                                embed.snapshot = {
-                                    fullSizeImageObjectIdentifier: object.identifier,
-                                    thumbnailImage: { src: thumbnailImageURL || '' }
-                                };
+    configuration.imageMigrationVer = 'v1';
 
-                                mutateObject(openmct, domainObject, 'configuration.entries', notebookEntries);
-                            }
-                        });
-                    });
+    // to avoid muliple notebookImageMigration calls updating images.
+    mutateObject(openmct, domainObject, 'configuration', configuration);
+
+    configuration.sections.forEach(section => {
+        const sectionId = section.id;
+        section.pages.forEach(page => {
+            const pageId = page.id;
+            const pageEntries = notebookEntries[sectionId][pageId] || [];
+            pageEntries.forEach(entry => {
+                entry.embeds.forEach(async (embed) => {
+                    const snapshot = embed.snapshot;
+                    const fullSizeImageURL = snapshot.src;
+                    if (fullSizeImageURL) {
+                        const thumbnailImageURL = await getThumbnailURLFromimageUrl(fullSizeImageURL);
+                        const object = await createNotebookImageDomainObject(openmct, fullSizeImageURL, thumbnailImageURL);
+
+                        embed.snapshot = {
+                            fullSizeImageObjectIdentifier: object.identifier,
+                            thumbnailImage: { src: thumbnailImageURL || '' }
+                        };
+
+                        mutateObject(openmct, domainObject, 'configuration.entries', notebookEntries);
+                    }
                 });
             });
         });
+    });
 }
