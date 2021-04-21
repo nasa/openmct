1015972ce0c2fb75656b3daad70922dece678fb6
David Tsay <david.e.tsay@nasa.gov>
2021-04-21 12:25:37 -0700

    test spec needs attention

--- src/plugins/autoflow/AutoflowTabularPluginSpec.js
+++ src/plugins/autoflow/AutoflowTabularPluginSpec.js
@@ -28,8 +28,10 @@ import {
     resetApplicationStatePromise,
     spyOnBuiltins
 } from 'utils/testing';
+import Vue from 'vue';
 
-describe("AutoflowTabularPlugin", () => {
+// TODO lots of its without expects
+xdescribe("AutoflowTabularPlugin", () => {
     let testType;
     let testObject;
     let mockmct;
@@ -101,7 +103,7 @@ describe("AutoflowTabularPlugin", () => {
                 });
             }
 
-            beforeEach((done) => {
+            beforeEach(() => {
                 callbacks = {};
 
                 spyOnBuiltins(['requestAnimationFrame']);
@@ -180,7 +182,7 @@ describe("AutoflowTabularPlugin", () => {
                 view = provider.view(testObject);
                 view.show(testContainer);
 
-                return done();
+                return Vue.nextTick();
             });
 
             afterEach(() => {
