f490323e7686f9c946322cb6a5aee8bb1d2b6a29
charlesh88 <charles.f.hacskaylo@nasa.gov>
2021-05-06 12:05:06 -0700

    Stub in static HTML for Timestrip Activity Inspection
    - Added static markup with placeholder values and display logic;

--- src/plugins/timeline/inspector/PlanProperties.vue
+++ src/plugins/timeline/inspector/PlanProperties.vue
@@ -20,8 +20,115 @@
  at runtime from the About dialog for additional information.
 -->
 <template>
-<div>
-    {{ name }}
+<div class="c-inspector__properties c-inspect-properties">
+    <template v-if="false">
+        <!-- If only one Activity is selected -->
+        <div class="c-inspect-properties__header">
+            Time
+        </div>
+        <ul class="c-inspect-properties__section">
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    Start Datetime
+                </div>
+                <div class="c-inspect-properties__value">
+                    2023-12-15 07:19:34.995Z
+                </div>
+            </li>
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    End Datetime
+                </div>
+                <div class="c-inspect-properties__value">
+                    2023-12-15 08:19:34.995Z
+                </div>
+            </li>
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    Duration
+                </div>
+                <div class="c-inspect-properties__value">
+                    01:00:00
+                </div>
+            </li>
+        </ul>
+    </template>
+
+    <template v-if="true">
+        <!-- If two or more Activities are selected -->
+        <div class="c-inspect-properties__header">
+            Convex Hull
+        </div>
+        <ul class="c-inspect-properties__section">
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    Earliest Start
+                </div>
+                <div class="c-inspect-properties__value">
+                    2023-12-15 08:19:34.995Z
+                </div>
+            </li>
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    Latest End
+                </div>
+                <div class="c-inspect-properties__value">
+                    2023-12-15 08:19:34.995Z
+                </div>
+            </li>
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    {{ (true) ? 'Gap' : 'Overlap' }}
+                </div>
+                <div class="c-inspect-properties__value">
+                    00:04:30
+                </div>
+            </li>
+            <li class="c-inspect-properties__row">
+                <div class="c-inspect-properties__label">
+                    Total Time
+                </div>
+                <div class="c-inspect-properties__value">
+                    02:04:30
+                </div>
+            </li>
+        </ul>
+    </template>
+
+    <template v-if="true">
+        <!-- If one or more properties exist for selected activities -->
+        <div class="c-inspect-properties__header">
+            Properties
+        </div>
+        <ul class="c-inspect-properties__section">
+            <li class="c-inspect-properties__row"
+                v-for="n  in 10" :key="n"
+            >
+                <div class="c-inspect-properties__label">
+                    Property {{n}} Name
+                </div>
+                <div class="c-inspect-properties__value">
+                    Property Value
+                </div>
+            </li>
+        </ul>
+    </template>
+
+    <template v-if="true">
+        <!-- If one or more links exist for selected activities -->
+        <div class="c-inspect-properties__header">
+            Links
+        </div>
+        <ul class="c-inspect-properties__section">
+            <li class="c-inspect-properties__row"
+                v-for="n  in 3" :key="n"
+            >
+                <div class="c-inspect-properties__value --span-all">
+                    Link {{n}}
+                </div>
+            </li>
+        </ul>
+    </template>
 </div>
 </template>
 
