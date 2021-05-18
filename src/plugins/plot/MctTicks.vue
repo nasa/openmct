7d590c64a448c5950b41f8b85a01c3c4e69426b3
Joshi <simplyrender@gmail.com>
2021-05-18 13:06:21 -0700

    Null check

--- src/plugins/plot/MctTicks.vue
+++ src/plugins/plot/MctTicks.vue
@@ -229,14 +229,15 @@ export default {
         doTickUpdate() {
             if (this.shouldCheckWidth) {
                 const tickElements = this.$refs.tickContainer && this.$refs.tickContainer.querySelectorAll('.gl-plot-tick > span');
+                if (tickElements) {
+                    const tickWidth = Number([].reduce.call(tickElements, function (memo, first) {
+                        return Math.max(memo, first.offsetWidth);
+                    }, 0));
 
-                const tickWidth = Number([].reduce.call(tickElements, function (memo, first) {
-                    return Math.max(memo, first.offsetWidth);
-                }, 0));
-
-                this.tickWidth = tickWidth;
-                this.$emit('plotTickWidth', tickWidth);
-                this.shouldCheckWidth = false;
+                    this.tickWidth = tickWidth;
+                    this.$emit('plotTickWidth', tickWidth);
+                    this.shouldCheckWidth = false;
+                }
             }
 
             this.tickUpdate = false;
