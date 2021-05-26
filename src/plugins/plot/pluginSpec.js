53232a1c70f004988a78cdf6c62adddd7bd3bd32
Joshi <simplyrender@gmail.com>
2021-05-26 10:39:04 -0700

    Fix failing test

--- src/plugins/plot/pluginSpec.js
+++ src/plugins/plot/pluginSpec.js
@@ -99,6 +99,11 @@ describe("the plugin", function () {
         element.appendChild(child);
         document.body.appendChild(element);
 
+        spyOn(window, 'ResizeObserver').and.returnValue({
+            observe() {},
+            disconnect() {}
+        });
+
         openmct.time.timeSystem("utc", {
             start: 0,
             end: 4
