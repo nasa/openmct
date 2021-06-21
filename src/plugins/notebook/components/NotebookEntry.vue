90f3b68a7423fdc03e8203098b6ec452cafcb2e9
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2021-06-21 14:26:23 -0700

    aded todo for remove notebook snapshot object using object remove API

--- src/plugins/notebook/components/NotebookEntry.vue
+++ src/plugins/notebook/components/NotebookEntry.vue
@@ -253,6 +253,7 @@ export default {
         },
         removeEmbed(id) {
             const embedPosition = this.findPositionInArray(this.entry.embeds, id);
+            // TODO: remove notebook snapshot object using object remove API
             this.entry.embeds.splice(embedPosition, 1);
 
             this.$emit('updateEntry', this.entry);
