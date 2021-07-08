3dd505a0f6e6331323bf037483846977b17eb636
charlesh88 <charles.f.hacskaylo@nasa.gov>
2021-07-08 15:43:12 -0700

    Refactor imagery compass rose as SVG - Completely new SVG-based markup for compass rose; - Associated CSS classes; - VERY WIP!

--- src/plugins/imagery/components/Compass/CompassRose.vue
+++ src/plugins/imagery/components/Compass/CompassRose.vue
@@ -21,79 +21,87 @@
  *****************************************************************************/
 
 <template>
-<div
+<div ref="compassRoseWrapper"
     class="w-direction-rose"
     :class="compassRoseSizingClasses"
+    @click="toggleLockCompass"
 >
-    <div ref="directionRose"
-         class="c-direction-rose"
-         @click="toggleLockCompass"
+    <svg ref="compassRoseSvg"
+         class="c-compass-rose-svg"
+         viewBox="0 0 100 100"
     >
-        <div
-            class="c-nsew"
-            :style="compassRoseStyle"
-        >
-        </div>
+        <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="100" height="100">
+            <circle cx="50" cy="50" r="50" fill="black"/>
+        </mask>
+        <g class="c-cr__compass-wrapper">
+            <g class="c-cr__compass-main" mask="url(#mask0)">
+                <rect class="c-cr__bg" width="100" height="100" fill="black"/>
+                <rect class="c-cr__edge" width="100" height="100" fill="url(#paint0_radial)"/>
+                <rect class="c-cr__sun" width="100" height="100" fill="url(#paint1_radial)"/>
 
-        <div
-            v-if="hasHeading"
-            class="c-spacecraft-body"
-            :style="headingStyle"
-        >
-        </div>
+                <!-- Cam FOV -->
+                <mask class="c-cr__cam-fov-l-mask" id="mask2" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="50" height="100">
+                    <rect width="51" height="100"/>
+                </mask>
+                <mask class="c-cr__cam-fov-r-mask" id="mask1" mask-type="alpha" maskUnits="userSpaceOnUse" x="50" y="0" width="50" height="100">
+                    <rect x="49" width="51" height="100"/>
+                </mask>
+                <g class="c-cr__cam-fov">
+                    <g mask="url(#mask1)">
+                        <rect class="c-cr__cam-fov-l" x="49" width="51" height="100"
+                            style="transform: rotate(-110deg)"
+                        />
+                    </g>
+                    <g mask="url(#mask2)">
+                        <rect class="c-cr__cam-fov-r" width="51" height="100"
+                              style="transform: rotate(110deg)"
+                        />
+                    </g>
+                </g>
+            </g>
 
-        <div
-            v-if="hasSunHeading"
-            class="c-sun"
-            :style="sunHeadingStyle"
-        ></div>
+            <!-- Spacecraft body -->
+            <path class="c-cr__spacecraft-body"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M37 49C35.3431 49 34 50.3431 34 52V82C34 83.6569 35.3431 85 37 85H63C64.6569 85 66 83.6569 66 82V52C66 50.3431 64.6569 49 63 49H37ZM50 52L58 60H55V67H45V60H42L50 52Z"
+            />
 
-        <div ref="camField"
-             class="c-cam-field"
-             :style="cameraPanStyle"
-        >
-            <svg ref="camFieldSVG"
-                 viewBox="0 0 100 100"
+            <!-- NSEW and ticks -->
+            <g class="c-cr__nsew"
+                style="transform: rotate(30deg)"
             >
-                <rect
-                    class="c-nsew__tick c-tick-ne"
-                    x="49"
-                    y="0"
-                    fill="red"
-                    width="2"
-                    height="5"
-                />
-                <rect
-                    class="c-nsew__tick c-tick-se"
-                    x="95"
-                    y="49"
-                    width="5"
-                    height="2"
-                />
-                <rect
-                    class="c-nsew__tick c-tick-sw"
-                    x="49"
-                    y="95"
-                    width="2"
-                    height="5"
-                />
-                <rect
-                    class="c-nsew__tick c-tick-nw"
-                    x="0"
-                    y="49"
-                    width="5"
-                    height="2"
-                />
-                <path fill="gray"
-                      d="M50, 50
-                        L30,8
-                        A40 40 0 0 1 70,8
-                        Z
-                        "
-                />
-            </svg>
-        </div>
-    </div>
+                <g class="c-cr__ticks-major">
+                    <path d="M50 3L43 10H57L50 3Z"/>
+                    <path d="M4 51V49H10V51H4Z" class="--hide-min"/>
+                    <path d="M49 96V90H51V96H49Z" class="--hide-min"/>
+                    <path d="M90 49V51H96V49H90Z" class="--hide-min"/>
+                </g>
+                <g class="c-cr__ticks-minor --hide-small">
+                    <path d="M4 51V49H10V51H4Z"/>
+                    <path d="M90 51V49H96V51H90Z"/>
+                    <path d="M51 96H49V90H51V96Z"/>
+                    <path d="M51 10L49 10V4L51 4V10Z"/>
+                </g>
+                <g class="c-cr__nsew-text">
+                    <path d="M25.7418 45.004H23.1378L21.7238 52.312H21.6958L20.2258 45.004H17.7758L16.3058 52.312H16.2778L14.8638 45.004H12.2598L14.9618 55H17.6078L18.9798 48.112H19.0078L20.3798 55H22.9838L25.7418 45.004Z"/>
+                    <path d="M79.104 55H87.21V52.76H81.708V50.856H86.608V48.84H81.708V47.09H87.07V45.004H79.104V55Z"/>
+                    <path d="M45.6531 83.64C45.6671 86.202 47.6971 87.21 49.9931 87.21C52.1911 87.21 54.3471 86.398 54.3471 83.864C54.3471 82.058 52.8911 81.386 51.4491 80.98C49.9931 80.574 48.5511 80.434 48.5511 79.664C48.5511 79.006 49.2511 78.81 49.8111 78.81C50.6091 78.81 51.4631 79.104 51.4211 80.014H54.0251C54.0111 77.76 52.0091 76.794 50.0211 76.794C48.1451 76.794 45.9471 77.648 45.9471 79.832C45.9471 81.666 47.4451 82.31 48.8731 82.716C50.3151 83.122 51.7431 83.29 51.7431 84.172C51.7431 84.914 50.9311 85.194 50.1471 85.194C49.0411 85.194 48.3131 84.816 48.2571 83.64H45.6531Z"/>
+                    <path d="M42.5935 31H46.7935V20.32H46.8415L52.7935 31H57.3775V13.864H53.1775V24.424H53.1295L47.1775 13.864H42.5935V31Z"/>
+                </g>
+            </g>
+        </g>
+        <defs>
+            <radialGradient id="paint0_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(50 50) rotate(90) scale(50)">
+                <stop offset="0.751387" stop-opacity="0"/>
+                <stop offset="1" stop-color="white"/>
+            </radialGradient>
+            <radialGradient id="paint1_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(50 -7) rotate(-90) scale(18.5)">
+                <stop offset="0.716377" stop-color="#FFCC00"/>
+                <stop offset="1" stop-color="#FF9900" stop-opacity="0"/>
+            </radialGradient>
+        </defs>
+    </svg>
 </div>
 </template>
 
@@ -229,12 +237,9 @@ export default {
     },
     methods: {
         resizeSvg() {
-            const svg = this.$refs.camFieldSVG;
-            svg.setAttribute('width', this.$refs.camField.clientWidth);
-            svg.setAttribute('height', this.$refs.camField.clientHeight);
-        },
-        rotateSVG() {
-            this.$refs.camField.style.transform = "rotate(45deg)";
+            const svg = this.$refs.compassRoseSvg;
+            svg.setAttribute('width', this.$refs.compassRoseWrapper.clientWidth);
+            svg.setAttribute('height', this.$refs.compassRoseWrapper.clientHeight);
         },
         toggleLockCompass() {
             this.lockCompass = !this.lockCompass;
