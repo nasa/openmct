edfeeadc223c0f075f6c0944bfff45b3e94d28f2
charlesh88 <charles.f.hacskaylo@nasa.gov>
2021-07-08 17:44:33 -0700

    Refactor imagery compass rose as SVG - Significant progress on SVG markup; - Wired up elements for data-driven transforms; - New approach to NSEW text for html2canvas compatibility; - WIP!

--- src/plugins/imagery/components/Compass/CompassRose.vue
+++ src/plugins/imagery/components/Compass/CompassRose.vue
@@ -35,26 +35,32 @@
         </mask>
         <g class="c-cr__compass-wrapper">
             <g class="c-cr__compass-main" mask="url(#mask0)">
+                <!-- Background and clipped elements -->
                 <rect class="c-cr__bg" width="100" height="100" fill="black"/>
                 <rect class="c-cr__edge" width="100" height="100" fill="url(#paint0_radial)"/>
-                <rect class="c-cr__sun" width="100" height="100" fill="url(#paint1_radial)"/>
+                <rect class="c-cr__sun" width="100" height="100" fill="url(#paint1_radial)"
+                      v-if="hasSunHeading"
+                      :style="sunHeadingStyle"
+                />
 
-                <!-- Cam FOV -->
+                <!-- Camera FOV -->
                 <mask class="c-cr__cam-fov-l-mask" id="mask2" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="50" height="100">
                     <rect width="51" height="100"/>
                 </mask>
                 <mask class="c-cr__cam-fov-r-mask" id="mask1" mask-type="alpha" maskUnits="userSpaceOnUse" x="50" y="0" width="50" height="100">
                     <rect x="49" width="51" height="100"/>
                 </mask>
-                <g class="c-cr__cam-fov">
-                    <g mask="url(#mask1)">
-                        <rect class="c-cr__cam-fov-l" x="49" width="51" height="100"
-                            style="transform: rotate(-110deg)"
+                <g class="c-cr__cam-fov"
+                    :style="cameraPanStyle"
+                >
+                    <g mask="url(#mask2)">
+                        <rect class="c-cr__cam-fov-r" x="49" width="51" height="100"
+                              :style="cameraFOVStyleRightHalf"
                         />
                     </g>
-                    <g mask="url(#mask2)">
-                        <rect class="c-cr__cam-fov-r" width="51" height="100"
-                              style="transform: rotate(110deg)"
+                    <g mask="url(#mask1)">
+                        <rect class="c-cr__cam-fov-l" width="51" height="100"
+                              :style="cameraFOVStyleLeftHalf"
                         />
                     </g>
                 </g>
@@ -65,11 +71,13 @@
                   fill-rule="evenodd"
                   clip-rule="evenodd"
                   d="M37 49C35.3431 49 34 50.3431 34 52V82C34 83.6569 35.3431 85 37 85H63C64.6569 85 66 83.6569 66 82V52C66 50.3431 64.6569 49 63 49H37ZM50 52L58 60H55V67H45V60H42L50 52Z"
+                  v-if="hasHeading"
+                  :style="headingStyle"
             />
 
             <!-- NSEW and ticks -->
             <g class="c-cr__nsew"
-                style="transform: rotate(30deg)"
+               :style="compassRoseStyle"
             >
                 <g class="c-cr__ticks-major">
                     <path d="M50 3L43 10H57L50 3Z"/>
@@ -84,10 +92,10 @@
                     <path d="M51 10L49 10V4L51 4V10Z"/>
                 </g>
                 <g class="c-cr__nsew-text">
-                    <path d="M25.7418 45.004H23.1378L21.7238 52.312H21.6958L20.2258 45.004H17.7758L16.3058 52.312H16.2778L14.8638 45.004H12.2598L14.9618 55H17.6078L18.9798 48.112H19.0078L20.3798 55H22.9838L25.7418 45.004Z"/>
-                    <path d="M79.104 55H87.21V52.76H81.708V50.856H86.608V48.84H81.708V47.09H87.07V45.004H79.104V55Z"/>
-                    <path d="M45.6531 83.64C45.6671 86.202 47.6971 87.21 49.9931 87.21C52.1911 87.21 54.3471 86.398 54.3471 83.864C54.3471 82.058 52.8911 81.386 51.4491 80.98C49.9931 80.574 48.5511 80.434 48.5511 79.664C48.5511 79.006 49.2511 78.81 49.8111 78.81C50.6091 78.81 51.4631 79.104 51.4211 80.014H54.0251C54.0111 77.76 52.0091 76.794 50.0211 76.794C48.1451 76.794 45.9471 77.648 45.9471 79.832C45.9471 81.666 47.4451 82.31 48.8731 82.716C50.3151 83.122 51.7431 83.29 51.7431 84.172C51.7431 84.914 50.9311 85.194 50.1471 85.194C49.0411 85.194 48.3131 84.816 48.2571 83.64H45.6531Z"/>
-                    <path d="M42.5935 31H46.7935V20.32H46.8415L52.7935 31H57.3775V13.864H53.1775V24.424H53.1295L47.1775 13.864H42.5935V31Z"/>
+                    <path class="c-cr__nsew-w" :style="cardinalTextRotateW" d="M56.7418 45.004H54.1378L52.7238 52.312H52.6958L51.2258 45.004H48.7758L47.3058 52.312H47.2778L45.8638 45.004H43.2598L45.9618 55H48.6078L49.9798 48.112H50.0078L51.3798 55H53.9838L56.7418 45.004Z"/>
+                    <path class="c-cr__nsew-e" :style="cardinalTextRotateE" d="M46.104 55H54.21V52.76H48.708V50.856H53.608V48.84H48.708V47.09H54.07V45.004H46.104V55Z"/>
+                    <path class="c-cr__nsew-s" :style="cardinalTextRotateS" d="M45.6531 51.64C45.6671 54.202 47.6971 55.21 49.9931 55.21C52.1911 55.21 54.3471 54.398 54.3471 51.864C54.3471 50.058 52.8911 49.386 51.4491 48.98C49.9931 48.574 48.5511 48.434 48.5511 47.664C48.5511 47.006 49.2511 46.81 49.8111 46.81C50.6091 46.81 51.4631 47.104 51.4211 48.014H54.0251C54.0111 45.76 52.0091 44.794 50.0211 44.794C48.1451 44.794 45.9471 45.648 45.9471 47.832C45.9471 49.666 47.4451 50.31 48.8731 50.716C50.3151 51.122 51.7431 51.29 51.7431 52.172C51.7431 52.914 50.9311 53.194 50.1471 53.194C49.0411 53.194 48.3131 52.816 48.2571 51.64H45.6531Z"/>
+                    <path class="c-cr__nsew-n" :style="cardinalTextRotateN" d="M42.5935 60H46.7935V49.32H46.8415L52.7935 60H57.3775V42.864H53.1775V53.424H53.1295L47.1775 42.864H42.5935V60Z"/>
                 </g>
             </g>
         </g>
@@ -154,32 +162,17 @@ export default {
         north() {
             return this.lockCompass ? rotate(-this.cameraPan) : 0;
         },
-        northTextTransform() {
-            return this.cardinalPointsTextTransform.north;
+        cardinalTextRotateN() {
+            return { transform: `translateY(-27%) rotate(${ -this.north }deg)` };
         },
-        eastTextTransform() {
-            return this.cardinalPointsTextTransform.east;
+        cardinalTextRotateS() {
+            return { transform: `translateY(30%) rotate(${ -this.north }deg)` };
         },
-        southTextTransform() {
-            return this.cardinalPointsTextTransform.south;
+        cardinalTextRotateE() {
+            return { transform: `translateX(30%) rotate(${ -this.north }deg)` };
         },
-        westTextTransform() {
-            return this.cardinalPointsTextTransform.west;
-        },
-        cardinalPointsTextTransform() {
-            /**
-             * cardinal points text must be rotated
-             * in the opposite direction that north is rotated
-             * to keep text vertically oriented
-             */
-            const rotation = `rotate(${ -this.north })`;
-
-            return {
-                north: `translate(50,23) ${ rotation }`,
-                east: `translate(82,50) ${ rotation }`,
-                south: `translate(18,50) ${ rotation }`,
-                west: `translate(50,82) ${ rotation }`
-            };
+        cardinalTextRotateW() {
+            return { transform: `translateX(-30%) rotate(${ -this.north }deg)` };
         },
         hasHeading() {
             return this.heading !== undefined;
@@ -188,7 +181,7 @@ export default {
             const rotation = rotate(this.north, this.heading);
 
             return {
-                transform: `translateX(-50%) rotate(${ rotation }deg)`
+                transform: `rotate(${ rotation }deg)`
             };
         },
         hasSunHeading() {
@@ -212,14 +205,14 @@ export default {
         // rotated counter-clockwise from camera pan angle
         cameraFOVStyleLeftHalf() {
             return {
-                transform: `translateX(50%) rotate(${ -this.cameraAngleOfView / 2 }deg)`
+                transform: `rotate(${ this.cameraAngleOfView / 2 }deg)`
             };
         },
         // right half of camera field of view
         // rotated clockwise from camera pan angle
         cameraFOVStyleRightHalf() {
             return {
-                transform: `translateX(-50%) rotate(${ this.cameraAngleOfView / 2 }deg)`
+                transform: `rotate(${ -this.cameraAngleOfView / 2 }deg)`
             };
         }
     },
@@ -242,6 +235,7 @@ export default {
             svg.setAttribute('height', this.$refs.compassRoseWrapper.clientHeight);
         },
         toggleLockCompass() {
+            console.log(this);
             this.lockCompass = !this.lockCompass;
         }
     }
