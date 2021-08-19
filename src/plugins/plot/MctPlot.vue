e6a71d85ac57ed5415cf69d5dc770af2090003f7
David Tsay <david.e.tsay@nasa.gov>
2021-08-19 10:11:46 -0700

    code cleanup

--- src/plugins/plot/MctPlot.vue
+++ src/plugins/plot/MctPlot.vue
@@ -555,7 +555,9 @@ export default {
                 }
             }
 
-            this.$emit('plotTickWidth', this.tickWidth);
+            const id = this.openmct.objects.makeKeyString(this.domainObject.identifier);
+
+            this.$emit('plotTickWidth', this.tickWidth, id);
         },
 
         trackMousePosition(event) {
