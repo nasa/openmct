5db7fbbf2c6f11815b26f60ff6fbe9a054e8bf23
charlesh88 <charles.f.hacskaylo@nasa.gov>
2021-08-26 10:37:05 -0700

    Add new ellipse drawing object to Display Layouts - Fix test and linting issues;

--- src/plugins/displayLayout/components/EllipseView.vue
+++ src/plugins/displayLayout/components/EllipseView.vue
@@ -22,101 +22,101 @@
 
 <template>
 <layout-frame
-        :item="item"
-        :grid-size="gridSize"
-        :is-editing="isEditing"
-        @move="(gridDelta) => $emit('move', gridDelta)"
-        @endMove="() => $emit('endMove')"
+    :item="item"
+    :grid-size="gridSize"
+    :is-editing="isEditing"
+    @move="(gridDelta) => $emit('move', gridDelta)"
+    @endMove="() => $emit('endMove')"
 >
     <div
-            class="c-ellipse-view u-style-receiver js-style-receiver"
-            :class="[styleClass]"
-            :style="style"
+        class="c-ellipse-view u-style-receiver js-style-receiver"
+        :class="[styleClass]"
+        :style="style"
     ></div>
 </layout-frame>
 </template>
 
 <script>
-    import LayoutFrame from './LayoutFrame.vue';
-    import conditionalStylesMixin from '../mixins/objectStyles-mixin';
+import LayoutFrame from './LayoutFrame.vue';
+import conditionalStylesMixin from '../mixins/objectStyles-mixin';
 
-    export default {
-        makeDefinition() {
-            return {
-                fill: '#666666',
-                stroke: '',
-                x: 1,
-                y: 1,
-                width: 10,
-                height: 10
-            };
+export default {
+    makeDefinition() {
+        return {
+            fill: '#666666',
+            stroke: '',
+            x: 1,
+            y: 1,
+            width: 10,
+            height: 10
+        };
+    },
+    components: {
+        LayoutFrame
+    },
+    mixins: [conditionalStylesMixin],
+    inject: ['openmct'],
+    props: {
+        item: {
+            type: Object,
+            required: true
         },
-        components: {
-            LayoutFrame
-        },
-        mixins: [conditionalStylesMixin],
-        inject: ['openmct'],
-        props: {
-            item: {
-                type: Object,
-                required: true
-            },
-            gridSize: {
-                type: Array,
-                required: true,
-                validator: (arr) => arr && arr.length === 2
+        gridSize: {
+            type: Array,
+            required: true,
+            validator: (arr) => arr && arr.length === 2
                     && arr.every(el => typeof el === 'number')
-            },
-            index: {
-                type: Number,
-                required: true
-            },
-            initSelect: Boolean,
-            isEditing: {
-                type: Boolean,
-                required: true
-            }
         },
-        computed: {
-            style() {
-                if (this.itemStyle) {
-                    return this.itemStyle;
-                } else {
-                    return {
-                        backgroundColor: this.item.fill,
-                        border: this.item.stroke ? '1px solid ' + this.item.stroke : ''
-                    };
-                }
-            }
+        index: {
+            type: Number,
+            required: true
         },
-        watch: {
-            index(newIndex) {
-                if (!this.context) {
-                    return;
-                }
-
-                this.context.index = newIndex;
-            },
-            item(newItem) {
-                if (!this.context) {
-                    return;
-                }
-
-                this.context.layoutItem = newItem;
-            }
-        },
-        mounted() {
-            this.context = {
-                layoutItem: this.item,
-                index: this.index
-            };
-            this.removeSelectable = this.openmct.selection.selectable(
-                this.$el, this.context, this.initSelect);
-        },
-        destroyed() {
-            if (this.removeSelectable) {
-                this.removeSelectable();
+        initSelect: Boolean,
+        isEditing: {
+            type: Boolean,
+            required: true
+        }
+    },
+    computed: {
+        style() {
+            if (this.itemStyle) {
+                return this.itemStyle;
+            } else {
+                return {
+                    backgroundColor: this.item.fill,
+                    border: this.item.stroke ? '1px solid ' + this.item.stroke : ''
+                };
             }
         }
-    };
+    },
+    watch: {
+        index(newIndex) {
+            if (!this.context) {
+                return;
+            }
+
+            this.context.index = newIndex;
+        },
+        item(newItem) {
+            if (!this.context) {
+                return;
+            }
+
+            this.context.layoutItem = newItem;
+        }
+    },
+    mounted() {
+        this.context = {
+            layoutItem: this.item,
+            index: this.index
+        };
+        this.removeSelectable = this.openmct.selection.selectable(
+            this.$el, this.context, this.initSelect);
+    },
+    destroyed() {
+        if (this.removeSelectable) {
+            this.removeSelectable();
+        }
+    }
+};
 </script>
--- src/plugins/displayLayout/pluginSpec.js
+++ src/plugins/displayLayout/pluginSpec.js
@@ -351,7 +351,7 @@ describe('the plugin', function () {
         it('provides controls including separators', () => {
             const displayLayoutToolbar = openmct.toolbars.get(selection);
 
-            expect(displayLayoutToolbar.length).toBe(9);
+            expect(displayLayoutToolbar.length).toBe(7);
         });
     });
 });
