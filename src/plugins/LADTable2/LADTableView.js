fc97c6b274bc9a19c482c14c8c38188491033c3a
Henry Hsu <henry.hsu@nasa.gov>
2021-09-07 11:07:13 -0700

    remove ladtable.vue

--- src/plugins/LADTable2/LADTableView.js
+++ src/plugins/LADTable2/LADTableView.js
@@ -1,5 +1,4 @@
 import TableView from '/src/plugins/telemetryTable/components/table.vue';
-// import TableView from './LADTable.vue';
 import LADTable from './LADTable.js';
 import Vue from 'vue';
 export default class LADTableView {
