44cc135867d6b7dde5a4359029242cc5bf25acec
David Tsay <david.e.tsay@nasa.gov>
2021-09-15 11:54:47 -0700

    add unit test skeleton code

---
+++ src/ui/inspector/InspectorDetailsSpec.js
@@ -0,0 +1,60 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2021, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+import {
+    createOpenMct,
+    resetApplicationState
+} from 'utils/testing';
+
+describe('the inspector', () => {
+    let openmct;
+
+    beforeEach((done) => {
+        openmct = createOpenMct();
+        openmct.on('start', done);
+        openmct.startHeadless();
+    });
+
+    afterEach(() => {
+        return resetApplicationState();
+    });
+
+    it('displays default details for selection', () => {
+
+    });
+
+    it('details show created date', () => {
+
+    });
+
+    it('details show modified date', () => {
+
+    });
+
+    it('displays custom details if provided through context', () => {
+
+    });
+
+    it('does not display details for multi selection', () => {
+
+    });
+});
