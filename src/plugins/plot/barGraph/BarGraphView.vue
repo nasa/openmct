dcb4f503e0ed9d707b215a0cb23e6cfbcbfe3391
Scott Bell <scott@traclabs.com>
2021-09-16 19:46:02 +0200

    fix labels

--- src/plugins/plot/barGraph/BarGraphView.vue
+++ src/plugins/plot/barGraph/BarGraphView.vue
@@ -217,7 +217,6 @@ export default {
                 name: telemetryObject.name,
                 x: xValues,
                 y: yValues,
-                text: yValues.map(String),
                 xAxisMetadata: axisMetadata.xAxisMetadata,
                 yAxisMetadata: axisMetadata.yAxisMetadata,
                 type: 'bar',
