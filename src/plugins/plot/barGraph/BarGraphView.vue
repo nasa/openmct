8243cf5d7be0d882c2facf5c8f152f5a464e8019
Shefali Joshi <simplyrender@gmail.com>
2021-09-27 14:25:33 -0700

    Fix missing object handling in several vues (#4259)
    * If there is a pending create request for an id, queue a duplicate request.
    * Fix downsteam errors when objects are missing
    * Changed error logging from console.log to console.warn

--- src/plugins/plot/barGraph/BarGraphView.vue
+++ src/plugins/plot/barGraph/BarGraphView.vue
@@ -150,6 +150,10 @@ export default {
         },
         getAxisMetadata(telemetryObject) {
             const metadata = this.openmct.telemetry.getMetadata(telemetryObject);
+            if (!metadata) {
+                return {};
+            }
+
             const yAxisMetadata = metadata.valuesForHints(['range'])[0];
             //Exclude 'name' and 'time' based metadata specifically, from the x-Axis values by using range hints only
             const xAxisMetadata = metadata.valuesForHints(['range']);
@@ -255,6 +259,9 @@ export default {
                     data.forEach((datum) => {
                         this.processData(telemetryObject, datum, axisMetadata);
                     });
+                })
+                .catch((error) => {
+                    console.warn(`Error fetching data`, error);
                 });
         },
         subscribeToObject(telemetryObject) {
