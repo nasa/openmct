f243ac9b772bc8a16a0704f4427950c62c31cc20
depperm <epper.marshall@gmail.com>
2021-10-31 07:00:47 -0400

    fixing lint

--- example/simpleVuePlugin/plugin.js
+++ example/simpleVuePlugin/plugin.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import HelloWorld from './HelloWorld.vue';
- import { TypeKeyConstants } from '@/plugins/PluginConstants.js'
+import { TypeKeyConstants } from '@/plugins/PluginConstants.js';
 
 function SimpleVuePlugin() {
     return function install(openmct) {
