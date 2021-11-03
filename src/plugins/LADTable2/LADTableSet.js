dabde04839d05ec79ea5f8a24ff2acc9c1b489af
David Tsay <david.e.tsay@nasa.gov>
2021-11-02 21:16:24 -0700

    tidy up lad table set
    pass lad table set down to view

--- src/plugins/LADTable2/LADTableSet.js
+++ src/plugins/LADTable2/LADTableSet.js
@@ -8,55 +8,43 @@ export default class LADTableSet extends EventEmitter {
         super();
         this.domainObject = domainObject;
         this.openmct = openmct;
-        this.updateFilters = this.updateFilters.bind(this);
         this.tables = {};
         this.headers = {};
 
-        this.composition = this.openmct.composition.get(this.domainObject);
+        this.composition = undefined;
         this.telemetryObjects = {};
-        //where do i remove events below?
-        this.composition.on('add', this.addLADTable.bind(this));
-        this.composition.on('remove', this.removeLADTable.bind(this));
     }
-    initialize() {
-        if (this.domainObject.type === 'LadTableSet') {
-            this.filterObserver = this.openmct.objects.observe(this.domainObject, 'configuration.filters', this.updateFilters);
-            this.filters = this.domainObject.configuration.filters;
-            this.loadComposition();
-        } else {
-            this.addLADTable(this.domainObject);
-        }
-    }
-    loadComposition() {
-        this.tableSetComposition = this.openmct.composition.get(this.domainObject);
 
-        if (this.tableSetComposition !== undefined) {
-            this.tableSetComposition.load().then((composition) => {
+    initialize() {
+        this.loadComposition();
+    }
+
+    loadComposition() {
+        this.composition = this.openmct.composition.get(this.domainObject);
+
+        if (this.composition !== undefined) {
+            this.composition.load().then((composition) => {
                 composition.forEach(this.addLADTable.bind(this));
+
+                this.composition.on('add', this.addLADTable);
+                this.composition.on('remove', this.removeLADTable);
+
+                this.emit('loaded');
             });
         }
     }
-    updateFilters(updatedFilters) {
-        let deepCopiedFilters = JSON.parse(JSON.stringify(updatedFilters));
 
-        if (this.filters && !_.isEqual(this.filters, deepCopiedFilters)) {
-            this.filters = deepCopiedFilters;
-            this.tableRows.clear();
-            this.clearAndResubscribe();
-        } else {
-            this.filters = deepCopiedFilters;
-        }
-    }
     addLADTable(domainObject) {
-        let key = domainObject.identifier.key;
-        this.tables[key] = new LADTable(domainObject, this.openmct);
+        const keyString = this.openmct.objects.makeKeyString(domainObject.identifier);
+        this.tables[keyString] = new LADTable(domainObject, this.openmct);
 
-        this.tables[key].once('loaded', () => {
-            this.addHeaders(this.tables[key]);
-            this.addTelemetryObjects(this.tables[key]);
+        this.tables[keyString].once('loaded', () => {
+            this.addHeaders(this.tables[keyString]);
+            this.addTelemetryObjects(this.tables[keyString]);
         });
-        this.tables[key].initialize();
-        this.emit('table-added', this.tables[key]);
+
+        this.tables[keyString].initialize();
+        this.emit('table-added', this.tables[keyString]);
     }
     removeLADTable(identifier) {
         let key = identifier.key;
