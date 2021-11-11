87c7f323c6c82d9ab1ddbeb3b3d7c2832ee1d535
unlikelyzero <jchill2@gmail.com>
2021-11-11 08:58:24 -0800

    Update tests and fix eslint

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -3,13 +3,21 @@
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+    testDir: 'tests',
+    webServer: {
+        command: 'npm run start',
+        port: 8080,
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI,
+    },
     use: {
         browserName: "chromium",
         baseURL: 'http://localhost:8080/',
         headless: true,
         ignoreHTTPSErrors: true,
         screenshot: 'on',
-        trace: 'on'
+        trace: 'on',
+        video: 'on'
     },
     reporter: [
         ['list'],
