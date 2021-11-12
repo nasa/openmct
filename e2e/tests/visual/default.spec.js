93049cdbc6c047697ca204893db9603b864b8c9f
unlikelyzero <jchill2@gmail.com>
2021-11-12 14:52:50 -0800

    added percy

---
+++ e2e/tests/visual/default.spec.js
@@ -0,0 +1,53 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2021, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+/* 
+
+*/
+
+const { test, expect } = require('@playwright/test');
+const percySnapshot = require('@percy/playwright');
+const path = require('path');
+
+test.beforeEach(async ({ context }) => {
+    // Install Sinon in all the pages in the context
+    await context.addInitScript({
+      path: path.join(__dirname, '..', './node_modules/sinon/pkg/sinon.js'),
+    });
+    // Auto-enable sinon right away
+    await context.addInitScript(() => {
+      window.__clock = sinon.useFakeTimers();
+    });
+  });
+  
+
+test('Visual - Dashboard', async ({ page }) => {
+
+    //Go to base URL
+    await page.goto('/', { waitUntil: 'networkidle' });
+
+    // Verify that Create button is actionable
+    const locator = page.locator('button:has-text("Create")');
+    await expect(locator).toBeEnabled();
+
+    await percySnapshot(page, 'Dashboard');
+});
