1fbfc7d0d7b561c6f2bb3e90ee9395ccb665f150
Joshi <simplyrender@gmail.com>
2021-12-10 15:06:37 -0800

    New view for plot underlays

---
+++ src/plugins/plot/UnderlayPlot.vue
@@ -0,0 +1,71 @@
+<!--
+ Open MCT, Copyright (c) 2014-2021, United States Government
+ as represented by the Administrator of the National Aeronautics and Space
+ Administration. All rights reserved.
+
+ Open MCT is licensed under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0.
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ License for the specific language governing permissions and limitations
+ under the License.
+
+ Open MCT includes source code licensed under additional open source
+ licenses. See the Open Source Licenses file (LICENSES.md) included with
+ this source code distribution or the Licensing information page available
+ at runtime from the About dialog for additional information.
+-->
+<template>
+<div ref="underlayWrapper">
+    <div ref="underlay"></div>
+</div>
+</template>
+
+<script>
+import Plotly from 'plotly-basic';
+
+export default {
+    inject: ['openmct'],
+    props: {
+        data: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    },
+    data() {
+        return {};
+    },
+    watch: {
+        data: {
+            immediate: false,
+            handler: 'updatePlot'
+        }
+    },
+    mounted() {
+        Plotly.newPlot(this.$refs.underlay, Array.from(this.data), this.getLayout(), {
+            responsive: true,
+            displayModeBar: false
+        });
+    },
+    methods: {
+        getLayout() {
+            return {
+                autosize: true
+            };
+        },
+        updatePlot() {
+            if (!this.$refs || !this.$refs.underlay) {
+                return;
+            }
+
+            Plotly.react(this.$refs.underlay, Array.from(this.data), this.getLayout());
+        }
+    }
+};
+</script>
