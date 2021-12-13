bba29b083fb65b185fc34e0dfb0df448e5c161b2
Shefali Joshi <simplyrender@gmail.com>
2021-12-13 11:19:54 -0800

    Merge 1.8.1 into master (#4562)
    * Transaction fix (#4421) (#4461)
    * When transaction is active, objects.get should search in dirty object first.
    * Bugfix/create tree node (#4472)
    * Transaction fix (#4421)
    * When transaction is active, objects.get should search in dirty object first.
    * find insert location prior to adding item to tree
    * no need to resort
    * add item should only add to direct descendants
    * remove unused function
    * copy composition before sorting
    * remove unused var
    * remove master pollution
    * Revert "remove master pollution"
    * add item to correct location
    * Changed descending to ascending in sort order method (#4480)
    * adding RAF to display layout alphanumerics (#4486)
    * [Tabs] Sizing of offscreen tabs causing issues (#4444)
    * [LAD Tables] Use RAF for updating template (#4500)
    * Fixes LAD rows for string telemetry (#4508)
    * Fixes LAD rows for string telemetry
    * saving the object if it was missing (#4471)
    * 4328 - Maintain reference to a focusedImage if the bounds change (#4545)
    * WIP: adding assertions to catch negative index state
    * just testing the flow
    * SUpdate the image history index to previous selected image
    * Cleaning up spacing and log statements
    * Converted focusedImageIndex assignment to ternary and general cleanup
    * imported objects are not persisting  (#4477)
    * imported objects are not persisting #4470
    * disabled karma spec reporter suppressErrorSummary
    * update version number
    * Delete importFromJsonAction directory since it was empty

--- src/ui/layout/mct-tree.vue
+++ src/ui/layout/mct-tree.vue
@@ -113,7 +113,6 @@ import search from '../components/search.vue';
 
 const ITEM_BUFFER = 25;
 const LOCAL_STORAGE_KEY__TREE_EXPANDED = 'mct-tree-expanded';
-const RETURN_ALL_DESCENDANTS = true;
 const SORT_MY_ITEMS_ALPH_ASC = true;
 const TREE_ITEM_INDENT_PX = 18;
 
@@ -432,7 +431,7 @@ export default {
 
             return scrollTopAmount >= treeStart && scrollTopAmount < treeEnd;
         },
-        sortNameDescending(a, b) {
+        sortNameAscending(a, b) {
             // sorting tree children items
             if (!(a.name && b.name)) {
                 if (a.object.name.toLowerCase()
@@ -459,15 +458,16 @@ export default {
 
             return 0;
         },
-
+        isSortable(parentObjectPath) {
+            // determine if any part of the parent's path includes a key value of mine; aka My Items
+            return Boolean(parentObjectPath.find(path => path.identifier.key === 'mine'));
+        },
         async loadAndBuildTreeItemsFor(domainObject, parentObjectPath, abortSignal) {
             let collection = this.openmct.composition.get(domainObject);
             let composition = await collection.load(abortSignal);
-            // determine if any part of the parent's path includes a key value of mine; aka My Items
-            const isNestedInMyItems = Boolean(parentObjectPath.find(path => path.identifier.key === 'mine'));
 
-            if (SORT_MY_ITEMS_ALPH_ASC && isNestedInMyItems) {
-                const sortedComposition = composition.sort(this.sortNameDescending);
+            if (SORT_MY_ITEMS_ALPH_ASC && this.isSortable(parentObjectPath)) {
+                const sortedComposition = composition.slice().sort(this.sortNameAscending);
                 composition = sortedComposition;
             }
 
@@ -513,17 +513,35 @@ export default {
         },
         compositionAddHandler(navigationPath) {
             return (domainObject) => {
-                let parentItem = this.getTreeItemByPath(navigationPath);
-                let newItem = this.buildTreeItem(domainObject, parentItem.objectPath, true);
-                let allDescendants = this.getChildrenInTreeFor(parentItem, RETURN_ALL_DESCENDANTS);
-                let afterItem = allDescendants.length ? allDescendants.pop() : parentItem;
+                const parentItem = this.getTreeItemByPath(navigationPath);
+                const newItem = this.buildTreeItem(domainObject, parentItem.objectPath, true);
+                const descendants = this.getChildrenInTreeFor(parentItem, true);
+                const directDescendants = this.getChildrenInTreeFor(parentItem);
 
-                this.addItemToTreeAfter(newItem, afterItem);
-                const isNestedInMyItems = Boolean(parentItem.objectPath && parentItem.objectPath.find(path => path.identifier.key === 'mine'));
+                if (directDescendants.length === 0) {
+                    this.addItemToTreeAfter(newItem, parentItem);
 
-                if (SORT_MY_ITEMS_ALPH_ASC && isNestedInMyItems) {
-                    this.sortTreeComposition(this.sortNameDescending, navigationPath);
+                    return;
                 }
+
+                if (SORT_MY_ITEMS_ALPH_ASC && this.isSortable(parentItem.objectPath)) {
+                    const newItemIndex = directDescendants
+                        .findIndex(descendant => this.sortNameAscending(descendant, newItem) > 0);
+                    const shouldInsertFirst = newItemIndex === 0;
+                    const shouldInsertLast = newItemIndex === -1;
+
+                    if (shouldInsertFirst) {
+                        this.addItemToTreeAfter(newItem, parentItem);
+                    } else if (shouldInsertLast) {
+                        this.addItemToTreeAfter(newItem, descendants.pop());
+                    } else {
+                        this.addItemToTreeBefore(newItem, directDescendants[newItemIndex]);
+                    }
+
+                    return;
+                }
+
+                this.addItemToTreeAfter(newItem, descendants.pop());
             };
         },
         compositionRemoveHandler(navigationPath) {
@@ -555,17 +573,18 @@ export default {
             const removeIndex = this.getTreeItemIndex(item.navigationPath);
             this.treeItems.splice(removeIndex, 1);
         },
-        sortTreeComposition(algorithem, parentPath) {
-            const parentIndex = this.getTreeItemIndex(parentPath);
-            const parentItem = this.treeItems[parentIndex];
+        addItemToTreeBefore(addItem, beforeItem) {
+            const addIndex = this.getTreeItemIndex(beforeItem.navigationPath);
 
-            const allDescendants = this.getChildrenInTreeFor(parentItem);
-            const sortedChildren = allDescendants.sort(algorithem);
-            this.treeItems.splice(parentIndex + 1, allDescendants.length, ...sortedChildren);
+            this.addItemToTree(addItem, addIndex);
         },
         addItemToTreeAfter(addItem, afterItem) {
             const addIndex = this.getTreeItemIndex(afterItem.navigationPath);
-            this.treeItems.splice(addIndex + 1, 0, addItem);
+
+            this.addItemToTree(addItem, addIndex + 1);
+        },
+        addItemToTree(addItem, index) {
+            this.treeItems.splice(index, 0, addItem);
 
             if (this.isTreeItemOpen(addItem)) {
                 this.openTreeItem(addItem);
