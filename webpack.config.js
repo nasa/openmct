29bf456bcbada6f35a3469de021500dda9ef584c
Joe Pea <joe.pea@nasa.gov>
2021-12-16 20:21:34 -0800

    add a module alias for the new test folder

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ const webpackConfig = {
             "MCT": path.join(__dirname, "src/MCT"),
             "testUtils": path.join(__dirname, "src/utils/testUtils.js"),
             "objectUtils": path.join(__dirname, "src/api/objects/object-utils.js"),
+            "test": path.join(__dirname, "src/test"),
             "utils": path.join(__dirname, "src/utils")
         }
     },
