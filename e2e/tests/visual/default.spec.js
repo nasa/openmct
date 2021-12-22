32511947eecf1fff6f996d8f17290fd3b1c4c056
John Hill <john.c.hill@nasa.gov>
2021-12-22 13:53:41 -0800

    Update testcase name and notes

--- e2e/tests/visual/default.spec.js
+++ e2e/tests/visual/default.spec.js
@@ -21,11 +21,14 @@
  *****************************************************************************/
 
 /*
-Collection of Visual Tests set to run in a default context. These should only use functional
-expect statements to verify assumptions about the state in a test and not for functional
-verification of correctness.
-Note: Larger testsuite sizes are OK due to the setup time associated with these tests. Visual
-tests are not supposed to "fail" on assertions.
+Collection of Visual Tests set to run in a default context. The tests within this suite
+are only meant to run against openmct's app.js started by `npm run start` within 
+
+These should only use functional expect statements to verify assumptions about the state 
+in a test and not for functional verification of correctness. Visual tests are not supposed
+to "fail" on assertions. Instead, they should be used to detect changes between builds or branches.
+
+Note: Larger testsuite sizes are OK due to the setup time associated with these tests. 
 */
 
 const { test, expect } = require('@playwright/test');
@@ -72,7 +75,7 @@ test('Visual - Root and About', async ({ page }) => {
     await percySnapshot(page, 'About');
 });
 
-test('create new button `condition set` creates new condition object', async ({ page }) => {
+test('Visual - Default Condition Set', async ({ page }) => {
     //Go to baseURL
     await page.goto('/', { waitUntil: 'networkidle' });
 
@@ -85,6 +88,7 @@ test('create new button `condition set` creates new condition object', async ({ 
     // Click text=OK
     await page.click('text=OK');
 
+    // Take a snapshot of the newly created Condition Set object
     await page.waitForTimeout(VISUAL_GRACE_PERIOD);
     await percySnapshot(page, 'Default Condition Set');
 });
