05d0ec5f580cff51ac3ac44eabda325f477a492c
Joshi <simplyrender@gmail.com>
2022-01-06 14:49:59 -0800

    Check for ref when trying to get the style receiver. Also ensure that the property being displayed in Properties is actually a part of the domainObject.

--- src/ui/components/ObjectView.vue
+++ src/ui/components/ObjectView.vue
@@ -141,11 +141,15 @@ export default {
             this.openmct.objectViews.off('clearData', this.clearData);
         },
         getStyleReceiver() {
-            let styleReceiver = this.$refs.objectViewWrapper.querySelector('.js-style-receiver')
-                || this.$refs.objectViewWrapper.querySelector(':first-child');
+            let styleReceiver;
 
-            if (styleReceiver === null) {
-                styleReceiver = undefined;
+            if (this.$refs.objectViewWrapper !== undefined) {
+                styleReceiver = this.$refs.objectViewWrapper.querySelector('.js-style-receiver')
+                  || this.$refs.objectViewWrapper.querySelector(':first-child');
+
+                if (styleReceiver === null) {
+                    styleReceiver = undefined;
+                }
             }
 
             return styleReceiver;
