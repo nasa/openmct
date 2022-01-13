64ee9e261e500e4ef75f3dad6406d87061bdfa97
Scott Bell <scott@traclabs.com>
2022-01-13 11:47:57 +0100

    remove parameter checking

--- src/tools/urlSpec.js
+++ src/tools/urlSpec.js
@@ -1,5 +1,5 @@
 import { createOpenMct, resetApplicationState } from "../utils/testing";
-import {paramsToArray, identifierToString, default as objectPathToUrl} from "./url";
+import {identifierToString, default as objectPathToUrl} from "./url";
 
 describe('the url tool', function () {
     let openmct;
