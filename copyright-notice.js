17dfa28082d44c6673aa46b5620af1e5857ece11
Scott Bell <scott@traclabs.com>
2022-01-13 16:01:42 +0100

    added script to update copyright

--- copyright-notice.js
+++ copyright-notice.js
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Open MCT, Copyright (c) 2014-2022, United States Government
+ * Open MCT, Copyright (c) 2014-2021, United States Government
  * as represented by the Administrator of the National Aeronautics and Space
  * Administration. All rights reserved.
  *
@@ -19,55 +19,3 @@
  * this source code distribution or the Licensing information page available
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
-//SPLIT
-const { resolve } = require('path');
-const { readdir, readFile, writeFile} = require('fs').promises;
-const path = require('path');
-
-async function getCopyrightString() {
-    // eslint-disable-next-line no-undef
-    const copyrightFile = (await readFile(__filename, 'UTF-8')).toString();
-    const justTheNotice = copyrightFile.split('//SPLIT')[0];
-
-    return justTheNotice;
-}
-
-async function* getFilesRecursively(directoryToSearch, directoriesToSkip, extensionsToInclude) {
-    const fileOrDirectoryEntries = await readdir(directoryToSearch, { withFileTypes: true });
-    for (const fileOrDirectoryEntry of fileOrDirectoryEntries) {
-        const resolvedFileOrDirectoryEntry = resolve(directoryToSearch, fileOrDirectoryEntry.name);
-        const extension = path.extname(resolvedFileOrDirectoryEntry);
-        if (fileOrDirectoryEntry.isDirectory() && !directoriesToSkip.includes(directoriesToSkip)) {
-            yield* getFilesRecursively(resolvedFileOrDirectoryEntry, directoriesToSkip, extensionsToInclude);
-        } else if (extension && extensionsToInclude.includes(extension)) {
-            yield resolvedFileOrDirectoryEntry;
-        } else {
-            console.debug(`🔃 skipping ${resolvedFileOrDirectoryEntry}`);
-        }
-    }
-}
-
-async function replaceCopyrightInSingleFile(file, copyrightNotice) {
-    // eslint-disable-next-line no-undef
-    const fileAsString = (await readFile(file)).toString();
-    const splitWithCopyright = fileAsString.split(copyrightNotice);
-    if (splitWithCopyright.length === 2) {
-        console.debug(`🖲 found copyright, replacing ${file}`);
-        const newFile = copyrightNotice.concat(splitWithCopyright[1]);
-        await writeFile(file, newFile);
-    } else {
-        console.debug(`❓ no copyright found in ${file}`);
-    }
-}
-
-async function replaceAllCopyrightWithCurrentYear(directoryToStartIn, directoriesToSkip, extensionsToInclude) {
-    const copyrightNotice = getCopyrightString();
-    for await (const foundFile of getFilesRecursively(directoryToStartIn, directoriesToSkip, extensionsToInclude)) {
-        replaceCopyrightInSingleFile(foundFile, copyrightNotice);
-    }
-}
-
-// eslint-disable-next-line no-undef
-const directoriesToSkip = [path.join(__dirname, 'node_modules')];
-const extensionsToInclude = ['.js', '.ts', '.html', '.scss', '.vue', '.sh'];
-replaceAllCopyrightWithCurrentYear('.', directoriesToSkip, extensionsToInclude);
