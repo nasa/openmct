6e2437b09e93eae452a165ce0271a18889a8c020
David Tsay <david.e.tsay@nasa.gov>
2022-01-18 11:19:35 -0800

    fix navigate after create path

--- src/plugins/formActions/CreateAction.js
+++ src/plugins/formActions/CreateAction.js
@@ -108,7 +108,7 @@ export default class CreateAction extends PropertiesAction {
         }
 
         const url = '#/browse/' + objectPath
-            .map(object => object && this.openmct.objects.makeKeyString(object.identifier.key))
+            .map(object => object && this.openmct.objects.makeKeyString(object.identifier))
             .reverse()
             .join('/');
 
