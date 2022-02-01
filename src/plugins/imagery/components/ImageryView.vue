c0334ed8d1de2f7d1fb07e02160bd41979974e01
Charles Hacskaylo <charlesh88@gmail.com>
2022-02-01 11:06:43 -0800

    Tweaks for mct34 - Added title attributes to zoom and pan buttons;

--- src/plugins/imagery/components/ImageryView.vue
+++ src/plugins/imagery/components/ImageryView.vue
@@ -33,20 +33,24 @@
             <div class="c-image-controls__control c-image-controls__zoom icon-magnify">
                 <div class="c-button-set c-button-set--strip-h">
                     <button class="c-button t-btn-zoom-out icon-minus"
+                            title="Zoom out"
                             @click="handleZoomButton(-1)"
                     ></button>
 
                     <button class="c-button t-btn-zoom-in icon-plus"
+                            title="Zoom in"
                             @click="handleZoomButton(1)"
                     ></button>
                 </div>
 
                 <button class="c-button t-btn-zoom-lock"
+                        title="Lock current zoom and pan across all images"
                         :class="{'icon-unlocked': !panZoomLocked, 'icon-lock': panZoomLocked}"
                         @click="lockPanZoomPosition"
                 ></button>
 
                 <button class="c-button icon-reset t-btn-zoom-reset"
+                        title="Remove zoom and pan"
                         @click="resetImage(true)"
                 ></button>
 
@@ -264,7 +268,6 @@ const ARROW_RIGHT = 39;
 const ARROW_LEFT = 37;
 
 const SCROLL_LATENCY = 250;
-
 
 const ZOOM_LIMITS_MAX_DEFAULT = 20;
 const ZOOM_LIMITS_MIN_DEFAULT = 1;
