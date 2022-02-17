d39d7354b72cd8ded819eaf3bd910acc0b6f9493
Michael Rogers <contact@mhrogers.com>
2022-02-17 15:44:48 -0600

    Condition manager zepto removal

--- src/plugins/summaryWidget/src/ConditionManager.js
+++ src/plugins/summaryWidget/src/ConditionManager.js
@@ -2,13 +2,13 @@ define ([
     './ConditionEvaluator',
     'objectUtils',
     'EventEmitter',
-    'zepto',
+    // 'zepto',
     'lodash'
 ], function (
     ConditionEvaluator,
     objectUtils,
     EventEmitter,
-    $,
+    // $,
     _
 ) {
 
@@ -232,7 +232,10 @@ define ([
 
             self.eventEmitter.emit('add', obj);
 
-            $('.w-summary-widget').removeClass('s-status-no-data');
+            const summaryWidget = document.querySelector('.w-summary-widget');
+            if (summaryWidget) {
+                summaryWidget.classList.remove('s-status-no-data');
+            }
         }
     };
 
@@ -256,7 +259,10 @@ define ([
         this.eventEmitter.emit('remove', identifier);
 
         if (_.isEmpty(this.compositionObjs)) {
-            $('.w-summary-widget').addClass('s-status-no-data');
+            const summaryWidget = document.querySelector('.w-summary-widget');
+            if (summaryWidget) {
+                summaryWidget.classList.add('s-status-no-data');
+            }
         }
     };
 
