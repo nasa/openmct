304a904fa37ea2108c835edfdf008fe4f1cf9a44
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2022-05-12 11:15:35 -0700

    made create button items dynamic each time the button is clicked, this will pick up any new types added after the create menu is created

--- e2e/tests/plugins/notebook/addRestrictedNotebook.js
+++ e2e/tests/plugins/notebook/addRestrictedNotebook.js
@@ -1,7 +1,6 @@
 (function () {
     document.addEventListener('DOMContentLoaded', () => {
         const openmct = window.openmct;
-
         openmct.install(openmct.plugins.RestrictedNotebook('Custom Name'));
     });
 }());
