0d006270384fe4caa00665d5915a8460b57b05e8
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2022-05-13 12:53:37 -0700

    latest e2e WIP

--- e2e/tests/plugins/notebook/restrictedNotebook.e2e.spec.js
+++ e2e/tests/plugins/notebook/restrictedNotebook.e2e.spec.js
@@ -20,14 +20,8 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-// test.fixme('make sure regular notebooks work exactly how they used to');
-// test.fixme('make sure you can pass in a name to the Notebook and RestrictedNotebook plugins and that they show in Open MCT');
-// test.fixme('create a restricted notebook');
-// test.fixme('make sure you can do anything that you can do to a normal notebook (observe "Lock Page" shows when an entry is started)');
-// test.fixme('open section/page sidebar');
-// test.fixme('Lock a page (verify confirmation)');
+
 // test.fixme('make sure locks show for page and the section that page is in');
-// test.fixme('make sure you cannot remove the restricted notebook (right click remove from tree)');
 // test.fixme('make sure you can nott edit section titles or section names if locked');
 // test.fixme('try to delete/edit an entry from a locked notebook (you should not be able)');
 // test.fixme('try to remove an embed from a locked page entry (make sure you can preview though)');
@@ -38,6 +32,34 @@
 
 const { test, expect } = require('@playwright/test');
 const path = require('path');
+
+const TEST_TEXT = 'Testing text for entries.';
+const CUSTOM_NAME = 'CUSTOM_NAME';
+const COMMIT_BUTTON_TEXT = 'button:has-text("Commit Entries")';
+
+async function enterTextEntry(page) {
+    // Click .c-notebook__drag-area
+    await page.locator('.c-notebook__drag-area').click();
+
+    // enter text
+    await page.locator('div.c-ne__text').click();
+    await page.locator('div.c-ne__text').fill(TEST_TEXT);
+    await page.locator('div.c-ne__text').press('Enter');
+
+    return;
+}
+
+async function openContextMenuRestrictedNotebook(page) {
+    // Click text=Open MCT My Items >> span >> nth=3
+    await page.locator('text=Open MCT My Items >> span').nth(3).click();
+
+    // Click a:has-text("Unnamed CUSTOM_NAME")
+    await page.locator(`a:has-text("Unnamed ${CUSTOM_NAME}")`).click({
+        button: 'right'
+    });
+
+    return;
+}
 
 test.describe('Restricted Notebook', () => {
 
@@ -54,7 +76,7 @@ test.describe('Restricted Notebook', () => {
         await page.click('button:has-text("Create")');
 
         // Click text=Example Imagery
-        await page.click('text=CUSTOM_NAME');
+        await page.click(`text=${CUSTOM_NAME}`); // this inherently tests renamability
 
         // Click text=OK
         await Promise.all([
@@ -65,11 +87,64 @@ test.describe('Restricted Notebook', () => {
         ]);
         //Wait until Save Banner is gone
         await page.waitForSelector('.c-message-banner__message', { state: 'detached'});
-        await expect(page.locator('.l-browse-bar__object-name')).toContainText('Unnamed CUSTOM_NAME');
+        await expect(page.locator('.l-browse-bar__object-name')).toContainText(`Unnamed ${CUSTOM_NAME}`);
     });
 
-    test('Can use Mouse Wheel to zoom in and out of latest image', async ({ page }) => {
-        await page.pause();
+    test('Can be deleted if there are no locked pages', async ({ page }) => {
+        await openContextMenuRestrictedNotebook(page);
+
+        const menuOptions = page.locator('.c-menu ul');
+
+        await expect.soft(menuOptions).toContainText('Remove');
+    });
+
+    test('Can be locked if at least one page has one entry', async ({ page }) => {
+
+        await enterTextEntry(page);
+
+        const commitButton = page.locator(COMMIT_BUTTON_TEXT);
+        expect.soft(commitButton).toBeDefined();
+    });
+
+    test.describe('With at least one entry and with the page locked', () => {
+
+        test.beforeEach(async ({ page }) => {
+            await enterTextEntry(page);
+
+            const commitButton = page.locator(COMMIT_BUTTON_TEXT);
+            await commitButton.click();
+
+            // confirmation dialog click
+            await page.locator('text=Lock Page').click();
+
+            // open sidebar
+            await page.locator('button.c-notebook__toggle-nav-button').click();
+        });
+
+        test('The page should now be in a locked state', async ({ page }) => {
+            // main lock message on page
+            const lockMessage = page.locator('text=This page has been committed and cannot be modified or removed');
+            expect(lockMessage).toBeDefined();
+
+            // lock icon on page in sidebar
+            const pageLockIcon = page.locator('ul.c-notebook__pages li div.icon-lock');
+            expect(pageLockIcon).toBeDefined();
+
+            // no way to remove a restricted notebook with a locked page
+            await openContextMenuRestrictedNotebook(page);
+
+            const menuOptions = page.locator('.c-menu ul');
+
+            await expect.soft(menuOptions).not.toContainText('Remove');
+
+        });
+
+        test.only('Some features should still be available', async ({ page }) => {
+            const lockMessage = page.locator('text=This page has been committed and cannot be modified or removed');
+            await page.pause();
+
+            expect(lockMessage).toBeDefined();
+        });
     });
 
 });
