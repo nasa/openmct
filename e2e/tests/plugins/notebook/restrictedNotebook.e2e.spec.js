dc909df6de5fef22d8d8b570d760f8e6fa2fbf01
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2022-05-16 16:00:54 -0700

    more e2e tests for restricted notebook

--- e2e/tests/plugins/notebook/restrictedNotebook.e2e.spec.js
+++ e2e/tests/plugins/notebook/restrictedNotebook.e2e.spec.js
@@ -20,31 +20,56 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-
-// test.fixme('make sure locks show for page and the section that page is in');
-// test.fixme('make sure you can nott edit section titles or section names if locked');
-// test.fixme('try to delete/edit an entry from a locked notebook (you should not be able)');
-// test.fixme('try to remove an embed from a locked page entry (make sure you can preview though)');
-// test.fixme('make sure if the notebook section/page was default, that it was NOT default once locked');
-// test.fixme('try to drop an item into a locked page (you should not be able), do the same with a snapshot');
-// test.fixme('make sure search works as expected');
-// test.fixme('in search, make sure there are locked and unlocked page entries with embeds, make sure you can remove unlocked ones and not remove locked ones');
-
 const { test, expect } = require('@playwright/test');
 const path = require('path');
 
 const TEST_TEXT = 'Testing text for entries.';
+const TEST_TEXT_NAME = 'Test Page';
 const CUSTOM_NAME = 'CUSTOM_NAME';
 const COMMIT_BUTTON_TEXT = 'button:has-text("Commit Entries")';
+const SINE_WAVE_GENERATOR = 'text=Unnamed Sine Wave Generator';
+const DROP_AREA = '.c-notebook__drag-area';
 
 async function enterTextEntry(page) {
     // Click .c-notebook__drag-area
-    await page.locator('.c-notebook__drag-area').click();
+    await page.locator(DROP_AREA).click();
 
     // enter text
     await page.locator('div.c-ne__text').click();
     await page.locator('div.c-ne__text').fill(TEST_TEXT);
     await page.locator('div.c-ne__text').press('Enter');
+
+    return;
+}
+
+async function dragAndDropEmbed(page) {
+    // Click button:has-text("Create")
+    await page.locator('button:has-text("Create")').click();
+    // Click li:has-text("Sine Wave Generator")
+    await page.locator('li:has-text("Sine Wave Generator")').click();
+    // Click form[name="mctForm"] >> text=My Items
+    await page.locator('form[name="mctForm"] >> text=My Items').click();
+    // Click text=OK
+    await page.locator('text=OK').click();
+    // Click text=Open MCT My Items >> span >> nth=3
+    await page.locator('text=Open MCT My Items >> span').nth(3).click();
+    // Click text=Unnamed CUSTOM_NAME
+    await Promise.all([
+        page.waitForNavigation(),
+        page.locator('text=Unnamed CUSTOM_NAME').click()
+    ]);
+
+    await page.dragAndDrop(SINE_WAVE_GENERATOR, DROP_AREA);
+
+    return;
+}
+
+async function lockPage(page) {
+    const commitButton = page.locator(COMMIT_BUTTON_TEXT);
+    await commitButton.click();
+
+    // confirmation dialog click
+    await page.locator('text=Lock Page').click();
 
     return;
 }
@@ -87,7 +112,7 @@ test.describe('Restricted Notebook', () => {
         ]);
         //Wait until Save Banner is gone
         await page.waitForSelector('.c-message-banner__message', { state: 'detached'});
-        await expect(page.locator('.l-browse-bar__object-name')).toContainText(`Unnamed ${CUSTOM_NAME}`);
+        await expect.soft(page.locator('.l-browse-bar__object-name')).toContainText(`Unnamed ${CUSTOM_NAME}`);
     });
 
     test('Can be deleted if there are no locked pages', async ({ page }) => {
@@ -103,19 +128,14 @@ test.describe('Restricted Notebook', () => {
         await enterTextEntry(page);
 
         const commitButton = page.locator(COMMIT_BUTTON_TEXT);
-        expect.soft(commitButton).toBeDefined();
+        expect.soft(await commitButton.count()).toEqual(1);
     });
 
     test.describe('With at least one entry and with the page locked', () => {
 
         test.beforeEach(async ({ page }) => {
             await enterTextEntry(page);
-
-            const commitButton = page.locator(COMMIT_BUTTON_TEXT);
-            await commitButton.click();
-
-            // confirmation dialog click
-            await page.locator('text=Lock Page').click();
+            await lockPage(page);
 
             // open sidebar
             await page.locator('button.c-notebook__toggle-nav-button').click();
@@ -124,11 +144,11 @@ test.describe('Restricted Notebook', () => {
         test('The page should now be in a locked state', async ({ page }) => {
             // main lock message on page
             const lockMessage = page.locator('text=This page has been committed and cannot be modified or removed');
-            expect(lockMessage).toBeDefined();
+            expect.soft(await lockMessage.count()).toEqual(1);
 
             // lock icon on page in sidebar
             const pageLockIcon = page.locator('ul.c-notebook__pages li div.icon-lock');
-            expect(pageLockIcon).toBeDefined();
+            expect.soft(await pageLockIcon.count()).toEqual(1);
 
             // no way to remove a restricted notebook with a locked page
             await openContextMenuRestrictedNotebook(page);
@@ -139,12 +159,69 @@ test.describe('Restricted Notebook', () => {
 
         });
 
-        test.only('Some features should still be available', async ({ page }) => {
-            const lockMessage = page.locator('text=This page has been committed and cannot be modified or removed');
-            await page.pause();
+        test('Can still: add page, rename, add entry, delete unlocked pages', async ({ page }) => {
+            // Click text=Page Add >> button
+            await Promise.all([
+                page.waitForNavigation(),
+                page.locator('text=Page Add >> button').click()
+            ]);
+            // Click text=Unnamed Page >> nth=1
+            await page.locator('text=Unnamed Page').nth(1).click();
+            // Press a with modifiers
+            await page.locator('text=Unnamed Page').nth(1).fill(TEST_TEXT_NAME);
 
-            expect(lockMessage).toBeDefined();
+            // expect to be able to rename unlocked pages
+            const newPageElement = page.locator(`text=${TEST_TEXT_NAME}`);
+            const newPageCount = await newPageElement.count();
+            await newPageElement.press('Enter'); // exit contenteditable state
+            expect.soft(newPageCount).toEqual(1);
+
+            // enter test text
+            await enterTextEntry(page);
+
+            // expect new page to be lockable
+            const commitButton = page.locator(COMMIT_BUTTON_TEXT);
+            expect.soft(await commitButton.count()).toEqual(1);
+
+            // Click text=Unnamed PageTest Page >> button
+            await page.locator('text=Unnamed PageTest Page >> button').click();
+            // Click text=Delete Page
+            await page.locator('text=Delete Page').click();
+            // Click text=Ok
+            await Promise.all([
+                page.waitForNavigation(),
+                page.locator('text=Ok').click()
+            ]);
+
+            // deleted page, should no longer exist
+            const deletedPageElement = page.locator(`text=${TEST_TEXT_NAME}`);
+            expect.soft(await deletedPageElement.count()).toEqual(0);
         });
     });
 
+    test.describe('With a page with embeds', () => {
+
+        test.beforeEach(async ({ page }) => {
+            await dragAndDropEmbed(page);
+        });
+
+        test('Allows embeds to be deleted if page unlocked', async ({ page }) => {
+
+            // Click .c-ne__embed__name .c-popup-menu-button
+            await page.locator('.c-ne__embed__name .c-popup-menu-button').click(); // embed popup menu
+
+            const embedMenu = page.locator('body >> .c-menu');
+            await expect.soft(embedMenu).toContainText('Remove This Embed');
+        });
+
+        test('Disallows embeds to be deleted if page locked', async ({ page }) => {
+            await lockPage(page);
+            // Click .c-ne__embed__name .c-popup-menu-button
+            await page.locator('.c-ne__embed__name .c-popup-menu-button').click(); // embed popup menu
+
+            const embedMenu = page.locator('body >> .c-menu');
+            await expect.soft(embedMenu).not.toContainText('Remove This Embed');
+        });
+
+    });
 });
