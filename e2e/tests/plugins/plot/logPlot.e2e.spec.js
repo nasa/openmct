6521b888d6f52322bf63cad91a855c6a66994024
Jesse Mazzella <ozyx@users.noreply.github.com>
2022-05-19 16:09:22 -0700

    Enable lint enforcement on e2e tests and fix the existing errors (#5229)
    * Add `e2e` folder to lint scripts
    
    * Fix or add exceptions to all new linting errors
    
    * fix an oopsie 👀

--- e2e/tests/plugins/plot/logPlot.e2e.spec.js
+++ e2e/tests/plugins/plot/logPlot.e2e.spec.js
@@ -242,6 +242,8 @@ async function saveOverlayPlot(page) {
 /**
  * @param {import('@playwright/test').Page} page
  */
+// FIXME: Remove this eslint exception once implemented
+// eslint-disable-next-line no-unused-vars
 async function testLogPlotPixels(page) {
     const pixelsMatch = await page.evaluate(async () => {
         // TODO get canvas pixels at a few locations to make sure they're the correct color, to test that the plot comes out as expected.
