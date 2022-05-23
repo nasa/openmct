9776d679c6f03802af2608758d009ec8bee0a658
Scott Bell <scott@traclabs.com>
2022-05-23 11:39:21 +0200

    add tag search test

--- src/api/annotation/AnnotationAPISpec.js
+++ src/api/annotation/AnnotationAPISpec.js
@@ -4,15 +4,32 @@ describe("The Annotation API", () => {
     let openmct;
     let mockObjectProvider;
     let mockDomainObject;
+    let mockAnnotationObject;
 
-    beforeEach((done) => {
+    beforeEach(async () => {
         openmct = createOpenMct();
+        const availableTags = openmct.annotation.getAvailableTags();
         mockDomainObject = {
             type: 'notebook',
             name: 'fooRabbitNotebook',
             identifier: {
                 key: 'some-object',
                 namespace: 'fooNameSpace'
+            }
+        };
+        mockAnnotationObject = {
+            type: 'annotation',
+            name: 'Some Notebook Annotation',
+            annotationType: openmct.annotation.ANNOTATION_TYPES.NOTEBOOK,
+            tags: [availableTags[0].id, availableTags[1].id],
+            identifier: {
+                key: 'anAnnotationKey',
+                namespace: 'fooNameSpace'
+            },
+            targets: {
+                mockDomainObject: {
+                    entryId: 'fooBarEntry'
+                }
             }
         };
 
@@ -22,18 +39,24 @@ describe("The Annotation API", () => {
             "get"
         ]);
         // eslint-disable-next-line require-await
-        mockObjectProvider.get = async () => {
-            return mockDomainObject;
+        mockObjectProvider.get = async (identifier) => {
+            if (identifier.key === mockDomainObject.identifier.key) {
+                return mockDomainObject;
+            } else if (identifier.key === mockAnnotationObject.identifier.key) {
+                return mockAnnotationObject;
+            } else {
+                return null;
+            }
         };
 
         mockObjectProvider.create.and.returnValue(Promise.resolve(true));
         mockObjectProvider.update.and.returnValue(Promise.resolve(true));
 
         openmct.objects.addProvider('fooNameSpace', mockObjectProvider);
-        openmct.on('start', () => {
-            done();
-        });
         openmct.startHeadless();
+        await openmct.on('start');
+        await openmct.objects.inMemorySearchProvider.index(mockDomainObject);
+        await openmct.objects.inMemorySearchProvider.index(mockAnnotationObject);
     });
     afterEach(async () => {
         await resetApplicationState(openmct);
@@ -44,7 +67,6 @@ describe("The Annotation API", () => {
 
     describe("Creation", () => {
         it("can create annotations", async () => {
-            console.trace();
             const annotationObject = await openmct.annotation.create('Test Annotation', mockDomainObject, openmct.annotation.ANNOTATION_TYPES.NOTEBOOK, ['sometag'], "fooContext", {'fooTarget': {}});
             console.debug(`annotationObject`, annotationObject);
             expect(annotationObject).toBeDefined();
@@ -56,6 +78,16 @@ describe("The Annotation API", () => {
             } catch (error) {
                 expect(error).toBeDefined();
             }
+        });
+    });
+
+    describe("Search", () => {
+        it("can search for tags", async () => {
+            console.trace();
+            const results = await openmct.annotation.searchForTags('S');
+            console.debug(`results`, results);
+            expect(results).toBeDefined();
+            expect(results.length).toEqual(1);
         });
     });
 });
