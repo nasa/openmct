676eb9fed2a15d8a7ac74f75dc40b68bae242ee1
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-05-26 09:36:06 -0700

    disable `.skip()` warning for memleak test suite

--- e2e/tests/performance/memleak-imagery.perf.spec.js
+++ e2e/tests/performance/memleak-imagery.perf.spec.js
@@ -36,8 +36,8 @@ const { test, expect } = require('@playwright/test');
 
 const filePath = 'e2e/test-data/PerformanceDisplayLayout.json';
 
-// NOTE: Not eligible for community contributions.
-test.describe.fixme('Memory Performance tests', () => {
+// eslint-disable-next-line playwright/no-skipped-test
+test.describe.skip('Memory Performance tests', () => {
     test.beforeEach(async ({ page, browser }, testInfo) => {
         // Go to baseURL
         await page.goto('/', { waitUntil: 'networkidle' });
