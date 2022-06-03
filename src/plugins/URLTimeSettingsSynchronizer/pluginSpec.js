d41804650397da563d0954f76677a42a1269a7a7
Scott Bell <scott@traclabs.com>
2022-06-03 03:03:09 +0200

    revert test change

--- src/plugins/URLTimeSettingsSynchronizer/pluginSpec.js
+++ src/plugins/URLTimeSettingsSynchronizer/pluginSpec.js
@@ -24,7 +24,7 @@ import {
     resetApplicationState
 } from 'utils/testing';
 
-xdescribe("The URLTimeSettingsSynchronizer", () => {
+describe("The URLTimeSettingsSynchronizer", () => {
     let appHolder;
     let openmct;
     let resolveFunction;
