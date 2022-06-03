eb9dc67ed4290340e43e1098d7b82816a22c6c86
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-06-03 12:56:01 -0700

    Update docs for `CouchStatusIndicator`

--- src/plugins/persistence/couch/CouchStatusIndicator.js
+++ src/plugins/persistence/couch/CouchStatusIndicator.js
@@ -20,36 +20,48 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-// Set of connection states; changing among these states will be
-// reflected in the indicator's appearance.
-// CONNECTED: Everything nominal, expect to be able to read/write
-// DISCONNECTED: HTTP request failed (network error). Unable to reach server at all.
-// PENDING: Still trying to connect, and haven't failed yet.
-// MAINTENANCE: CouchDB is connected but not accepting requests.
+/**
+ * @typedef {Object} IndicatorState
+ * An object defining the visible state of the indicator.
+ * @property {string} statusClass - The class to apply to the indicator.
+ * @property {string} text - The text to display in the indicator.
+ * @property {string} description - The description to display in the indicator.
+ */
+
+/**
+ * Set of CouchDB connection states; changes among these states will be
+ * reflected in the indicator's appearance.
+ * CONNECTED: Everything nominal, expect to be able to read/write.
+ * DISCONNECTED: HTTP request failed (network error). Unable to reach server at all.
+ * PENDING: Still trying to connect, and haven't failed yet.
+ * MAINTENANCE: CouchDB is connected but not accepting requests.
+ */
+
+/** @type {IndicatorState} */
 export const CONNECTED = {
     statusClass: "s-status-on",
     text: "CouchDB is connected",
     description: "CouchDB is online and accepting requests."
 };
+/** @type {IndicatorState} */
 export const PENDING = {
     statusClass: "s-status-warning-lo",
     text: "Attempting to connect to CouchDB...",
     description: "Checking status of CouchDB, please stand by..."
 };
+/** @type {IndicatorState} */
 export const DISCONNECTED = {
     statusClass: "s-status-warning-hi",
     text: "CouchDB is offline",
     description: "CouchDB is offline and unavailable for requests."
 };
+/** @type {IndicatorState} */
 export const MAINTENANCE = {
     statusClass: "s-status-warning-lo",
     text: "CouchDB is in maintenance mode",
     description: "CouchDB is online, but not currently accepting requests."
 };
 
-/**
- *
- */
 export default class CouchStatusIndicator {
     constructor(simpleIndicator) {
         this.indicator = simpleIndicator;
@@ -65,7 +77,7 @@ export default class CouchStatusIndicator {
 
     /**
      * Set the indicator to the given state.
-     * @param {*} state
+     * @param {IndicatorState} state
      */
     setIndicatorToState(state) {
         this.indicator.text(state.text);
