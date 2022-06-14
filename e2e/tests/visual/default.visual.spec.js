4f720cd328cf6759ce978198818a418b64e6be9d
unlikelyzero <jchill2@gmail.com>
2022-06-14 09:53:34 -0700

    Add fixture support to visual tests

--- e2e/tests/visual/default.visual.spec.js
+++ e2e/tests/visual/default.visual.spec.js
@@ -32,7 +32,8 @@ to "fail" on assertions. Instead, they should be used to detect changes between 
 Note: Larger testsuite sizes are OK due to the setup time associated with these tests.
 */
 
-const { test, expect } = require('@playwright/test');
+const { test } = require('../../fixtures.js');
+const { expect } = require('@playwright/test');
 const percySnapshot = require('@percy/playwright');
 const path = require('path');
 const sinon = require('sinon');
