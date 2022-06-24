c7875138dae3872aedd64815433005b3651b91ae
Joshi <simplyrender@gmail.com>
2022-06-24 15:49:43 -0700

    Update stackedplot item to emit loading event

--- src/plugins/plot/stackedPlot/StackedPlotItem.vue
+++ src/plugins/plot/stackedPlot/StackedPlotItem.vue
@@ -185,7 +185,7 @@ export default {
         },
         setStatus(status) {
             this.status = status;
-            this.$emit('status', status);
+            this.updateComponentProp('status', status);
         },
         setSelection() {
             let childContext = {};
@@ -200,7 +200,8 @@ export default {
         },
         loadingUpdated(loaded) {
             this.loading = loaded;
-            this.$emit('loading', loaded);
+            this.updateComponentProp('loading', loaded);
+            this.$emit('loadingUpdated', loaded);
         },
         getProps() {
             return {
