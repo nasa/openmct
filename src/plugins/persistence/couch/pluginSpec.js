f312b90804d08edeb53a02fec441a2e2ca36c7ea
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-07-07 10:01:18 -0700

    adjust test to hopefully avoid race condition

--- src/plugins/persistence/couch/pluginSpec.js
+++ src/plugins/persistence/couch/pluginSpec.js
@@ -158,7 +158,6 @@ describe('the plugin', () => {
                 }
             };
 
-            // eslint-disable-next-line require-await
             provider.request = async function (subPath, method, body, signal) {
                 return {
                     body: fakeUpdateEvent,
@@ -173,8 +172,7 @@ describe('the plugin', () => {
             expect(provider.create).toHaveBeenCalled();
             expect(provider.observe).toHaveBeenCalled();
             expect(provider.isObservingObjectChanges).toHaveBeenCalled();
-            expect(provider.isObservingObjectChanges.calls.mostRecent().returnValue).toBe(true);
-
+            
             //Set modified timestamp it detects a change and persists the updated model.
             mockDomainObject.modified = mockDomainObject.persisted + 1;
             const updatedResult = await openmct.objects.save(mockDomainObject);
@@ -184,6 +182,7 @@ describe('the plugin', () => {
             expect(updatedResult).toBeTrue();
             expect(provider.update).toHaveBeenCalled();
             expect(provider.fetchChanges).toHaveBeenCalled();
+            expect(provider.isObservingObjectChanges.calls.mostRecent().returnValue).toBe(true);
             sharedWorkerCallback(fakeUpdateEvent);
             expect(provider.onEventMessage).toHaveBeenCalled();
 
