e402a515917b1a681b92539348311ac120733c33
John Hill <john.c.hill@nasa.gov>
2022-07-19 17:00:30 -0700

    updated documentation

--- e2e/baseFixtures.js
+++ e2e/baseFixtures.js
@@ -1,8 +1,34 @@
-/* eslint-disable no-undef */
-/* This file extends the base functionality of the playwright test framework to enable
- * code coverage instrumentation, console log error detection and working with a 3rd
- * party Chrome-as-a-service extension called Browserless.
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2022, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+/**
+ * This file is dedicated to extending the base functionality of the @playwright/test framework.
+ * The functions in this file should be viewed as temporary or a shim to be removed as the RFEs in
+ * the playwright github repo are implemented. Functions which serve those RFEs are marked with corresponding
+ * GitHub issues.
  */
+
+/* eslint-disable no-undef */
+
 const base = require('@playwright/test');
 const { expect } = base;
 const fs = require('fs');
@@ -10,7 +36,8 @@ const path = require('path');
 const { v4: uuid } = require('uuid');
 
 /**
- * Takes a `ConsoleMessage` and returns a formatted string
+ * Takes a `ConsoleMessage` and returns a formatted string. Used to enable console log error detection
+ * @see {@link https://github.com/microsoft/playwright/discussions/11690 Github Discussion}
  * @private
  * @param {import('@playwright/test').ConsoleMessage} msg
  * @returns {String} formatted string with message type, text, url, and line and column numbers
@@ -22,8 +49,9 @@ function _consoleMessageToString(msg) {
 }
 
 /**
- * Wait for all animations within the given element and subtrees to finish
- * Playwright RFE: https://github.com/microsoft/playwright/issues/15660
+ * Wait for all animations within the given element and subtrees to finish. Useful when
+ * verifying that css transitions have completed.
+ * @see {@link https://github.com/microsoft/playwright/issues/15660 Github RFE}
  * @param {import('@playwright/test').Locator} locator
  * @return {Promise<Animation[]>}
  */
@@ -36,11 +64,18 @@ function waitForAnimations(locator) {
                     .map((animation) => animation.finished)));
 }
 
-//The following is based on https://github.com/mxschmitt/playwright-test-coverage
+/**
+ * This is part of our codecoverage shim.
+ * @see {@link https://github.com/mxschmitt/playwright-test-coverage Github Example Project}
+ * @constant {string}
+ */
 const istanbulCLIOutput = path.join(process.cwd(), '.nyc_output');
 
 exports.test = base.test.extend({
-    //The following is based on https://github.com/mxschmitt/playwright-test-coverage
+    /**
+     * Extends the base context class to add codecoverage shim.
+     * @see {@link https://github.com/mxschmitt/playwright-test-coverage Github Project}
+     */
     context: async ({ context }, use) => {
         await context.addInitScript(() =>
             window.addEventListener('beforeunload', () =>
@@ -58,6 +93,10 @@ exports.test = base.test.extend({
             await page.evaluate(() => (window).collectIstanbulCoverage(JSON.stringify((window).__coverage__)));
         }
     },
+    /**
+     * Extends the base page class to enable console log error detection.
+     * @see {@link https://github.com/microsoft/playwright/discussions/11690 Github Discussion}
+     */
     page: async ({ baseURL, page }, use) => {
         const messages = [];
         page.on('console', (msg) => messages.push(msg));
@@ -66,6 +105,11 @@ exports.test = base.test.extend({
             msg => expect.soft(msg.type(), `Console error detected: ${_consoleMessageToString(msg)}`).not.toEqual('error')
         );
     },
+    /**
+     * Extends the base browser class to enable CDP connection definition in playwright.config.js. Once
+     * that RFE is implemented, this function can be removed.
+     * @see {@link https://github.com/microsoft/playwright/issues/8379 Github RFE}
+     */
     browser: async ({ playwright, browser }, use, workerInfo) => {
         // Use browserless if configured
         if (workerInfo.project.name.match(/browserless/)) {
