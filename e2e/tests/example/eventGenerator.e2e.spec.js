f560466e7ee2ccb2aaf64099b727f1376c8997f7
John Hill <john.c.hill@nasa.gov>
2022-07-27 08:34:52 -0700

    refactor eventGenerator test

--- e2e/tests/example/eventGenerator.e2e.spec.js
+++ e2e/tests/example/eventGenerator.e2e.spec.js
@@ -24,39 +24,33 @@
 This test suite is dedicated to tests which verify the basic operations surrounding the example event generator.
 */
 
-const { test, expect } = require('../../baseFixtures.js');
+const { test, expect } = require('../../baseFixtures');
+const { createDomainObjectWithDefaults } = require('../../appActions');
 
-test.describe('Example Event Generator Operations', () => {
-    test('Can create example event generator with a name', async ({ page }) => {
+test.describe('Example Event Generator CRUD Operations', () => {
+    test('Can create a Test Event Generator and it results in the table View', async ({ page }) => {
         //Go to baseURL
         await page.goto('./', { waitUntil: 'networkidle' });
-        // let's make an event generator
-        await page.locator('button:has-text("Create")').click();
-        // Click li:has-text("Event Message Generator")
-        await page.locator('li:has-text("Event Message Generator")').click();
-        // Click text=Properties Title Notes >> input[type="text"]
-        await page.locator('text=Properties Title Notes >> input[type="text"]').click();
-        // Fill text=Properties Title Notes >> input[type="text"]
-        await page.locator('text=Properties Title Notes >> input[type="text"]').fill('Test Event Generator');
-        // Press Enter
-        await page.locator('text=Properties Title Notes >> input[type="text"]').press('Enter');
-        // Click text=OK
-        await Promise.all([
-            page.waitForNavigation({ url: /.*&view=table/ }),
-            page.locator('text=OK').click()
-        ]);
 
-        await expect(page.locator('.l-browse-bar__object-name')).toContainText('Test Event Generator');
-        // Click button:has-text("Fixed Timespan")
-        await page.locator('button:has-text("Fixed Timespan")').click();
+        //Create a name for the object
+        const newObjectName = 'Test Event Generator';
+
+        await createDomainObjectWithDefaults(page, 'Event Message Generator', newObjectName);
+
+        //Assertions against newly created object which define standard behavior
+        await expect(page.waitForURL(/.*&view=table/)).toBeTruthy();
+        await expect(page.locator('.l-browse-bar__object-name')).toContainText(newObjectName);
     });
+});
+
+test.describe('Example Event Generator Telemetry Event Verficiation', () => {
 
     test.fixme('telemetry is coming in for test event', async ({ page }) => {
-        // Go to object created in step one
-        // Verify the telemetry table is filled with > 1 row
+    // Go to object created in step one
+    // Verify the telemetry table is filled with > 1 row
     });
     test.fixme('telemetry is sorted by time ascending', async ({ page }) => {
-        // Go to object created in step one
-        // Verify the telemetry table has a class with "is-sorting asc"
+    // Go to object created in step one
+    // Verify the telemetry table has a class with "is-sorting asc"
     });
 });
