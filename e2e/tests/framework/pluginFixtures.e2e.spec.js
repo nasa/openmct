08db59269ea5c5e5f30b6ee4a82196f3aa7d5e2f
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-07-29 15:41:26 -0700

    scorch pluginFixtures tests

--- e2e/tests/framework/pluginFixtures.e2e.spec.js
+++ e2e/tests/framework/pluginFixtures.e2e.spec.js
@@ -25,22 +25,22 @@ This test suite is dedicated to testing our use of our custom fixtures to verify
 that they are working as expected.
 */
 
-const { test, expect } = require('../../pluginFixtures.js');
+const { test } = require('../../pluginFixtures.js');
 
 // eslint-disable-next-line playwright/no-skipped-test
 test.describe.skip('pluginFixtures tests', () => {
-    test.use({ domainObjectName: 'Timer' });
-    let timerUUID;
+    // test.use({ domainObjectName: 'Timer' });
+    // let timerUUID;
 
-    test('Creates a timer object @framework @unstable', ({ domainObject }) => {
-        const { uuid } = domainObject;
-        const uuidRegexp = /[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/;
-        expect(uuid).toMatch(uuidRegexp);
-        timerUUID = uuid;
-    });
+    // test('Creates a timer object @framework @unstable', ({ domainObject }) => {
+    //     const { uuid } = domainObject;
+    //     const uuidRegexp = /[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/;
+    //     expect(uuid).toMatch(uuidRegexp);
+    //     timerUUID = uuid;
+    // });
 
-    test('Provides same uuid for subsequent uses of the same object @framework', ({ domainObject }) => {
-        const { uuid } = domainObject;
-        expect(uuid).toEqual(timerUUID);
-    });
+    // test('Provides same uuid for subsequent uses of the same object @framework', ({ domainObject }) => {
+    //     const { uuid } = domainObject;
+    //     expect(uuid).toEqual(timerUUID);
+    // });
 });
