66c7c53ea56ff723259d65d30f6d2caaff5adbc8
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-07-29 09:57:22 -0700

    exampleImagery tests compatibility fixes

--- e2e/tests/functional/plugins/imagery/exampleImagery.e2e.spec.js
+++ e2e/tests/functional/plugins/imagery/exampleImagery.e2e.spec.js
@@ -28,6 +28,7 @@ but only assume that example imagery is present.
 
 const { waitForAnimations } = require('../../../../baseFixtures');
 const { test, expect } = require('../../../../pluginFixtures');
+const { createDomainObjectWithDefaults } = require('../../../../appActions');
 const backgroundImageSelector = '.c-imagery__main-image__background-image';
 const panHotkey = process.platform === 'linux' ? ['Control', 'Alt'] : ['Alt'];
 const expectedAltText = process.platform === 'linux' ? 'Ctrl+Alt drag to pan' : 'Alt drag to pan';
@@ -39,24 +40,10 @@ test.describe('Example Imagery Object', () => {
         //Go to baseURL
         await page.goto('./', { waitUntil: 'networkidle' });
 
-        //Click the Create button
-        await page.click('button:has-text("Create")');
+        // Create a default 'Example Imagery' object
+        createDomainObjectWithDefaults(page, 'Example Imagery');
 
-        // Click text=Example Imagery
-        await page.click('text=Example Imagery');
-
-        // Click text=OK
-        await Promise.all([
-            page.waitForNavigation({waitUntil: 'networkidle'}),
-            page.click('text=OK'),
-            //Wait for Save Banner to appear
-            page.waitForSelector('.c-message-banner__message')
-        ]);
-        // Close Banner
-        await page.locator('.c-message-banner__close-button').click();
-
-        //Wait until Save Banner is gone
-        await page.waitForSelector('.c-message-banner__message', { state: 'detached'});
+        // Verify that the created imagery object is focused
         await expect(page.locator('.l-browse-bar__object-name')).toContainText('Unnamed Example Imagery');
         await page.locator(backgroundImageSelector).hover({trial: true});
     });
