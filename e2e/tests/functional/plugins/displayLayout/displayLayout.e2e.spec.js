7b647d672e915fc3192fa504821bedf11df7dd73
Khalid Adil <khalidadil29@gmail.com>
2022-08-08 14:03:06 -0500

    Add more documentation to clarify test intent

--- e2e/tests/functional/plugins/displayLayout/displayLayout.e2e.spec.js
+++ e2e/tests/functional/plugins/displayLayout/displayLayout.e2e.spec.js
@@ -25,32 +25,36 @@ const { createDomainObjectWithDefaults, setStartOffset, setFixedTimeMode, setRea
 
 test.describe('Testing Display Layout @unstable', () => {
     let sineWaveObject;
-    test.beforeEach(async ({ page, openmctConfig }) => {
+    test.beforeEach(async ({ page }) => {
         await page.goto('./', { waitUntil: 'networkidle' });
         await setRealTimeMode(page);
+
+        // Create Sine Wave Generator
         sineWaveObject = await createDomainObjectWithDefaults(page, {
             type: 'Sine Wave Generator',
             name: "Test Sine Wave Generator"
         });
-
-        const { myItemsFolderName } = openmctConfig;
-        await page.locator(`text=${myItemsFolderName}`).first().click();
     });
     test('alpha-numeric widget telemetry value exactly matches latest telemetry value received in real time', async ({ page }) => {
+        // Create a Display Layout
         await createDomainObjectWithDefaults(page, {
             type: 'Display Layout',
             name: "Test Display Layout"
         });
+        // Edit Display Layout
         await page.locator('[title="Edit"]').click();
 
+        // Expand the 'My Items' folder in the left tree
         await page.locator('.c-tree__item__view-control.c-disclosure-triangle').click();
+        // Add the Sine Wave Generator to the Display Layout and save changes
         await page.dragAndDrop('text=Test Sine Wave Generator', '.l-layout__grid-holder');
-
         await page.locator('button[title="Save"]').click();
         await page.locator('text=Save and Finish Editing').click();
 
+        // Subscribe to the Sine Wave Generator data
+        // On getting data, check if the value found in the  Display Layout is the most recent value
+        // from the Sine Wave Generator
         const getTelemValuePromise = await subscribeToTelemetry(page, sineWaveObject.uuid);
-
         const formattedTelemetryValue = await getTelemValuePromise;
         const displayLayoutValuePromise = await page.waitForSelector(`text="${formattedTelemetryValue}"`);
         const displayLayoutValue = await displayLayoutValuePromise.textContent();
@@ -59,22 +63,29 @@ test.describe('Testing Display Layout @unstable', () => {
         await expect(trimmedDisplayValue).toBe(formattedTelemetryValue);
     });
     test('alpha-numeric widget telemetry value exactly matches latest telemetry value received in fixed time', async ({ page }) => {
+        // Create a Display Layout
         await createDomainObjectWithDefaults(page, {
             type: 'Display Layout',
             name: "Test Display Layout"
         });
+        // Edit Display Layout
         await page.locator('[title="Edit"]').click();
 
+        // Expand the 'My Items' folder in the left tree
         await page.locator('.c-tree__item__view-control.c-disclosure-triangle').click();
+        // Add the Sine Wave Generator to the Display Layout and save changes
         await page.dragAndDrop('text=Test Sine Wave Generator', '.l-layout__grid-holder');
-
         await page.locator('button[title="Save"]').click();
         await page.locator('text=Save and Finish Editing').click();
 
+        // Subscribe to the Sine Wave Generator data
         const getTelemValuePromise = await subscribeToTelemetry(page, sineWaveObject.uuid);
+        // Set an offset of 1 minute and then change the time mode to fixed to set a 1 minute historical window
         await setStartOffset(page, { mins: '1' });
         await setFixedTimeMode(page);
 
+        // On getting data, check if the value found in the Display Layout is the most recent value
+        // from the Sine Wave Generator
         const formattedTelemetryValue = await getTelemValuePromise;
         const displayLayoutValuePromise = await page.waitForSelector(`text="${formattedTelemetryValue}"`);
         const displayLayoutValue = await displayLayoutValuePromise.textContent();
@@ -86,8 +97,12 @@ test.describe('Testing Display Layout @unstable', () => {
 
 /**
  * Util for subscribing to a telemetry object by object identifier
+ * Limitations: Currently only works to return telemetry once to the node scope
+ * To Do: See if there's a way to await this multiple times to allow for multiple
+ * values to be returned over time
  * @param {import('@playwright/test').Page} page
  * @param {string} objectIdentifier identifier for object
+ * @returns {Promise<string>} the formatted sin telemetry value
  */
 async function subscribeToTelemetry(page, objectIdentifier) {
     const getTelemValuePromise = new Promise(resolve => page.exposeFunction('getTelemValue', resolve));
--- e2e/tests/functional/plugins/lad/lad.e2e.spec.js
+++ e2e/tests/functional/plugins/lad/lad.e2e.spec.js
@@ -25,30 +25,35 @@ const { createDomainObjectWithDefaults, setStartOffset, setFixedTimeMode, setRea
 
 test.describe('Testing LAD table @unstable', () => {
     let sineWaveObject;
-    test.beforeEach(async ({ page, openmctConfig }) => {
+    test.beforeEach(async ({ page }) => {
         await page.goto('./', { waitUntil: 'networkidle' });
         await setRealTimeMode(page);
+
+        // Create Sine Wave Generator
         sineWaveObject = await createDomainObjectWithDefaults(page, {
             type: 'Sine Wave Generator',
             name: "Test Sine Wave Generator"
         });
-
-        const { myItemsFolderName } = openmctConfig;
-        await page.locator(`text=${myItemsFolderName}`).first().click();
     });
     test('telemetry value exactly matches latest telemetry value received in real time', async ({ page }) => {
+        // Create LAD table
         await createDomainObjectWithDefaults(page, {
             type: 'LAD Table',
             name: "Test LAD Table"
         });
+        // Edit LAD table
         await page.locator('[title="Edit"]').click();
 
+        // Expand the 'My Items' folder in the left tree
         await page.locator('.c-tree__item__view-control.c-disclosure-triangle').click();
+        // Add the Sine Wave Generator to the LAD table and save changes
         await page.dragAndDrop('text=Test Sine Wave Generator', '.c-lad-table-wrapper');
-
         await page.locator('button[title="Save"]').click();
         await page.locator('text=Save and Finish Editing').click();
 
+        // Subscribe to the Sine Wave Generator data
+        // On getting data, check if the value found in the LAD table is the most recent value
+        // from the Sine Wave Generator
         const getTelemValuePromise = await subscribeToTelemetry(page, sineWaveObject.uuid);
         const subscribeTelemValue = await getTelemValuePromise;
         const ladTableValuePromise = await page.waitForSelector(`text="${subscribeTelemValue}"`);
@@ -57,22 +62,29 @@ test.describe('Testing LAD table @unstable', () => {
         expect(ladTableValue).toBe(subscribeTelemValue);
     });
     test('telemetry value exactly matches latest telemetry value received in fixed time', async ({ page }) => {
+        // Create LAD table
         await createDomainObjectWithDefaults(page, {
             type: 'LAD Table',
             name: "Test LAD Table"
         });
+        // Edit LAD table
         await page.locator('[title="Edit"]').click();
 
+        // Expand the 'My Items' folder in the left tree
         await page.locator('.c-tree__item__view-control.c-disclosure-triangle').click();
+        // Add the Sine Wave Generator to the LAD table and save changes
         await page.dragAndDrop('text=Test Sine Wave Generator', '.c-lad-table-wrapper');
-
         await page.locator('button[title="Save"]').click();
         await page.locator('text=Save and Finish Editing').click();
 
+        // Subscribe to the Sine Wave Generator data
         const getTelemValuePromise = await subscribeToTelemetry(page, sineWaveObject.uuid);
+        // Set an offset of 1 minute and then change the time mode to fixed to set a 1 minute historical window
         await setStartOffset(page, { mins: '1' });
         await setFixedTimeMode(page);
 
+        // On getting data, check if the value found in the LAD table is the most recent value
+        // from the Sine Wave Generator
         const subscribeTelemValue = await getTelemValuePromise;
         const ladTableValuePromise = await page.waitForSelector(`text="${subscribeTelemValue}"`);
         const ladTableValue = await ladTableValuePromise.textContent();
@@ -83,8 +95,12 @@ test.describe('Testing LAD table @unstable', () => {
 
 /**
  * Util for subscribing to a telemetry object by object identifier
+ * Limitations: Currently only works to return telemetry once to the node scope
+ * To Do: See if there's a way to await this multiple times to allow for multiple
+ * values to be returned over time
  * @param {import('@playwright/test').Page} page
  * @param {string} objectIdentifier identifier for object
+ * @returns {Promise<string>} the formatted sin telemetry value
  */
 async function subscribeToTelemetry(page, objectIdentifier) {
     const getTelemValuePromise = new Promise(resolve => page.exposeFunction('getTelemValue', resolve));
