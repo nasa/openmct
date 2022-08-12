fa21b0ba141a0ae5c8e2886b0a37fe305a6a7bc9
John Hill <john.c.hill@nasa.gov>
2022-08-12 07:50:35 -0700

    first push

---
+++ e2e/tests/functional/couchdb.e2e.spec.js
@@ -0,0 +1,64 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2022, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+/*
+* This test suite is meant to be executed against a couchdb container. More doc to come
+*
+*/
+
+const { test, expect } = require('../../baseFixtures');
+const { createDomainObjectWithDefaults } = require('../../appActions');
+
+test.describe("CouchDB Status Indicator", async () => {
+    //TODO BeforeAll Verify CouchDB Connectivity with APIContext
+    test('Shows green if connected', async ({ page, context }) => {
+        context.routeFromHAR(path.join(__dirname, './har/CouchDBResponse200.har'), {
+            // Respond with 200 for all CouchDB requests
+            url: '**/couchdb/**'
+        });
+
+        //Go to baseURL
+        await page.goto('./#/browse/mine?hideTree=true&hideInspector=true', { waitUntil: 'networkidle' });
+        await expect(page.locator('div:has-text("CouchDB is connected")').nth(3)).toBeVisible();
+    });
+    test('Shows red if not connected', async ({ page, context }) => {
+        context.routeFromHAR(path.join(__dirname, './har/CouchDBResponse503.har'), {
+            // Respond with 503 for all CouchDB requests
+            url: '**/couchdb/**'
+        });
+
+        //Go to baseURL
+        await page.goto('./#/browse/mine?hideTree=true&hideInspector=true', { waitUntil: 'networkidle' });
+        await expect(page.locator('div:has-text("CouchDB is offline")').nth(3)).toBeVisible();
+    });
+    test('Shows unknown if it receives an unexpected response code', async ({ page, context }) => {
+        context.routeFromHAR(path.join(__dirname, './har/CouchDBResponse418.har'), {
+            // Respond with 418 for all CouchDB requests
+            url: '**/couchdb/**'
+        });
+
+        //Go to baseURL
+        await page.goto('./#/browse/mine?hideTree=true&hideInspector=true', { waitUntil: 'networkidle' });
+        await expect(page.locator('div:has-text("CouchDB connectivity unknown")').nth(3)).toBeVisible();
+    });
+});
+
