313390f1fef3e265c1cc4bf27c864baa5d250115
Shefali <simplyrender@gmail.com>
2022-08-22 11:19:18 -0700

    Use a transaction when saving properties for an object

--- src/plugins/formActions/EditPropertiesAction.js
+++ src/plugins/formActions/EditPropertiesAction.js
@@ -64,9 +64,17 @@ export default class EditPropertiesAction extends PropertiesAction {
                 }
 
                 this.openmct.objects.mutate(this.domainObject, `${key}`, value);
-                this.openmct.notifications.info('Save successful');
             });
+            if (this.openmct.editor.isEditing()) {
+                this.openmct.editor.save();
+            }
+
+            this.openmct.notifications.info('Save successful');
         } catch (error) {
+            if (this.openmct.editor.isEditing()) {
+                this.openmct.editor.cancel();
+            }
+
             this.openmct.notifications.error('Error saving objects');
             console.error(error);
         }
@@ -77,6 +85,9 @@ export default class EditPropertiesAction extends PropertiesAction {
      */
     _onCancel() {
         //noop
+        if (this.openmct.editor.isEditing()) {
+            this.openmct.editor.cancel();
+        }
     }
 
     /**
@@ -88,6 +99,12 @@ export default class EditPropertiesAction extends PropertiesAction {
         const createWizard = new CreateWizard(this.openmct, this.domainObject, objectPath[1]);
         const formStructure = createWizard.getFormStructure(false);
         formStructure.title = 'Edit ' + this.domainObject.name;
+        //If we're in edit mode AND want to edit properties for the same domain object
+        //In this case, saving will put the object in view-only mode
+        //TODO: Maybe we should block editing properties if someone is in Edit mode?
+        if (!this.openmct.editor.isEditing()) {
+            this.openmct.editor.edit();
+        }
 
         return this.openmct.forms.showForm(formStructure)
             .then(this._onSave.bind(this))
