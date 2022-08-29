5d357053eab23bcd7a94a0bf70aaf6adb64694d4
Jamie V <jamie.j.vigliotta@nasa.gov>
2022-08-29 13:56:40 -0700

    checking inf binding is the issue

--- src/plugins/persistence/couch/CouchObjectProvider.js
+++ src/plugins/persistence/couch/CouchObjectProvider.js
@@ -42,6 +42,7 @@ class CouchObjectProvider {
         this.batchIds = [];
         this.onEventMessage = this.onEventMessage.bind(this);
         this.onEventError = this.onEventError.bind(this);
+        this.#startSharedWorker = this.#startSharedWorker.bind(this);
     }
 
     /**
