b282c14c072941cb2a727160245e9d627012b20f
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-09-08 16:36:32 -0700

    Increase visual test timeout to 2 minutes

--- e2e/playwright-visual.config.js
+++ e2e/playwright-visual.config.js
@@ -7,7 +7,7 @@ const config = {
     retries: 0, // visual tests should never retry due to snapshot comparison errors
     testDir: 'tests/visual',
     testMatch: '**/*.visual.spec.js', // only run visual tests
-    timeout: 60 * 1000,
+    timeout: (60 * 1000) * 2, // 2 minutes
     workers: 2, //Limit to 2 for CircleCI Agent
     webServer: {
         command: 'cross-env NODE_ENV=test npm run start',
