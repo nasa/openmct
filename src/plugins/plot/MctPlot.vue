90322330db5342205f394f612cf9ae4f070748f2
Khalid Adil <khalidadil29@gmail.com>
2022-09-27 07:11:58 -0500

    [Plot] Set yKey on each plot series (#5790)
    * Set yKey on each model
    
    * Add a test

--- src/plugins/plot/MctPlot.vue
+++ src/plugins/plot/MctPlot.vue
@@ -1109,7 +1109,9 @@ export default {
         },
 
         setYAxisKey(yKey) {
-            this.config.series.models[0].set('yKey', yKey);
+            this.config.series.models.forEach((model) => {
+                model.set('yKey', yKey);
+            });
         },
 
         pause() {
