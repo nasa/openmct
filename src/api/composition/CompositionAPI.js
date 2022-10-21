762cc36d3c16b6be665452f611a7163131c6df06
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2022-10-21 14:14:37 -0700

    Rename file and fix references

--- src/api/composition/CompositionAPI.js
+++ src/api/composition/CompositionAPI.js
@@ -20,7 +20,7 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-import CompositionProvider from './CompositionProvider';
+import CompositionProvider from './DefaultCompositionProvider';
 import CompositionCollection from './CompositionCollection';
 
 /**
