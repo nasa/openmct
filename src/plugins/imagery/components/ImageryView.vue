f92b0a45a1bb36ce120e497f4619cf81cb9c1500
Jesse Mazzella <ozyx@users.noreply.github.com>
2022-12-28 11:12:00 -0800

    fix(imagery): Unblock 'latest' strategy requests for Related Telemetry in realtime mode (#6080)
    * fix: use ephemeral timeContext for thumbnail metadata requests
    
    * fix(TEMP): use `eval-source-map`
    
    - **!!! REVERT THIS CHANGE BEFORE MERGE !!!**
    
    * fix: only mutate if object supports mutation
    
    * fix: pass identifier instead of whole domainObject
    
    * fix: add start and end bounds to request
    
    * Revert "fix(TEMP): use `eval-source-map`"
    
    This reverts commit 7972d8c33ab621e8852d65743e2adf946e5c14fb.
    
    * docs: add comments

--- src/plugins/imagery/components/ImageryView.vue
+++ src/plugins/imagery/components/ImageryView.vue
@@ -774,7 +774,7 @@ export default {
             }
         },
         persistVisibleLayers() {
-            if (this.domainObject.configuration) {
+            if (this.domainObject.configuration && this.openmct.objects.supportsMutation(this.domainObject.identifier)) {
                 this.openmct.objects.mutate(this.domainObject, 'configuration.layers', this.layers);
             }
 
