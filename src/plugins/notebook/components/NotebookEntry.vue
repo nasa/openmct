1a69382738ad028a2d3623ac4cb5d5708169fa86
Jamie V <jamie.j.vigliotta@nasa.gov>
2022-12-30 10:45:46 -0800

    removing contenteditable=false since it is not needed anymore do to toggling content editability on mouseover

--- src/plugins/notebook/components/NotebookEntry.vue
+++ src/plugins/notebook/components/NotebookEntry.vue
@@ -153,7 +153,7 @@ import Moment from 'moment';
 const SANITIZATION_SCHEMA = {
     allowedTags: ['a'],
     allowedAttributes: {
-        'a': ['target', 'contenteditable', 'class', 'href']
+        'a': ['target', 'class', 'href']
     }
 }
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
