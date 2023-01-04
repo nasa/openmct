f7336833b51bb586e533f6e4428cbfa6922811f5
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2023-01-04 15:00:30 -0800

    fix: identifier regexp works for telemetry also

--- src/api/objects/ObjectAPI.js
+++ src/api/objects/ObjectAPI.js
@@ -747,7 +747,7 @@ export default class ObjectAPI {
      * @returns {DomainObject[]} objectPath
      */
     async getRelativeObjectPath(navigationPath) {
-        const identifierRegexp = /mine|[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/gi;
+        const identifierRegexp = /mine|\/.*\/?(?=\?)/gi;
         const identifiers = navigationPath.split('?')[0].match(identifierRegexp);
         if (!identifiers) {
             return [];
