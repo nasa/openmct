eddc021b4dd58779ddc797fb5d8d955e0b4ec59a
David Tsay <david.e.tsay@nasa.gov>
2023-01-17 11:31:44 -0800

    update to use new inspector tabs

--- src/ui/inspector/Inspector.vue
+++ src/ui/inspector/Inspector.vue
@@ -24,84 +24,27 @@
 <div class="c-inspector">
     <object-name />
     <InspectorTabs
+        :selection="selection"
         :is-editing="isEditing"
         @select-tab="selectTab"
     />
     <InspectorViews
+        :selection="selection"
         :selected-tab="selectedTab"
     />
-    <div
-        v-if="showStyles"
-        class="c-inspector__tabs c-tabs"
-    >
-        <div
-            v-for="tabbedView in tabbedViews"
-            :key="tabbedView.key"
-            class="c-inspector__tab c-tab"
-            :class="{'is-current': isCurrent(tabbedView)}"
-            @click="updateCurrentTab(tabbedView)"
-        >
-            {{ tabbedView.name }}
-        </div>
-
-    </div>
-    <div class="c-inspector__content">
-        <multipane
-            v-show="currentTabbedView.key === '__properties'"
-            type="vertical"
-        >
-            <pane class="c-inspector__properties">
-                <Properties
-                    v-if="!activity"
-                />
-                <InspectorViews />
-            </pane>
-        </multipane>
-        <multipane
-            v-show="currentTabbedView.key === '__styles'"
-            type="vertical"
-        >
-            <pane class="c-inspector__styles">
-                <StylesInspectorView />
-            </pane>
-            <pane
-                v-if="isEditing"
-                class="c-inspector__saved-styles"
-                handle="before"
-                label="Saved Styles"
-            >
-                <SavedStylesInspectorView :is-editing="isEditing" />
-            </pane>
-        </multipane>
-    </div>
 </div>
 </template>
 
 <script>
-import multipane from '../layout/multipane.vue';
-import pane from '../layout/pane.vue';
-import Properties from './details/Properties.vue';
 import ObjectName from './ObjectName.vue';
 import InspectorTabs from './InspectorTabs.vue';
 import InspectorViews from './InspectorViews.vue';
-import _ from "lodash";
-import stylesManager from '@/ui/inspector/styles/StylesManager';
-import StylesInspectorView from '@/ui/inspector/styles/StylesInspectorView.vue';
-import SavedStylesInspectorView from '@/ui/inspector/styles/SavedStylesInspectorView.vue';
 
 export default {
     components: {
-        StylesInspectorView,
-        SavedStylesInspectorView,
-        multipane,
-        pane,
-        Properties,
         ObjectName,
         InspectorTabs,
         InspectorViews
-    },
-    provide: {
-        stylesManager: stylesManager
     },
     inject: ['openmct'],
     props: {
@@ -112,78 +55,19 @@ export default {
     },
     data() {
         return {
-            selectedTab: undefined,
-            hasComposition: false,
-            showStyles: false,
-            tabbedViews: [{
-                key: '__properties',
-                name: 'Properties'
-            }, {
-                key: '__styles',
-                name: 'Styles'
-            }],
-            currentTabbedView: {},
-            activity: undefined
+            selection: this.openmct.selection.get(),
+            selectedTab: undefined
         };
     },
     mounted() {
-        this.excludeObjectTypes = ['folder', 'webPage', 'conditionSet', 'summary-widget', 'hyperlink'];
-        this.openmct.selection.on('change', this.updateInspectorViews);
+        this.openmct.selection.on('change', this.setSelection);
     },
     destroyed() {
-        this.openmct.selection.off('change', this.updateInspectorViews);
+        this.openmct.selection.off('change', this.setSelection);
     },
     methods: {
-        updateInspectorViews(selection) {
-            this.refreshComposition(selection);
-
-            if (this.openmct.types.get('conditionSet')) {
-                this.refreshTabs(selection);
-            }
-
-            this.setActivity(selection);
-        },
-        refreshComposition(selection) {
-            if (selection.length > 0 && selection[0].length > 0) {
-                let parentObject = selection[0][0].context.item;
-
-                this.hasComposition = Boolean(parentObject && this.openmct.composition.get(parentObject));
-            }
-        },
-        refreshTabs(selection) {
-            if (selection.length > 0 && selection[0].length > 0) {
-                //layout items are not domain objects but should allow conditional styles
-                this.showStyles = selection[0][0].context.layoutItem;
-                let object = selection[0][0].context.item;
-                if (object) {
-                    let type = this.openmct.types.get(object.type);
-                    this.showStyles = this.isLayoutObject(selection[0], object.type) || this.isCreatableObject(object, type);
-                }
-
-                if (!this.currentTabbedView.key || (!this.showStyles && this.currentTabbedView.key === this.tabbedViews[1].key)) {
-                    this.updateCurrentTab(this.tabbedViews[0]);
-                }
-            }
-        },
-        isLayoutObject(selection, objectType) {
-            //we allow conditionSets to be styled if they're part of a layout
-            return selection.length > 1
-                && ((objectType === 'conditionSet') || (this.excludeObjectTypes.indexOf(objectType) < 0));
-        },
-        isCreatableObject(object, type) {
-            return (this.excludeObjectTypes.indexOf(object.type) < 0) && type.definition.creatable;
-        },
-        updateCurrentTab(view) {
-            this.currentTabbedView = view;
-        },
-        isCurrent(view) {
-            return _.isEqual(this.currentTabbedView, view);
-        },
-        setActivity(selection) {
-            this.activity = selection
-                && selection.length
-                && selection[0].length
-                && selection[0][0].activity;
+        setSelection(selection) {
+            this.selection = selection;
         },
         selectTab(tab) {
             this.selectedTab = tab;
