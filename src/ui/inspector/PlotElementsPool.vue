ec5524f2ff7cf9b829137d845e6e1380279b640a
Rukmini Bose <rukmini.bose15@gmail.com>
2023-01-17 17:22:07 -0800

    Add instruction message to Elements Pool

--- src/ui/inspector/PlotElementsPool.vue
+++ src/ui/inspector/PlotElementsPool.vue
@@ -36,6 +36,7 @@
             id="inspector-elements-tree"
             class="c-tree c-elements-pool__tree"
         >
+            <div class="c-elements-pool__instructions"> Select and drag an element to move it into a different axis. </div>
             <element-item-group
                 v-for="(yAxis, index) in yAxes"
                 :key="`element-group-yaxis-${yAxis.id}`"
