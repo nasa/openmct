7c0ff51cc90dbdb37c67e6cfc69149a370dae21b
David Tsay <david.e.tsay@nasa.gov>
2023-01-19 14:35:41 -0800

    also apply bandaid to this test

--- e2e/tests/functional/moveAndLinkObjects.e2e.spec.js
+++ e2e/tests/functional/moveAndLinkObjects.e2e.spec.js
@@ -166,7 +166,7 @@ test.describe('Move & link item tests', () => {
 
         // Attempt to link parent to its own grandparent
         await page.locator(`text=Open MCT ${myItemsFolderName} >> span`).nth(3).click();
-        await page.locator('.c-disclosure-triangle >> nth=0').click();
+        // await page.locator('.c-disclosure-triangle >> nth=0').click();
 
         await page.locator(`a:has-text("Parent Folder") >> nth=0`).click({
             button: 'right'
@@ -187,7 +187,7 @@ test.describe('Move & link item tests', () => {
         await page.locator('[aria-label="Cancel"]').click();
 
         // Link Child Folder from Parent Folder to My Items
-        await page.locator('.c-disclosure-triangle >> nth=0').click();
+        // await page.locator('.c-disclosure-triangle >> nth=0').click();
         await page.locator('.c-disclosure-triangle >> nth=1').click();
 
         await page.locator(`a:has-text("Child Folder") >> nth=0`).click({
