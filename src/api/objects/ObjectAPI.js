cda137cc5749880bec366a5d0caf0c2958e064ae
Jamie V <jamie.j.vigliotta@nasa.gov>
2023-02-06 09:56:50 -0800

    checking if domainObject supports mutation before getting mutable

--- src/api/objects/ObjectAPI.js
+++ src/api/objects/ObjectAPI.js
@@ -225,22 +225,21 @@ export default class ObjectAPI {
             throw new Error('Provider does not support get!');
         }
 
-        let objectPromise = provider.get(identifier, abortSignal).then(result => {
+        let objectPromise = provider.get(identifier, abortSignal).then(domainObject => {
             delete this.cache[keystring];
+            domainObject = this.applyGetInterceptors(identifier, domainObject);
 
-            result = this.applyGetInterceptors(identifier, result);
+            if (this.supportsMutation(identifier)) {
+                const mutableDomainObject = this.toMutable(domainObject);
+                mutableDomainObject.$refresh(domainObject);
+                this.destroyMutable(mutableDomainObject);
+            }
 
-            const mutableDomainObject = this.toMutable(result);
-            mutableDomainObject.$refresh(result);
-            this.destroyMutable(mutableDomainObject);
-
-            return result;
-        }).catch((result) => {
-            console.warn(`Failed to retrieve ${keystring}:`, result);
-
+            return domainObject;
+        }).catch((error) => {
+            console.warn(`Failed to retrieve ${keystring}:`, error);
             delete this.cache[keystring];
-
-            result = this.applyGetInterceptors(identifier);
+            const result = this.applyGetInterceptors(identifier);
 
             return result;
         });
