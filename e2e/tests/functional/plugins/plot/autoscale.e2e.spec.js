bab8bf0b2ac0eb837cecf980cd25522a14518e04
Shefali <simplyrender@gmail.com>
2023-02-07 10:18:19 -0800

    Fix autoscale e2e test

--- e2e/tests/functional/plugins/plot/autoscale.e2e.spec.js
+++ e2e/tests/functional/plugins/plot/autoscale.e2e.spec.js
@@ -47,10 +47,26 @@ test.describe('ExportAsJSON', () => {
 
         await testYTicks(page, ['-1.00', '-0.50', '0.00', '0.50', '1.00']);
 
+        // enter edit mode
+        await page.locator('text=Unnamed Overlay Plot Snapshot >> button').nth(3).click();
+
         await turnOffAutoscale(page);
 
-        // Make sure that after turning off autoscale, the user selected range values start at the same values the plot had prior.
-        await testYTicks(page, ['-1.00', '-0.50', '0.00', '0.50', '1.00']);
+        await setUserDefinedMinAndMax(page, '-2', '2');
+
+        // save
+        await page.locator('text=Snapshot Save and Finish Editing Save and Continue Editing >> button').nth(1).click();
+        await Promise.all([
+            page.locator('text=Save and Finish Editing').click(),
+            //Wait for Save Banner to appear
+            page.waitForSelector('.c-message-banner__message')
+        ]);
+        //Wait until Save Banner is gone
+        await page.locator('.c-message-banner__close-button').click();
+        await page.waitForSelector('.c-message-banner__message', { state: 'detached'});
+
+        // Make sure that after turning off autoscale, the user entered range values are reflexted in the ticks.
+        await testYTicks(page, ['-2.00', '-1.50', '-1.00', '-0.50', '0.00', '0.50', '1.00', '1.50', '2.00']);
 
         const canvas = page.locator('canvas').nth(1);
 
@@ -76,7 +92,7 @@ test.describe('ExportAsJSON', () => {
         await page.keyboard.up('Alt');
 
         // Ensure the drag worked.
-        await testYTicks(page, ['0.00', '0.50', '1.00', '1.50', '2.00']);
+        await testYTicks(page, ['-1.00', '-0.50', '0.00', '0.50', '1.00', '1.50', '2.00', '2.50', '3.00']);
 
         await canvas.hover({trial: true});
 
@@ -152,22 +168,25 @@ async function createSinewaveOverlayPlot(page, myItemsFolderName) {
  * @param {import('@playwright/test').Page} page
  */
 async function turnOffAutoscale(page) {
-    // enter edit mode
-    await page.locator('text=Unnamed Overlay Plot Snapshot >> button').nth(3).click();
-
     // uncheck autoscale
     await page.getByRole('listitem').filter({ hasText: 'Auto scale' }).getByRole('checkbox').uncheck();
+}
 
-    // save
-    await page.locator('text=Snapshot Save and Finish Editing Save and Continue Editing >> button').nth(1).click();
-    await Promise.all([
-        page.locator('text=Save and Finish Editing').click(),
-        //Wait for Save Banner to appear
-        page.waitForSelector('.c-message-banner__message')
-    ]);
-    //Wait until Save Banner is gone
-    await page.locator('.c-message-banner__close-button').click();
-    await page.waitForSelector('.c-message-banner__message', { state: 'detached'});
+/**
+ * @param {import('@playwright/test').Page} page
+ * @param {string} min
+ * @param {string} max
+ */
+async function setUserDefinedMinAndMax(page, min, max) {
+    // set minimum value
+    const minRangeInput = page.getByRole('listitem').filter({ hasText: 'Minimum Value' }).locator('input[type="number"]');
+    await minRangeInput.click();
+    await minRangeInput.fill(min);
+
+    // set maximum value
+    const maxRangeInput = page.getByRole('listitem').filter({ hasText: 'Maximum Value' }).locator('input[type="number"]');
+    await maxRangeInput.click();
+    await maxRangeInput.fill(max);
 }
 
 /**
