a1ace6ec8e7b20d09f324e574049fb7ca4dfbe45
Vitor Henckel <vitor@henckel.com.br>
2023-03-31 14:48:30 -0300

    test: After deactivating the button, new objects must be inserted and the button becomes active again

--- e2e/tests/functional/recentObjects.e2e.spec.js
+++ e2e/tests/functional/recentObjects.e2e.spec.js
@@ -274,6 +274,44 @@ test.describe('Recent Objects', () => {
                 .isEnabled()
         ).toBe(false);
     });
+    test("After deactivating the button, new objects must be inserted and the button becomes active again.", async ({ page }) => {
+        // Assert that the list initially contains 3 objects (clock, folder, my items)
+        expect(await recentObjectsList.locator('.c-recentobjects-listitem').count()).toBe(3);
+
+        // Click the aria-label="Clear Recently Viewed" button
+        await page.getByRole("button", { name: "Clear Recently Viewed" }).click();
+
+        // Click on the "OK" button in the confirmation dialog
+        await page.getByRole("button", { name: "OK" }).click();
+
+        // Assert that the list is empty
+        expect(
+            await recentObjectsList.locator(".c-recentobjects-listitem").count()
+        ).toBe(0);
+
+        // Assert that the button is disabled
+        expect(
+            await page
+                .getByRole("button", { name: "Clear Recently Viewed" })
+                .isEnabled()
+        ).toBe(false);
+
+        // Create a new object
+        clock = await createDomainObjectWithDefaults(page, {
+            type: 'Clock',
+            parent: folderA.uuid
+        });
+
+        // Assert that the list contains 1 object
+        expect(await recentObjectsList.locator('.c-recentobjects-listitem').count()).toBe(1);
+
+        // Assert that the button is enabled
+        expect(
+            await page
+                .getByRole("button", { name: "Clear Recently Viewed" })
+                .isEnabled()
+        ).toBe(true);
+    });
 
     function assertInitialRecentObjectsListState() {
         expect(recentObjectsList.getByRole('listitem', { name: clock.name })).toBeTruthy();
