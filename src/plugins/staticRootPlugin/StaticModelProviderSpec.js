8b295ab56e5125f4d867aef5c875d69d0075f66e
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2023-04-10 16:20:35 -0700

    tets: add non-empty namespace tests (wip)

--- src/plugins/staticRootPlugin/StaticModelProviderSpec.js
+++ src/plugins/staticRootPlugin/StaticModelProviderSpec.js
@@ -21,129 +21,235 @@
  *****************************************************************************/
 
 import testStaticData from './static-provider-test.json';
+import testStaticData2 from './static-provider-test2.json';
 import StaticModelProvider from './StaticModelProvider';
 
 describe('StaticModelProvider', function () {
+    describe('with empty namespace', function () {
 
-    let staticProvider;
-
-    beforeEach(function () {
-        const staticData = JSON.parse(JSON.stringify(testStaticData));
-        staticProvider = new StaticModelProvider(staticData, {
-            namespace: 'my-import',
-            key: 'root'
-        });
-    });
-
-    describe('rootObject', function () {
-        let rootModel;
+        let staticProvider;
 
         beforeEach(function () {
-            rootModel = staticProvider.get({
+            const staticData = JSON.parse(JSON.stringify(testStaticData));
+            staticProvider = new StaticModelProvider(staticData, {
                 namespace: 'my-import',
                 key: 'root'
             });
         });
 
-        it('is located at top level', function () {
-            expect(rootModel.location).toBe('ROOT');
+        describe('rootObject', function () {
+            let rootModel;
+
+            beforeEach(function () {
+                rootModel = staticProvider.get({
+                    namespace: 'my-import',
+                    key: 'root'
+                });
+            });
+
+            it('is located at top level', function () {
+                expect(rootModel.location).toBe('ROOT');
+            });
+
+            it('has new-format identifier', function () {
+                expect(rootModel.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: 'root'
+                });
+            });
+
+            it('has new-format composition', function () {
+                expect(rootModel.composition).toContain({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+                expect(rootModel.composition).toContain({
+                    namespace: 'my-import',
+                    key: '2'
+                });
+            });
         });
 
-        it('has new-format identifier', function () {
-            expect(rootModel.identifier).toEqual({
+        describe('childObjects', function () {
+            let swg;
+            let layout;
+            let fixed;
+
+            beforeEach(function () {
+                swg = staticProvider.get({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+                layout = staticProvider.get({
+                    namespace: 'my-import',
+                    key: '2'
+                });
+                fixed = staticProvider.get({
+                    namespace: 'my-import',
+                    key: '3'
+                });
+            });
+
+            it('match expected ordering', function () {
+                // this is a sanity check to make sure the identifiers map in
+                // the correct order.
+                expect(swg.type).toBe('generator');
+                expect(layout.type).toBe('layout');
+                expect(fixed.type).toBe('telemetry.fixed');
+            });
+
+            it('have new-style identifiers', function () {
+                expect(swg.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+                expect(layout.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: '2'
+                });
+                expect(fixed.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: '3'
+                });
+            });
+
+            it('have new-style composition', function () {
+                expect(layout.composition).toContain({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+                expect(layout.composition).toContain({
+                    namespace: 'my-import',
+                    key: '3'
+                });
+                expect(fixed.composition).toContain({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+            });
+
+            it('rewrites locations', function () {
+                expect(swg.location).toBe('my-import:root');
+                expect(layout.location).toBe('my-import:root');
+                expect(fixed.location).toBe('my-import:2');
+            });
+
+            it('rewrites matched identifiers in objects', function () {
+                expect(layout.configuration.layout.panels['my-import:1'])
+                    .toBeDefined();
+                expect(layout.configuration.layout.panels['my-import:3'])
+                    .toBeDefined();
+                expect(layout.configuration.layout.panels['483c00d4-bb1d-4b42-b29a-c58e06b322a0'])
+                    .not.toBeDefined();
+                expect(layout.configuration.layout.panels['20273193-f069-49e9-b4f7-b97a87ed755d'])
+                    .not.toBeDefined();
+                expect(fixed.configuration['fixed-display'].elements[0].id)
+                    .toBe('my-import:1');
+            });
+
+        });
+    });
+    describe('with non-empty namespace', function () {
+
+        let staticProvider;
+
+        beforeEach(function () {
+            const staticData = JSON.parse(JSON.stringify(testStaticData2));
+            staticProvider = new StaticModelProvider(staticData, {
                 namespace: 'my-import',
                 key: 'root'
             });
         });
 
-        it('has new-format composition', function () {
-            expect(rootModel.composition).toContain({
-                namespace: 'my-import',
-                key: '1'
-            });
-            expect(rootModel.composition).toContain({
-                namespace: 'my-import',
-                key: '2'
-            });
-        });
-    });
+        describe('rootObject', function () {
+            let rootModel;
 
-    describe('childObjects', function () {
-        let swg;
-        let layout;
-        let fixed;
+            beforeEach(function () {
+                rootModel = staticProvider.get({
+                    namespace: 'my-import',
+                    key: 'root'
+                });
+            });
 
-        beforeEach(function () {
-            swg = staticProvider.get({
-                namespace: 'my-import',
-                key: '1'
+            it('is located at top level', function () {
+                expect(rootModel.location).toBe('ROOT');
             });
-            layout = staticProvider.get({
-                namespace: 'my-import',
-                key: '2'
-            });
-            fixed = staticProvider.get({
-                namespace: 'my-import',
-                key: '3'
-            });
-        });
 
-        it('match expected ordering', function () {
-            // this is a sanity check to make sure the identifiers map in
-            // the correct order.
-            expect(swg.type).toBe('generator');
-            expect(layout.type).toBe('layout');
-            expect(fixed.type).toBe('telemetry.fixed');
-        });
+            it('has new-format identifier', function () {
+                expect(rootModel.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: 'root'
+                });
+            });
 
-        it('have new-style identifiers', function () {
-            expect(swg.identifier).toEqual({
-                namespace: 'my-import',
-                key: '1'
-            });
-            expect(layout.identifier).toEqual({
-                namespace: 'my-import',
-                key: '2'
-            });
-            expect(fixed.identifier).toEqual({
-                namespace: 'my-import',
-                key: '3'
+            it('has new-format composition', function () {
+                expect(rootModel.composition).toContain({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+                expect(rootModel.composition).toContain({
+                    namespace: 'my-import',
+                    key: '2'
+                });
             });
         });
 
-        it('have new-style composition', function () {
-            expect(layout.composition).toContain({
-                namespace: 'my-import',
-                key: '1'
+        describe('childObjects', function () {
+            let clock;
+            let layout;
+            let swg;
+
+            beforeEach(function () {
+                layout = staticProvider.get({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+                swg = staticProvider.get({
+                    namespace: 'my-import',
+                    key: '2'
+                });
+                clock = staticProvider.get({
+                    namespace: 'my-import',
+                    key: '3'
+                });
             });
-            expect(layout.composition).toContain({
-                namespace: 'my-import',
-                key: '3'
+
+            it('match expected ordering', function () {
+                // this is a sanity check to make sure the identifiers map in
+                // the correct order.
+                expect(swg.type).toBe('generator');
+                expect(layout.type).toBe('layout');
+                expect(clock.type).toBe('clock');
             });
-            expect(fixed.composition).toContain({
-                namespace: 'my-import',
-                key: '1'
+
+            it('have new-style identifiers', function () {
+                expect(swg.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: '2'
+                });
+                expect(layout.identifier).toEqual({
+                    namespace: 'my-import',
+                    key: '1'
+                });
+            });
+
+            it('have new-style composition', function () {
+                expect(layout.composition).toContain({
+                    namespace: 'my-import',
+                    key: '2'
+                });
+                expect(layout.composition).toContain({
+                    namespace: 'my-import',
+                    key: '3'
+                });
+            });
+
+            it('rewrites locations', function () {
+                expect(swg.location).toBe('my-import:root');
+                expect(layout.location).toBe('my-import:root');
+                expect(clock.location).toBe('my-import:root');
             });
         });
-
-        it('rewrites locations', function () {
-            expect(swg.location).toBe('my-import:root');
-            expect(layout.location).toBe('my-import:root');
-            expect(fixed.location).toBe('my-import:2');
-        });
-
-        it('rewrites matched identifiers in objects', function () {
-            expect(layout.configuration.layout.panels['my-import:1'])
-                .toBeDefined();
-            expect(layout.configuration.layout.panels['my-import:3'])
-                .toBeDefined();
-            expect(layout.configuration.layout.panels['483c00d4-bb1d-4b42-b29a-c58e06b322a0'])
-                .not.toBeDefined();
-            expect(layout.configuration.layout.panels['20273193-f069-49e9-b4f7-b97a87ed755d'])
-                .not.toBeDefined();
-            expect(fixed.configuration['fixed-display'].elements[0].id)
-                .toBe('my-import:1');
-        });
-
     });
 });
+
