91c3505ec0ebc6d22ce0f8d5387b170ef53b8f21
John Hill <john.c.hill@nasa.gov>
2023-04-14 14:24:19 -0700

    echo

--- .circleci/quick-bash.sh
+++ .circleci/quick-bash.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Get the container ID of the Docker container with image starting with "mcr.microsoft.com/playwright"
-container_id=$(docker ps --format '{{.ID}}\t{{.Image}}' | grep "mcr.microsoft.com/playwright" | cut -f1)
+playwright_container_id=$(docker ps --format '{{.ID}}\t{{.Image}}' | grep "mcr.microsoft.com/playwright" | cut -f1)
 
-if [ -z "$container_id" ]; then
+echo "Container ID: $playwright_container_id"
+
+if [ -z "$playwright_container_id" ]; then
   echo "No container found with image starting with 'mcr.microsoft.com/playwright'"
   exit 1
 fi
 
+echo "docker inspect $playwright_container_id"
 # Get the network associated with the found container
-network_name=$(docker inspect $container_id -f '{{range .NetworkSettings.Networks}}{{.NetworkID}}{{end}}')
+network_name=$(docker inspect $playwright_container_id -f '{{range .NetworkSettings.Networks}}{{.NetworkID}}{{end}}')
 
 if [ -z "$network_name" ]; then
   echo "No network found associated with the container"
@@ -18,4 +21,4 @@ fi
 
 # Connect the couch_couchdb1 network to the found network
 docker network connect couch_couchdb1 $network_name
-echo "Connected couch_couchdb1 to the network of the container with ID $container_id"
+echo "Connected couch_couchdb1 to the network of the container with ID $playwright_container_id"
