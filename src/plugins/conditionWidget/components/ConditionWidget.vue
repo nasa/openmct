bb96216f24d9e9a4ec0038f34874d187e45ac5c2
David Tsay <david.e.tsay@nasa.gov>
2023-04-21 17:57:55 -0700

    remove * listener
    react to url change

--- src/plugins/conditionWidget/components/ConditionWidget.vue
+++ src/plugins/conditionWidget/components/ConditionWidget.vue
@@ -23,9 +23,9 @@
 <template>
 <component
     :is="urlDefined ? 'a' : 'span'"
+    ref="conditionWidgetElement"
     class="c-condition-widget u-style-receiver js-style-receiver"
     :href="url"
-    :target="url ? '_BLANK' : ''"
 >
     <div class="c-condition-widget__label">
         {{ label }}
@@ -40,19 +40,26 @@ export default {
     inject: ['openmct', 'domainObject'],
     data: function () {
         return {
-            conditionalLabel: '',
-            conditionSetIdentifier: null,
-            domainObjectLabel: '',
-            url: null,
-            urlDefined: false,
-            useConditionSetOutputAsLabel: false
+            conditionalLabel: ''
         };
     },
     computed: {
+        urlDefined() {
+            return this.domainObject.url?.length > 0;
+        },
+        url() {
+            return this.urlDefined ? sanitizeUrl(this.domainObject.url) : null;
+        },
+        useConditionSetOutputAsLabel() {
+            return this.conditionSetIdentifier && this.domainObject.configuration.useConditionSetOutputAsLabel;
+        },
+        conditionSetIdentifier() {
+            return this.domainObject.configuration?.objectStyles?.conditionSetIdentifier;
+        },
         label() {
             return this.useConditionSetOutputAsLabel
                 ? this.conditionalLabel
-                : this.domainObjectLabel
+                : this.domainObject.label
             ;
         }
     },
@@ -66,19 +73,18 @@ export default {
                 this.listenToConditionSetChanges();
             },
             deep: true
+        },
+        urlDefined() {
+            console.log(this.urlDefined);
+            this.updateElementDataset();
         }
     },
     mounted() {
-        this.unlisten = this.openmct.objects.observe(this.domainObject, '*', this.updateDomainObject);
-
         if (this.domainObject) {
-            this.updateDomainObject(this.domainObject);
             this.listenToConditionSetChanges();
         }
     },
     beforeDestroy() {
-        this.conditionSetIdentifier = null;
-
         if (this.unlisten) {
             this.unlisten();
         }
@@ -122,30 +128,18 @@ export default {
 
             this.conditionalLabel = latestDatum.output || '';
         },
-        updateDomainObject(domainObject) {
-            if (this.domainObjectLabel !== domainObject.label) {
-                this.domainObjectLabel = domainObject.label;
-            }
+        async updateElementDataset() {
+            console.log('update');
+            let element = this.$refs.conditionWidgetElement;
+            let dataset = element.dataset;
+            const keys = Object.keys(dataset);
 
-            const urlDefined = domainObject.url && domainObject.url.length > 0;
-            if (this.urlDefined !== urlDefined) {
-                this.urlDefined = urlDefined;
-            }
+            console.log(Object.keys(dataset));
+            await this.$nextTick();
+            element = this.$refs.conditionWidgetElement;
+            dataset = element.dataset;
 
-            const url = this.urlDefined ? sanitizeUrl(domainObject.url) : null;
-            if (this.url !== url) {
-                this.url = url;
-            }
-
-            const conditionSetIdentifier = domainObject.configuration?.objectStyles?.conditionSetIdentifier;
-            if (conditionSetIdentifier && this.conditionSetIdentifier !== conditionSetIdentifier) {
-                this.conditionSetIdentifier = conditionSetIdentifier;
-            }
-
-            const useConditionSetOutputAsLabel = this.conditionSetIdentifier && domainObject.configuration.useConditionSetOutputAsLabel;
-            if (this.useConditionSetOutputAsLabel !== useConditionSetOutputAsLabel) {
-                this.useConditionSetOutputAsLabel = useConditionSetOutputAsLabel;
-            }
+            console.log(Object.keys(dataset));
         }
     }
 };
