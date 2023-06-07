20101911d38e611a6e545ae13ea839aa2f59ea6c
Michael Rogers <contact@mhrogers.com>
2023-06-07 17:36:53 -0500

    Reconnect channel on error and UserIndicator updates

--- src/api/user/RoleChannel.js
+++ src/api/user/RoleChannel.js
@@ -1,17 +1,32 @@
 import { BROADCAST_CHANNEL_NAME } from './constants';
 
 class RoleChannel {
+    constructor(openmct, channelName = BROADCAST_CHANNEL_NAME) {
+        this.openmct = openmct;
+        this.channelName = channelName;
+        this.roleChannel = undefined;
+    }
 
     createRoleChannel() {
-        this.roleChannel = new BroadcastChannel(BROADCAST_CHANNEL_NAME);
+        this.roleChannel = new BroadcastChannel(this.channelName);
+    }
+    subscribeToRole(cb) {
         this.roleChannel.onmessage = (event => {
             const role = event.data;
             this.openmct.user.setActiveRole(role);
+            if (cb) {
+                cb(role);
+            }
         });
     }
     unsubscribeToRole() {
         this.roleChannel.close();
     }
+    reconnect() {
+        this.roleChannel.close();
+        this.createRoleChannel();
+    }
+
     broadcastNewRole(role) {
         if (!this.roleChannel.name) {
             return false;
@@ -20,6 +35,8 @@ class RoleChannel {
         try {
             this.roleChannel.postMessage(role);
         } catch (e) {
+            this.reconnect();
+            this.broadcastNewRole(role);
             /** FIXME: there doesn't seem to be a reliable way to test for open/closed
              * status of a broadcast channel; channel.name exists even after the
              * channel is closed. Failure to update the subscribed tabs, should
@@ -31,5 +48,5 @@ class RoleChannel {
     }
 }
 
-export default new RoleChannel();
+export default RoleChannel;
 
