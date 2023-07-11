7e9bbb07dc08743c86a8c3770d94dc85a15e8643
Rukmini Bose <rukmini.bose15@gmail.com>
2023-07-11 14:54:27 -0700

    Fix to make theme easy to run

--- .webpack/webpack.common.js
+++ .webpack/webpack.common.js
@@ -39,7 +39,8 @@ const config = {
     couchDBChangesFeed: './src/plugins/persistence/couch/CouchChangesFeed.js',
     inMemorySearchWorker: './src/api/objects/InMemorySearchWorker.js',
     espressoTheme: './src/plugins/themes/espresso-theme.scss',
-    snowTheme: './src/plugins/themes/snow-theme.scss'
+    snowTheme: './src/plugins/themes/snow-theme.scss',
+    diyTheme: './src/plugins/themes/diy-theme.scss'
   },
   output: {
     globalObject: 'this',
