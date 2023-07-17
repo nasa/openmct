fd03cb91fa18b2d14e2a2b02ac824ee0f5c95af9
Jamie V <jamie.j.vigliotta@nasa.gov>
2023-07-17 15:10:21 -0700

    adding start/stop to independent time context as well

--- src/api/time/IndependentTimeContext.js
+++ src/api/time/IndependentTimeContext.js
@@ -226,10 +226,12 @@ class IndependentTimeContext extends TimeContext {
     const previousClock = this.activeClock;
     if (previousClock) {
       previousClock.off('tick', this.tick);
+      previousClock.stop();
     }
 
     this.activeClock = clock;
     this.activeClock.on('tick', this.tick);
+    this.activeClock.start();
 
     /**
      * The active clock has changed.
