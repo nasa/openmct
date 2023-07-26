f846e6c5a085ed6b2ab51b73167d68cb036cf480
Charles Hacskaylo <charlesh88@gmail.com>
2023-07-26 10:42:04 -0700

    Closes #6829 - CSS fixes for Firefox for popup width and input widths. - Changed markup and CSS for Safari   - Cannot apply CSS grid to form.   - Changed ul/li approach to calendar picker, other CSS mods (align-items: center) to fix height issues. - Modded _constants value to account for reduced height of new TC design. - Resolved conflicts while applying stashed changes.

--- src/plugins/timeConductor/timePopupRealtime.vue
+++ src/plugins/timeConductor/timePopupRealtime.vue
@@ -1,141 +1,143 @@
 <template>
-  <form ref="deltaInput" class="c-tc-input-popup__input-grid">
-    <div class="pr-time-label icon-minus">Hrs</div>
-    <div class="pr-time-label">Mins</div>
-    <div class="pr-time-label">Secs</div>
-    <div class="pr-time-label"></div>
-    <div class="pr-time-label icon-plus">Hrs</div>
-    <div class="pr-time-label">Mins</div>
-    <div class="pr-time-label">Secs</div>
-    <div class="pr-time-label"></div>
+  <form ref="deltaInput">
+    <div class="c-tc-input-popup__input-grid">
+      <div class="pr-time-label icon-minus">Hrs</div>
+      <div class="pr-time-label">Mins</div>
+      <div class="pr-time-label">Secs</div>
+      <div class="pr-time-label"></div>
+      <div class="pr-time-label icon-plus">Hrs</div>
+      <div class="pr-time-label">Mins</div>
+      <div class="pr-time-label">Secs</div>
+      <div class="pr-time-label"></div>
 
-    <div class="pr-time-input">
-      <input
-        ref="startInputHrs"
-        v-model="startInputHrs"
-        class="pr-time-input__hrs"
-        step="1"
-        type="number"
-        min="0"
-        max="23"
-        title="Enter 0 - 23"
-        aria-label="Start offset hours"
-        @change="validate()"
-        @keyup="validate()"
-        @focusin="selectAll($event)"
-        @focusout="format('startInputHrs')"
-        @wheel="increment($event, 'startInputHrs')"
-      />
-      <b>:</b>
-    </div>
-    <div class="pr-time-input">
-      <input
-        ref="startInputMins"
-        v-model="startInputMins"
-        type="number"
-        class="pr-time-input__mins"
-        min="0"
-        max="59"
-        title="Enter 0 - 59"
-        step="1"
-        aria-label="Start offset minutes"
-        @change="validate()"
-        @keyup="validate()"
-        @focusin="selectAll($event)"
-        @focusout="format('startInputMins')"
-        @wheel="increment($event, 'startInputMins')"
-      />
-      <b>:</b>
-    </div>
-    <div class="pr-time-input">
-      <input
-        ref="startInputSecs"
-        v-model="startInputSecs"
-        type="number"
-        class="pr-time-input__secs"
-        min="0"
-        max="59"
-        title="Enter 0 - 59"
-        step="1"
-        aria-label="Start offset seconds"
-        @change="validate()"
-        @keyup="validate()"
-        @focusin="selectAll($event)"
-        @focusout="format('startInputSecs')"
-        @wheel="increment($event, 'startInputSecs')"
-      />
-    </div>
+      <div class="pr-time-input">
+        <input
+          ref="startInputHrs"
+          v-model="startInputHrs"
+          class="pr-time-input__hrs"
+          step="1"
+          type="number"
+          min="0"
+          max="23"
+          title="Enter 0 - 23"
+          aria-label="Start offset hours"
+          @change="validate()"
+          @keyup="validate()"
+          @focusin="selectAll($event)"
+          @focusout="format('startInputHrs')"
+          @wheel="increment($event, 'startInputHrs')"
+        />
+        <b>:</b>
+      </div>
+      <div class="pr-time-input">
+        <input
+          ref="startInputMins"
+          v-model="startInputMins"
+          type="number"
+          class="pr-time-input__mins"
+          min="0"
+          max="59"
+          title="Enter 0 - 59"
+          step="1"
+          aria-label="Start offset minutes"
+          @change="validate()"
+          @keyup="validate()"
+          @focusin="selectAll($event)"
+          @focusout="format('startInputMins')"
+          @wheel="increment($event, 'startInputMins')"
+        />
+        <b>:</b>
+      </div>
+      <div class="pr-time-input">
+        <input
+          ref="startInputSecs"
+          v-model="startInputSecs"
+          type="number"
+          class="pr-time-input__secs"
+          min="0"
+          max="59"
+          title="Enter 0 - 59"
+          step="1"
+          aria-label="Start offset seconds"
+          @change="validate()"
+          @keyup="validate()"
+          @focusin="selectAll($event)"
+          @focusout="format('startInputSecs')"
+          @wheel="increment($event, 'startInputSecs')"
+        />
+      </div>
 
-    <div class="pr-time-input pr-time-input__start-end-sep icon-arrows-right-left"></div>
+      <div class="pr-time-input pr-time-input__start-end-sep icon-arrows-right-left"></div>
 
-    <div class="pr-time-input">
-      <input
-        ref="endInputHrs"
-        v-model="endInputHrs"
-        class="pr-time-input__hrs"
-        step="1"
-        type="number"
-        min="0"
-        max="23"
-        title="Enter 0 - 23"
-        aria-label="End offset hours"
-        @change="validate()"
-        @keyup="validate()"
-        @focusin="selectAll($event)"
-        @focusout="format('endInputHrs')"
-        @wheel="increment($event, 'endInputHrs')"
-      />
-      <b>:</b>
-    </div>
-    <div class="pr-time-input">
-      <input
-        ref="endInputMins"
-        v-model="endInputMins"
-        type="number"
-        class="pr-time-input__mins"
-        min="0"
-        max="59"
-        title="Enter 0 - 59"
-        step="1"
-        @change="validate()"
-        @keyup="validate()"
-        @focusin="selectAll($event)"
-        @focusout="format('endInputMins')"
-        @wheel="increment($event, 'endInputMins')"
-      />
-      <b>:</b>
-    </div>
-    <div class="pr-time-input">
-      <input
-        ref="endInputSecs"
-        v-model="endInputSecs"
-        type="number"
-        class="pr-time-input__secs"
-        min="0"
-        max="59"
-        title="Enter 0 - 59"
-        step="1"
-        aria-label="End offset seconds"
-        @change="validate()"
-        @keyup="validate()"
-        @focusin="selectAll($event)"
-        @focusout="format('endInputSecs')"
-        @wheel="increment($event, 'endInputSecs')"
-      />
-    </div>
+      <div class="pr-time-input">
+        <input
+          ref="endInputHrs"
+          v-model="endInputHrs"
+          class="pr-time-input__hrs"
+          step="1"
+          type="number"
+          min="0"
+          max="23"
+          title="Enter 0 - 23"
+          aria-label="End offset hours"
+          @change="validate()"
+          @keyup="validate()"
+          @focusin="selectAll($event)"
+          @focusout="format('endInputHrs')"
+          @wheel="increment($event, 'endInputHrs')"
+        />
+        <b>:</b>
+      </div>
+      <div class="pr-time-input">
+        <input
+          ref="endInputMins"
+          v-model="endInputMins"
+          type="number"
+          class="pr-time-input__mins"
+          min="0"
+          max="59"
+          title="Enter 0 - 59"
+          step="1"
+          @change="validate()"
+          @keyup="validate()"
+          @focusin="selectAll($event)"
+          @focusout="format('endInputMins')"
+          @wheel="increment($event, 'endInputMins')"
+        />
+        <b>:</b>
+      </div>
+      <div class="pr-time-input">
+        <input
+          ref="endInputSecs"
+          v-model="endInputSecs"
+          type="number"
+          class="pr-time-input__secs"
+          min="0"
+          max="59"
+          title="Enter 0 - 59"
+          step="1"
+          aria-label="End offset seconds"
+          @change="validate()"
+          @keyup="validate()"
+          @focusin="selectAll($event)"
+          @focusout="format('endInputSecs')"
+          @wheel="increment($event, 'endInputSecs')"
+        />
+      </div>
 
-    <div class="pr-time-input pr-time-input--buttons">
-      <button
-        class="c-button c-button--major icon-check"
-        :disabled="isDisabled"
-        aria-label="Submit time offsets"
-        @click.prevent="submit"
-      ></button>
-      <button
-        class="c-button icon-x"
-        aria-label="Discard time offsets"
-        @click.prevent="hide"
-      ></button>
+      <div class="pr-time-input pr-time-input--buttons">
+        <button
+          class="c-button c-button--major icon-check"
+          :disabled="isDisabled"
+          aria-label="Submit time offsets"
+          @click.prevent="submit"
+        ></button>
+        <button
+          class="c-button icon-x"
+          aria-label="Discard time offsets"
+          @click.prevent="hide"
+        ></button>
+      </div>
     </div>
   </form>
 </template>
