d336b397af5e1e01ea770afeb0ffe61657dd01c8
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2023-08-11 16:35:19 -0700

    fix: repair bad merge

--- src/plugins/imagery/mixins/imageryData.js
+++ src/plugins/imagery/mixins/imageryData.js
@@ -168,9 +168,6 @@ export default {
         return;
       }
 
-      // forcibly reset the imageContainer size to prevent an aspect ratio distortion
-      delete this.imageContainerWidth;
-      delete this.imageContainerHeight;
       this.bounds = bounds; // setting bounds for ImageryView watcher
     },
     timeSystemChanged() {
