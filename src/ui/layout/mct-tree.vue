2448635395c7005bb88bd88cfd7f8f8e80211e34
Khalid Adil <khalidadil29@gmail.com>
2023-08-16 13:59:16 -0500

    Remove handing for scroll

--- src/ui/layout/mct-tree.vue
+++ src/ui/layout/mct-tree.vue
@@ -26,7 +26,6 @@
     :class="{
       'c-selector': isSelectorTree
     }"
-    @scroll=""
   >
     <div ref="search" class="c-tree-and-search__search">
       <search
@@ -120,7 +119,6 @@ import _ from 'lodash';
 import treeItem from './tree-item.vue';
 import search from '../components/search.vue';
 import { markRaw, reactive } from 'vue';
-import tooltipHelpers from '../../api/tooltips/tooltipMixins';
 
 const ITEM_BUFFER = 25;
 const LOCAL_STORAGE_KEY__TREE_EXPANDED = 'mct-tree-expanded';
@@ -134,7 +132,6 @@ export default {
     search,
     treeItem
   },
-  mixins: [tooltipHelpers],
   inject: ['openmct'],
   props: {
     isSelectorTree: {
