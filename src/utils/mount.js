bafe8c33a94a0a1ce1752aab791b510819558405
Andrew Henry <akhenry@gmail.com>
2023-08-22 11:25:10 -0700

    Automatically apply One Weird Trick to all views

--- src/utils/mount.js
+++ src/utils/mount.js
@@ -3,6 +3,17 @@ import { h, render } from 'vue';
 export default function mount(component, { props, children, element, app } = {}) {
   let el = element;
 
+  if (component.components !== undefined) {
+    component.components = Object.keys(component.components).reduce(
+      (componentMap, componentKey) => {
+        componentMap[componentKey] = Object.assign({}, component.components[componentKey]);
+
+        return componentMap;
+      },
+      {}
+    );
+  }
+
   let vNode = h(component, props, children);
   if (app && app._context) {
     vNode.appContext = app._context;
