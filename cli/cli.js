e9210a505061212eb12d24ded42d29162b0e395e
Even Stensberg <evenstensberg@gmail.com>
2023-08-31 22:28:25 +0200

    chore: initalize cli

---
+++ cli/cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+if (argv.hmr) {
+  console.log('HMR!');
+}
