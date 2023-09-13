584d9a689219f46348261bfe3592c9d88166a0e6
Shefali <simplyrender@gmail.com>
2023-09-13 11:59:53 -0700

    Fix ActionsAPI action collection and applicable actions leak.

--- src/api/actions/ActionsAPI.js
+++ src/api/actions/ActionsAPI.js
@@ -93,7 +93,7 @@ class ActionsAPI extends EventEmitter {
     if (this._actionCollections.has(key)) {
       let actionCollection = this._actionCollections.get(key);
       actionCollection.off('destroy', this._updateCachedActionCollections);
-
+      delete actionCollection.applicableActions;
       this._actionCollections.delete(key);
     }
   }
