fe9f740611dbef55129b399a27054e5d948cc4bd
Jamie V <jamie.j.vigliotta@nasa.gov>
2023-09-19 13:02:22 -0700

    large plot value testing

--- example/generator/SinewaveStalenessProvider.js
+++ example/generator/SinewaveStalenessProvider.js
@@ -23,135 +23,135 @@
 import EventEmitter from 'EventEmitter';
 
 export default class SinewaveLimitProvider extends EventEmitter {
-    #openmct;
-    #observingStaleness;
-    #watchingTheClock;
-    #isRealTime;
+  #openmct;
+  #observingStaleness;
+  #watchingTheClock;
+  #isRealTime;
 
-    constructor(openmct) {
-        super();
+  constructor(openmct) {
+    super();
 
-        this.#openmct = openmct;
-        this.#observingStaleness = {};
-        this.#watchingTheClock = false;
-        this.#isRealTime = undefined;
+    this.#openmct = openmct;
+    this.#observingStaleness = {};
+    this.#watchingTheClock = false;
+    this.#isRealTime = undefined;
+  }
+
+  supportsStaleness(domainObject) {
+    return domainObject.type === 'generator';
+  }
+
+  isStale(domainObject, options) {
+    if (!this.#providingStaleness(domainObject)) {
+      return;
     }
 
-    supportsStaleness(domainObject) {
-        return domainObject.type === 'generator';
+    const id = this.#getObjectKeyString(domainObject);
+
+    if (!this.#observerExists(id)) {
+      this.#createObserver(id);
     }
 
-    isStale(domainObject, options) {
-        if (!this.#providingStaleness(domainObject)) {
-            return;
-        }
+    return Promise.resolve({
+      isStale: this.#observingStaleness[id].isStale,
+      utc: Date.now()
+    });
+  }
 
-        const id = this.#getObjectKeyString(domainObject);
+  subscribeToStaleness(domainObject, callback) {
+    const id = this.#getObjectKeyString(domainObject);
 
-        if (!this.#observerExists(id)) {
-            this.#createObserver(id);
-        }
-
-        return Promise.resolve({
-            isStale: this.#observingStaleness[id].isStale,
-            utc: Date.now()
-        });
+    if (this.#isRealTime === undefined) {
+      this.#updateRealTime(this.#openmct.time.getMode());
     }
 
-    subscribeToStaleness(domainObject, callback) {
-        const id = this.#getObjectKeyString(domainObject);
+    this.#handleClockUpdate();
 
-        if (this.#isRealTime === undefined) {
-            this.#updateRealTime(this.#openmct.time.clock());
-        }
-
-        this.#handleClockUpdate();
-
-        if (this.#observerExists(id)) {
-            this.#addCallbackToObserver(id, callback);
-        } else {
-            this.#createObserver(id, callback);
-        }
-
-        const intervalId = setInterval(() => {
-            if (this.#providingStaleness(domainObject)) {
-                this.#updateStaleness(id, !this.#observingStaleness[id].isStale);
-            }
-        }, 10000);
-
-        return () => {
-            clearInterval(intervalId);
-            this.#updateStaleness(id, false);
-            this.#handleClockUpdate();
-            this.#destroyObserver(id);
-        };
+    if (this.#observerExists(id)) {
+      this.#addCallbackToObserver(id, callback);
+    } else {
+      this.#createObserver(id, callback);
     }
 
-    #handleClockUpdate() {
-        let observers = Object.values(this.#observingStaleness).length > 0;
+    const intervalId = setInterval(() => {
+      if (this.#providingStaleness(domainObject)) {
+        this.#updateStaleness(id, !this.#observingStaleness[id].isStale);
+      }
+    }, 10000);
 
-        if (observers && !this.#watchingTheClock) {
-            this.#watchingTheClock = true;
-            this.#openmct.time.on('clock', this.#updateRealTime, this);
-        } else if (!observers && this.#watchingTheClock) {
-            this.#watchingTheClock = false;
-            this.#openmct.time.off('clock', this.#updateRealTime, this);
-        }
+    return () => {
+      clearInterval(intervalId);
+      this.#updateStaleness(id, false);
+      this.#handleClockUpdate();
+      this.#destroyObserver(id);
+    };
+  }
+
+  #handleClockUpdate() {
+    let observers = Object.values(this.#observingStaleness).length > 0;
+
+    if (observers && !this.#watchingTheClock) {
+      this.#watchingTheClock = true;
+      this.#openmct.time.on('modeChanged', this.#updateRealTime, this);
+    } else if (!observers && this.#watchingTheClock) {
+      this.#watchingTheClock = false;
+      this.#openmct.time.off('modeChanged', this.#updateRealTime, this);
     }
+  }
 
-    #updateRealTime(clock) {
-        this.#isRealTime = clock !== undefined;
+  #updateRealTime(mode) {
+    this.#isRealTime = mode !== 'fixed';
 
-        if (!this.#isRealTime) {
-            Object.keys(this.#observingStaleness).forEach((id) => {
-                this.#updateStaleness(id, false);
-            });
-        }
+    if (!this.#isRealTime) {
+      Object.keys(this.#observingStaleness).forEach((id) => {
+        this.#updateStaleness(id, false);
+      });
     }
+  }
 
-    #updateStaleness(id, isStale) {
-        this.#observingStaleness[id].isStale = isStale;
-        this.#observingStaleness[id].utc = Date.now();
-        this.#observingStaleness[id].callback({
-            isStale: this.#observingStaleness[id].isStale,
-            utc: this.#observingStaleness[id].utc
-        });
-        this.emit('stalenessEvent', {
-            id,
-            isStale: this.#observingStaleness[id].isStale
-        });
+  #updateStaleness(id, isStale) {
+    this.#observingStaleness[id].isStale = isStale;
+    this.#observingStaleness[id].utc = Date.now();
+    this.#observingStaleness[id].callback({
+      isStale: this.#observingStaleness[id].isStale,
+      utc: this.#observingStaleness[id].utc
+    });
+    this.emit('stalenessEvent', {
+      id,
+      isStale: this.#observingStaleness[id].isStale
+    });
+  }
+
+  #createObserver(id, callback) {
+    this.#observingStaleness[id] = {
+      isStale: false,
+      utc: Date.now()
+    };
+
+    if (typeof callback === 'function') {
+      this.#addCallbackToObserver(id, callback);
     }
+  }
 
-    #createObserver(id, callback) {
-        this.#observingStaleness[id] = {
-            isStale: false,
-            utc: Date.now()
-        };
-
-        if (typeof callback === 'function') {
-            this.#addCallbackToObserver(id, callback);
-        }
+  #destroyObserver(id) {
+    if (this.#observingStaleness[id]) {
+      delete this.#observingStaleness[id];
     }
+  }
 
-    #destroyObserver(id) {
-        if (this.#observingStaleness[id]) {
-            delete this.#observingStaleness[id];
-        }
-    }
+  #providingStaleness(domainObject) {
+    return domainObject.telemetry?.staleness === true && this.#isRealTime;
+  }
 
-    #providingStaleness(domainObject) {
-        return domainObject.telemetry?.staleness === true && this.#isRealTime;
-    }
+  #getObjectKeyString(object) {
+    return this.#openmct.objects.makeKeyString(object.identifier);
+  }
 
-    #getObjectKeyString(object) {
-        return this.#openmct.objects.makeKeyString(object.identifier);
-    }
+  #addCallbackToObserver(id, callback) {
+    this.#observingStaleness[id].callback = callback;
+  }
 
-    #addCallbackToObserver(id, callback) {
-        this.#observingStaleness[id].callback = callback;
-    }
-
-    #observerExists(id) {
-        return this.#observingStaleness?.[id];
-    }
+  #observerExists(id) {
+    return this.#observingStaleness?.[id];
+  }
 }
--- example/generator/generatorWorker.js
+++ example/generator/generatorWorker.js
@@ -21,204 +21,254 @@
  *****************************************************************************/
 
 (function () {
+  var FIFTEEN_MINUTES = 15 * 60 * 1000;
 
-    var FIFTEEN_MINUTES = 15 * 60 * 1000;
+  var handlers = {
+    subscribe: onSubscribe,
+    unsubscribe: onUnsubscribe,
+    request: onRequest
+  };
 
-    var handlers = {
-        subscribe: onSubscribe,
-        unsubscribe: onUnsubscribe,
-        request: onRequest
-    };
+  var subscriptions = {};
 
-    var subscriptions = {};
-
-    function workSubscriptions(timestamp) {
-        var now = Date.now();
-        var nextWork = Math.min.apply(Math, Object.values(subscriptions).map(function (subscription) {
-            return subscription(now);
-        }));
-        var wait = nextWork - now;
-        if (wait < 0) {
-            wait = 0;
-        }
-
-        if (Number.isFinite(wait)) {
-            setTimeout(workSubscriptions, wait);
-        }
+  function workSubscriptions(timestamp) {
+    var now = Date.now();
+    var nextWork = Math.min.apply(
+      Math,
+      Object.values(subscriptions).map(function (subscription) {
+        return subscription(now);
+      })
+    );
+    var wait = nextWork - now;
+    if (wait < 0) {
+      wait = 0;
     }
 
-    function onSubscribe(message) {
-        var data = message.data;
-
-        // Keep
-        var start = Date.now();
-        var step = 1000 / data.dataRateInHz;
-        var nextStep = start - (start % step) + step;
-        let work;
-        if (data.spectra) {
-            work = function (now) {
-                while (nextStep < now) {
-                    const messageCopy = Object.create(message);
-                    message.data.start = nextStep - (60 * 1000);
-                    message.data.end = nextStep;
-                    onRequest(messageCopy);
-                    nextStep += step;
-                }
-
-                return nextStep;
-            };
-        } else {
-            work = function (now) {
-                while (nextStep < now) {
-                    self.postMessage({
-                        id: message.id,
-                        data: {
-                            name: data.name,
-                            utc: nextStep,
-                            yesterday: nextStep - 60 * 60 * 24 * 1000,
-                            sin: sin(nextStep, data.period, data.amplitude, data.offset, data.phase, data.randomness, data.infinityValues),
-                            wavelengths: wavelengths(),
-                            intensities: intensities(),
-                            cos: cos(nextStep, data.period, data.amplitude, data.offset, data.phase, data.randomness, data.infinityValues)
-                        }
-                    });
-                    nextStep += step;
-                }
-
-                return nextStep;
-            };
-        }
-
-        subscriptions[message.id] = work;
-        workSubscriptions();
+    if (Number.isFinite(wait)) {
+      setTimeout(workSubscriptions, wait);
     }
+  }
 
-    function onUnsubscribe(message) {
-        delete subscriptions[message.data.id];
-    }
+  function onSubscribe(message) {
+    var data = message.data;
 
-    function onRequest(message) {
-        var request = message.data;
-        if (request.end === undefined) {
-            request.end = Date.now();
+    // Keep
+    var start = Date.now();
+    var step = 1000 / data.dataRateInHz;
+    var nextStep = start - (start % step) + step;
+    let work;
+    if (data.spectra) {
+      work = function (now) {
+        while (nextStep < now) {
+          const messageCopy = Object.create(message);
+          message.data.start = nextStep - 60 * 1000;
+          message.data.end = nextStep;
+          onRequest(messageCopy);
+          nextStep += step;
         }
 
-        if (request.start === undefined) {
-            request.start = request.end - FIFTEEN_MINUTES;
-        }
-
-        var now = Date.now();
-        var start = request.start;
-        var end = request.end > now ? now : request.end;
-        var amplitude = request.amplitude;
-        var period = request.period;
-        var offset = request.offset;
-        var dataRateInHz = request.dataRateInHz;
-        var phase = request.phase;
-        var randomness = request.randomness;
-        var loadDelay = Math.max(request.loadDelay, 0);
-        var infinityValues = request.infinityValues;
-
-        var step = 1000 / dataRateInHz;
-        var nextStep = start - (start % step) + step;
-
-        var data = [];
-
-        for (; nextStep < end && data.length < 5000; nextStep += step) {
-            data.push({
-                utc: nextStep,
-                yesterday: nextStep - 60 * 60 * 24 * 1000,
-                sin: sin(nextStep, period, amplitude, offset, phase, randomness, infinityValues),
-                wavelengths: wavelengths(),
-                intensities: intensities(),
-                cos: cos(nextStep, period, amplitude, offset, phase, randomness, infinityValues)
-            });
-        }
-
-        if (loadDelay === 0) {
-            postOnRequest(message, request, data);
-        } else {
-            setTimeout(() => postOnRequest(message, request, data), loadDelay);
-        }
-    }
-
-    function postOnRequest(message, request, data) {
-        self.postMessage({
+        return nextStep;
+      };
+    } else {
+      work = function (now) {
+        while (nextStep < now) {
+          self.postMessage({
             id: message.id,
-            data: request.spectra ? {
-                wavelength: data.map((item) => {
-                    return item.wavelength;
-                }),
-                cos: data.map((item) => {
-                    return item.cos;
-                })
-            } : data
-        });
+            data: {
+              name: data.name,
+              utc: nextStep,
+              yesterday: nextStep - 60 * 60 * 24 * 1000,
+              sin: sin(
+                nextStep,
+                data.period,
+                data.amplitude,
+                data.offset,
+                data.phase,
+                data.randomness,
+                data.infinityValues,
+                data.veryLargeValues
+              ),
+              wavelengths: wavelengths(),
+              intensities: intensities(),
+              cos: cos(
+                nextStep,
+                data.period,
+                data.amplitude,
+                data.offset,
+                data.phase,
+                data.randomness,
+                data.infinityValues,
+                data.veryLargeValues
+              )
+            }
+          });
+          nextStep += step;
+        }
+
+        return nextStep;
+      };
     }
 
-    function cos(timestamp, period, amplitude, offset, phase, randomness, infinityValues) {
-        if (infinityValues && Math.random() > 0.5) {
+    subscriptions[message.id] = work;
+    workSubscriptions();
+  }
+
+  function onUnsubscribe(message) {
+    delete subscriptions[message.data.id];
+  }
+
+  function onRequest(message) {
+    var request = message.data;
+    if (request.end === undefined) {
+      request.end = Date.now();
+    }
+
+    if (request.start === undefined) {
+      request.start = request.end - FIFTEEN_MINUTES;
+    }
+
+    var now = Date.now();
+    var start = request.start;
+    var end = request.end > now ? now : request.end;
+    var amplitude = request.amplitude;
+    var period = request.period;
+    var offset = request.offset;
+    var dataRateInHz = request.dataRateInHz;
+    var phase = request.phase;
+    var randomness = request.randomness;
+    var loadDelay = Math.max(request.loadDelay, 0);
+    var infinityValues = request.infinityValues;
+    var veryLargeValues = request.veryLargeValues;
+
+    var step = 1000 / dataRateInHz;
+    var nextStep = start - (start % step) + step;
+
+    var data = [];
+
+    for (; nextStep < end && data.length < 5000; nextStep += step) {
+      data.push({
+        utc: nextStep,
+        yesterday: nextStep - 60 * 60 * 24 * 1000,
+        sin: sin(nextStep, period, amplitude, offset, phase, randomness, infinityValues, veryLargeValues),
+        wavelengths: wavelengths(),
+        intensities: intensities(),
+        cos: cos(nextStep, period, amplitude, offset, phase, randomness, infinityValues, veryLargeValues)
+      });
+    }
+
+    if (loadDelay === 0) {
+      postOnRequest(message, request, data);
+    } else {
+      setTimeout(() => postOnRequest(message, request, data), loadDelay);
+    }
+  }
+
+  function postOnRequest(message, request, data) {
+    self.postMessage({
+      id: message.id,
+      data: request.spectra
+        ? {
+            wavelength: data.map((item) => {
+              return item.wavelength;
+            }),
+            cos: data.map((item) => {
+              return item.cos;
+            })
+          }
+        : data
+    });
+  }
+
+  function cos(timestamp, period, amplitude, offset, phase, randomness, infinityValues, veryLargeValues) {
+    const randomValue = Math.random();
+
+    if (infinityValues && veryLargeValues) {
+        if (randomValue < 0.33) return Number.POSITIVE_INFINITY;
+        if (randomValue < 0.66) return (randomValue > 0.5 ? -1 : 1) * 1e+177;
+        // if neither condition is met, it just proceeds to the normal return value
+    } else {
+        if (infinityValues && randomValue > 0.5) {
             return Number.POSITIVE_INFINITY;
         }
-
-        return amplitude
-            * Math.cos(phase + (timestamp / period / 1000 * Math.PI * 2)) + (amplitude * Math.random() * randomness) + offset;
+        if (veryLargeValues && randomValue > 0.5) {
+            return (randomValue > 0.5 ? -1 : 1) * 1e+177;
+        }
     }
 
-    function sin(timestamp, period, amplitude, offset, phase, randomness, infinityValues) {
-        if (infinityValues && Math.random() > 0.5) {
+    return (
+      amplitude * Math.cos(phase + (timestamp / period / 1000) * Math.PI * 2) +
+      amplitude * Math.random() * randomness +
+      offset
+    );
+  }
+
+  function sin(timestamp, period, amplitude, offset, phase, randomness, infinityValues, veryLargeValues) {
+    const randomValue = Math.random();
+
+    if (infinityValues && veryLargeValues) {
+        if (randomValue < 0.33) return Number.POSITIVE_INFINITY;
+        if (randomValue < 0.66) return (randomValue > 0.5 ? -1 : 1) * 1e+177;
+        // if neither condition is met, it just proceeds to the normal return value
+    } else {
+        if (infinityValues && randomValue > 0.5) {
             return Number.POSITIVE_INFINITY;
         }
-
-        return amplitude
-            * Math.sin(phase + (timestamp / period / 1000 * Math.PI * 2)) + (amplitude * Math.random() * randomness) + offset;
-    }
-
-    function wavelengths() {
-        let values = [];
-        while (values.length < 5) {
-            const randomValue = Math.random() * 100;
-            if (!values.includes(randomValue)) {
-                values.push(String(randomValue));
-            }
+        if (veryLargeValues && randomValue > 0.5) {
+            return (randomValue > 0.5 ? -1 : 1) * 1e+177;
         }
-
-        return values;
     }
 
-    function intensities() {
-        let values = [];
-        while (values.length < 5) {
-            const randomValue = Math.random() * 10;
-            if (!values.includes(randomValue)) {
-                values.push(String(randomValue));
-            }
-        }
+    return (
+      amplitude * Math.sin(phase + (timestamp / period / 1000) * Math.PI * 2) +
+      amplitude * Math.random() * randomness +
+      offset
+    );
+  }
 
-        return values;
+  function wavelengths() {
+    let values = [];
+    while (values.length < 5) {
+      const randomValue = Math.random() * 100;
+      if (!values.includes(randomValue)) {
+        values.push(String(randomValue));
+      }
     }
 
-    function sendError(error, message) {
-        self.postMessage({
-            error: error.name + ': ' + error.message,
-            message: message,
-            id: message.id
-        });
+    return values;
+  }
+
+  function intensities() {
+    let values = [];
+    while (values.length < 5) {
+      const randomValue = Math.random() * 10;
+      if (!values.includes(randomValue)) {
+        values.push(String(randomValue));
+      }
     }
 
-    self.onmessage = function handleMessage(event) {
-        var message = event.data;
-        var handler = handlers[message.request];
+    return values;
+  }
 
-        if (!handler) {
-            sendError(new Error('unknown message type'), message);
-        } else {
-            try {
-                handler(message);
-            } catch (e) {
-                sendError(e, message);
-            }
-        }
-    };
+  function sendError(error, message) {
+    self.postMessage({
+      error: error.name + ': ' + error.message,
+      message: message,
+      id: message.id
+    });
+  }
 
-}());
+  self.onmessage = function handleMessage(event) {
+    var message = event.data;
+    var handler = handlers[message.request];
+
+    if (!handler) {
+      sendError(new Error('unknown message type'), message);
+    } else {
+      try {
+        handler(message);
+      } catch (e) {
+        sendError(e, message);
+      }
+    }
+  };
+})();
