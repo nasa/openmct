49fa8a85f27648121c2f82ae145f23386bab76b5
Shefali <simplyrender@gmail.com>
2023-11-02 22:30:12 -0700

    Change refresh to emit the new model

--- src/api/objects/MutableDomainObject.js
+++ src/api/objects/MutableDomainObject.js
@@ -129,7 +129,7 @@ class MutableDomainObject {
     this._globalEventEmitter.emit(qualifiedEventName(this, '$_synchronize_model'), model);
 
     //Emit wildcard event, with path so that callback knows what changed
-    this._globalEventEmitter.emit(qualifiedEventName(this, '*'), this);
+    this._globalEventEmitter.emit(qualifiedEventName(this, '*'), model);
   }
 
   $on(event, callback) {
