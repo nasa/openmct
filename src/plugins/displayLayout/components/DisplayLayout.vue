de99517a5f77376dc23648029dc4982682213a69
David Tsay <david.e.tsay@nasa.gov>
2023-11-06 16:11:31 -0800

    Revert "only show marquee for selected item"
    This reverts commit d17af210c2d8fcb332abcd239bdcec82d2ae03c0.

--- src/plugins/displayLayout/components/DisplayLayout.vue
+++ src/plugins/displayLayout/components/DisplayLayout.vue
@@ -191,13 +191,13 @@ export default {
       };
     },
     showMarquee() {
-      const layoutItem = this.selection[0]?.[0]?.context?.layoutItem;
-      const isSelectedLayoutItem =
-        layoutItem &&
-        this.layoutItems.some((item) => this.openmct.objects.areIdsEqual(item, layoutItem));
-      const isSingleSelectedLine = this.selection.length === 1 && layoutItem?.type === 'line-view';
+      let selectionPath = this.selection[0];
+      let singleSelectedLine =
+        this.selection.length === 1 &&
+        selectionPath[0].context.layoutItem &&
+        selectionPath[0].context.layoutItem.type === 'line-view';
 
-      return isSelectedLayoutItem && this.isEditing && !isSingleSelectedLine;
+      return this.isEditing && selectionPath && selectionPath.length > 1 && !singleSelectedLine;
     }
   },
   watch: {
