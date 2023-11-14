15df590931ba7c2a0b6c4ca9b42441d3e9b282d0
David Tsay <david.e.tsay@nasa.gov>
2023-11-14 12:52:19 -0800

    code cleanup

--- e2e/tests/visual/displayLayout.visual.spec.js
+++ e2e/tests/visual/displayLayout.visual.spec.js
@@ -21,63 +21,48 @@
  *****************************************************************************/
 
 const { test } = require('../../pluginFixtures');
-const {
-  expandTreePaneItemByName,
-  createDomainObjectWithDefaults
-} = require('../../appActions');
-const percySnapshot = require('@percy/playwright');
+const { expandTreePaneItemByName, createDomainObjectWithDefaults } = require('../../appActions');
 const VISUAL_URL = require('../../constants').VISUAL_URL;
+const percySnapshot = require('@percy/playwright');
 
 const snapshotScope = '.l-shell__pane-main .l-pane__contents';
-// const treePane = "[role=tree][aria-label='Main Tree']";
 
 test.describe('Visual - Display Layout', () => {
   test('Edit Marquee', async ({ page, theme, openmctConfig }) => {
     const { myItemsFolderName } = openmctConfig;
-    await page.goto(VISUAL_URL, { waitUntil: 'domcontentloaded' });
 
-    //Open Tree
+    // Load Open MCT visual test baseline
+    await page.goto(VISUAL_URL, { waitUntil: 'domcontentloaded' });
+    // Open Tree
     await page.getByRole('button', { name: 'Browse' }).click();
 
+    const treePane = page.getByRole('tree', {
+      name: 'Main Tree'
+    });
+
+    const layoutGridHolder = await page.locator('.l-layout__grid-holder').first();
+    const editButton = page.locator('[title="Edit"]');
+    const saveButton = page.locator('button[title="Save"]');
+    const confirmSaveAndFinishEditingButton = page.locator('text=Save and Finish Editing');
     const parentLayout = await createDomainObjectWithDefaults(page, {
       type: 'Display Layout',
       name: 'Parent Layout'
     });
-
     const child1Layout = await createDomainObjectWithDefaults(page, {
       type: 'Display Layout',
       name: 'Child 1 Layout'
     });
-
     const child2Layout = await createDomainObjectWithDefaults(page, {
       type: 'Display Layout',
       name: 'Child 2 Layout'
     });
-
     const swg1 = await createDomainObjectWithDefaults(page, {
       type: 'Sine Wave Generator',
       name: 'SWG 1'
     });
-
     const swg2 = await createDomainObjectWithDefaults(page, {
       type: 'Sine Wave Generator',
       name: 'SWG 2'
-    });
-
-    await expandTreePaneItemByName(page, myItemsFolderName);
-
-    await page.goto(child1Layout.url);
-    await page.dragAndDrop(`role=treeitem[name=/${swg1.name}/]`, '.c-object-view');
-    await page.locator('button[title="Save"]').click();
-    await page.locator('text=Save and Finish Editing').click();
-
-    await page.goto(child2Layout.url);
-    await page.dragAndDrop(`role=treeitem[name=/${swg2.name}/]`, '.c-object-view');
-    await page.locator('button[title="Save"]').click();
-    await page.locator('text=Save and Finish Editing').click();
-
-    const treePane = page.getByRole('tree', {
-      name: 'Main Tree'
     });
     const child1LayoutTreeItem = treePane.getByRole('treeitem', {
       name: new RegExp(child1Layout.name)
@@ -85,11 +70,36 @@ test.describe('Visual - Display Layout', () => {
     const child2LayoutTreeItem = treePane.getByRole('treeitem', {
       name: new RegExp(child2Layout.name)
     });
+    const swg1TreeItem = treePane.getByRole('treeitem', {
+      name: new RegExp(swg1.name)
+    });
+    const swg2TreeItem = treePane.getByRole('treeitem', {
+      name: new RegExp(swg2.name)
+    });
+
+    // Expand My Items folder
+    await expandTreePaneItemByName(page, myItemsFolderName);
+
+    // Add swg1 to child1Layout
+    await page.goto(child1Layout.url);
+    await editButton.click();
+    await swg1TreeItem.dragTo(layoutGridHolder, { targetPosition: { x: 0, y: 0 } });
+    await saveButton.click();
+    await confirmSaveAndFinishEditingButton.click();
+
+    // Add swg1 to child1Layout
+    await page.goto(child2Layout.url);
+    await editButton.click();
+    await swg2TreeItem.dragTo(layoutGridHolder, { targetPosition: { x: 0, y: 0 } });
+    await saveButton.click();
+    await confirmSaveAndFinishEditingButton.click();
+
+    // Add child1Layout and child2Layout to parentLayout
     await page.goto(parentLayout.url);
-    // Edit Display Layout
-    await page.locator('[title="Edit"]').click();
-    const layoutGridHolder = await page.locator('.l-layout__grid-holder').first();
-    await child1LayoutTreeItem.dragTo(layoutGridHolder, { targetPosition: { x: 500, y: 0 } });
+    await editButton.click();
+    await child1LayoutTreeItem.dragTo(layoutGridHolder, { targetPosition: { x: 350, y: 0 } });
     await child2LayoutTreeItem.dragTo(layoutGridHolder, { targetPosition: { x: 0, y: 0 } });
+
+    
   });
 });
