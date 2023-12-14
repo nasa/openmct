014aa3ae400a6f17a6d0a8e246068c0da24bfa70
John Hill <john.c.hill@nasa.gov>
2023-12-14 12:39:25 -0800

    section 508

--- e2e/avpFixtures.js
+++ e2e/avpFixtures.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 /*****************************************************************************
  * Open MCT, Copyright (c) 2014-2023, United States Government
  * as represented by the Administrator of the National Aeronautics and Space
@@ -21,37 +20,27 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-/**
- * avpFixtures.js
- *
- * @file This module provides custom fixtures specifically tailored for Accessibility, Visual, and Performance (AVP) tests.
- * These fixtures extend the base functionality of the Playwright fixtures and appActions, and are designed to be
- * generalized across all plugins. They offer functionalities like generating accessibility reports, integrating
- * with axe-core, and more.
- *
- * IMPORTANT NOTE: This fixture file is not intended to be extended further by other fixtures. If you find yourself
- * needing to do so, please consult the documentation and consider creating a specialized fixture or modifying the
- * existing ones.
- */
-
 const fs = require('fs');
 const path = require('path');
 const base = require('./pluginFixtures');
 const AxeBuilder = require('@axe-core/playwright').default;
+
+// Constants for repeated values
+const TEST_RESULTS_DIR = './test-results';
 
 /**
  * Extend base test by providing "makeAxeBuilder" functionality.
  *
  * @typedef {object} AxeBuilderParams
  * @property {import('playwright').Page} page - The page object from Playwright.
- * @property {string[]} [tags=['wcag2aa']] - The tags for accessibility checks.
+ * @property {string[]} [tags=DEFAULT_AXE_TAGS] - The tags for accessibility checks.
  *
  * @param {AxeBuilderParams} params
  * @returns {AxeBuilder} The axe builder instance.
  */
 exports.test = base.test.extend({
   makeAxeBuilder: async ({ page }, use, testInfo) => {
-    const makeAxeBuilder = () => new AxeBuilder({ page }).withTags(['wcag2aa']);
+    const makeAxeBuilder = () => new AxeBuilder({ page }).withTags(['section508']);
     await use(makeAxeBuilder);
   }
 });
@@ -70,22 +59,20 @@ exports.expect = base.expect;
  *
  * @returns {Promise<object>} Returns the accessibility scan results.
  */
-
 exports.generateAccessibilityReport = async function (page, testCaseName, options = {}) {
   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
   let reportName = options.reportName || testCaseName;
   let sanitizedReportName = reportName.replace(/\//g, '_');
-  let reportPath = path.join('./test-results', `${sanitizedReportName}.json`);
+  let reportPath = path.join(TEST_RESULTS_DIR, `${sanitizedReportName}.json`);
 
   try {
-    // Ensure directory exists
-    if (!fs.existsSync('./test-results')) {
-      fs.mkdirSync('./test-results');
+    if (!fs.existsSync(TEST_RESULTS_DIR)) {
+      fs.mkdirSync(TEST_RESULTS_DIR);
     }
 
     fs.writeFileSync(reportPath, JSON.stringify(accessibilityScanResults, null, 2));
-    console.log(`Accessibility report saved successfully as ${reportPath}!`);
+    console.log(`Accessibility report saved successfully as ${reportPath}`);
     return accessibilityScanResults;
   } catch (err) {
     console.error(`Error writing the accessibility report to file ${reportPath}:`, err);
