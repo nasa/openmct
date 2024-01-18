900dba2515e349ae8b47bfe373ae041543d3ed7f
Michael Rogers <contact@mhrogers.com>
2024-01-18 11:44:28 -0600

    Get actions collection on Preview Container update

--- src/ui/preview/PreviewContainer.vue
+++ src/ui/preview/PreviewContainer.vue
@@ -108,6 +108,9 @@ export default {
       this.addExistingViewBackToParent();
     }
   },
+  updated() {
+    this.getActionsCollection(this.view);
+  },
   methods: {
     clear() {
       if (this.view) {
