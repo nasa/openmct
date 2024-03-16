162f8d6eb54533c07a454b9bbacace940b7157ac
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2024-03-15 18:32:51 -0700

    chore: export test framework from openmct-e2e

---
+++ e2e/index.js
@@ -0,0 +1,7 @@
+// Import everything from the specific fixture files
+import * as appActions from './appActions.js';
+import * as avpFixtures from './avpFixtures.js';
+import * as pluginFixtures from './pluginFixtures.js';
+
+// Export these as named exports
+export { appActions, avpFixtures, pluginFixtures };
