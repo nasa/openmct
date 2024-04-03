aee5364f591eb53697df38886fbad13d9cf92c59
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2024-04-03 14:10:28 -0700

    docs: types for StatusAPI

--- src/api/status/StatusAPI.js
+++ src/api/status/StatusAPI.js
@@ -20,13 +20,29 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-import EventEmitter from 'EventEmitter';
+/**
+ * @typedef {import('openmct').OpenMCT} OpenMCT
+ * @typedef {import('openmct').Identifier} Identifier
+ * @typedef {string} Status
+ */
 
+import EventEmitter from 'EventEmitter';
+/**
+ * Get, set, and observe statuses for Open MCT objects. A status is a string
+ * that represents the current state of an object.
+ *
+ * @extends EventEmitter
+ */
 export default class StatusAPI extends EventEmitter {
+  /**
+   * Constructs a new instance of the StatusAPI class.
+   * @param {OpenMCT} openmct - The Open MCT application instance.
+   */
   constructor(openmct) {
     super();
 
     this._openmct = openmct;
+    /** @type {Record<string, Status>} */
     this._statusCache = {};
 
     this.get = this.get.bind(this);
@@ -34,19 +50,33 @@ export default class StatusAPI extends EventEmitter {
     this.observe = this.observe.bind(this);
   }
 
+  /**
+   * Retrieves the status of the object with the given identifier.
+   * @param {Identifier} identifier - The identifier of the object.
+   * @returns {Status | undefined} The status of the object, or undefined if the object's status is not cached.
+   */
   get(identifier) {
     let keyString = this._openmct.objects.makeKeyString(identifier);
 
     return this._statusCache[keyString];
   }
 
-  set(identifier, value) {
+  /**
+   * Sets the status of the object with the given identifier.
+   * @param {Identifier} identifier - The identifier of the object.
+   * @param {Status} status - The new status value for the object.
+   */
+  set(identifier, status) {
     let keyString = this._openmct.objects.makeKeyString(identifier);
 
-    this._statusCache[keyString] = value;
-    this.emit(keyString, value);
+    this._statusCache[keyString] = status;
+    this.emit(keyString, status);
   }
 
+  /**
+   * Deletes the status of the object with the given identifier.
+   * @param {Identifier} identifier - The identifier of the object.
+   */
   delete(identifier) {
     let keyString = this._openmct.objects.makeKeyString(identifier);
 
@@ -55,6 +85,13 @@ export default class StatusAPI extends EventEmitter {
     delete this._statusCache[keyString];
   }
 
+  /**
+   * Observes the status of the object with the given identifier, and calls the provided callback
+   * function whenever the status changes.
+   * @param {Identifier} identifier - The identifier of the object.
+   * @param {(value: any) => void} callback - The function to be called whenever the status changes.
+   * @returns {() => void} A function that can be called to stop observing the status.
+   */
   observe(identifier, callback) {
     let key = this._openmct.objects.makeKeyString(identifier);
 
