fe4d3b039d3bd4833524cf64113ee6649d14685b
Rukmini Bose <rukmini.bose15@gmail.com>
2024-04-10 13:58:27 -0700

    Add comment about this theme being in beta mode

--- src/plugins/themes/darkmatter.js
+++ src/plugins/themes/darkmatter.js
@@ -1,3 +1,4 @@
+// Note: This darkmatter theme is in Beta and is not yet ready for prime time. It needs some more tweaking.
 import { installTheme } from './installTheme.js';
 
 export default function plugin() {
