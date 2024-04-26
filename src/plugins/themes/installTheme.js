ac278818c866ae3a83931794dab5fd1e6d7faa5f
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2024-04-26 15:57:03 -0700

    feat: dynamically generate static asset urls

--- src/plugins/themes/installTheme.js
+++ src/plugins/themes/installTheme.js
@@ -1,4 +1,5 @@
 const dataAttribute = 'theme';
+import BackgroundImage from '../../ui/layout/assets/images/darkmatter-bg.png';
 
 export function installTheme(openmct, themeName) {
   const currentTheme = document.querySelector(`link[data-${dataAttribute}]`);
@@ -12,6 +13,8 @@ export function installTheme(openmct, themeName) {
   // eslint-disable-next-line no-undef
   const href = `${openmct.getAssetPath()}${__OPENMCT_ROOT_RELATIVE__}${themeName}Theme.css`;
   newTheme.setAttribute('href', href);
+  // Set the CSS variable
+  document.documentElement.style.setProperty('--darkmatter-bg-url', `url(${BackgroundImage})`);
   newTheme.dataset[dataAttribute] = themeName;
 
   document.head.appendChild(newTheme);
