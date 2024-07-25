988bfd35b77955a9757d896360787ccbf9d079dc
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2024-07-25 14:31:24 -0700

    test(e2e): add tests for validating time conductor popup
    - update appAction for setting time conductor in fixed mode
    - add a11y to time conductor in fixed mode
    - update tests using `setTimeConductorBounds`

--- e2e/tests/functional/plugins/timeConductor/timeConductor.e2e.spec.js
+++ e2e/tests/functional/plugins/timeConductor/timeConductor.e2e.spec.js
@@ -30,67 +30,79 @@ import {
 import { expect, test } from '../../../../pluginFixtures.js';
 
 test.describe('Time conductor operations', () => {
-  test('validate start time does not exceeds end time', async ({ page }) => {
+  test('validate start time does not exceed end time', async ({ page }) => {
     // Go to baseURL
     await page.goto('./', { waitUntil: 'domcontentloaded' });
     const year = new Date().getFullYear();
 
-    let startDate = 'xxxx-01-01 01:00:00.000Z';
-    startDate = year + startDate.substring(4);
+    // Set initial valid time bounds
+    const startDate = `${year}-01-01`;
+    const startTime = '01:00:00';
+    const endDate = `${year}-01-01`;
+    const endTime = '02:00:00';
+    await setTimeConductorBounds(page, { startDate, startTime, endDate, endTime });
 
-    let endDate = 'xxxx-01-01 02:00:00.000Z';
-    endDate = year + endDate.substring(4);
+    // Open the time conductor popup
+    await page.getByRole('button', { name: 'Time Conductor Mode', exact: true }).click();
 
-    await setTimeConductorBounds(page, startDate, endDate);
+    // Test invalid start date
+    const invalidStartDate = `${year}-01-02`;
+    await page.getByLabel('Start date').fill(invalidStartDate);
+    await expect(page.getByLabel('Submit time bounds')).toBeDisabled();
+    await page.getByLabel('Start date').fill(startDate);
+    await expect(page.getByLabel('Submit time bounds')).toBeEnabled();
 
-    // invalid start date
-    startDate = year + 1 + startDate.substring(4);
-    await setTimeConductorBounds(page, startDate);
+    // Test invalid end date
+    const invalidEndDate = `${year - 1}-12-31`;
+    await page.getByLabel('End date').fill(invalidEndDate);
+    await expect(page.getByLabel('Submit time bounds')).toBeDisabled();
+    await page.getByLabel('End date').fill(endDate);
+    await expect(page.getByLabel('Submit time bounds')).toBeEnabled();
 
-    // Bring up the time conductor popup
-    const timeConductorMode = page.locator('.c-compact-tc');
-    await timeConductorMode.click();
-    const startDateLocator = page.locator('input[type="text"]').first();
-    const endDateLocator = page.locator('input[type="text"]').nth(2);
+    // Test invalid start time
+    const invalidStartTime = '42:00:00';
+    await page.getByLabel('Start time').fill(invalidStartTime);
+    await expect(page.getByLabel('Submit time bounds')).toBeDisabled();
+    await page.getByLabel('Start time').fill(startTime);
+    await expect(page.getByLabel('Submit time bounds')).toBeEnabled();
 
-    await endDateLocator.click();
+    // Test invalid end time
+    const invalidEndTime = '43:00:00';
+    await page.getByLabel('End time').fill(invalidEndTime);
+    await expect(page.getByLabel('Submit time bounds')).toBeDisabled();
+    await page.getByLabel('End time').fill(endTime);
+    await expect(page.getByLabel('Submit time bounds')).toBeEnabled();
 
-    const startDateValidityStatus = await startDateLocator.evaluate((element) =>
-      element.checkValidity()
+    // Submit valid time bounds
+    await page.getByLabel('Submit time bounds').click();
+
+    // Verify the submitted time bounds
+    await expect(page.getByLabel('Start bounds')).toHaveText(
+      new RegExp(`${startDate} ${startTime}.000Z`)
     );
-    expect(startDateValidityStatus).not.toBeTruthy();
-
-    // fix to valid start date
-    startDate = year - 1 + startDate.substring(4);
-    await setTimeConductorBounds(page, startDate);
-
-    // invalid end date
-    endDate = year - 2 + endDate.substring(4);
-    await setTimeConductorBounds(page, undefined, endDate);
-
-    await startDateLocator.click();
-
-    const endDateValidityStatus = await endDateLocator.evaluate((element) =>
-      element.checkValidity()
+    await expect(page.getByLabel('End bounds')).toHaveText(
+      new RegExp(`${endDate} ${endTime}.000Z`)
     );
-    expect(endDateValidityStatus).not.toBeTruthy();
   });
 });
 
-// Testing instructions:
-// Try to change the realtime offsets when in realtime (local clock) mode.
-test.describe('Time conductor input fields real-time mode', () => {
-  test('validate input fields in real-time mode', async ({ page }) => {
+test.describe('Global Time Conductor', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('./', { waitUntil: 'domcontentloaded' });
+  });
+
+  test('Real-Time Mode: input field validation', async ({ page }) => {
     const startOffset = {
+      startHours: '01',
+      startMins: '29',
       startSecs: '23'
     };
 
     const endOffset = {
+      endHours: '01',
+      endMins: '30',
       endSecs: '31'
     };
-
-    // Go to baseURL
-    await page.goto('./', { waitUntil: 'domcontentloaded' });
 
     // Switch to real-time mode
     await setRealTimeMode(page);
@@ -99,13 +111,29 @@ test.describe('Time conductor input fields real-time mode', () => {
     await setStartOffset(page, startOffset);
 
     // Verify time was updated on time offset button
-    await expect(page.locator('.c-compact-tc__setting-value.icon-minus')).toContainText('00:30:23');
+    await expect(page.getByLabel('Start offset: 01:29:23')).toBeVisible();
 
     // Set end time offset
     await setEndOffset(page, endOffset);
 
     // Verify time was updated on preceding time offset button
-    await expect(page.locator('.c-compact-tc__setting-value.icon-plus')).toContainText('00:00:31');
+    await expect(page.getByLabel('End offset: 01:30:31')).toBeVisible();
+
+    await setStartOffset(page, {
+      startHours: '00',
+      startMins: '30',
+      startSecs: '00',
+      submitChanges: false
+    });
+    await expect(page.getByLabel('Start offset: 00:30:00')).toBeHidden();
+  });
+
+  test('Fixed Time Mode: input field validation', async ({ page }) => {
+    // Switch to fixed time mode
+    await setFixedTimeMode(page);
+
+    // Set start time offset
+    await setStartBo;
   });
 
   /**
@@ -147,14 +175,15 @@ test.describe('Time conductor input fields real-time mode', () => {
     await setRealTimeMode(page);
 
     // Verify updated start time offset persists after mode switch
-    await expect(page.locator('.c-compact-tc__setting-value.icon-minus')).toContainText('00:30:23');
+    await expect(page.getByLabel('Start offset: 00:30:23')).toBeVisible();
 
     // Verify updated end time offset persists after mode switch
-    await expect(page.locator('.c-compact-tc__setting-value.icon-plus')).toContainText('00:00:01');
+    await expect(page.getByLabel('End offset: 00:00:01')).toBeVisible();
 
     // Verify url parameters persist after mode switch
-    expect(page.url()).toContain(`startDelta=${startDelta}`);
-    expect(page.url()).toContain(`endDelta=${endDelta}`);
+    // eslint-disable-next-line no-useless-escape
+    const urlRegex = new RegExp(`.*tc\.startDelta=${startDelta}&tc\.endDelta=${endDelta}.*`);
+    await page.waitForURL(urlRegex);
   });
 
   test.fixme(
