8b4eed938a5553d2aef963e0ed5545e06467aa22
Scott Bell <scott@traclabs.com>
2024-09-09 09:45:22 +0200

    good job code scanner. changing to or condition

--- src/plugins/comps/CompsMathWorker.js
+++ src/plugins/comps/CompsMathWorker.js
@@ -71,7 +71,7 @@ function calculate(dataFrame, parameters, expression) {
     otherParameters.forEach((parameter) => {
       const otherDataFrame = dataFrame[parameter.keyString];
       const otherTelemetry = otherDataFrame.get(referenceTime);
-      if (otherTelemetry === undefined && otherTelemetry === null) {
+      if (otherTelemetry === undefined || otherTelemetry === null) {
         missingData = true;
         return;
       }
