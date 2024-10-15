02b15e006dde7d30790a1a5487a8a21949163ee1
Jesse Mazzella <jesse.d.mazzella@nasa.gov>
2024-10-15 09:27:52 -0700

    fix: set and use default if none selected

--- src/plugins/faultManagement/FaultManagementView.vue
+++ src/plugins/faultManagement/FaultManagementView.vue
@@ -330,7 +330,8 @@ export default {
         }
 
         shelveData.comment = data.comment || '';
-        shelveData.shelveDuration = data.shelveDuration;
+        shelveData.shelveDuration =
+          data.shelveDuration === undefined ? this.shelveDurations[0].value : data.shelveDuration;
       } else {
         shelveData = {
           shelved: false
