0243751cd90eedb3f4a401acd127c95f11b13139
Andrew Henry <akhenry@gmail.com>
2025-04-19 16:12:49 -0700

    Single return statement

--- src/plugins/CouchDBSearchFolder/plugin.js
+++ src/plugins/CouchDBSearchFolder/plugin.js
@@ -35,13 +35,17 @@ export default function (folderName, couchPlugin, searchFilter) {
         );
       },
       load() {
+        let searchResults;
+
         if (searchFilter.viewName !== undefined) {
           // Use a view to search, instead of an _all_docs find
-          return couchProvider.getObjectsByView(searchFilter);
+          searchResults = couchProvider.getObjectsByView(searchFilter);
         } else {
           // Use the _find endpoint to search _all_docs
-          return couchProvider.getObjectsByFilter(searchFilter);
+          searchResults = couchProvider.getObjectsByFilter(searchFilter);
         }
+
+        return searchResults;
       }
     });
   };
