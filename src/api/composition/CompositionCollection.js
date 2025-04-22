72340e4ef3b8cebeb9afdd40bbc72b394ca2a739
Andrew Henry <akhenry@gmail.com>
2025-04-22 10:00:42 -0700

    Support keystrings as well as identifiers

--- src/api/composition/CompositionCollection.js
+++ src/api/composition/CompositionCollection.js
@@ -20,7 +20,7 @@
  * at runtime from the About dialog for additional information.
  *****************************************************************************/
 
-import { isIdentifier } from '../objects/object-utils';
+import { isIdentifier, isKeyString } from '../objects/object-utils';
 
 /**
  * @typedef {import('openmct').DomainObject} DomainObject
@@ -212,7 +212,7 @@ export default class CompositionCollection {
     const children = await this.#provider.load(this.domainObject);
     const childObjects = await Promise.all(
       children.map((child) => {
-        if (isIdentifier(child)) {
+        if (isKeyString(child) || isIdentifier(child)) {
           return this.#publicAPI.objects.get(child, abortSignal);
         } else {
           return Promise.resolve(child);
