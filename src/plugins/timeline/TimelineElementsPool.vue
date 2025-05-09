02539f810bb1de51a99b9ef479d970113f3f296f
David Tsay <david.e.tsay@nasa.gov>
2025-05-09 15:32:06 -0700

    working resize with persistence add swimLaneLabelWidth input to inspector

--- src/plugins/timeline/TimelineElementsPool.vue
+++ src/plugins/timeline/TimelineElementsPool.vue
@@ -1,21 +1,84 @@
 <template>
   <ElementsPool>
-    <template #content="{ index, object }">
-      <TimelineElementsContent :index="index" :object="object" />
+    <template #content="{ index }">
+      <TimelineElementsContent
+        :index="index"
+        :container="domainObject.configuration.containers[index]"
+      />
+    </template>
+    <template #custom>
+      <div class="c-inspector__properties c-inspect-properties" aria-label="Swim Lane Label Width">
+        <div class="c-inspect-properties__header">Swin Lane Label Width</div>
+        <div class="c-inspect-properties__row">
+          <input
+            :value="swimLaneLabelWidth"
+            aria-labelledby="Width in pixels"
+            class="field control"
+            :pattern="/\d+/"
+            type="number"
+            name="value"
+            min="0"
+            @change="changeSwimLaneLabelWidth"
+          />
+          <span>px</span>
+        </div>
+      </div>
     </template>
   </ElementsPool>
 </template>
 <script>
+import { inject, ref } from 'vue';
+
 import ElementsPool from '@/plugins/inspectorViews/elements/ElementsPool.vue';
 
+import { configuration } from './configuration.js';
 import TimelineElementsContent from './TimelineElementsContent.vue';
+
 export default {
   components: {
     ElementsPool,
     TimelineElementsContent
   },
   setup() {
-    return {};
+    const openmct = inject('openmct');
+    const domainObject = inject('domainObject');
+
+    const selectionContext = openmct.selection.get()[0][0].context;
+    const initialSwimLaneLabelWidth =
+      domainObject.configuration.swimLaneLabelWidth ?? configuration.swimLaneLabelWidth;
+    // get initial containers configuration from selection context,
+    // as domain.configuration.containers not resilient to composition modifications made outside of view
+    const initialContainers =
+      selectionContext.containers ??
+      domainObject.configuration.containers ??
+      configuration.containers;
+    const swimLaneLabelWidth = ref(initialSwimLaneLabelWidth);
+    const containers = ref(initialContainers);
+    openmct.objects.observe(domainObject, 'configuration.containers', updateContainers);
+    openmct.objects.observe(
+      domainObject,
+      'configuration.swimLaneLabelWidth',
+      updateSwimLaneLabelWidth
+    );
+
+    function updateSwimLaneLabelWidth(_swimLaneLabelWidth) {
+      swimLaneLabelWidth.value = _swimLaneLabelWidth;
+    }
+
+    function updateContainers(_containers) {
+      containers.value = _containers;
+    }
+
+    function changeSwimLaneLabelWidth(event) {
+      const _size = Number(event.target.value);
+      openmct.objectViews.emit(
+        `contextAction:${openmct.objects.makeKeyString(domainObject.identifier)}`,
+        'changeSwimLaneLabelWidthContextAction',
+        _size
+      );
+    }
+
+    return { domainObject, changeSwimLaneLabelWidth, swimLaneLabelWidth };
   }
 };
 </script>
