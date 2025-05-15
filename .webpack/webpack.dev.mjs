5782e4ca87cfb622dcc94e77c50b73b88c350646
Jamie V <jamie.j.vigliotta@nasa.gov>
2025-05-15 12:40:54 -0700

    temp debug

--- .webpack/webpack.dev.mjs
+++ .webpack/webpack.dev.mjs
@@ -29,7 +29,7 @@ export default merge(common, {
       __OPENMCT_ROOT_RELATIVE__: '"dist/"'
     })
   ],
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   devServer: {
     devMiddleware: {
       writeToDisk: (filePathString) => {
