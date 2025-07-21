a377848737bd7b4c2c90e8f3804c5bd2f97babdc
Shefali <simplyrender@gmail.com>
2025-07-21 13:57:33 -0700

    Remove LAD Table priority of 100. Use Default instead

--- src/plugins/LADTable/LADTableViewProvider.js
+++ src/plugins/LADTable/LADTableViewProvider.js
@@ -46,8 +46,4 @@ export default class LADTableViewProvider {
   view(domainObject, objectPath) {
     return new LADTableView(this.openmct, domainObject, objectPath);
   }
-
-  priority() {
-    return this.openmct.priority.HIGH;
-  }
 }
