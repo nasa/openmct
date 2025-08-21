2b1e2fe45435457f8e452228a52195b0f14c06e1
David Tsay <david.e.tsay@nasa.gov>
2025-08-21 14:29:03 -0700

    swimlane width tests * default size 200px * resize using drag and drop * resize using inspector elements tab entry

---
+++ e2e/tests/functional/plugins/timeline/timeStrip.e2e.spec.js
@@ -0,0 +1,168 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2024, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+import { createDomainObjectWithDefaults } from '../../../../appActions.js';
+import { expect, test } from '../../../../pluginFixtures.js';
+
+const MIN_CONTAINER_SIZE = 5;
+const SWIM_LANE_DEFAULT_WIDTH = 200;
+
+// Helper function to get container sizes
+async function getContainerSizes(page) {
+  const containers = page.locator('.timeline-container');
+  const sizes = await containers.evaluateAll((elements) =>
+    elements.map((el) => parseFloat(window.getComputedStyle(el).flexGrow))
+  );
+  return sizes;
+}
+
+// Helper function to get container heights
+async function getContainerHeights(page) {
+  const containers = page.locator('.timeline-container');
+  const heights = await containers.evaluateAll((elements) => elements.map((el) => el.offsetHeight));
+  return heights;
+}
+
+test.describe('Timeline Swim Lane Configuration', () => {
+  /** @type {import('@playwright/test').Locator} */
+  let timeStrip;
+  let timeStripContentHolder;
+  let timeAxis;
+  let swimLaneLabels;
+  let showItemInTreeButton;
+  let editButton;
+  let treePane;
+  // TODO: why the extra pixel?
+  const fudge = 1;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+
+    timeStrip = await createDomainObjectWithDefaults(page, {
+      type: 'Time Strip',
+      name: 'Time Strip'
+    });
+
+    // Create Swim Lanes
+    for (let i = 0; i <= 4; i++) {
+      await createDomainObjectWithDefaults(page, {
+        type: 'Overlay Plot',
+        name: `Swim Lane ${i}`
+      });
+    }
+
+    await page.goto(timeStrip.url);
+    timeAxis = page.getByLabel('Time Axis');
+    timeStripContentHolder = page.getByLabel('Time Strip');
+    swimLaneLabels = timeStripContentHolder.locator('.c-swimlane__lane-label');
+    showItemInTreeButton = page.getByLabel('Show selected item in tree');
+    editButton = page.getByRole('button', { name: 'Edit Object', exact: true });
+    treePane = page.getByRole('tree', {
+      name: 'Main Tree'
+    });
+  });
+
+  test('allows for swim lane widths to be configured', async ({ page }) => {
+    // The Time Axis is a special instance of a swim lane
+    const timeAxisLabelCount = await swimLaneLabels.count();
+    await expect(timeAxisLabelCount).toBe(1);
+
+    // Edit the Time Strip
+    await editButton.click();
+    // Expand the 'My Items' folder in the left tree
+    await showItemInTreeButton.click();
+    // Add objects to the Time Strip
+    for (let i = 0; i <= 1; i++) {
+      const overlayPlotTreeItem = treePane.getByRole('treeitem', {
+        name: `Swim Lane ${i}`
+      });
+      await overlayPlotTreeItem.dragTo(timeAxis);
+    }
+
+    const labelCount = await swimLaneLabels.count();
+    await expect(labelCount).toBe(2 + timeAxisLabelCount);
+
+    await test.step(`swim lane widths default to ${SWIM_LANE_DEFAULT_WIDTH}px`, async () => {
+      // Verify the swim lane widths are identical
+      for (let i = 0; i < labelCount; i++) {
+        await expect((await swimLaneLabels.nth(i).boundingBox()).width).toBe(
+          SWIM_LANE_DEFAULT_WIDTH + fudge
+        );
+      }
+    });
+
+    await test.step('using drag and drop to resize swim lanes', async () => {
+      let widthAfterDragRight;
+      let widthAfterDragLeft;
+      const dragDistance = 50;
+      const swimLaneLabelWidthHandle = page.locator('.c-swimlane__handle');
+      const box = await swimLaneLabelWidthHandle.first().boundingBox();
+      await swimLaneLabelWidthHandle.first().hover();
+      await page.mouse.down();
+      await page.mouse.move(box.x + dragDistance, box.y);
+      await page.mouse.up();
+      widthAfterDragRight = (await swimLaneLabels.nth(0).boundingBox()).width;
+
+      // Verify the swim lane widths are identical and updated
+      for (let i = 0; i < labelCount; i++) {
+        const currentWidth = (await swimLaneLabels.nth(i).boundingBox()).width;
+
+        await expect(currentWidth).toBe(widthAfterDragRight);
+        await expect(currentWidth).toBeGreaterThan(SWIM_LANE_DEFAULT_WIDTH + fudge);
+
+        widthAfterDragRight = currentWidth;
+      }
+
+      const bigBox = await swimLaneLabelWidthHandle.first().boundingBox();
+      await swimLaneLabelWidthHandle.first().hover();
+      await page.mouse.down();
+      await page.mouse.move(bigBox.x - dragDistance / 2, bigBox.y);
+      await page.mouse.up();
+
+      widthAfterDragLeft = (await swimLaneLabels.nth(0).boundingBox()).width;
+
+      // Verify the swim lane widths are identical and updated
+      for (let i = 0; i < labelCount; i++) {
+        const currentWidth = (await swimLaneLabels.nth(i).boundingBox()).width;
+
+        await expect(currentWidth).toBe(widthAfterDragLeft);
+        await expect(currentWidth).toBeGreaterThan(SWIM_LANE_DEFAULT_WIDTH + fudge);
+        await expect(currentWidth).toBeLessThan(widthAfterDragRight);
+
+        widthAfterDragLeft = currentWidth;
+      }
+    });
+
+    await test.step('using input entry in inspector Elements tab to resize swim lanes ', async () => {
+      // Click the Elements tag in the Inspector
+      await page.getByRole('tab', { name: 'Elements' }).click();
+
+      const input = page.getByLabel('Swim Lane Label Width').locator('input[type="number"]');
+      await input.fill('100');
+      await input.blur();
+
+      // Verify the swim lane widths are identical and updated
+      for (let i = 0; i < (await swimLaneLabels.count()); i++) {
+        await expect((await swimLaneLabels.nth(i).boundingBox()).width).toBe(100 + fudge);
+      }
+    });
+  });
+});
