d6e77c32fd20f9d180dde6605b2423f21a8babe6
damien <damienniyonshuti@gmail.com>
2025-09-03 21:09:18 -0700

    fix curly

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,12 @@
-const LOG_LEVEL = (typeof process !== 'undefined' && process.env && process.env.OPENMCT_LOG) ||
-    (typeof window !== 'undefined' && window.__OPENMCT_DEBUG__) || '';
+const LOG_LEVEL = (typeof process !== 'undefined' && process.env && process.env.OPENMCT_LOG)
+    || (typeof window !== 'undefined' && !!window.__OPENMCT_DEBUG__ && 'debug')
+    || '';
 
 export function info(...args) {
     console.info('[openmct]', ...args);
 }
-
 export function debug(...args) {
-    if (LOG_LEVEL === 'debug') {
+    if (LOG_LEVEL === 'debug' || LOG_LEVEL === true) {
         console.debug('[openmct][debug]', ...args);
     }
 }
