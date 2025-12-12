1e4e3ac786329b35a62bd16afc55268b88fa42d8
heathdutton <heathdutton@gmail.com>
2025-12-12 01:03:48 -0500

    [Folders] Add LAD Table and Stacked Plot views (#8105)
    Add default telemetry views for folders containing telemetry objects. When viewing a folder, users can now select LAD Table or Stacked Plot views to visualize telemetry items within the folder's composition. Simple test included.

---
+++ src/plugins/folderView/FolderStackedPlotViewProvider.js
@@ -0,0 +1,84 @@
+/*****************************************************************************
+ * Open MCT, Copyright (c) 2014-2025, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space
+ * Administration. All rights reserved.
+ *
+ * Open MCT is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Open MCT includes source code licensed under additional open source
+ * licenses. See the Open Source Licenses file (LICENSES.md) included with
+ * this source code distribution or the Licensing information page available
+ * at runtime from the About dialog for additional information.
+ *****************************************************************************/
+
+import mount from 'utils/mount';
+
+import StackedPlot from '../plot/stackedPlot/StackedPlot.vue';
+import { ALLOWED_FOLDER_TYPES } from './constants.js';
+
+export default function FolderStackedPlotViewProvider(openmct) {
+  return {
+    key: 'stacked-plot-for-folder',
+    name: 'Stacked Plot',
+    cssClass: 'icon-telemetry',
+    canView(domainObject) {
+      return ALLOWED_FOLDER_TYPES.includes(domainObject.type);
+    },
+    view: function (domainObject, objectPath) {
+      let _destroy = null;
+      let component = null;
+
+      return {
+        show: function (element, isEditing, { renderWhenVisible }) {
+          const { vNode, destroy } = mount(
+            {
+              el: element,
+              components: {
+                StackedPlot
+              },
+              provide: {
+                openmct,
+                domainObject,
+                objectPath,
+                renderWhenVisible
+              },
+              data() {
+                return {
+                  options: {}
+                };
+              },
+              template: '<stacked-plot ref="plotComponent" :options="options"></stacked-plot>'
+            },
+            {
+              app: openmct.app,
+              element
+            }
+          );
+          _destroy = destroy;
+          component = vNode.componentInstance;
+        },
+        getViewContext() {
+          if (!component) {
+            return {};
+          }
+
+          return component.$refs.plotComponent.getViewContext();
+        },
+        destroy: function () {
+          if (_destroy) {
+            _destroy();
+          }
+        }
+      };
+    }
+  };
+}
