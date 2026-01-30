15008481ba930e8ade0a9c530f14dc3a91c363a2
Ram <163444056+chilliflake1313@users.noreply.github.com>
2026-01-30 18:41:12 +0530

    Fix accessibility: include visible view label in View Switcher accessible name

--- src/plugins/imagery/components/ImageryViewMenuSwitcher.vue
+++ src/plugins/imagery/components/ImageryViewMenuSwitcher.vue
@@ -1,31 +1,10 @@
-<!--
- Open MCT, Copyright (c) 2014-2024, United States Government
- as represented by the Administrator of the National Aeronautics and Space
- Administration. All rights reserved.
-
- Open MCT is licensed under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0.
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- License for the specific language governing permissions and limitations
- under the License.
-
- Open MCT includes source code licensed under additional open source
- licenses. See the Open Source Licenses file (LICENSES.md) included with
- this source code distribution or the Licensing information page available
- at runtime from the About dialog for additional information.
--->
 <template>
   <div class="c-switcher-menu">
     <button
       :id="id"
       class="c-button c-button--menu c-switcher-menu__button"
       :class="iconClass"
-      :aria-label="title"
+      :aria-label="accessibleLabel"
       :title="title"
       @click="toggleMenu"
     />
@@ -60,6 +39,12 @@ export default {
       showMenu: false
     };
   },
+  computed: {
+    accessibleLabel() {
+      const slotText = this.$slots.default?.[0]?.text?.trim();
+      return slotText ? `${this.title} for ${slotText}` : this.title;
+    }
+  },
   mounted() {
     document.addEventListener('click', this.hideMenu);
   },
@@ -74,7 +59,6 @@ export default {
       if (this.id === e.target.id) {
         return;
       }
-
       this.showMenu = false;
     }
   }
