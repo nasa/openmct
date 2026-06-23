7e09fe48343ee88d4ea7abb66e8f8a9a05504d90
Andrew Henry <andrew.k.henry@nasa.gov>
2026-06-22 18:32:31 -0700

    Fix lint error

--- e2e/tests/functional/plugins/imagery/exampleImagery.e2e.spec.js
+++ e2e/tests/functional/plugins/imagery/exampleImagery.e2e.spec.js
@@ -481,7 +481,6 @@ test.describe('Example Imagery in Display Layout', () => {
       type: 'issue',
       description: 'https://github.com/nasa/openmct/issues/3647'
     });
-    debugger;
     // set realtime mode
     await setRealTimeMode(page);
 
